4a3eb808a5786531fe2d2a394bb0dde5c8ddaeef
Tobias Oberlies <tobias.oberlies@sap.com>
2011-05-10 13:34:00 +0200

    Added missing copyright header
    - Also: Add "and others". It's not wrong and saves us from having to
      modify the copyright header in case of a change (at least this
      year).

--- tycho-its/src/test/java/org/eclipse/tycho/test/pomDependencyConsider/p2Data/reuse/PomDependencyOnRemoteTychoArtifactTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/pomDependencyConsider/p2Data/reuse/PomDependencyOnRemoteTychoArtifactTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 SAP AG.
+ * Copyright (c) 2011 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,6 @@
  * Contributors:
  *    SAP AG - initial API and implementation
  *******************************************************************************/
-
 package org.eclipse.tycho.test.pomDependencyConsider.p2Data.reuse;
 
 import java.io.File;
