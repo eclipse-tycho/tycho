20c9c012e375a6c94ace5213bf43157adc339eab
Tobias Oberlies <tobias.oberlies@sap.com>
2011-05-17 15:15:19 +0200

    Fixed copyright headers
    - The code in the touched files has been written by SAP employees.
    - It seems that the project "org.eclipse.tycho.p2.resolver.impl"
      (formerly "org.eclipse.tycho.p2.impl") was omitted in error when
      the copyright headers were initially generated in commit be5c841.

--- tycho-p2-resolver/tycho-p2-resolver-impl/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/impl/publisher/CategoryDependenciesAction.java
+++ tycho-p2-resolver/tycho-p2-resolver-impl/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/impl/publisher/CategoryDependenciesAction.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Sonatype Inc. and others.
+ * Copyright (c) 2010, 2011 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Sonatype Inc. - initial API and implementation
+ *    SAP AG - initial API and implementation
  *******************************************************************************/
 package org.eclipse.tycho.p2.impl.publisher;
 
