68294729c5290b5db247bc350de9a461b51c875c
Tobias Oberlies <tobias.oberlies@sap.com>
2011-08-12 16:23:57 +0200

    340852 Skip symlink test on Windows machines with Cygwin
    - Only execute the symlink integration test, if the running VM
      understands symbolic links.
    - Skip otherwise, and mark test as skipped (rather than passed) in
      surefire.

--- tycho-its/src/test/java/org/eclipse/tycho/test/symlinks/SymbolicLinkTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/symlinks/SymbolicLinkTest.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.tycho.test.symlinks;
 
+import static org.junit.Assume.assumeTrue;
+
 import java.io.File;
 import java.util.Arrays;
 
@@ -18,7 +20,7 @@ import org.codehaus.plexus.util.cli.CommandLineUtils;
 import org.codehaus.plexus.util.cli.Commandline;
 import org.codehaus.plexus.util.cli.DefaultConsumer;
 import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
-import org.junit.Assume;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SymbolicLinkTest extends AbstractTychoIntegrationTest {
@@ -26,21 +28,30 @@ public class SymbolicLinkTest extends AbstractTychoIntegrationTest {
     private static final String BASEDIR = "/symLink";
     private static final String LINK_NAME = "linkToProject";
 
-    private Verifier verifier;
+    private static Verifier verifier;
 
-    @Test
-    public void testBuildWithSymbolicLinkOnProjectPath() throws Exception {
-        verifier = getVerifier(BASEDIR, false);
+    @BeforeClass
+    public static void checkLinkCreation() throws Exception {
+        SymbolicLinkTest instance = new SymbolicLinkTest();
+
+        verifier = instance.getVerifier(BASEDIR, false);
         // Note that calling getBaseDir() will trigger copying the test project
         File projectDir = new File(verifier.getBasedir(), "project");
         File linkToProjectDir = new File(projectDir.getParentFile(), LINK_NAME);
-        Assume.assumeTrue(createSymbolicLink(projectDir, linkToProjectDir));
+
+        // if these checks fail, the test will be marked as skipped by surefire
+        assumeTrue(createSymbolicLink(projectDir, linkToProjectDir));
+        assumeTrue(new File(linkToProjectDir, "pom.xml").isFile());
+    }
+
+    @Test
+    public void testBuildWithSymbolicLinkOnProjectPath() throws Exception {
         verifier.getCliOptions().addAll(Arrays.asList("-f", LINK_NAME + "/pom.xml"));
         verifier.executeGoal("package");
         verifier.verifyErrorFreeLog();
     }
 
-    private boolean createSymbolicLink(File target, File link) {
+    private static boolean createSymbolicLink(File target, File link) {
         Commandline commandline = new Commandline();
         commandline.setWorkingDirectory(link.getParentFile());
         commandline.addArguments(new String[] { "ln", "-s", target.getAbsolutePath(), link.getAbsolutePath() });
