a10a55a2b1afdbe9b47dfeb11063b7c65cb3c085
Tobias Oberlies <tobias.oberlies@sap.com>
2011-10-21 09:38:33 +0200

    361619 Do not zip project folder during dependency-only publishing
    - For eclipse-plugin and eclipse-test-plugin modules, the original
      BundlesAction is used for dependency-only publishing. This action
      publishes the bundles artifact, if the corresponding attribute is
      set in the IPublisherInfo and and artifact repository is present.
      Being called on the project directory, the BundlesAction assumed to
      be facing an unpacked bundle, and hence packed the directory
      content. This change prevents this unnecessary and potentially
      long-running step.

--- tycho-bundles/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/impl/publisher/DefaultDependencyMetadataGenerator.java
+++ tycho-bundles/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/impl/publisher/DefaultDependencyMetadataGenerator.java
@@ -16,10 +16,8 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
-import org.eclipse.equinox.p2.publisher.IPublisherInfo;
 import org.eclipse.equinox.p2.publisher.PublisherInfo;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
-import org.eclipse.tycho.p2.impl.publisher.repo.TransientArtifactRepository;
 import org.eclipse.tycho.p2.metadata.DependencyMetadataGenerator;
 import org.eclipse.tycho.p2.metadata.IArtifactFacade;
 
@@ -35,9 +33,6 @@ public class DefaultDependencyMetadataGenerator extends P2GeneratorImpl implemen
         LinkedHashSet<IArtifactDescriptor> artifactDescriptors = new LinkedHashSet<IArtifactDescriptor>();
 
         PublisherInfo publisherInfo = new PublisherInfo();
-        publisherInfo.setArtifactOptions(IPublisherInfo.A_INDEX | IPublisherInfo.A_PUBLISH);
-        publisherInfo.setArtifactRepository(new TransientArtifactRepository());
-
         super.generateMetadata(artifact, environments, units, artifactDescriptors, publisherInfo);
 
         return new LinkedHashSet<Object>(units);
--- tycho-bundles/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/impl/publisher/SourcesBundleDependencyMetadataGenerator.java
+++ tycho-bundles/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/impl/publisher/SourcesBundleDependencyMetadataGenerator.java
@@ -21,13 +21,11 @@ import java.util.Set;
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.publisher.IPublisherAction;
 import org.eclipse.equinox.p2.publisher.IPublisherAdvice;
-import org.eclipse.equinox.p2.publisher.IPublisherInfo;
 import org.eclipse.equinox.p2.publisher.PublisherInfo;
 import org.eclipse.equinox.p2.publisher.eclipse.BundlesAction;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
 import org.eclipse.osgi.service.resolver.BundleDescription;
 import org.eclipse.osgi.service.resolver.StateObjectFactory;
-import org.eclipse.tycho.p2.impl.publisher.repo.TransientArtifactRepository;
 import org.eclipse.tycho.p2.metadata.DependencyMetadataGenerator;
 import org.eclipse.tycho.p2.metadata.IArtifactFacade;
 import org.eclipse.tycho.p2.metadata.IReactorArtifactFacade;
@@ -46,9 +44,6 @@ public class SourcesBundleDependencyMetadataGenerator extends AbstractMetadataGe
         LinkedHashSet<IArtifactDescriptor> artifactDescriptors = new LinkedHashSet<IArtifactDescriptor>();
 
         PublisherInfo publisherInfo = new PublisherInfo();
-        publisherInfo.setArtifactOptions(IPublisherInfo.A_INDEX | IPublisherInfo.A_PUBLISH);
-        publisherInfo.setArtifactRepository(new TransientArtifactRepository());
-
         super.generateMetadata(artifact, environments, units, artifactDescriptors, publisherInfo);
 
         return new LinkedHashSet<Object>(units);
