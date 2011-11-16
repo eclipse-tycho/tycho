21ebfe23b839c5923e449f255f7339b61684c7f4
Jan Sievers <jan.sievers@sap.com>
2011-11-16 10:16:37 +0100

    Fix outdated and hardcoded tycho version
    Use maven resopurce filtering in all cases
    (SNAPSHOT or released version).
    
    As a consequence of this bug, tycho p2 runtime
    metadata was still on outdated version 0.13.0-SNAPSHOT.

--- tycho-core/src/test/java/org/eclipse/tycho/core/utils/TychoVersionTest.java
+++ tycho-core/src/test/java/org/eclipse/tycho/core/utils/TychoVersionTest.java
@@ -1,40 +1,41 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Sonatype Inc. and others.
+ * Copyright (c) 2011 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Sonatype Inc. - initial API and implementation
+ *     SAP AG - initial API and implementation
  *******************************************************************************/
+
 package org.eclipse.tycho.core.utils;
+
+import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 import org.codehaus.plexus.util.IOUtil;
+import org.junit.Test;
 
-public class TychoVersion {
+public class TychoVersionTest {
 
-    private static final String TYCHO_VERSION = readVersion();
-
-    public static String getTychoVersion() {
-        return TYCHO_VERSION;
+    @Test
+    public void testGetTychoVersion() throws IOException {
+        assertEquals(readTestVersion(), TychoVersion.getTychoVersion());
     }
 
-    private static String readVersion() {
-        InputStream stream = TychoVersion.class.getResourceAsStream("version.properties");
+    private String readTestVersion() throws IOException {
+        Properties p = new Properties();
+        InputStream stream = TychoVersionTest.class.getResourceAsStream("testversion.properties");
         try {
-            Properties p = new Properties();
             p.load(stream);
-            return p.getProperty("version");
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
         } finally {
             IOUtil.close(stream);
         }
+        return p.getProperty("testversion");
     }
 
 }
