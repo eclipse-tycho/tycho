9ba5f155ceb06a172c0733c7087604f0cb0cf009
Jan Sievers <jan.sievers@sap.com>
2011-12-16 14:13:35 +0100

    365012 Make test helper multi-process safe
    Make test helper class which accesses the
    local repo index file multi-process safe.
    Instead of deleting the whole index file, only
    remove the GAV(s) which should be hidden
    for the test.

--- tycho-testing-harness/src/main/java/org/eclipse/tycho/test/util/LocalMavenRepositoryTool.java
+++ tycho-testing-harness/src/main/java/org/eclipse/tycho/test/util/LocalMavenRepositoryTool.java
@@ -10,12 +10,32 @@
  *******************************************************************************/
 package org.eclipse.tycho.test.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.eclipse.tycho.locking.facade.FileLockService;
+import org.eclipse.tycho.locking.facade.FileLocker;
+
+@Component(role = LocalMavenRepositoryTool.class)
 public class LocalMavenRepositoryTool {
 
     private File localRepo;
+
+    @Requirement
+    private FileLockService fileLockService;
 
     /**
      * Creates a default instance of the <code>LocalRepositoryUtils</code>, using the local Maven
@@ -28,10 +48,6 @@ public class LocalMavenRepositoryTool {
         }
     }
 
-//    public File getArtifactFile(GAV gav, String classifier, String extension) {
-//        return getArtifactFile(gav.getGroupId(), gav.getArtifactId(), gav.getVersion(), classifier, extension);
-//    }
-
     public File getArtifactFile(String groupId, String artifactId, String version, String classifier, String extension) {
         String groupPath = groupId.replace('.', '/');
         String artifactPath = groupPath + '/' + artifactId + '/' + version;
@@ -41,21 +57,46 @@ public class LocalMavenRepositoryTool {
     }
 
     /**
-     * Hides all artifacts built and installed by Tycho from future Tycho builds.
+     * Hides specified artifacts built and installed by Tycho from future Tycho builds by removing
+     * the corresponding lines with GAV coordinates format "g:a:v" from the local metadata index
+     * file.
      * <p>
      * Background: Artifacts built and installed by Tycho are visible to all other Tycho builds in
      * order to allow re-builds of individual modules. This method allows to prevent this for future
      * builds.
      * 
+     * TODO should rather reuse FileBasedTychoRepositoryIndex here
+     * 
      * @throws IOException
-     *             if the list of locally built Tycho artifacts cannot be emptied.
+     *             if index file could not be saved
      */
-    public void hideAllLocalTychoArtifacts() throws IOException {
-        File listOfTychoBuiltArtifact = new File(localRepo, ".meta/p2-local-metadata.properties");
-        if (listOfTychoBuiltArtifact.exists()) {
-            boolean success = listOfTychoBuiltArtifact.delete();
-            if (!success)
-                throw new IOException("Could not delete " + listOfTychoBuiltArtifact);
+    public void removeLinesFromMetadataIndex(String... linesToBeRemoved) throws IOException {
+        Set<String> toBeRemoved = new HashSet<String>(Arrays.asList(linesToBeRemoved));
+        File indexFile = new File(localRepo, ".meta/p2-local-metadata.properties");
+        FileLocker locker = fileLockService.getFileLocker(indexFile);
+        locker.lock();
+        try {
+            List<String> lines = new ArrayList<String>();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(indexFile), "UTF-8"));
+            try {
+                for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                    if (!toBeRemoved.contains(line)) {
+                        lines.add(line);
+                    }
+                }
+            } finally {
+                reader.close();
+            }
+            Writer writer = new OutputStreamWriter(new FileOutputStream(indexFile), "UTF-8");
+            try {
+                for (String line : lines) {
+                    writer.write(line + "\n");
+                }
+            } finally {
+                writer.close();
+            }
+        } finally {
+            locker.release();
         }
     }
 
