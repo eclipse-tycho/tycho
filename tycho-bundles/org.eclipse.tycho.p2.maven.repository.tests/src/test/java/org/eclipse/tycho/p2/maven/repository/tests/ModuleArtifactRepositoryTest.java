c57225a9dea1dad96ab4e9f1c83509b908fee13c
Tobias Oberlies <tobias.oberlies@sap.com>
2012-01-04 13:09:09 +0100

    Refactoring: Modernized Module*RepositoryTest
    - Use TemporaryFolder rule.
    - Make all assertions in test methods and not in helper methods.
    - Use assertThat.

--- tycho-bundles/org.eclipse.tycho.p2.maven.repository.tests/src/test/java/org/eclipse/tycho/p2/maven/repository/tests/ModuleArtifactRepositoryTest.java
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository.tests/src/test/java/org/eclipse/tycho/p2/maven/repository/tests/ModuleArtifactRepositoryTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 SAP AG and others.
+ * Copyright (c) 2010, 2012 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,17 +10,17 @@
  *******************************************************************************/
 package org.eclipse.tycho.p2.maven.repository.tests;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import org.eclipse.equinox.internal.p2.core.helpers.FileUtils;
 import org.eclipse.equinox.internal.p2.metadata.ArtifactKey;
 import org.eclipse.equinox.p2.core.IProvisioningAgent;
 import org.eclipse.equinox.p2.metadata.IArtifactKey;
@@ -29,9 +29,10 @@ import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactRepository;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactRepositoryManager;
 import org.eclipse.tycho.p2.maven.repository.ModuleArtifactRepository;
-import org.junit.After;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 @SuppressWarnings("restriction")
 public class ModuleArtifactRepositoryTest {
@@ -46,57 +47,53 @@ public class ModuleArtifactRepositoryTest {
 
     private static final int SOURCE_ARTIFACT_SIZE = 418;
 
-    private static File moduleDir;
+    private static File existingModuleDir;
 
-    private File tempDir = null;
+    @Rule
+    public TemporaryFolder tempManager = new TemporaryFolder();
 
-    private ModuleArtifactRepository subject;
+    private IArtifactRepository subject;
 
     @BeforeClass
-    public static void init() throws Exception {
-        moduleDir = new File("resources/repositories/module/target").getAbsoluteFile();
+    public static void initExistingRepository() throws Exception {
+        // this folder contains a ModuleArtifactRepository with BUNDLE_ARTIFACT_KEY and SOURCE_ARTIFACT_KEY ...
+        existingModuleDir = new File("resources/repositories/module/target").getAbsoluteFile();
 
-        generateBinaryTestFile(new File(moduleDir, "the-bundle.jar"), BUNDLE_ARTIFACT_SIZE);
-        generateBinaryTestFile(new File(moduleDir, "the-sources.jar"), SOURCE_ARTIFACT_SIZE);
-    }
-
-    @After
-    public void cleanUp() {
-        if (tempDir != null)
-            FileUtils.deleteAll(tempDir);
-        tempDir = null;
+        // ... except for the binary files
+        generateBinaryTestFile(new File(existingModuleDir, "the-bundle.jar"), BUNDLE_ARTIFACT_SIZE);
+        generateBinaryTestFile(new File(existingModuleDir, "the-sources.jar"), SOURCE_ARTIFACT_SIZE);
     }
 
     @Test
     public void testLoadRepository() throws Exception {
-        subject = new ModuleArtifactRepository(null, moduleDir);
+        subject = new ModuleArtifactRepository(null, existingModuleDir);
 
-        assertGetArtifact(subject, BUNDLE_ARTIFACT_KEY, BUNDLE_ARTIFACT_SIZE);
-        assertGetArtifact(subject, SOURCE_ARTIFACT_KEY, SOURCE_ARTIFACT_SIZE);
+        assertThat(artifactSizeOf(BUNDLE_ARTIFACT_KEY, subject), is(BUNDLE_ARTIFACT_SIZE));
+        assertThat(artifactSizeOf(SOURCE_ARTIFACT_KEY, subject), is(SOURCE_ARTIFACT_SIZE));
     }
 
     @Test
     public void testLoadRepositoryWithFactory() throws Exception {
-        tempDir = createTempDir();
-        IProvisioningAgent agent = Activator.createProvisioningAgent(tempDir.toURI());
+        File agentDir = tempManager.newFolder("agent");
+        IProvisioningAgent agent = Activator.createProvisioningAgent(agentDir.toURI());
         IArtifactRepositoryManager repoManager = (IArtifactRepositoryManager) agent
                 .getService(IArtifactRepositoryManager.SERVICE_NAME);
 
-        IArtifactRepository subject = repoManager.loadRepository(moduleDir.toURI(), null);
+        subject = repoManager.loadRepository(existingModuleDir.toURI(), null);
 
-        assertEquals(subject.getArtifactDescriptors(SOURCE_ARTIFACT_KEY).length, 1);
+        assertThat(subject.getArtifactDescriptors(SOURCE_ARTIFACT_KEY).length, is(1));
     }
 
-    private static void assertGetArtifact(IArtifactRepository subject, IArtifactKey artifactKey, int expectedSize) {
+    private static int artifactSizeOf(IArtifactKey artifactKey, IArtifactRepository subject) {
         IArtifactDescriptor[] artifactDescriptors = subject.getArtifactDescriptors(artifactKey);
         assertEquals(1, artifactDescriptors.length);
 
         ByteArrayOutputStream artifactContent = new ByteArrayOutputStream();
         subject.getArtifact(artifactDescriptors[0], artifactContent, null);
-        assertEquals(expectedSize, artifactContent.size());
+        return artifactContent.size();
     }
 
-    private static void generateBinaryTestFile(File file, int size) throws FileNotFoundException, IOException {
+    private static void generateBinaryTestFile(File file, int size) throws IOException {
         file.getParentFile().mkdirs();
         FileOutputStream fos = new FileOutputStream(file);
         try {
@@ -109,19 +106,5 @@ public class ModuleArtifactRepositoryTest {
             fos.close();
         }
         file.deleteOnExit();
-    }
-
-    private static File createTempDir() throws IOException {
-        return createTempDir(ModuleArtifactRepositoryTest.class);
-    }
-
-    static File createTempDir(Class<?> testClass) throws IOException {
-        final File tempFile = File.createTempFile(testClass.getSimpleName(), "");
-        tempFile.delete();
-
-        final File tempDir = tempFile;
-        if (!tempDir.mkdirs())
-            throw new IOException("Could not create temporary directory: " + tempDir);
-        return tempFile;
     }
 }
--- tycho-bundles/org.eclipse.tycho.p2.maven.repository.tests/src/test/java/org/eclipse/tycho/p2/maven/repository/tests/ModuleMetadataRepositoryTest.java
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository.tests/src/test/java/org/eclipse/tycho/p2/maven/repository/tests/ModuleMetadataRepositoryTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 SAP AG and others.
+ * Copyright (c) 2010, 2012 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,13 +10,14 @@
  *******************************************************************************/
 package org.eclipse.tycho.p2.maven.repository.tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-import org.eclipse.equinox.internal.p2.core.helpers.FileUtils;
 import org.eclipse.equinox.p2.core.IProvisioningAgent;
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.metadata.IVersionedId;
@@ -26,66 +27,56 @@ import org.eclipse.equinox.p2.query.QueryUtil;
 import org.eclipse.equinox.p2.repository.metadata.IMetadataRepository;
 import org.eclipse.equinox.p2.repository.metadata.IMetadataRepositoryManager;
 import org.eclipse.tycho.p2.maven.repository.ModuleMetadataRepository;
-import org.junit.After;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class ModuleMetadataRepositoryTest {
 
     private static final IVersionedId BUNDLE_UNIT = new VersionedId("bundle", "1.2.3.201011101425");
-
     private static final IVersionedId SOURCE_UNIT = new VersionedId("bundle.source", "1.2.3.TAGNAME");
 
     private static File moduleDir;
 
-    private File tempDir = null;
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
 
-    private ModuleMetadataRepository subject;
+    private IMetadataRepository subject;
 
     @BeforeClass
     public static void init() throws Exception {
         moduleDir = new File("resources/repositories/module/target").getAbsoluteFile();
     }
 
-    @SuppressWarnings("restriction")
-    @After
-    public void cleanUp() {
-        if (tempDir != null)
-            FileUtils.deleteAll(tempDir);
-        tempDir = null;
-    }
-
     @Test
     public void testLoadRepository() throws Exception {
         subject = new ModuleMetadataRepository(null, moduleDir);
 
-        assertQueryForUnit(subject, BUNDLE_UNIT);
-        assertQueryForUnit(subject, SOURCE_UNIT);
+        assertThat(unitsIn(subject), hasItem(BUNDLE_UNIT));
+        assertThat(unitsIn(subject), hasItem(SOURCE_UNIT));
     }
 
     @Test
     public void testLoadRepositoryWithFactory() throws Exception {
-        tempDir = createTempDir();
-        IProvisioningAgent agent = Activator.createProvisioningAgent(tempDir.toURI());
+        IProvisioningAgent agent = Activator.createProvisioningAgent(tempFolder.newFolder("agent").toURI());
         IMetadataRepositoryManager repoManager = (IMetadataRepositoryManager) agent
                 .getService(IMetadataRepositoryManager.SERVICE_NAME);
 
-        IMetadataRepository subject = repoManager.loadRepository(moduleDir.toURI(), null);
+        subject = repoManager.loadRepository(moduleDir.toURI(), null);
 
-        assertQueryForUnit(subject, BUNDLE_UNIT);
-        assertQueryForUnit(subject, SOURCE_UNIT);
+        assertThat(unitsIn(subject), hasItem(BUNDLE_UNIT));
+        assertThat(unitsIn(subject), hasItem(SOURCE_UNIT));
     }
 
-    private static void assertQueryForUnit(IMetadataRepository subject, IVersionedId unitIdentifier) {
-        IQueryResult<IInstallableUnit> result = subject.query(QueryUtil.createIUQuery(unitIdentifier.getId()), null);
-        Iterator<IInstallableUnit> iterator = result.iterator();
-        assertEquals(true, iterator.hasNext());
-        IInstallableUnit unit = iterator.next();
-        assertEquals(false, iterator.hasNext());
-        assertEquals(unitIdentifier.getVersion(), unit.getVersion());
-    }
-
-    private static File createTempDir() throws IOException {
-        return ModuleArtifactRepositoryTest.createTempDir(ModuleMetadataRepositoryTest.class);
+    private static List<IVersionedId> unitsIn(IMetadataRepository repo) {
+        IQueryResult<IInstallableUnit> units = repo.query(QueryUtil.ALL_UNITS, null);
+        List<IVersionedId> unitIds = new ArrayList<IVersionedId>();
+        for (Iterator<IInstallableUnit> unitIterator = units.iterator(); unitIterator.hasNext();) {
+            IInstallableUnit unit = unitIterator.next();
+            VersionedId unitId = new VersionedId(unit.getId(), unit.getVersion());
+            unitIds.add(unitId);
+        }
+        return unitIds;
     }
 }
