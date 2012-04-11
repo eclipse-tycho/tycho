49edf2175d3df17b3a249bba00b2db14c9a70b9d
Igor Fedorenko <igor@ifedorenko.com>
2012-04-11 16:20:36 -0400

    362883 make build timestamp generation logic customizable
    Introduced BuildTimestampProvider component interface that allows
    custom build timestamp generation logic. The defaut behaviour remains
    unchanged, i.e. the timestamp derived from the build start time is
    used for all reactor projects. See BuildQualifierMojo javadoc for
    info how to enable custom timestamp generation logic.
    
    Change-Id: I7bc5fcfcb6262e00dc9c24835060ceebd825d12c
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

---
+++ tycho-packaging-plugin/src/main/java/org/eclipse/tycho/buildversion/BuildTimestampProvider.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Sonatype Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Sonatype Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.buildversion;
+
+import java.util.Date;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.MojoExecution;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+
+public interface BuildTimestampProvider {
+
+    /**
+     * Returns build timestamp of the current project.
+     */
+    public Date getTimestamp(MavenSession session, MavenProject project, MojoExecution execution)
+            throws MojoExecutionException;
+
+}
