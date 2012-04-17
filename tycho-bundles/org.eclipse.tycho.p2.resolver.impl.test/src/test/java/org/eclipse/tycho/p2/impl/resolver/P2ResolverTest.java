e2c5ea19ea1b05054a93fdaacb9ccb08146d4af2
Tobias Oberlies <tobias.oberlies@sap.com>
2012-04-17 17:33:36 +0200

    376981 Hold remote agent instance in a service
    - Hold agent for remote repositories in a new service, the
      RemoteAgentManager, instead of a static variable. With this change,
      the test-only "purgeAgents" method is no longer needed.
    
    Change-Id: I76f4d8f66d56dafdfaf3e853d18fdd58dc0fe3ad

--- tycho-bundles/org.eclipse.tycho.p2.resolver.impl.test/src/test/java/org/eclipse/tycho/p2/impl/resolver/P2ResolverTest.java
+++ tycho-bundles/org.eclipse.tycho.p2.resolver.impl.test/src/test/java/org/eclipse/tycho/p2/impl/resolver/P2ResolverTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Sonatype Inc. and others.
+ * Copyright (c) 2008, 2012 Sonatype Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -40,10 +40,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class P2ResolverTest extends P2ResolverTestBase {
-
-    static File getLocalRepositoryLocation() throws IOException {
-        return new File("target/localrepo").getCanonicalFile();
-    }
 
     @Before
     public void initDefaultResolver() throws Exception {
