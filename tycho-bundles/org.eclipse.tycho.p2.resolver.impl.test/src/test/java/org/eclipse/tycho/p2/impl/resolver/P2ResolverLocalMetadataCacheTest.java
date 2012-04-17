b50f8fab1eac465ee54a89216e81c587ab031d9a
Tobias Oberlies <tobias.oberlies@sap.com>
2012-04-17 17:11:43 +0200

    376981 Removed unneeded cases in p2 agent cache
    - Dropped possibility for different agents for different
      offline/localMavenRepository settings, because these settings are
      always the same within one Maven session/lifetime of the OSGi
      runtime.
    
    Change-Id: Id1c0e076d52e18815a2e8149f140e39b2fe9572d

--- tycho-bundles/org.eclipse.tycho.p2.resolver.impl.test/src/test/java/org/eclipse/tycho/p2/impl/resolver/P2ResolverLocalMetadataCacheTest.java
+++ tycho-bundles/org.eclipse.tycho.p2.resolver.impl.test/src/test/java/org/eclipse/tycho/p2/impl/resolver/P2ResolverLocalMetadataCacheTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Sonatype Inc. and others.
+ * Copyright (c) 2008, 2012 Sonatype Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -104,6 +104,7 @@ public class P2ResolverLocalMetadataCacheTest extends P2ResolverTestBase {
         delete(getLocalRepositoryLocation());
 
         boolean offline = true;
+        P2ResolverFactoryImpl.purgeAgents();
         P2ResolverFactoryImpl p2ResolverFactory = createP2ResolverFactory(offline);
         context = p2ResolverFactory.createTargetPlatformBuilder(null, DISABLE_MIRRORS);
 
