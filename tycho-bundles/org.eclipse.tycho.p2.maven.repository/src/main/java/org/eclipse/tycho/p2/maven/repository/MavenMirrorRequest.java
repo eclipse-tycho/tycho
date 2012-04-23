0c3cbaf19b67929f663f5d9e4d8342ff1d977e10
Igor Fedorenko <igor@ifedorenko.com>
2012-04-22 23:30:33 -0400

    cleanup: removed remnants of tycho/maven repository support
    Historical reference https://issues.sonatype.org/browse/TYCHO-335
    
    Change-Id: I701dfc451a67478cf7153228cf1b67c715a32314
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/p2/maven/repository/MavenMirrorRequest.java
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/p2/maven/repository/MavenMirrorRequest.java
@@ -11,12 +11,10 @@
 package org.eclipse.tycho.p2.maven.repository;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.equinox.internal.p2.artifact.repository.MirrorRequest;
 import org.eclipse.equinox.internal.p2.repository.Transport;
 import org.eclipse.equinox.p2.metadata.IArtifactKey;
-import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactRepository;
 
 @SuppressWarnings("restriction")
@@ -34,32 +32,7 @@ public class MavenMirrorRequest extends MirrorRequest {
     public void perform(IArtifactRepository sourceRepository, IProgressMonitor monitor) {
         setSourceRepository(sourceRepository);
 
-        IArtifactDescriptor descriptor = getArtifactDescriptor();
-
-        if (source instanceof AbstractMavenArtifactRepository) {
-            IStatus result = null;
-
-            if (descriptor != null) {
-                // resolve artifact
-                result = ((AbstractMavenArtifactRepository) source).resolve(descriptor);
-
-                // update local metadata tycho index if successful
-                if (result != null && result.isOK()) {
-                    localRepository.addDescriptor(descriptor);
-                }
-            }
-
-            if (result == null) {
-                result = new Status(IStatus.ERROR, Activator.ID, "Could not resovle artifact " + artifact
-                        + " from repository " + source.getName());
-            }
-
-            setResult(result);
-
-            return;
-        }
-
-        // not a maven repository, check local repo to avoid duplicate downloads
+        // check local repo to avoid duplicate downloads
 
         if (localRepository.contains(getArtifactKey())) {
             setResult(Status.OK_STATUS);
@@ -70,16 +43,6 @@ public class MavenMirrorRequest extends MirrorRequest {
         // not a maven repo and not in maven local repo, delegate to p2 implementation
 
         super.perform(sourceRepository, monitor);
-    }
-
-    private IArtifactDescriptor getArtifactDescriptor() {
-        for (IArtifactDescriptor descriptor : source.getArtifactDescriptors(getArtifactKey())) {
-            if (descriptor.getProperty(IArtifactDescriptor.FORMAT) == null) {
-                return descriptor;
-            }
-        }
-
-        return null;
     }
 
 }
