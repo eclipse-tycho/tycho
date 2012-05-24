df0de76bfe3007b603892ef2f4180a17c86ba9c4
Jan Sievers <jan.sievers@sap.com>
2012-05-24 16:39:20 +0200

    366967 Validate module baseDirs are unique
    While it seems that maven core tolerates this,
    we don't support mutliple reactor modules in the
    same baseDir.
    This would lead to irreproducible and unexpected
    results with mvn clean.
    Also, we use the module baseDir in several places as
    unique key.
    Detect this situation early and fail the build.
    
    Change-Id: I9747551815731626621fe2c9eac6b6a3a2bc98cc

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/bug366967/NonUniqueBasedirsTest.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2012 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SAP AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.test.bug366967;
+
+import org.apache.maven.it.VerificationException;
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NonUniqueBasedirsTest extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void testNonUniqueBasedirFailure() throws Exception {
+        Verifier verifier = getVerifier("/nonUniqueModuleBaseDir", false);
+        try {
+            verifier.executeGoal("clean");
+            Assert.fail("build failure expected");
+        } catch (VerificationException e) {
+            // expected
+        }
+        verifier.verifyTextInLog("Multiple modules with the same basedir are not supported");
+    }
+
+}
