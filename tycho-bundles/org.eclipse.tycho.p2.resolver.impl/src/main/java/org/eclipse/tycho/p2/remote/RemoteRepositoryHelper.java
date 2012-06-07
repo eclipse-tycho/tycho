f374a7797df6c5b70e038ad76e7c4624f66d6560
Tobias Oberlies <tobias.oberlies@sap.com>
2012-06-07 14:58:15 +0200

    356016 377086 Use URL as fallback ID for mirrors/credentials
    - When looking for credentials or mirrors of p2 repositories in the
      Maven settings.xml, use the URL as ID if there is no ID specified
      explicitly, e.g. in a target file.
    - The ID is computed as URI.toString() followed by a removal of a
      trailing slash (if present).
    
    Change-Id: I70deab0f63d662322ac3d97b26b7e88d5f258b1f

--- tycho-bundles/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/remote/RemoteRepositoryHelper.java
+++ tycho-bundles/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/remote/RemoteRepositoryHelper.java
@@ -37,7 +37,6 @@ class RemoteRepositoryHelper implements IRepositoryIdManager {
             return;
 
         URI key = normalize(location);
-//        boolean overwrite =  knownLocations.containsKey(key);
         String previousValue = knownMavenRepositoryIds.put(key, mavenRepositoryId);
 
         if (previousValue != null && !(mavenRepositoryId.equals(previousValue))) {
@@ -47,13 +46,11 @@ class RemoteRepositoryHelper implements IRepositoryIdManager {
     }
 
     private static URI normalize(URI location) {
-        // make sure path ends in '/'
+        // remove trailing slashes
         try {
-            if (location.getPath() == null) {
-                return new URI(location.getScheme(), location.getAuthority(), "/", location.getQuery(),
-                        location.getFragment());
-            } else if (!location.getPath().endsWith("/")) {
-                return new URI(location.getScheme(), location.getAuthority(), location.getPath() + "/",
+            String path = location.getPath();
+            if (path != null && path.endsWith("/")) {
+                return new URI(location.getScheme(), location.getAuthority(), path.substring(0, path.length() - 1),
                         location.getQuery(), location.getFragment());
             } else {
                 return location;
@@ -64,37 +61,70 @@ class RemoteRepositoryHelper implements IRepositoryIdManager {
     }
 
     public URI getEffectiveLocation(URI location) {
-        MavenRepositoryLocation effectiveLocation = effectiveLocationFor(location);
+        if (certainlyNoRemoteURL(location)) {
+            return location;
+        }
+
+        MavenRepositoryLocation effectiveLocation = effectiveLocationFor(location, false);
         return effectiveLocation.getURL();
     }
 
     public URI getEffectiveLocationAndPrepareLoad(URI location) throws ProvisionException {
-        MavenRepositoryLocation effectiveLocation = effectiveLocationFor(location);
+        if (certainlyNoRemoteURL(location)) {
+            return location;
+        }
 
+        MavenRepositoryLocation effectiveLocation = effectiveLocationFor(location, true);
         setPasswordForLoading(effectiveLocation);
-
         return effectiveLocation.getURL();
     }
 
-    private MavenRepositoryLocation effectiveLocationFor(URI location) {
+    private MavenRepositoryLocation effectiveLocationFor(URI location, boolean forLoading) {
         URI normalizedLocation = normalize(location);
+
         String id = knownMavenRepositoryIds.get(normalizedLocation);
+        if (id == null) {
+            // fall back to URL as ID
+            id = normalizedLocation.toString();
+        }
         MavenRepositoryLocation locationWithID = new MavenRepositoryLocation(id, normalizedLocation);
 
         MavenRepositoryLocation mirror = settings.getMirror(locationWithID);
-        if (mirror != null)
+        if (mirror != null) {
+            if (forLoading) {
+                logger.info("Loading repository '" + id + "' from mirror '" + mirror.getId() + "' at '"
+                        + mirror.getURL() + "'");
+            }
             return mirror;
-        else {
+        } else {
             return locationWithID;
         }
     }
 
+    /**
+     * Sets passwords configured in the Maven settings in p2.
+     * 
+     * <p>
+     * Warning: This method heavily relies on side-effects. Instead of remembering the credentials
+     * just for the given location, p2 associates the password with the host. This allows to load
+     * children of a composite repository with the same credentials as the parent, without having to
+     * specify all children in the Maven settings. This feature can easily break if repositories are
+     * loaded in parallel. If this shall be supported, a lock is needed here (TODO).
+     */
     private void setPasswordForLoading(MavenRepositoryLocation location) throws ProvisionException {
         MavenRepositorySettings.Credentials credentials = settings.getCredentials(location);
         if (credentials != null) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Setting credentials for p2 repository '" + location.getId() + "'");
+            }
             P2PasswordUtil.setCredentials(location.getURL(), credentials.getUserName(), credentials.getPassword(),
                     logger);
         }
+    }
+
+    private static boolean certainlyNoRemoteURL(URI location) {
+        // e.g. in-memory composite artifact repositories; see see org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository.createMemoryComposite(IProvisioningAgent)
+        return location.isOpaque() || !location.isAbsolute();
     }
 
 }
