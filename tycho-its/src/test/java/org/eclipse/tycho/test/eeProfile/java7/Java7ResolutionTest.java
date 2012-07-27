ff409dded7f86586141d16d3f754ea93ea95d3a2
Tobias Oberlies <tobias.oberlies@sap.com>
2012-07-27 11:23:18 +0200

    384494 Retain a.jre IUs in resolved project dependencies
    - The p2 director and the p2 repository aggregation (still) work on the
      resolved project dependencies (cf. bug 372780). Therefore these
      resolved dependencies need to retain the "a.jre" IUs representing
      the execution environment/JRE capablilities.
    - Includes an integration test for installation and repository
      aggregation scenarios for both bundles and products requiring
      Java 7 (see also bug 385703). Compilation against a Java 7 JDK is
      disabled so that the test runs on older JDKs.
    
    Bug: 384494
    Change-Id: I279b3e3d2488d998d82259f387087dbe99ee5c21

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/eeProfile/java7/Java7ResolutionTest.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2012 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SAP AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.test.eeProfile.java7;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.eclipse.tycho.test.util.P2RepositoryTool;
+import org.eclipse.tycho.test.util.ResourceUtil.P2Repositories;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Java7ResolutionTest extends AbstractTychoIntegrationTest {
+
+    private static File buildResult;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        buildResult = new Java7ResolutionTest().runBuild();
+    }
+
+    public File runBuild() throws Exception {
+        Verifier verifier = getVerifier("eeProfile.java7", false);
+
+        // workaround for bug 385835
+        verifier.getSystemProperties().setProperty("launchers-repo", P2Repositories.ECLIPSE_342.toString());
+
+        verifier.executeGoal("verify");
+
+        // with bug 384494, the product could not be materialized
+        verifier.verifyErrorFreeLog();
+
+        return new File(verifier.getBasedir());
+    }
+
+    @Test
+    public void testRepositoryAggregationForJava7() throws Exception {
+        /*
+         * A p2 repository that only contains the bundle importing the package javax.xml.ws.spi.http
+         * (which is new in Java 7).
+         */
+        P2RepositoryTool bundleOnlyRepo = P2RepositoryTool.forEclipseRepositoryModule(new File(buildResult,
+                "repository1"));
+
+        /*
+         * With bug 384494, there was no matching export to the package import of the bundle in the
+         * repository (despite includeAllDependencies=true) and hence a p2 client may have been
+         * unable to install the bundle from the repository.
+         */
+        List<String> availablePackages = bundleOnlyRepo.getAllProvidedPackages();
+        assertThat(availablePackages, hasItem("javax.xml.ws.spi.http"));
+    }
+
+    @Test
+    public void testProductBuildForJava7() throws Exception {
+        // a p2 repository that contains a product for Java 7
+        P2RepositoryTool productRepo = P2RepositoryTool
+                .forEclipseRepositoryModule(new File(buildResult, "repository2"));
+
+        // with bug 384494, the "a.jre" IU required by the product was missing in the p2 repository
+        List<String> jreUnitVersions = productRepo.getUnitVersions("a.jre.javase");
+        assertThat(jreUnitVersions, hasItem("1.7.0"));
+    }
+}
