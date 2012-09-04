9a43caa13171c169bcbf760e5ed6c1434c1d4c58
Jan Sievers <jan.sievers@sap.com>
2012-09-04 16:02:52 +0200

    386481 PoC update maven surefire to 2.17
    - adapt to various surefire changes
    - remove OsgiEnabled* workaround classes 
    - test class scanning is now done outside of the forked process
    - TODO check if we can provided a shaded jar of surefire-booter
    and surefire-api dependencies stripped down on class level to only what
    we need
    - TODO fix one failing IT ParallelTestExecutionTest
    
    Bug: 386481
    Change-Id: Ibcb439a24add880c4cdafe67b42e29ca3cb14ff1
    Signed-off-by: Jan Sievers <jan.sievers@sap.com>

--- tycho-surefire/org.eclipse.tycho.surefire.osgibooter/src/main/java/org/eclipse/tycho/surefire/osgibooter/TychoClasspathConfiguration.java
+++ tycho-surefire/org.eclipse.tycho.surefire.osgibooter/src/main/java/org/eclipse/tycho/surefire/osgibooter/TychoClasspathConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 SAP AG and others.
+ * Copyright (c) 2011,2014 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,30 +13,19 @@ package org.eclipse.tycho.surefire.osgibooter;
 import org.apache.maven.surefire.booter.ClasspathConfiguration;
 import org.apache.maven.surefire.booter.SurefireExecutionException;
 
+// TODO check if this class is necessary
 public class TychoClasspathConfiguration extends ClasspathConfiguration {
 
-    private ClassLoader testClassLoader;
-    private ClassLoader surefireClassLoader;
+    private ClassLoader mergedClassloader;
 
-    public TychoClasspathConfiguration(ClassLoader testClassLoader, ClassLoader surefireCLassLoader) {
+    public TychoClasspathConfiguration(ClassLoader mergedClassLoader) {
         super(false, false);
-        this.testClassLoader = testClassLoader;
-        this.surefireClassLoader = surefireCLassLoader;
+        this.mergedClassloader = mergedClassLoader;
     }
 
     @Override
-    public ClassLoader createSurefireClassLoader(ClassLoader parent) throws SurefireExecutionException {
-        return surefireClassLoader;
-    }
-
-    @Override
-    public ClassLoader createTestClassLoader() throws SurefireExecutionException {
-        return testClassLoader;
-    }
-
-    @Override
-    public ClassLoader createTestClassLoader(boolean childDelegation) throws SurefireExecutionException {
-        return testClassLoader;
+    public ClassLoader createMergedClassLoader() throws SurefireExecutionException {
+        return mergedClassloader;
     }
 
 }
