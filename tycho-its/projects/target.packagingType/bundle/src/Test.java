d6522adb9b68eb8fb01bd1f92b9d1abaa08db2c4
Jan Sievers <jan.sievers@sap.com>
2012-10-05 17:47:12 +0200

    383865 ITs for packaging type eclipse-target-definition
    Test both cases:
    
    1. target defintion file is resolved from within reactor
    2. target definition file was previously installed to local
       repository using the new packaging type and can nwo be
       resolved from there
    
    Change-Id: I3de25a8e30951275f29fd4477b0b88984e7ed80c

---
+++ tycho-its/projects/target.packagingType/bundle/src/Test.java
@@ -0,0 +1,19 @@
+import org.osgi.framework.Constants;
+
+/*******************************************************************************
+ * Copyright (c) 2012 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SAP AG - initial API and implementation
+ *******************************************************************************/
+
+public class Test
+{
+
+    String s = Constants.BUNDLE_SYMBOLICNAME;
+    
+}
