da2d87fe9ca03daa81b9d71d1a86e9f3db3726e6
Jan Sievers <jan.sievers@sap.com>
2012-12-07 17:34:55 +0100

    395773 add integration test
    Add integration test for the scenario
    "generate source feature for feature with license feature"
    
    check that effective source feature.properties is merged from
    
    1. license feature
    2. feature
    3. sourceTemplate feature
    
    Change-Id: I6618eb6c2b3c5746655c58946c61eb8e573b3460

---
+++ tycho-source-feature-plugin/src/it/license-feature/verify.groovy
@@ -0,0 +1,22 @@
+import java.util.regex.Pattern;
+import java.util.zip.ZipFile;
+import java.io.*;
+
+import junit.framework.Assert;
+
+File sourceFeature = new File(basedir, "feature/target/feature-sources-feature.jar");
+Assert.assertTrue("Missing expected file " + sourceFeature, sourceFeature.canRead());
+
+ZipFile featureZip = new ZipFile(sourceFeature);
+Assert.assertNotNull("feature.properties not found in " + sourceFeature, featureZip.entries().find {it.name.equals("feature.properties")})
+
+// test bug 395773
+Properties actual = new Properties();
+actual.load(featureZip.getInputStream(featureZip.getEntry("feature.properties")));
+
+// content must be merged from 1. license feature, 2. feature, 3. sourceTemplate
+def expected = [label:"source feature label", description:"feature description", copyright:"license feature copyright", licenseURL:"license.html", license:"license feature license"]
+
+Assert.assertEquals(expected, actual);
+
+return true;
