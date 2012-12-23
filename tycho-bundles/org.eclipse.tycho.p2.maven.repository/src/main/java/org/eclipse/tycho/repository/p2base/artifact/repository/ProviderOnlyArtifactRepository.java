4b4991c8637ddff0c0f5d59df50b08b2a4ec14ed
Tobias Oberlies <tobias.oberlies@sap.com>
2012-12-23 15:08:12 +0100

    393004 p2 ArtifactRepository abstract base with fixed interface
    - Created an AbstractArtifactRepository2 class that fixes the
      obviously broken methods in the p2 IArtifactRepository API (as good
      as possible -> "fix" means deprecate here). This change should be
      pushed back into p2 eventually.
    - Additionally, methods with empty implementation in p2's base class
      AbstractArtifactRepository are replaced by abstract methods (by
      providing implementations that delegate to new, abstract methods).
    - Implemented ProviderOnlyArtifactRepository with that new base class.
    
    Bug: 393004
    Change-Id: I9362e45dee75f8db8b5e2fa40d77c56aba546fc6

--- tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/repository/p2base/artifact/repository/ProviderOnlyArtifactRepository.java
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/repository/p2base/artifact/repository/ProviderOnlyArtifactRepository.java
@@ -16,9 +16,6 @@ import java.net.URI;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.MultiStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.equinox.p2.core.IProvisioningAgent;
 import org.eclipse.equinox.p2.core.ProvisionException;
 import org.eclipse.equinox.p2.metadata.IArtifactKey;
@@ -26,17 +23,14 @@ import org.eclipse.equinox.p2.query.IQuery;
 import org.eclipse.equinox.p2.query.IQueryResult;
 import org.eclipse.equinox.p2.query.IQueryable;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
-import org.eclipse.equinox.p2.repository.artifact.IArtifactRequest;
 import org.eclipse.equinox.p2.repository.artifact.IFileArtifactRepository;
-import org.eclipse.equinox.p2.repository.artifact.spi.AbstractArtifactRepository;
-import org.eclipse.tycho.p2.maven.repository.Activator;
 import org.eclipse.tycho.repository.p2base.artifact.provider.IRawArtifactFileProvider;
 
 /**
  * Read-only repository which delegates artifact read operations to a provider instance. Adapter
  * from {@link IRawArtifactFileProvider} to {@link IFileArtifactRepository}.
  */
-public class ProviderOnlyArtifactRepository extends AbstractArtifactRepository implements IFileArtifactRepository,
+public class ProviderOnlyArtifactRepository extends AbstractArtifactRepository2 implements IFileArtifactRepository,
         IRawArtifactFileProvider {
 
     private final IRawArtifactFileProvider delegate;
@@ -65,12 +59,6 @@ public class ProviderOnlyArtifactRepository extends AbstractArtifactRepository i
 
     public IStatus getArtifact(IArtifactKey key, OutputStream destination, IProgressMonitor monitor) {
         return delegate.getArtifact(key, destination, monitor);
-    }
-
-    @Override
-    public IStatus getArtifact(IArtifactDescriptor descriptor, OutputStream destination, IProgressMonitor monitor) {
-        // letting the caller decide which format to use is non-sense
-        return delegate.getArtifact(descriptor.getArtifactKey(), destination, monitor);
     }
 
     // delegated methods for raw artifacts
@@ -104,28 +92,6 @@ public class ProviderOnlyArtifactRepository extends AbstractArtifactRepository i
         throw new UnsupportedOperationException();
     }
 
-    // default implementations
-
-    // TODO shouldn't this be implemented in the super class from p2?
-    @Override
-    public IStatus getArtifacts(IArtifactRequest[] requests, IProgressMonitor monitor) {
-        SubMonitor subMonitor = SubMonitor.convert(monitor, requests.length);
-        try {
-            MultiStatus result = new MultiStatus(Activator.ID, 0, "Error while getting requested artifacts", null);
-            for (IArtifactRequest request : requests) {
-                request.perform(this, subMonitor.newChild(1));
-                result.add(request.getResult());
-            }
-            if (!result.isOK()) {
-                return result;
-            } else {
-                return Status.OK_STATUS;
-            }
-        } finally {
-            monitor.done();
-        }
-    }
-
     // unsupported methods due to read-only
 
     @Override
@@ -134,77 +100,42 @@ public class ProviderOnlyArtifactRepository extends AbstractArtifactRepository i
     }
 
     @Override
-    public void addDescriptor(IArtifactDescriptor descriptor) {
+    protected void internalAddDescriptor(IArtifactDescriptor descriptor) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public void addDescriptor(IArtifactDescriptor descriptor, IProgressMonitor monitor) {
+    protected void internalRemoveDescriptor(IArtifactDescriptor descriptor) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public void addDescriptors(IArtifactDescriptor[] descriptors) {
+    protected void internalRemoveDescriptors(IArtifactDescriptor[] descriptors) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public void addDescriptors(IArtifactDescriptor[] descriptors, IProgressMonitor monitor) {
+    protected void internalRemoveDescriptors(IArtifactKey key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void internalRemoveDescriptors(IArtifactKey[] keys) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void internalRemoveAllDescriptors() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void internalStore(IProgressMonitor monitor) {
         throw new UnsupportedOperationException();
     }
 
     @Override
     public OutputStream getOutputStream(IArtifactDescriptor descriptor) throws ProvisionException {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void removeAll() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void removeAll(IProgressMonitor monitor) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void removeDescriptor(IArtifactDescriptor descriptor) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void removeDescriptor(IArtifactDescriptor descriptor, IProgressMonitor monitor) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void removeDescriptor(IArtifactKey key) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void removeDescriptor(IArtifactKey key, IProgressMonitor monitor) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void removeDescriptors(IArtifactDescriptor[] descriptors) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void removeDescriptors(IArtifactDescriptor[] descriptors, IProgressMonitor monitor) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void removeDescriptors(IArtifactKey[] keys) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void removeDescriptors(IArtifactKey[] keys, IProgressMonitor monitor) {
         throw new UnsupportedOperationException();
     }
 
