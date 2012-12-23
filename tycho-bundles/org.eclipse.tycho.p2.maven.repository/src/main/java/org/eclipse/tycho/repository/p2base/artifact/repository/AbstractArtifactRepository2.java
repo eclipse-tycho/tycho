31ed3233a955164b9458d3771a6a3172f1fd403c
Tobias Oberlies <tobias.oberlies@sap.com>
2012-12-23 15:08:12 +0100

    397355 p2 ArtifactRepository abstract base with fixed interface
    - Created an AbstractArtifactRepository2 class that fixes the
      obviously broken methods in the p2 IArtifactRepository API (as good
      as possible -> "fix" means deprecate here). This change should be
      pushed back into p2 eventually.
    - Additionally, methods with empty implementation in p2's base class
      AbstractArtifactRepository are replaced by abstract methods (by
      providing implementations that delegate to new, abstract methods).
    - Implemented ProviderOnlyArtifactRepository with that new base class.
    
    Bug: 397355
    Bug: 393004
    Change-Id: I9362e45dee75f8db8b5e2fa40d77c56aba546fc6

---
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/repository/p2base/artifact/repository/AbstractArtifactRepository2.java
@@ -0,0 +1,164 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2013 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SAP AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.repository.p2base.artifact.repository;
+
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.equinox.p2.core.IProvisioningAgent;
+import org.eclipse.equinox.p2.metadata.IArtifactKey;
+import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
+import org.eclipse.equinox.p2.repository.artifact.IArtifactRequest;
+import org.eclipse.equinox.p2.repository.artifact.spi.AbstractArtifactRepository;
+import org.eclipse.tycho.p2.maven.repository.Activator;
+import org.eclipse.tycho.repository.p2base.artifact.provider.IArtifactProvider;
+
+public abstract class AbstractArtifactRepository2 extends AbstractArtifactRepository implements IArtifactProvider {
+
+    protected AbstractArtifactRepository2(IProvisioningAgent agent, String name, String type, String version,
+            URI location, String description, String provider, Map<String, String> properties) {
+        super(agent, name, type, version, location, description, provider, properties);
+    }
+
+    @Override
+    public final IStatus getArtifact(IArtifactDescriptor descriptor, OutputStream destination, IProgressMonitor monitor) {
+        // letting the caller decide which format to use is non-sense
+        return getArtifact(descriptor.getArtifactKey(), destination, monitor);
+    }
+
+    protected abstract void internalAddDescriptor(IArtifactDescriptor descriptor);
+
+    protected abstract void internalStore(IProgressMonitor monitor);
+
+    // methods that should really not be public; they violate the consistency of repositories
+    @Deprecated
+    @Override
+    public final void addDescriptor(IArtifactDescriptor descriptor) {
+        addDescriptor(descriptor, null);
+    }
+
+    @Deprecated
+    @Override
+    public final void addDescriptor(IArtifactDescriptor descriptor, IProgressMonitor monitor) {
+        internalAddDescriptor(descriptor);
+        internalStore(monitor);
+    }
+
+    @Deprecated
+    @Override
+    public final void addDescriptors(IArtifactDescriptor[] descriptors) {
+        addDescriptors(descriptors, null);
+    }
+
+    @Deprecated
+    @Override
+    public final void addDescriptors(IArtifactDescriptor[] descriptors, IProgressMonitor monitor) {
+        for (IArtifactDescriptor descriptor : descriptors) {
+            internalAddDescriptor(descriptor);
+        }
+        internalStore(monitor);
+    }
+
+    // ensure that remove operations are properly implemented
+
+    protected abstract void internalRemoveDescriptor(IArtifactDescriptor descriptor);
+
+    protected abstract void internalRemoveDescriptors(IArtifactDescriptor[] descriptors);
+
+    protected abstract void internalRemoveDescriptors(IArtifactKey key);
+
+    protected abstract void internalRemoveDescriptors(IArtifactKey[] keys);
+
+    protected abstract void internalRemoveAllDescriptors();
+
+    @Override
+    public final void removeDescriptor(IArtifactDescriptor descriptor) {
+        removeDescriptor(descriptor, null);
+    }
+
+    @Override
+    public final void removeDescriptor(IArtifactDescriptor descriptor, IProgressMonitor monitor) {
+        internalRemoveDescriptor(descriptor);
+        internalStore(monitor);
+    }
+
+    @Override
+    public final void removeDescriptors(IArtifactDescriptor[] descriptors) {
+        removeDescriptors(descriptors, null);
+    }
+
+    @Override
+    public final void removeDescriptors(IArtifactDescriptor[] descriptors, IProgressMonitor monitor) {
+        internalRemoveDescriptors(descriptors);
+        internalStore(monitor);
+    }
+
+    @Override
+    public final void removeDescriptor(IArtifactKey key) {
+        removeDescriptor(key, null);
+    }
+
+    @Override
+    public final void removeDescriptor(IArtifactKey key, IProgressMonitor monitor) {
+        internalRemoveDescriptors(key);
+        internalStore(monitor);
+    }
+
+    @Override
+    public final void removeDescriptors(IArtifactKey[] keys) {
+        removeDescriptors(keys, null);
+    }
+
+    @Override
+    public final void removeDescriptors(IArtifactKey[] keys, IProgressMonitor monitor) {
+        internalRemoveDescriptors(keys);
+        internalStore(monitor);
+    }
+
+    @Override
+    public final void removeAll() {
+        removeAll(null);
+    }
+
+    @Override
+    public final void removeAll(IProgressMonitor monitor) {
+        internalRemoveAllDescriptors();
+        internalStore(monitor);
+    }
+
+    // add default implementations missing in AbstractArtifactRepository
+
+    @Override
+    public IStatus getArtifacts(IArtifactRequest[] requests, IProgressMonitor monitor) {
+        SubMonitor subMonitor = SubMonitor.convert(monitor, requests.length);
+        try {
+            MultiStatus result = new MultiStatus(Activator.ID, 0, "Error while getting requested artifacts", null);
+            for (IArtifactRequest request : requests) {
+                request.perform(this, subMonitor.newChild(1));
+                result.add(request.getResult());
+            }
+            if (!result.isOK()) {
+                return result;
+            } else {
+                return Status.OK_STATUS;
+            }
+        } finally {
+            monitor.done();
+        }
+    }
+
+}
