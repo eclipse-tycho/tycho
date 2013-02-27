152f44b7d577c92fc6a13ccc1c629d51bb168ac9
Jan Sievers <jan.sievers@sap.com>
2013-02-27 12:03:23 +0100

    358292 Update versions for products in eclipse-repository
    tycho-versions-plugin will now update the product version product files
    in eclipse-repository. The product version will only be updated if it
    matched the POM version before the change.
    
    Bug: 358292
    Change-Id: I4ce15339cff4eb417f79b3a9432c5920a006b27b

---
+++ tycho-release/tycho-versions-plugin/src/main/java/org/eclipse/tycho/versions/engine/ProductConfigurations.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2013 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SAP AG - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.tycho.versions.engine;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.tycho.model.ProductConfiguration;
+
+/**
+ * Value object to hold multiple product files in eclipse-repository projects registered in
+ * {@link ProjectMetadata}.
+ */
+public class ProductConfigurations {
+
+    private Map<File, ProductConfiguration> productConfigurations = new HashMap<File, ProductConfiguration>();
+
+    public void addProductConfiguration(File productFile, ProductConfiguration productConfiguration) {
+        productConfigurations.put(productFile, productConfiguration);
+    }
+
+    public Map<File, ProductConfiguration> getProductConfigurations() {
+        return productConfigurations;
+    }
+}
