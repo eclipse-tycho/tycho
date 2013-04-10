1bcfdcc92884cc0e2c0d30ba4f45a41655ca5c18
Jan Sievers <jan.sievers@sap.com>
2013-04-10 14:36:21 +0200

    404595 allow -Dtycho.debug.resolver=true
    for partial builds or small reactors, it's tedious
    to lookup the artifactId for which to switch on
    resolver debug mode.
    Keep the current behaviour, but also allow -Dtycho.debug.resolver=true for convenience.
    
    Bug: 404595
    Change-Id: Iae633739e00cb86a7259fb0f4aa6e4d7267f755c

--- tycho-core/src/main/java/org/eclipse/tycho/core/osgitools/DebugUtils.java
+++ tycho-core/src/main/java/org/eclipse/tycho/core/osgitools/DebugUtils.java
@@ -16,6 +16,6 @@ import org.apache.maven.project.MavenProject;
 public class DebugUtils {
     public static boolean isDebugEnabled(MavenSession session, MavenProject project) {
         String config = session.getUserProperties().getProperty("tycho.debug.resolver");
-        return config != null && config.trim().equals(project.getArtifactId());
+        return config != null && (config.trim().equals(project.getArtifactId()) || "true".equals(config.trim()));
     }
 }
