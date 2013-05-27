8057d03328ef7591ba58a546bf9c8200a984e623
Jan Sievers <jan.sievers@sap.com>
2013-05-27 10:21:56 +0200

    407972 Fix ITs, take 2
    Fix compile error.
    Note to self: avoid last-minute weekend fixes.
    
    Bug: 407972
    Change-Id: If6b67b4fdef34ccf5b930aa2a83b18cc26de0a0f

--- tycho-its/src/test/java/org/eclipse/tycho/test/TYCHO188P2EnabledRcp/Tycho188P2EnabledRcpTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/TYCHO188P2EnabledRcp/Tycho188P2EnabledRcpTest.java
@@ -106,7 +106,7 @@ public class Tycho188P2EnabledRcpTest extends AbstractTychoIntegrationTest {
 
     static private void assertProductIUs(Document contentXml, Product product, Environment env) {
         assertTrue(product.unitId + " IU with org.eclipse.equinox.p2.type.product property value true does not exist",
-                +Util.containsIUWithProperty(contentXml, product.unitId, "org.eclipse.equinox.p2.type.product", "true"));
+                Util.containsIUWithProperty(contentXml, product.unitId, "org.eclipse.equinox.p2.type.product", "true"));
 
         final String p2InfAdded = "p2.inf.added-property";
         assertEquals("Property " + p2InfAdded + " in " + product.unitId, product.p2InfProperty,
