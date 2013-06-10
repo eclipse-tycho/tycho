155fab33b96490c0a6321855766aa2ed5973d50c
Tobias Oberlies <tobias.oberlies@sap.com>
2013-06-10 12:43:36 +0200

    393004 Test retry behaviour of RepositoryArtifactProvider
    - Test that RepositoryArtifactProvider correctly handles cases
      where an IArtifactRepository returns errors with code
      CODE_RETRY, indicating that you've hit a bad mirror but there may be
      other mirrors to try.
    - Use Mockito stubs to simulate behaviour that is otherwise hard to
      enforce in a set-up with real implementations.
    
    Bug: 393004
    CQ: 7358 (test-only CQ for Mockito)
    CQ: 7357 (dependency of Mockito: hamcrest-all)
    CQ: 7359 (dependency of Mockito: objenesis)
    
    Change-Id: Ic5b66a504d554e8e7d536bd86e207f3f8ec44bb4
    Signed-off-by: Tobias Oberlies <tobias.oberlies@sap.com>

--- tycho-bundles/org.eclipse.tycho.p2.maven.repository.tests/src/test/java/org/eclipse/tycho/repository/p2base/artifact/repository/RepositoryArtifactProviderTest.java
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository.tests/src/test/java/org/eclipse/tycho/repository/p2base/artifact/repository/RepositoryArtifactProviderTest.java
@@ -26,16 +26,29 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.io.OutputStream;
 import java.net.URI;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
+import org.eclipse.equinox.p2.repository.artifact.IArtifactRepository;
 import org.eclipse.tycho.p2.maven.repository.tests.TestRepositoryContent;
 import org.eclipse.tycho.repository.p2base.artifact.provider.CompositeArtifactProviderTestBase;
 import org.eclipse.tycho.repository.p2base.artifact.provider.IRawArtifactProvider;
 import org.eclipse.tycho.repository.p2base.artifact.provider.formats.ArtifactTransferPolicies;
 import org.eclipse.tycho.repository.p2base.artifact.provider.formats.ArtifactTransferPolicy;
+import org.eclipse.tycho.repository.p2base.artifact.repository.RepositoryArtifactProvider.RepositoryLoader;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -115,4 +128,58 @@ public class RepositoryArtifactProviderTest extends CompositeArtifactProviderTes
         assertThat(status, is(errorStatus()));
         assertThat(testSink.writeIsCommitted(), is(false));
     }
+
+    @Test
+    public void testGetArtifactWithSomeMirrorFailures() throws Exception {
+        IArtifactRepository failingMirrorsRepository = mock(IArtifactRepository.class);
+        when(failingMirrorsRepository.contains(BUNDLE_A_KEY)).thenReturn(true);
+        when(failingMirrorsRepository.getArtifactDescriptors(BUNDLE_A_KEY)).thenReturn(
+                new IArtifactDescriptor[] { canonicalDescriptorFor(BUNDLE_A_KEY) });
+        when(
+                failingMirrorsRepository.getArtifact(argThat(is(canonicalDescriptorFor(BUNDLE_A_KEY))),
+                        any(OutputStream.class), any(IProgressMonitor.class)))
+                .thenReturn(errorWithRetry("mirror 1 failure")) //
+                .thenReturn(errorWithRetry("mirror 2 failure")) //
+                .thenReturn(Status.OK_STATUS);
+        subject = new RepositoryArtifactProvider(loaderFor(failingMirrorsRepository), TRANSFER_POLICY);
+
+        testSink = newArtifactSinkFor(BUNDLE_A_KEY);
+        status = subject.getArtifact(testSink, null);
+
+        assertThat(status, is(warningStatus()));
+        assertThat(status.toString(), containsString("mirror 1"));
+        assertThat(status.toString(), containsString("mirror 2"));
+    }
+
+    @Test
+    public void testGetArtifactWithInfiniteMirrorFailures() throws Exception {
+        IArtifactRepository failingMirrorsRepository = mock(IArtifactRepository.class);
+        when(failingMirrorsRepository.contains(BUNDLE_A_KEY)).thenReturn(true);
+        when(failingMirrorsRepository.getArtifactDescriptors(BUNDLE_A_KEY)).thenReturn(
+                new IArtifactDescriptor[] { canonicalDescriptorFor(BUNDLE_A_KEY) });
+        when(
+                failingMirrorsRepository.getArtifact(argThat(is(canonicalDescriptorFor(BUNDLE_A_KEY))),
+                        any(OutputStream.class), any(IProgressMonitor.class))).thenReturn(
+                errorWithRetry("mirror failure"));
+        subject = new RepositoryArtifactProvider(loaderFor(failingMirrorsRepository), TRANSFER_POLICY);
+
+        testSink = newArtifactSinkFor(BUNDLE_A_KEY);
+        status = subject.getArtifact(testSink, null); // should give up if all mirrors fail
+
+        assertThat(status, is(errorStatus()));
+    }
+
+    private static IStatus errorWithRetry(String message) {
+        return new Status(IStatus.ERROR, "test", IArtifactRepository.CODE_RETRY, "Stub error: " + message, null);
+    }
+
+    private RepositoryLoader loaderFor(final IArtifactRepository repository) throws Exception {
+        return new RepositoryLoader(null, p2Context.getAgent()) {
+            @Override
+            List<IArtifactRepository> loadRepositories() {
+                return Collections.singletonList(repository);
+            }
+        };
+    }
+
 }
