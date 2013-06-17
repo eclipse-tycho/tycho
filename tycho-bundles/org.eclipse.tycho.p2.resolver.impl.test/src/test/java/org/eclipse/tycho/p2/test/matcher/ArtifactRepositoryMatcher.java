3bed23772626b207000aa656604ed473e8546d29
Tobias Oberlies <tobias.oberlies@sap.com>
2013-06-17 20:14:12 +0200

    393004 Use new base class for TPBundlesPublisher artifact repository
    - Migrate p2 artifact repository of TargetPlatformBundlePublisher to
      the new artifact repository base class. With this change, the
      repository also implements the IRawArtifactProvider interface and
      can be included in the joint target platform artifacts (in a
      subsequent commit).
    - Also: Added documentation why the TargetPlatformBundlePublisher needs
      its own artifact repository implementation.
    
    Bug: 393004
    Change-Id: I51a970f6e917d7b784886a65c2a0d5ec85594c6e
    Signed-off-by: Tobias Oberlies <tobias.oberlies@sap.com>

--- tycho-bundles/org.eclipse.tycho.p2.resolver.impl.test/src/test/java/org/eclipse/tycho/p2/test/matcher/ArtifactRepositoryMatcher.java
+++ tycho-bundles/org.eclipse.tycho.p2.resolver.impl.test/src/test/java/org/eclipse/tycho/p2/test/matcher/ArtifactRepositoryMatcher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 SAP AG and others.
+ * Copyright (c) 2011, 2013 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,14 +10,8 @@
  *******************************************************************************/
 package org.eclipse.tycho.p2.test.matcher;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.equinox.p2.metadata.IArtifactKey;
-import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
-import org.eclipse.equinox.p2.repository.artifact.IArtifactRepository;
+import org.eclipse.tycho.repository.p2base.artifact.provider.IArtifactProvider;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -28,59 +22,18 @@ public class ArtifactRepositoryMatcher {
         return artifactKey.getClassifier() + ":" + artifactKey.getId() + ":" + artifactKey.getVersion();
     }
 
-    public static Matcher<IArtifactRepository> containsEntry(final IArtifactKey artifactKey) {
-        return new TypeSafeMatcher<IArtifactRepository>() {
+    public static Matcher<IArtifactProvider> contains(final IArtifactKey artifactKey) {
+        return new TypeSafeMatcher<IArtifactProvider>() {
 
             public void describeTo(Description description) {
                 description.appendText("artifact repository with entry " + keyToString(artifactKey));
             }
 
             @Override
-            public boolean matchesSafely(IArtifactRepository repo) {
+            public boolean matchesSafely(IArtifactProvider repo) {
                 return repo.contains(artifactKey);
             }
         };
     }
 
-    public static EntryMatcher entry(IArtifactKey artifactKey) {
-        return new EntryMatcher(artifactKey);
-    }
-
-    public static class EntryMatcher {
-
-        private final IArtifactKey artifactKey;
-
-        public EntryMatcher(IArtifactKey artifactKey) {
-            this.artifactKey = artifactKey;
-        }
-
-        public Matcher<IArtifactRepository> hasContent(final File expectedArtifactContent) {
-            if (!expectedArtifactContent.canRead())
-                throw new IllegalArgumentException("Cannot read expected content from " + expectedArtifactContent);
-
-            return new TypeSafeMatcher<IArtifactRepository>() {
-
-                public void describeTo(Description description) {
-                    description.appendText("artifact repository with entry " + keyToString(artifactKey)
-                            + " whose artifact is equal to " + expectedArtifactContent);
-                }
-
-                @Override
-                public boolean matchesSafely(IArtifactRepository repo) {
-                    IArtifactDescriptor[] descriptors = repo.getArtifactDescriptors(artifactKey);
-                    if (descriptors.length == 0)
-                        return false;
-
-                    OutputStream assertingSink = new ComparingOutputStream(expectedArtifactContent);
-                    try {
-                        IStatus status = repo.getArtifact(descriptors[0], assertingSink, null);
-                        assertingSink.close();
-                        return status.isOK();
-                    } catch (IOException e) {
-                        return false;
-                    }
-                }
-            };
-        }
-    }
 }
