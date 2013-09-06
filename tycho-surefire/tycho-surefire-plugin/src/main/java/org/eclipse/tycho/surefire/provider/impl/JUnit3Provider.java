960c532844e05dcac262ab9ccd09ae5526a35fc9
Jan Sievers <jan.sievers@sap.com>
2013-09-06 15:49:42 +0200

    386988 Run tests on provisioned installation
    Add boolean plugin parameter "provisionInstallation"
    which can be used to provision the installation under test
    using p2 director before running the tests.
    
    If provisionInstallation is true,
    the test bundle and surefire test harness bundles
    (and their respective dependencies) are installed using
    p2 director.
    A product under test can be installed using
    extraRequirements in pom.xml, see example in javadoc.
    
    An already existing product can also be tested simply by
    copying it into target/work before tests are executed.
    Then, only test and test harness bundles are installed on top.
    Make sure to configure the correct p2 profile in this case
    using mojo parameter "profileName".
    
    Also:
    - refactor TestMojo.createEclipseInstallation() to allow for
      reuse in createProvisionedInstallation()
    - rename ProviderSelector to ProviderHelper
    
    Bug: 386988
    Change-Id: Idc3669bda32c85192296f3556ec562d38b1a1cc1
    Also-By: Mickael Istria <mistria@redhat.com>
    Signed-off-by: Jan Sievers <jan.sievers@sap.com>

--- tycho-surefire/tycho-surefire-plugin/src/main/java/org/eclipse/tycho/surefire/provider/impl/JUnit3Provider.java
+++ tycho-surefire/tycho-surefire-plugin/src/main/java/org/eclipse/tycho/surefire/provider/impl/JUnit3Provider.java
@@ -13,7 +13,7 @@ package org.eclipse.tycho.surefire.provider.impl;
 
 import static java.util.Collections.singleton;
 import static java.util.Collections.singletonList;
-import static org.eclipse.tycho.surefire.provider.impl.ProviderSelector.newDependency;
+import static org.eclipse.tycho.surefire.provider.impl.ProviderHelper.newDependency;
 
 import java.util.List;
 import java.util.Set;
