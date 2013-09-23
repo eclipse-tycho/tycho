dd1e6a7c97962a646816dc0e4eb591e2264d3734
Tobias Oberlies <tobias.oberlies@sap.com>
2013-09-23 10:38:00 +0200

    412416 Add a target-platform goal to calculate the final TP
    - Added a mojo that computes the final target platform. This target
      platform object only includes final metadata from reactor
      projects (read from the p2content.xml, which is written by the
      publishing built steps), so we can be certain that no dependency-
      only metadata leaks into the build result (bug 369676). The new 
      mojo is also a step towards deferring the dependency resolution
      (bug 353889), where we will only have the target platform with
      final metadata.
    - Include those reactor projects in the target platform which are
      (transitively) referenced by the current project. The transitive
      project traversal is necessary for cases where dependencies are
      only injected into the Maven model in a shallow way. This happens
      with source features, where there are no dependency-only IUs
      but the dependency is only marked through an extraRequirement at
      dependency resolution time. (For an example see the
      extra-source-bundles IT of the tycho-source-feature-plugin.)
      Don't include all projects which have been built before, so that
      enabling parallel build would not have an effect on the target
      platform.
    - Moved ReactorRepositoryManagerImpl to p2.resolver.impl bundle to
      allow adding the target platform registration logic. The service
      shall become the central place where modules publish their build
      results and consume build results of upstream modules (via the
      final target platform).
    - Also: Added comments to interfaces and classes where I had
      forgotten what they were good for.
    
    Bug: 412416
    Bug: 369676
    Bug: 353889
    Change-Id: I8a14122f3dc5dfde84944a128133459a2767754b
    Signed-off-by: Tobias Oberlies <tobias.oberlies@sap.com>

---
+++ target-platform-configuration/src/main/java/org/eclipse/tycho/target/TargetPlatformMojo.java
@@ -0,0 +1,139 @@
+/*******************************************************************************
+ * Copyright (c) 2013 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SAP AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.target;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.sisu.equinox.EquinoxServiceFactory;
+import org.eclipse.tycho.ReactorProject;
+import org.eclipse.tycho.ReactorProjectIdentities;
+import org.eclipse.tycho.core.osgitools.DefaultReactorProject;
+import org.eclipse.tycho.p2.repository.GAV;
+import org.eclipse.tycho.p2.repository.RepositoryLayoutHelper;
+import org.eclipse.tycho.repository.registry.facade.ReactorRepositoryManagerFacade;
+
+/**
+ * @goal target-platform
+ */
+public class TargetPlatformMojo extends AbstractMojo {
+
+    // TODO site doc (including steps & parameters handled in afterProjectsRead?)
+    /**
+     * @parameter expression="${project}"
+     * @readonly
+     */
+    private MavenProject project;
+
+    /** @component */
+    private EquinoxServiceFactory osgiServices;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        ReactorRepositoryManagerFacade repositoryManager = osgiServices
+                .getService(ReactorRepositoryManagerFacade.class);
+        List<ReactorProjectIdentities> upstreamProjects = getReferencedTychoProjects();
+        repositoryManager.computeFinalTargetPlatform(DefaultReactorProject.adapt(project), upstreamProjects);
+    }
+
+    private List<ReactorProjectIdentities> getReferencedTychoProjects() throws MojoExecutionException {
+        List<ReactorProjectIdentities> result = new ArrayList<ReactorProjectIdentities>();
+        HashSet<GAV> considered = new HashSet<GAV>();
+
+        getTransitivelyReferencedTychoProjects(project.getProjectReferences().values(), considered, result);
+
+        return result;
+    }
+
+    private void getTransitivelyReferencedTychoProjects(Collection<MavenProject> candidateProjects,
+            HashSet<GAV> consideredProjects, List<ReactorProjectIdentities> result) throws MojoExecutionException {
+
+        for (MavenProject reactorProject : candidateProjects) {
+            if (!enterProject(reactorProject, consideredProjects)) {
+                continue;
+            }
+
+            // check for target platform relevant build results (registered by either p2-metadata-default or attach-artifacts)
+            File metadataXml = getAttachedArtifact(reactorProject, RepositoryLayoutHelper.CLASSIFIER_P2_METADATA);
+            if (metadataXml == null) {
+                continue;
+            }
+            File artifactXml = getAttachedArtifact(reactorProject, RepositoryLayoutHelper.CLASSIFIER_P2_ARTIFACTS);
+
+            // found a Tycho project -> include in target platform
+            ReactorProject tychoReactorProject = DefaultReactorProject.adapt(reactorProject);
+            verifyIndexFileLocations(tychoReactorProject, metadataXml, artifactXml);
+            result.add(tychoReactorProject.getIdentities());
+
+            getTransitivelyReferencedTychoProjects(reactorProject.getProjectReferences().values(), consideredProjects,
+                    result);
+        }
+    }
+
+    private boolean enterProject(MavenProject project, HashSet<GAV> consideredProjects) {
+        GAV projectGav = new GAV(project.getGroupId(), project.getArtifactId(), project.getVersion());
+
+        if (consideredProjects.contains(projectGav)) {
+            return false;
+        } else {
+            consideredProjects.add(projectGav);
+            return true;
+        }
+    }
+
+    private static File getAttachedArtifact(MavenProject project, String classifier) {
+        for (Artifact artifact : project.getAttachedArtifacts()) {
+            if (classifier.equals(artifact.getClassifier())) {
+                return artifact.getFile();
+            }
+        }
+        return null;
+    }
+
+    private static void verifyIndexFileLocations(ReactorProject project, File metadataXml, File artifactXml)
+            throws MojoExecutionException {
+        verifyArtifactLocationInTargetFolder(project, RepositoryLayoutHelper.CLASSIFIER_P2_METADATA,
+                RepositoryLayoutHelper.FILE_NAME_P2_METADATA, metadataXml);
+        verifyArtifactLocationInTargetFolder(project, RepositoryLayoutHelper.CLASSIFIER_P2_ARTIFACTS,
+                RepositoryLayoutHelper.FILE_NAME_P2_ARTIFACTS, artifactXml);
+        verifyFilePresenceInTargetFolder(project, RepositoryLayoutHelper.FILE_NAME_LOCAL_ARTIFACTS);
+    }
+
+    private static void verifyArtifactLocationInTargetFolder(ReactorProject project, String artifactClassifier,
+            String expectedPathInTarget, File actualLocation) throws MojoExecutionException {
+        File expectedLocation = project.getBuildDirectory().getChild(expectedPathInTarget);
+        if (actualLocation == null) {
+            throw new MojoExecutionException("Unexpected build result of " + project + ": Artifact with classifier '"
+                    + artifactClassifier + "' expected at location \"" + expectedLocation + "\", but is missing");
+        } else if (!(expectedLocation.equals(actualLocation.getAbsoluteFile()))) {
+            throw new MojoExecutionException("Unexpected build result of " + project + ": Artifact with classifier '"
+                    + artifactClassifier + "' expected at location \"" + expectedLocation + "\", but is at \""
+                    + actualLocation.getAbsolutePath() + "\"");
+        }
+    }
+
+    private static void verifyFilePresenceInTargetFolder(ReactorProject project, String expectedPathInTarget)
+            throws MojoExecutionException {
+        File expectedLocation = project.getBuildDirectory().getChild(expectedPathInTarget);
+        if (!expectedLocation.isFile()) {
+            throw new MojoExecutionException("Unexpected build result of " + project + ": File \"" + expectedLocation
+                    + "\" is missing");
+        }
+    }
+
+}
