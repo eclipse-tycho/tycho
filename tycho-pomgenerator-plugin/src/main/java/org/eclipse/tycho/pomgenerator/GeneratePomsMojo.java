3cbd6291977428b78ac85d1b8d4b67f74e3f4ac1
Thanh Ha <thanh.ha@eclipse.org>
2013-10-25 16:12:57 -0400

    420120 tycho-pomgenerator-plugin:generate-poms creates deprecated eclipse-update-site packaging type
    - Now detects category.xml and creates a eclipse-repository
    - Prints a warning when detected a site.xml file instead of category.xml
    - Added a new test for eclipse-repository
    
    Change-Id: Id972777e09ebd9c12ca6726d171e7d13ee914e8e
    Signed-off-by: Thanh Ha <thanh.ha@eclipse.org>

--- tycho-pomgenerator-plugin/src/main/java/org/eclipse/tycho/pomgenerator/GeneratePomsMojo.java
+++ tycho-pomgenerator-plugin/src/main/java/org/eclipse/tycho/pomgenerator/GeneratePomsMojo.java
@@ -297,7 +297,8 @@ public class GeneratePomsMojo extends AbstractMojo {
     }
 
     private boolean isProjectDir(File dir) {
-        return isPluginProject(dir) || isFeatureProject(dir) || isUpdateSiteProject(dir);
+        return isPluginProject(dir) || isFeatureProject(dir) || isUpdateSiteProject(dir)
+                || isEclipseRepositoryProject(dir);
     }
 
     private void reorderModules(Model parent, File basedir, File testSuiteLocation) throws MojoExecutionException {
@@ -398,8 +399,15 @@ public class GeneratePomsMojo extends AbstractMojo {
         } else if (isFeatureProject(basedir)) {
             getLog().debug("Found feature PDE project " + toString(basedir));
             generateFeaturePom(parent, basedir);
+        } else if (isEclipseRepositoryProject(basedir)) {
+            getLog().debug("Found eclipse-repository PDE project " + toString(basedir));
+            generateEclipseRepositoryPom(parent, basedir);
         } else if (isUpdateSiteProject(basedir)) {
             getLog().debug("Found update site PDE project " + toString(basedir));
+            getLog().warn(
+                    "Old style update site found for " + toString(basedir)
+                            + ". It is recommended you rename your site.xml to category.xml "
+                            + "and use packaging type eclipse-repository instead.");
             generateUpdateSitePom(parent, basedir);
         } else {
             getLog().debug("Not a PDE project " + toString(basedir));
@@ -410,6 +418,10 @@ public class GeneratePomsMojo extends AbstractMojo {
 
     private boolean isUpdateSiteProject(File dir) {
         return new File(dir, "site.xml").canRead();
+    }
+
+    private boolean isEclipseRepositoryProject(File dir) {
+        return new File(dir, "category.xml").canRead();
     }
 
     private boolean isFeatureProject(File dir) {
@@ -443,16 +455,38 @@ public class GeneratePomsMojo extends AbstractMojo {
         updateSites.put(basedir, parent);
     }
 
+    private void generateEclipseRepositoryPom(Model parent, File basedir) throws MojoExecutionException {
+        if (groupId == null) {
+            throw new MojoExecutionException(
+                    "groupId parameter is required to generate pom.xml for Eclipse Repository project "
+                            + basedir.getName());
+        }
+        if (version == null) {
+            throw new MojoExecutionException(
+                    "version parameter is required to generate pom.xml for Eclipse Repository project "
+                            + basedir.getName());
+        }
+
+        Model model = readPomTemplate("repository-pom.xml");
+        setParent(basedir, model, parent);
+        model.setGroupId(groupId);
+        model.setArtifactId(basedir.getName());
+        model.setVersion(version);
+        writePom(basedir, model);
+    }
+
     private Set<File> getSiteFeaturesAndPlugins(File basedir) throws MojoExecutionException {
         try {
             Set<File> result = new LinkedHashSet<File>();
 
-            UpdateSite site = UpdateSite.read(new File(basedir, "site.xml"));
-
-            for (FeatureRef feature : site.getFeatures()) {
-                addFeature(result, feature.getId());
+            UpdateSite site;
+            File siteFile = new File(basedir, "site.xml");
+            if (siteFile.exists()) {
+                site = UpdateSite.read(siteFile);
+                for (FeatureRef feature : site.getFeatures()) {
+                    addFeature(result, feature.getId());
+                }
             }
-
             return result;
         } catch (Exception e) {
             throw new MojoExecutionException("Could not collect update site features and plugins", e);
