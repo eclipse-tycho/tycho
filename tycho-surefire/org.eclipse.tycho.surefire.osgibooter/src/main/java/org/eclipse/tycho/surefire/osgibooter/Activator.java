f3098cc8e6ceca35623d3a6aeec44626825c9385
Mickael Istria <mistria@redhat.com>
2013-11-29 19:35:19 +0100

    [422857] Avoid using PlatformAdmin
    Change-Id: I7b75bf45271219aabcef0082d3fe10dd2ba2d4e7
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- tycho-surefire/org.eclipse.tycho.surefire.osgibooter/src/main/java/org/eclipse/tycho/surefire/osgibooter/Activator.java
+++ tycho-surefire/org.eclipse.tycho.surefire.osgibooter/src/main/java/org/eclipse/tycho/surefire/osgibooter/Activator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Sonatype Inc. and others.
+ * Copyright (c) 2008, 2014 Sonatype Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Sonatype Inc. - initial API and implementation
+ *    Red Hat Inc.  - Improve message about PlatformAdmin
  *******************************************************************************/
 package org.eclipse.tycho.surefire.osgibooter;
 
@@ -65,7 +66,7 @@ public class Activator implements BundleActivator {
     public static Set<ResolverError> getResolutionErrors(Bundle bundle) {
         Set<ResolverError> errors = new LinkedHashSet<ResolverError>();
         if (platformAdmin == null) {
-            System.err.println("Could not acquire PlatformAdmin server");
+            System.err.println("Could not compute resolution errors (PlatformAdmin service not available)");
             return errors;
         }
         State state = platformAdmin.getState(false /* mutable */);
