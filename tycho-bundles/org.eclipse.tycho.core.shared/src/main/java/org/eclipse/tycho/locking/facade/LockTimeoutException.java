713fb1989551a4840d5dedd4ed8c7290e307f003
Aurelien Pupier <aurelien.pupier@bonitasoft.com>
2014-03-23 20:23:54 -0700

    [cleanup]Add missing UUID for Serializable class LockTimeoutException
    Change-Id: Id5d03916d7cd7d2e27ba10577484a2f94552945d
    Signed-off-by: Aurelien Pupier <aurelien.pupier@bonitasoft.com>

--- tycho-bundles/org.eclipse.tycho.core.shared/src/main/java/org/eclipse/tycho/locking/facade/LockTimeoutException.java
+++ tycho-bundles/org.eclipse.tycho.core.shared/src/main/java/org/eclipse/tycho/locking/facade/LockTimeoutException.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 SAP AG and others.
+ * Copyright (c) 2011-2014 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,6 +12,8 @@
 package org.eclipse.tycho.locking.facade;
 
 public class LockTimeoutException extends RuntimeException {
+
+    private static final long serialVersionUID = -5817180143050849842L;
 
     public LockTimeoutException(String message) {
         super(message);
