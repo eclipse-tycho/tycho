983ac0c8071db2fbfbf49c1d586a9ef5e972790a
Jan Sievers <jan.sievers@sap.com>
2014-04-23 15:28:20 +0200

    429809 Use commons-compress to create .tar.gz product archives
    Use commons-compress to create the tar.gz product archives. This enables
    creating symlink archive entries for symlinks instead of inlining them
    (see root cause http://jira.codehaus.org/browse/PLXCOMP-117)
    
    - Read PosixFilePermissions and convert them to octal
    (chmod style) file mode so they are preserved in the tar archive.
    - Detect symlinks and create a symlink archive entry if the link
    target resolves below the archive root.
    - Preserve lastModified timestamp of archive entries.
    - Preserve user name / group name of file owner
    - Use GNU extensions for tar entries with path length > 100 chars.
    - Fallback to old implementation (based plexus-archiver) if java.nio
    not present (i.e. for JDK version <= 6)
    - Add "emergency off" switch -Dtycho.tar=plexus in case we
      want to force the use of the old tar archiver
    
    Bug: 429809
    Change-Id: Ic91eb1f895f6f9d0a1cc09f885fce291c51403c2
    Signed-off-by: Jan Sievers <jan.sievers@sap.com>

---
+++ tycho-p2/tycho-p2-director-plugin/src/main/java/org/eclipse/tycho/plugins/tar/FilePermissionHelper.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SAP AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.plugins.tar;
+
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.Set;
+
+public class FilePermissionHelper {
+
+    private static final int OWNER_READ_FILEMODE = 0400;
+    private static final int OWNER_WRITE_FILEMODE = 0200;
+    private static final int OWNER_EXEC_FILEMODE = 0100;
+    private static final int GROUP_READ_FILEMODE = 0040;
+    private static final int GROUP_WRITE_FILEMODE = 0020;
+    private static final int GROUP_EXEC_FILEMODE = 0010;
+    private static final int OTHERS_READ_FILEMODE = 0004;
+    private static final int OTHERS_WRITE_FILEMODE = 0002;
+    private static final int OTHERS_EXEC_FILEMODE = 0001;
+
+    private FilePermissionHelper() {
+    }
+
+    /**
+     * Converts a set of {@link PosixFilePermission} to chmod-style octal file mode.
+     */
+    public static int toOctalFileMode(Set<PosixFilePermission> permissions) {
+        int result = 0;
+        for (PosixFilePermission permissionBit : permissions) {
+            switch (permissionBit) {
+            case OWNER_READ:
+                result |= OWNER_READ_FILEMODE;
+                break;
+            case OWNER_WRITE:
+                result |= OWNER_WRITE_FILEMODE;
+                break;
+            case OWNER_EXECUTE:
+                result |= OWNER_EXEC_FILEMODE;
+                break;
+            case GROUP_READ:
+                result |= GROUP_READ_FILEMODE;
+                break;
+            case GROUP_WRITE:
+                result |= GROUP_WRITE_FILEMODE;
+                break;
+            case GROUP_EXECUTE:
+                result |= GROUP_EXEC_FILEMODE;
+                break;
+            case OTHERS_READ:
+                result |= OTHERS_READ_FILEMODE;
+                break;
+            case OTHERS_WRITE:
+                result |= OTHERS_WRITE_FILEMODE;
+                break;
+            case OTHERS_EXECUTE:
+                result |= OTHERS_EXEC_FILEMODE;
+                break;
+            }
+        }
+        return result;
+    }
+
+}
