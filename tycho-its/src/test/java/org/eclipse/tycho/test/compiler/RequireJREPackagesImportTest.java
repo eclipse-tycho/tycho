3ca6f9cb8b7d346285da3bbf3f436652c2c3aca5
Jan Sievers <jan.sievers@sap.com>
2014-05-16 09:25:20 +0200

    Revert "430986 Use Luna M7 version of JDT compiler"
    This reverts commit 928ca6b1ba9ead7e20d155f51ab0ccb3aa461f7f.
    
    Change-Id: If11a1af8bfd4a40a0af16b8621ca60411e98d186

--- tycho-its/src/test/java/org/eclipse/tycho/test/compiler/RequireJREPackagesImportTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/compiler/RequireJREPackagesImportTest.java
@@ -28,7 +28,7 @@ public class RequireJREPackagesImportTest extends AbstractTychoIntegrationTest {
             fail();
         } catch (VerificationException e) {
             // expected
-            verifier.verifyTextInLog("[ERROR] Access restriction: The type 'InitialContext' is not API");
+            verifier.verifyTextInLog("[ERROR] Access restriction: The type Context is not accessible due to restriction on classpath entry");
         }
     }
 }
