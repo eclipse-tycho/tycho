5a9b3aa7c197d052edb5be1bca8e213eb172c80e
Pascal Rapicault <pascal@rapicorp.com>
2014-08-05 18:26:14 +0200

    428310 IU packaging type
    Add p2-installable-unit as a new packaging type and allow it to be used
    like any other entity that Tycho usually deals with.
    
    Bug: 428310
    Change-Id: If388f77f5d3a8d7dc2899824d1325df1d85a3181
    Signed-off-by: Pascal Rapicault <pascal@rapicorp.com>

---
+++ tycho-metadata-model/src/main/java/org/eclipse/tycho/model/IU.java
@@ -0,0 +1,235 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Rapicorp, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Rapicorp, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.model;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.List;
+
+import org.codehaus.plexus.util.IOUtil;
+
+import de.pdark.decentxml.Document;
+import de.pdark.decentxml.Element;
+import de.pdark.decentxml.XMLIOSource;
+import de.pdark.decentxml.XMLParser;
+import de.pdark.decentxml.XMLWriter;
+
+public class IU {
+    public static final String SOURCE_FILE_NAME = "p2iu.xml";
+
+    private static final String UNIT = "unit";
+    public static final String ID = "id";
+    public static final String VERSION = "version";
+
+    public static final String NAMESPACE = "namespace";
+    public static final String NAME = "name";
+
+    private static final String PROPERTIES = "properties";
+    private static final String PROPERTY = "property";
+
+    private static final String REQUIRES = "requires";
+    private static final String REQUIRED = "required";
+    public static final String RANGE = "range";
+
+    private static final String ARTIFACTS = "artifacts";
+    private static final String CLASSIFIER = "classifier";
+    private static final String ARTIFACT = "artifact";
+
+    private static final String PROVIDES = "provides";
+    private static final String PROVIDED = "provided";
+
+    public static final String P2_IU_NAMESPACE = "org.eclipse.equinox.p2.iu";
+
+    private static XMLParser parser = new XMLParser();
+
+    private final Document document;
+
+    private final Element iuDom;
+
+    public IU(Document document, Element element) {
+        this.document = document;
+        this.iuDom = element;
+    }
+
+    public String getId() {
+        return iuDom.getAttributeValue(ID);
+    }
+
+    public String getVersion() {
+        return iuDom.getAttributeValue(VERSION);
+    }
+
+    public void setVersion(String version) {
+        iuDom.setAttribute(VERSION, version);
+    }
+
+    public List<Element> getProvidedCapabilites() {
+        List<Element> provides = iuDom.getChildren(PROVIDES);
+        if (provides == null || provides.size() == 0)
+            return null;
+        return provides.get(0).getChildren(PROVIDED);
+    }
+
+    public Element getSelfCapability() {
+        List<Element> providedCapabilities = getProvidedCapabilites();
+        if (providedCapabilities == null)
+            return null;
+        for (Element capability : providedCapabilities) {
+
+            if (getId().equals(capability.getAttributeValue(NAME))
+                    && P2_IU_NAMESPACE.equals(capability.getAttributeValue(NAMESPACE)))
+                return capability;
+        }
+        return null;
+    }
+
+    public void addSelfCapability() {
+        Element provides = iuDom.getChild(PROVIDES);
+        if (provides == null) {
+            provides = new Element(PROVIDES);
+            iuDom.addNode(provides);
+        }
+        Element newCapability = new Element(PROVIDED);
+        newCapability.addAttribute(NAMESPACE, P2_IU_NAMESPACE);
+        newCapability.addAttribute(NAME, getId());
+        newCapability.addAttribute(VERSION, getVersion());
+
+        provides.addNode(newCapability);
+    }
+
+    public List<Element> getRequiredCapabilites() {
+        List<Element> requires = iuDom.getChildren(REQUIRES);
+        if (requires == null || requires.size() == 0)
+            return null;
+        return requires.get(0).getChildren(REQUIRED);
+    }
+
+    public List<Element> getProperties() {
+        List<Element> properties = iuDom.getChildren(PROPERTIES);
+        if (properties == null || properties.size() == 0)
+            return null;
+        return properties.get(0).getChildren(PROPERTY);
+    }
+
+    public void addProperty(String name, String value) {
+        Element properties = iuDom.getChild(PROPERTIES);
+        if (properties == null) {
+            iuDom.addNode(new Element(PROPERTIES));
+            properties = iuDom.getChild(PROPERTIES);
+        }
+        Element elt = new Element(PROPERTY);
+        elt.setAttribute(NAME, name);
+        elt.setAttribute("value", value);
+        properties.addNode(elt);
+    }
+
+    public List<Element> getArtifacts() {
+        Element artifacts = iuDom.getChild(ARTIFACTS);
+        if (artifacts == null)
+            return null;
+        return artifacts.getChildren(ARTIFACT);
+    }
+
+    public void addArtifact(String classifier, String id, String version) {
+        Element artifacts = iuDom.getChild(ARTIFACTS);
+        if (artifacts == null) {
+            artifacts = new Element(ARTIFACTS);
+            iuDom.addNode(artifacts);
+        }
+        Element newArtifact = new Element(ARTIFACT);
+        newArtifact.addAttribute(CLASSIFIER, classifier);
+        newArtifact.addAttribute(ID, id);
+        newArtifact.addAttribute(VERSION, version);
+
+        artifacts.addNode(newArtifact);
+    }
+
+    public Element getSelfArtifact() {
+        List<Element> artifacts = getArtifacts();
+        if (artifacts == null)
+            return null;
+        for (Element artifact : artifacts) {
+            if (getId().equals(artifact.getAttributeValue(ID))
+                    && "binary".equals(artifact.getAttributeValue(CLASSIFIER)))
+                return artifact;
+        }
+        return null;
+    }
+
+    public static IU read(File file) throws IOException {
+        FileInputStream is = new FileInputStream(file);
+        try {
+
+            Document iuDocument = parser.parse(new XMLIOSource(is));
+            Element root = iuDocument.getChild(UNIT);
+            if (root == null)
+                throw new RuntimeException("No iu found.");
+
+            IU result = new IU(iuDocument, root);
+            if (result.getId() == null)
+                throw new RuntimeException(String.format("The IU defined in %s is missing an id.",
+                        file.getAbsolutePath()));
+            if (result.getVersion() == null)
+                throw new RuntimeException(String.format("The IU defined in %s is missing a version.",
+                        file.getAbsolutePath()));
+            return result;
+        } finally {
+            IOUtil.close(is);
+        }
+    }
+
+    /**
+     * Convenience method to load p2iu.xml file
+     * 
+     * @throws RuntimeException
+     *             if iu descriptor can not be read or parsed.
+     */
+    public static IU loadIU(File location) {
+        try {
+            if (location.isDirectory()) {
+                return IU.read(new File(location, IU.SOURCE_FILE_NAME));
+            } else {
+                throw new RuntimeException("Could not read iu descriptor at " + location.getAbsolutePath());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read iu descriptor at " + location.getAbsolutePath(), e);
+        }
+    }
+
+    public static void write(IU iu, File file) throws IOException {
+        write(iu, file, "\t");
+    }
+
+    public static void write(IU iu, File file, String indent) throws IOException {
+        OutputStream os = new BufferedOutputStream(new FileOutputStream(file));
+
+        Document document = iu.document;
+        try {
+            String enc = document.getEncoding() != null ? document.getEncoding() : "UTF-8";
+            Writer w = new OutputStreamWriter(os, enc);
+            XMLWriter xw = new XMLWriter(w);
+            xw.setIndent(indent);
+            try {
+                document.toXML(xw);
+            } finally {
+                xw.flush();
+            }
+        } finally {
+            IOUtil.close(os);
+        }
+    }
+}
---
+++ tycho-packaging-plugin/src/main/java/org/eclipse/tycho/packaging/PackageIUMojo.java
@@ -0,0 +1,144 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Rapicorp, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Rapicorp, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.packaging;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.codehaus.plexus.archiver.Archiver;
+import org.codehaus.plexus.archiver.ArchiverException;
+import org.codehaus.plexus.archiver.util.DefaultFileSet;
+import org.codehaus.plexus.archiver.zip.ZipArchiver;
+import org.eclipse.tycho.artifacts.TargetPlatform;
+import org.eclipse.tycho.core.osgitools.DefaultReactorProject;
+import org.eclipse.tycho.core.utils.TychoProjectUtils;
+import org.eclipse.tycho.model.IU;
+
+/**
+ * Creates the zip for the IU and attaches it as an artifact
+ */
+@Mojo(name = "package-iu", defaultPhase = LifecyclePhase.PACKAGE, requiresDependencyResolution = ResolutionScope.RUNTIME)
+public class PackageIUMojo extends AbstractTychoPackagingMojo {
+
+    @Parameter(property = "project.build.directory", required = true, readonly = true)
+    protected File outputDirectory;
+
+    /**
+     * Folder containing the files to include in the final zip.
+     */
+    @Parameter(property = "project.build.outputDirectory")
+    private String artifactContentFolder;
+
+    @Parameter(property = "project.basedir", required = true, readonly = true)
+    private File basedir;
+
+    @Component
+    private IUXmlTransformer iuTransformer;
+
+    @Component(role = Archiver.class, hint = "zip")
+    private ZipArchiver zipArchiver;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        outputDirectory.mkdirs();
+
+        IU iu = IU.loadIU(basedir);
+        File iuXML = new File(outputDirectory, IU.SOURCE_FILE_NAME);
+        try {
+            addSelfCapability(iu);
+            addArtifactReference(iu);
+            addMavenProperties(iu);
+            expandVersions(iu);
+            IU.write(iu, iuXML);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error updating " + IU.SOURCE_FILE_NAME, e);
+        }
+
+        //Create the artifact
+        File artifactForIU = createArtifact();
+        project.getArtifact().setFile(artifactForIU);
+
+    }
+
+    private void addMavenProperties(IU iu) {
+        iuTransformer.injectMavenProperties(iu, project);
+    }
+
+    private void addSelfCapability(IU iu) {
+        iuTransformer.addSelfCapability(iu);
+    }
+
+    private void addArtifactReference(IU iu) {
+        if (!hasPayload())
+            return;
+        if (hasArtifactReference(iu))
+            return;
+        iu.addArtifact("binary", iu.getId(), iu.getVersion());
+    }
+
+    private void expandVersions(IU iu) throws MojoFailureException {
+        iuTransformer.replaceSelfQualifiers(iu, DefaultReactorProject.adapt(project).getExpandedVersion());
+        iuTransformer.replaceQualifierInCapabilities(iu, DefaultReactorProject.adapt(project).getBuildQualifier());
+
+        TargetPlatform targetPlatform = TychoProjectUtils.getTargetPlatformIfAvailable(project);
+        if (targetPlatform == null) {
+            getLog().warn(
+                    "Skipping version reference expansion in p2iu project using the deprecated -Dtycho.targetPlatform configuration");
+            return;
+        }
+        iuTransformer.replaceZerosInRequirements(iu, targetPlatform);
+    }
+
+    private boolean hasPayload() {
+        return getPayloadDir().isDirectory();
+    }
+
+    private File createArtifact() throws MojoExecutionException {
+        try {
+            File payload = getPayloadDir();
+            File newArtifact = new File(outputDirectory, project.getArtifactId() + "-" + project.getVersion() + ".zip");
+            if (newArtifact.exists()) {
+                newArtifact.delete();
+            }
+
+            if (hasPayload()) {
+                DefaultFileSet fs = new DefaultFileSet();
+                fs.setDirectory(payload);
+                zipArchiver.addFileSet(fs);
+                zipArchiver.setDestFile(newArtifact);
+                zipArchiver.setCompress(true);
+                zipArchiver.createArchive();
+            } else {
+                //Force create the file
+                newArtifact.createNewFile();
+            }
+            return newArtifact;
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error assembling ZIP", e);
+        } catch (ArchiverException e) {
+            throw new MojoExecutionException("Error assembling ZIP", e);
+        }
+    }
+
+    private File getPayloadDir() {
+        return new File(artifactContentFolder);
+    }
+
+    private boolean hasArtifactReference(IU iu) {
+        return iu.getSelfArtifact() != null;
+    }
+}
