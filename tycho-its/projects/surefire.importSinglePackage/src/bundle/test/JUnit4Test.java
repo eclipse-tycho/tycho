1c5697e76118eba517b0d561f19f6afd8d612ae7
Jan Sievers <jan.sievers@sap.com>
2014-08-08 14:42:56 +0200

    369266 add integration test
    issue
    http://jira.codehaus.org/browse/SUREFIRE-825
    is fixed by updating tycho to use surefire-2.17
    (bug 386481)
    
    Bug: 369266
    Change-Id: I63b753728bb6c9aca46bb56845ab772924be218c
    Signed-off-by: Jan Sievers <jan.sievers@sap.com>

---
+++ tycho-its/projects/surefire.importSinglePackage/src/bundle/test/JUnit4Test.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SAP SE and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SAP SE - initial API and implementation
+ *******************************************************************************/
+package bundle.test;
+
+import org.junit.Test;
+
+public class JUnit4Test {
+
+    @Test
+    public void testUsingSingleImportPackage() {
+
+    }
+}
