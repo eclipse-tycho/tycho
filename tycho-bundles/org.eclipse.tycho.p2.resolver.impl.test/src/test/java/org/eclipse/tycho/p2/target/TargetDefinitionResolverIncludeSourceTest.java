e64e8782a1edd47134cc2222320c446bb15c3b77
Mickael Istria <mistria@redhat.com>
2014-08-25 19:51:04 +0200

    411664 Ability to override the "includeSources" behaviour
    This allows to decide in the target-platform-configuration whether
    the "includeSources" flag will be honored or override (forced or
    ignored)
    
    Change-Id: I78ffcbd7b46d7dd70383ad2af335b93c709a7389
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- tycho-bundles/org.eclipse.tycho.p2.resolver.impl.test/src/test/java/org/eclipse/tycho/p2/target/TargetDefinitionResolverIncludeSourceTest.java
+++ tycho-bundles/org.eclipse.tycho.p2.resolver.impl.test/src/test/java/org/eclipse/tycho/p2/target/TargetDefinitionResolverIncludeSourceTest.java
@@ -19,9 +19,11 @@ import static org.junit.Assert.assertThat;
 
 import org.eclipse.equinox.p2.metadata.IVersionedId;
 import org.eclipse.equinox.p2.metadata.VersionedId;
+import org.eclipse.tycho.core.resolver.shared.IncludeSourcesMode;
 import org.eclipse.tycho.p2.target.TargetDefinitionResolverTest.LocationStub;
 import org.eclipse.tycho.p2.target.TargetDefinitionResolverTest.TestRepositories;
 import org.eclipse.tycho.p2.target.facade.TargetDefinition;
+import org.eclipse.tycho.p2.target.facade.TargetDefinition.IncludeMode;
 import org.eclipse.tycho.p2.target.facade.TargetDefinition.InstallableUnitLocation;
 import org.eclipse.tycho.p2.target.facade.TargetDefinitionResolutionException;
 import org.eclipse.tycho.test.util.LogVerifier;
@@ -53,14 +55,15 @@ public class TargetDefinitionResolverIncludeSourceTest {
     @Test(expected = TargetDefinitionResolutionException.class)
     public void testConflictingIncludeSourceLocations() throws Exception {
         TargetDefinition definition = definitionWith(new WithSourceLocationStub(TestRepositories.SOURCES,
-                BUNDLE_WITH_SOURCES), new WithoutSourceLocationStub(TestRepositories.SOURCES));
+                IncludeMode.SLICER, BUNDLE_WITH_SOURCES), new WithoutSourceLocationStub(TestRepositories.SOURCES,
+                IncludeMode.SLICER));
         subject.resolveContentWithExceptions(definition);
     }
 
     @Test
     public void testResultContainsSourceBundle() throws Exception {
         TargetDefinition definition = definitionWith(new WithSourceLocationStub(TestRepositories.SOURCES,
-                BUNDLE_WITH_SOURCES));
+                IncludeMode.SLICER, BUNDLE_WITH_SOURCES));
         TargetDefinitionContent content = subject.resolveContentWithExceptions(definition);
         assertThat(versionedIdsOf(content), hasItem(BUNDLE_WITH_SOURCES));
         assertThat(versionedIdsOf(content), hasItem(SOURCE_BUNDLE));
@@ -70,31 +73,103 @@ public class TargetDefinitionResolverIncludeSourceTest {
     @Test
     public void testCanResolveBundlesWithoutSources() throws Exception {
         TargetDefinition definition = definitionWith(new WithSourceLocationStub(TestRepositories.SOURCES,
-                NOSOURCE_BUNDLE));
+                IncludeMode.SLICER, NOSOURCE_BUNDLE));
         TargetDefinitionContent content = subject.resolveContentWithExceptions(definition);
         assertThat(versionedIdsOf(content), hasItem(NOSOURCE_BUNDLE));
         assertThat(content.getUnits().size(), is(1));
     }
 
+    @Test
+    public void testIgnoreIncludeSource() throws Exception {
+        TargetDefinition definition = definitionWith(new WithSourceLocationStub(TestRepositories.SOURCES,
+                IncludeMode.SLICER, BUNDLE_WITH_SOURCES));
+        TargetDefinitionContent content = subject.resolveContentWithExceptions(definition, IncludeSourcesMode.ignore);
+        assertThat(versionedIdsOf(content), hasItem(BUNDLE_WITH_SOURCES));
+        assertThat(content.getUnits().size(), is(1));
+    }
+
+    @Test(expected = TargetDefinitionResolutionException.class)
+    public void testConflictingIncludeSourceLocationsPlanner() throws Exception {
+        TargetDefinition definition = definitionWith(new WithSourceLocationStub(TestRepositories.SOURCES,
+                IncludeMode.PLANNER, BUNDLE_WITH_SOURCES), new WithoutSourceLocationStub(TestRepositories.SOURCES,
+                IncludeMode.PLANNER));
+        subject.resolveContentWithExceptions(definition);
+    }
+
+    @Test
+    public void testResultContainsSourceBundlePlanner() throws Exception {
+        TargetDefinition definition = definitionWith(new WithSourceLocationStub(TestRepositories.SOURCES,
+                IncludeMode.PLANNER, BUNDLE_WITH_SOURCES));
+        TargetDefinitionContent content = subject.resolveContentWithExceptions(definition);
+        assertThat(versionedIdsOf(content), hasItem(BUNDLE_WITH_SOURCES));
+        assertThat(versionedIdsOf(content), hasItem(SOURCE_BUNDLE));
+        assertThat(content.getUnits().size(), is(2));
+    }
+
+    @Test
+    public void testCanResolveBundlesWithoutSourcesPlanner() throws Exception {
+        TargetDefinition definition = definitionWith(new WithSourceLocationStub(TestRepositories.SOURCES,
+                IncludeMode.PLANNER, NOSOURCE_BUNDLE));
+        TargetDefinitionContent content = subject.resolveContentWithExceptions(definition);
+        assertThat(versionedIdsOf(content), hasItem(NOSOURCE_BUNDLE));
+        assertThat(content.getUnits().size(), is(1));
+    }
+
+    @Test
+    public void testIgnoreIncludeSourcePlanner() throws Exception {
+        TargetDefinition definition = definitionWith(new WithSourceLocationStub(TestRepositories.SOURCES,
+                IncludeMode.PLANNER, BUNDLE_WITH_SOURCES));
+        TargetDefinitionContent content = subject.resolveContentWithExceptions(definition, IncludeSourcesMode.ignore);
+        assertThat(versionedIdsOf(content), hasItem(BUNDLE_WITH_SOURCES));
+        assertThat(content.getUnits().size(), is(1));
+    }
+
+    @Test
+    public void testForceIncludeSourcePlanner() throws Exception {
+        TargetDefinition definition = definitionWith(new WithoutSourceLocationStub(TestRepositories.SOURCES,
+                IncludeMode.PLANNER, BUNDLE_WITH_SOURCES));
+        TargetDefinitionContent content = subject.resolveContentWithExceptions(definition, IncludeSourcesMode.force);
+        assertThat(versionedIdsOf(content), hasItem(BUNDLE_WITH_SOURCES));
+        assertThat(versionedIdsOf(content), hasItem(SOURCE_BUNDLE));
+        assertThat(content.getUnits().size(), is(2));
+    }
+
     static class WithSourceLocationStub extends LocationStub implements InstallableUnitLocation {
-        public WithSourceLocationStub(TestRepositories repositories, IVersionedId... seedUnits) {
+        private IncludeMode includeMode;
+
+        public WithSourceLocationStub(TestRepositories repositories, IncludeMode includeMode, IVersionedId... seedUnits) {
             super(repositories, seedUnits);
+            this.includeMode = includeMode;
         }
 
         @Override
         public boolean includeSource() {
             return true;
         }
+
+        @Override
+        public IncludeMode getIncludeMode() {
+            return this.includeMode;
+        }
     }
 
     static class WithoutSourceLocationStub extends LocationStub implements InstallableUnitLocation {
-        public WithoutSourceLocationStub(TestRepositories repositories, IVersionedId... seedUnits) {
+        private IncludeMode includeMode;
+
+        public WithoutSourceLocationStub(TestRepositories repositories, IncludeMode includeMode,
+                IVersionedId... seedUnits) {
             super(repositories, seedUnits);
+            this.includeMode = includeMode;
         }
 
         @Override
         public boolean includeSource() {
             return false;
+        }
+
+        @Override
+        public IncludeMode getIncludeMode() {
+            return this.includeMode;
         }
     }
 }
--- tycho-core/src/main/java/org/eclipse/tycho/core/TargetPlatformConfiguration.java
+++ tycho-core/src/main/java/org/eclipse/tycho/core/TargetPlatformConfiguration.java
@@ -19,6 +19,7 @@ import java.util.Map;
 
 import org.apache.maven.model.Dependency;
 import org.eclipse.tycho.artifacts.TargetPlatformFilter;
+import org.eclipse.tycho.core.resolver.shared.IncludeSourcesMode;
 import org.eclipse.tycho.core.resolver.shared.OptionalResolutionAction;
 import org.eclipse.tycho.core.shared.TargetEnvironment;
 
@@ -55,6 +56,8 @@ public class TargetPlatformConfiguration implements DependencyResolverConfigurat
     private boolean includePackedArtifacts;
 
     private Map<String, String> resolverProfileProperties = new HashMap<String, String>();
+
+    private IncludeSourcesMode includesSourcesMode = IncludeSourcesMode.honor;
 
     /**
      * Returns the list of configured target environments, or the running environment if no
@@ -182,5 +185,13 @@ public class TargetPlatformConfiguration implements DependencyResolverConfigurat
 
     public void addProfileProperty(String key, String value) {
         resolverProfileProperties.put(key, value);
+    }
+
+    public void setIncludeSourcesMode(IncludeSourcesMode includeSourcesMode) {
+        this.includesSourcesMode = includeSourcesMode;
+    }
+
+    public IncludeSourcesMode getIncludeSourcesMode() {
+        return this.includesSourcesMode;
     }
 }
