fe08d1a0840c760fec443aca236c2320496b79ed
Martin Schreiber <m.schreiber@bachmann.info>
2016-05-17 08:02:38 +0200

    WIP - A first version of the Tycho surefire TestNG plugin
    _Adding a new plugin for the Surefire TestNG Test Framework Provider.
    _Extending the TestFrameworkProvider interface with a method that each
    provider can provider its own settings which will end up in the surefire
    properties file.
    _Adding a list property to the TestMojo called "suiteXmlFiles" (like it
    is in surefire) where the user can specifiy suite xml files.
    
    TODO: ITs - will be done in a separate commit
    
    CQ: 11219: surefire-testng
    CQ: 11220: surefire-testng-util
    
    Bug: 491432
    Change-Id: I8b4b6f8263bb9716cfbe6d0d92f5db3f90c91737
    Signed-off-by: Martin Schreiber <m.schreiber@bachmann.info>

---
+++ tycho-surefire/tycho-surefire-plugin/src/main/java/org/eclipse/tycho/surefire/provider/impl/TestNGProvider.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Bachmann electronic GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bachmann electronic GmbH - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.tycho.surefire.provider.impl;
+
+import static java.util.Collections.singletonList;
+import static org.eclipse.tycho.surefire.provider.impl.ProviderHelper.newDependency;
+
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.maven.model.Dependency;
+import org.codehaus.plexus.component.annotations.Component;
+import org.eclipse.tycho.classpath.ClasspathEntry;
+import org.eclipse.tycho.surefire.provider.spi.TestFrameworkProvider;
+import org.osgi.framework.Version;
+
+@Component(role = TestFrameworkProvider.class, hint = "testng")
+public class TestNGProvider implements TestFrameworkProvider {
+
+    private static final Version VERSION = Version.parseVersion("6.9.10");
+
+    @Override
+    public String getType() {
+        return "testng";
+    }
+
+    @Override
+    public Version getVersion() {
+        return VERSION;
+    }
+
+    @Override
+    public String getSurefireProviderClassName() {
+        return "org.apache.maven.surefire.testng.TestNGProvider";
+    }
+
+    @Override
+    public boolean isEnabled(List<ClasspathEntry> testBundleClassPath, Properties surefireProperties) {
+        //TODO: Find a way to automatically enable this provider 
+        // For now the user must provide the tycho surefire parameter 
+        // <providerHint>testng</providerHint> 
+        // in order to enable this testprovider
+        return false;
+    }
+
+    @Override
+    public List<Dependency> getRequiredBundles() {
+        return singletonList(newDependency("org.eclipse.tycho", "org.eclipse.tycho.surefire.testng"));
+    }
+
+    @Override
+    public Properties getProviderSpecifcProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("testng.configurator", "org.apache.maven.surefire.testng.conf.TestNG652Configurator");
+        return properties;
+    }
+
+}
