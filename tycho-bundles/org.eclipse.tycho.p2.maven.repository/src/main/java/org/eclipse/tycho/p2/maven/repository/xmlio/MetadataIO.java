37c01ca85138c4fefb8ed3b5a2e45450b8500a1a
Guillaume Dufour <guillaume.duff@gmail.com>
2017-04-30 18:44:50 +0200

    WIP 453708: Support for site/repository-reference/@location in eclipse-repository
    Change-Id: I5d37fb0ac363b2b8d8f7e5faa466d8e79057cbee
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=453708
    Signed-off-by: Guillaume Dufour <guillaume.duff@gmail.com>

--- tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/p2/maven/repository/xmlio/MetadataIO.java
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/p2/maven/repository/xmlio/MetadataIO.java
@@ -18,7 +18,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -28,15 +31,19 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.URIUtil;
+import org.eclipse.equinox.internal.p2.core.helpers.OrderedProperties;
 import org.eclipse.equinox.internal.p2.metadata.repository.Messages;
 import org.eclipse.equinox.internal.p2.metadata.repository.io.MetadataParser;
 import org.eclipse.equinox.internal.p2.metadata.repository.io.MetadataWriter;
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.metadata.MetadataFactory;
 import org.eclipse.equinox.p2.metadata.MetadataFactory.InstallableUnitDescription;
+import org.eclipse.equinox.p2.repository.IRepositoryReference;
 import org.eclipse.tycho.p2.maven.repository.Activator;
 import org.eclipse.tycho.repository.util.internal.BundleConstants;
 import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -60,6 +67,35 @@ public class MetadataIO {
             flush();
         }
 
+        public void writeRepositoryReferences(Set<IRepositoryReference> references) {
+            start(REPOSITORY_REFERENCES_ELEMENT);
+
+            attribute(COLLECTION_SIZE_ATTRIBUTE, references.size());
+            for (IRepositoryReference reference : references) {
+                writeInstallableUnit(reference);
+            }
+
+            end(REPOSITORY_REFERENCES_ELEMENT);
+            flush();
+        }
+
+        private void writeInstallableUnit(IRepositoryReference reference) {
+            start(REPOSITORY_REFERENCE_ELEMENT);
+            attribute(URI_ATTRIBUTE, reference.getLocation().toString());
+
+            try {
+                // we write the URL attribute for backwards compatibility with 3.4.x
+                // this attribute should be removed if we make a breaking format change.
+                attribute(URL_ATTRIBUTE, URIUtil.toURL(reference.getLocation()).toExternalForm());
+            } catch (MalformedURLException e) {
+                attribute(URL_ATTRIBUTE, reference.getLocation().toString());
+            }
+
+            attribute(TYPE_ATTRIBUTE, Integer.toString(reference.getType()));
+            attribute(OPTIONS_ATTRIBUTE, Integer.toString(reference.getOptions()));
+            end(REPOSITORY_REFERENCE_ELEMENT);
+        }
+
     }
 
     private static class Parser extends MetadataParser {
@@ -70,6 +106,8 @@ public class MetadataIO {
         private PARSER_MODE mode;
 
         private List<InstallableUnitDescription> units;
+        private Set<IRepositoryReference> references;
+        private OrderedProperties properties;
 
         public Parser(PARSER_MODE mode) {
             super(Activator.getContext(), BundleConstants.BUNDLE_ID);
@@ -96,15 +134,14 @@ public class MetadataIO {
                 // TODO: currently not caching the parser since we make no assumptions
                 // or restrictions on concurrent parsing
                 getParser();
-                InstallableUnitsHandler handler = new InstallableUnitsHandler();
-                if (mode.equals(PARSER_MODE.REPO))
-                    xmlReader.setContentHandler(new RepositoryDocHandler(INSTALLABLE_UNITS_ELEMENT, handler));
-                else
-                    xmlReader.setContentHandler(handler);
+                RepositoryRootHandler handler = new RepositoryRootHandler(mode);
+                xmlReader.setContentHandler(handler);
 
                 xmlReader.parse(new InputSource(stream));
                 if (isValidXML()) {
                     units = handler.getUnits();
+                    references = handler.getRepoRefs();
+                    properties = handler.getProperties();
                 }
             } catch (SAXException e) {
                 if (!(e.getException() instanceof OperationCanceledException))
@@ -117,42 +154,122 @@ public class MetadataIO {
             }
         }
 
-        private final class RepositoryDocHandler extends DocHandler {
+        private final class RepositoryRootHandler extends RootHandler {
+            private InstallableUnitsHandler unitsHandler;
+            private PropertiesHandler propertiesHandler;
+            private RepositoryReferencesHandler repositoryReferencesHandler;
 
-            public RepositoryDocHandler(String rootName, RootHandler rootHandler) {
-                super(rootName, rootHandler);
+            public RepositoryRootHandler(PARSER_MODE mode) {
+                super();
             }
 
-            @Override
-            public void processingInstruction(String target, String data) throws SAXException {
-                // if (PI_REPOSITORY_TARGET.equals(target)) {
-                // Version repositoryVersion = extractPIVersion(target, data);
-                // if (!MetadataRepositoryIO.XMLConstants.XML_TOLERANCE.isIncluded(repositoryVersion)) {
-                // throw new SAXException(NLS.bind(Messages.io_IncompatibleVersion, repositoryVersion,
-                // MetadataRepositoryIO.XMLConstants.XML_TOLERANCE));
-                // }
-                // }
+            public Set<IRepositoryReference> getRepoRefs() {
+                if (repositoryReferencesHandler == null) {
+                    return new HashSet<>();
+                }
+                return repositoryReferencesHandler.getRepoRefs();
             }
-        }
 
-        private final class InstallableUnitsHandler extends RootHandler {
+            public List<InstallableUnitDescription> getUnits() {
+                if (unitsHandler == null) {
+                    return new ArrayList<>();
+                }
+                return unitsHandler.getUnits();
+            }
 
-            private List<InstallableUnitDescription> units = new ArrayList<>();
+            public OrderedProperties getProperties() {
+                if (propertiesHandler == null) {
+                    return null;
+                }
+                return propertiesHandler.getProperties();
+            }
+
+            public void startElement(String name, Attributes attributes) throws SAXException {
+                checkCancel();
+                if (mode.equals(PARSER_MODE.REPO)) {
+                    if (PROPERTIES_ELEMENT.equals(name)) {
+                        if (propertiesHandler == null) {
+                            propertiesHandler = new PropertiesHandler(this, attributes);
+                        } else {
+                            duplicateElement(this, name, attributes);
+                        }
+                    } else if (INSTALLABLE_UNITS_ELEMENT.equals(name)) {
+                        if (unitsHandler == null) {
+                            unitsHandler = new InstallableUnitsHandler(this, attributes);
+                        } else {
+                            duplicateElement(this, name, attributes);
+                        }
+                    } else if (REPOSITORY_REFERENCES_ELEMENT.equals(name)) {
+                        if (repositoryReferencesHandler == null) {
+                            repositoryReferencesHandler = new RepositoryReferencesHandler(this, attributes);
+                        } else {
+                            duplicateElement(this, name, attributes);
+                        }
+                    } else {
+                        invalidElement(name, attributes);
+                    }
+                } else if (mode.equals(PARSER_MODE.IU)) {
+                    new InstallableUnitsHandler(this, attributes);
+                }
+            }
 
             @Override
             protected void handleRootAttributes(Attributes attributes) {
                 // TODO Auto-generated method stub
 
             }
+        }
+
+        private abstract class AbstractSizedMetadataHandler extends AbstractMetadataHandler {
+
+            public AbstractSizedMetadataHandler(ContentHandler parentHandler, String elementHandled) {
+                super(parentHandler, elementHandled);
+            }
+
+            protected int getOptionalSize(Attributes attributes, int dflt) {
+                String sizeStr = parseOptionalAttribute(attributes, COLLECTION_SIZE_ATTRIBUTE);
+                return sizeStr != null ? Integer.parseInt(sizeStr) : dflt;
+            }
+        }
+
+        protected class InstallableUnitsHandler extends AbstractSizedMetadataHandler {
+            private ArrayList<InstallableUnitDescription> units;
+
+            public InstallableUnitsHandler(AbstractHandler parentHandler, Attributes attributes) {
+                super(parentHandler, INSTALLABLE_UNITS_ELEMENT);
+                units = new ArrayList<InstallableUnitDescription>(getOptionalSize(attributes, 4));
+            }
 
             public List<InstallableUnitDescription> getUnits() {
                 return units;
             }
 
-            @Override
-            public void startElement(String name, Attributes attributes) throws SAXException {
+            public void startElement(String name, Attributes attributes) {
                 if (name.equals(INSTALLABLE_UNIT_ELEMENT)) {
                     new InstallableUnitHandler(this, attributes, units);
+                } else {
+                    invalidElement(name, attributes);
+                }
+            }
+        }
+
+        private final class RepositoryReferencesHandler extends AbstractSizedMetadataHandler {
+
+            private Set<IRepositoryReference> repoRefs = new LinkedHashSet<>();
+
+            public RepositoryReferencesHandler(AbstractHandler parentHandler, Attributes attributes) {
+                super(parentHandler, REPOSITORY_REFERENCES_ELEMENT);
+                references = new HashSet<IRepositoryReference>(getOptionalSize(attributes, 4));
+            }
+
+            public Set<IRepositoryReference> getRepoRefs() {
+                return repoRefs;
+            }
+
+            @Override
+            public void startElement(String name, Attributes attributes) throws SAXException {
+                if (name.equals(REPOSITORY_REFERENCE_ELEMENT)) {
+                    new RepositoryReferenceHandler(this, attributes, repoRefs);
                 } else {
                     invalidElement(name, attributes);
                 }
@@ -162,6 +279,15 @@ public class MetadataIO {
         public List<InstallableUnitDescription> getUnits() {
             return units;
         }
+
+        public Set<IRepositoryReference> getRepoRefs() {
+            return references;
+        }
+
+        public OrderedProperties getProperties() {
+            return properties;
+        }
+
     }
 
     public InstallableUnitDescription readOneIU(InputStream is) throws IOException {
@@ -171,30 +297,69 @@ public class MetadataIO {
         return parser.getUnits().get(0);
     }
 
-    public Set<IInstallableUnit> readXML(InputStream is) throws IOException {
+    public ReadXmlResult readXML(InputStream is) throws IOException {
         Parser parser = new Parser(Parser.PARSER_MODE.REPO);
 
         parser.parse(is, new NullProgressMonitor());
 
-        Set<IInstallableUnit> units = new LinkedHashSet<>();
+        ReadXmlResult result = new ReadXmlResult();
 
         for (InstallableUnitDescription desc : parser.getUnits()) {
-            units.add(MetadataFactory.createInstallableUnit(desc));
+            result.add(MetadataFactory.createInstallableUnit(desc));
         }
 
-        return units;
+        for (IRepositoryReference desc : parser.getRepoRefs()) {
+            result.add(desc);
+        }
+
+        result.setProperties(parser.getProperties());
+
+        return result;
     }
 
-    public void writeXML(Set<IInstallableUnit> units, OutputStream os) throws IOException {
+    public class ReadXmlResult {
+        private LinkedHashSet<IInstallableUnit> units = new LinkedHashSet<>();
+        private LinkedHashSet<IRepositoryReference> references = new LinkedHashSet<>();
+        private OrderedProperties properties = null;
+
+        public Set<IInstallableUnit> getUnits() {
+            return Collections.unmodifiableSet(units);
+        }
+
+        public void setProperties(OrderedProperties properties) {
+            this.properties = properties;
+        }
+
+        public Set<IRepositoryReference> getRepoRefs() {
+            return Collections.unmodifiableSet(references);
+        }
+
+        public void add(IInstallableUnit unit) {
+            units.add(unit);
+        }
+
+        public void add(IRepositoryReference ref) {
+            references.add(ref);
+        }
+    }
+
+    public void writeInstallableUnits(Set<IInstallableUnit> units, OutputStream os) throws IOException {
         new Writer(os).write(units);
     }
 
-    public void writeXML(Set<IInstallableUnit> units, File file) throws IOException {
+    private void writeRepositoryReferences(Set<IRepositoryReference> references, OutputStream os) throws IOException {
+        new Writer(os).writeRepositoryReferences(references);
+    }
+
+    public void writeXML(Set<IInstallableUnit> units, Set<IRepositoryReference> references, File file)
+            throws IOException {
         OutputStream os = new BufferedOutputStream(new FileOutputStream(file));
         try {
-            writeXML(units, os);
+            writeInstallableUnits(units, os);
+            writeRepositoryReferences(references, os);
         } finally {
             os.close();
         }
     }
+
 }
