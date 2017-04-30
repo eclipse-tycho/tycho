bf754510ffbb0e649e4df2f9b634785c83214dbd
Guillaume Dufour <guillaume.duff@gmail.com>
2017-04-30 18:44:50 +0200

    453708: Support for site/repository-reference/@location in eclipse-repository
    Change-Id: I5d37fb0ac363b2b8d8f7e5faa466d8e79057cbee
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=453708
    Signed-off-by: Guillaume Dufour <guillaume.duff@gmail.com>

--- tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/repository/local/LocalMetadataRepository.java
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/repository/local/LocalMetadataRepository.java
@@ -18,6 +18,7 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
+import org.eclipse.equinox.p2.repository.IRepositoryReference;
 import org.eclipse.tycho.p2.maven.repository.AbstractMavenMetadataRepository;
 import org.eclipse.tycho.p2.maven.repository.xmlio.MetadataIO;
 import org.eclipse.tycho.p2.repository.GAV;
@@ -80,16 +81,24 @@ public class LocalMetadataRepository extends AbstractMavenMetadataRepository {
 
         for (GAV gav : changedGAVs) {
             Set<IInstallableUnit> gavUnits = unitsMap.get(gav);
+            Set<IRepositoryReference> repoRefUnits = repoRefMap.get(gav);
 
-            if (gavUnits != null && !gavUnits.isEmpty()) {
+            if ((gavUnits != null && !gavUnits.isEmpty()) || (repoRefUnits != null && !repoRefUnits.isEmpty())) {
                 String relpath = RepositoryLayoutHelper.getRelativePath(gav,
                         RepositoryLayoutHelper.CLASSIFIER_P2_METADATA, RepositoryLayoutHelper.EXTENSION_P2_METADATA);
 
                 File file = new File(basedir, relpath);
                 file.getParentFile().mkdirs();
 
+                if (gavUnits == null) {
+                    gavUnits = new LinkedHashSet<>();
+                }
+                if (repoRefUnits == null) {
+                    repoRefUnits = new LinkedHashSet<>();
+                }
+
                 try {
-                    io.writeXML(gavUnits, file);
+                    io.writeXML(gavUnits, repoRefUnits, file);
 
                     metadataIndex.addGav(gav);
                 } catch (IOException e) {
