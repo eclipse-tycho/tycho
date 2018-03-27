faeab462d3cc321f6a011012483e6de4c66b4c8a
Jan Sievers <jan.sievers@sap.com>
2018-03-27 17:34:16 +0200

    Bug 522475: [WIP] add support for JUnit 5
    FIRST DRAFT works for a simple JUnit5 eclipse-test-plugin
    similar to
    https://github.com/junit-team/junit5-samples/tree/r5.0.2/junit5-maven-consumer
    
    TODOs/issues:
    
    had to wrap too many JUnit5 jars in org.eclipse.tycho.surefire.junit5
    as all of these are required by junit-platform-surefire-provider
    we can't simply require them because the surefire bundles are dynamically
    added to the OSGi test runtime and do not take part in dependency resolution...
    also, this way we are pinned to version 5.0.0/1.0.0 and users might decide to use
    newer versions of the junit jupiter API in their test bundle.
    Add integration and unit tests
    Clean up p2 repo handling in tycho-surefire (use global .target file instead of
    p2 repos in pom.xml)
    
    Change-Id: I5961c8fd0a283f6a5465d5e2ff6659936d800cd0

---
+++ tycho-surefire/tycho-surefire-plugin/src/main/java/org/eclipse/tycho/surefire/provider/impl/JUnit5Provider.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2018 SAP SE and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SAP SE - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.tycho.surefire.provider.impl;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static org.eclipse.tycho.surefire.provider.impl.ProviderHelper.newDependency;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.maven.model.Dependency;
+import org.codehaus.plexus.component.annotations.Component;
+import org.eclipse.tycho.surefire.provider.spi.TestFrameworkProvider;
+import org.osgi.framework.Version;
+import org.osgi.framework.VersionRange;
+
+@Component(role = TestFrameworkProvider.class, hint = "junit5")
+public class JUnit5Provider extends AbstractJUnitProvider {
+
+    private static final Version VERSION = Version.parseVersion("5.0.0");
+
+    @Override
+    protected Set<String> getJUnitBundleNames() {
+        return new HashSet<>(asList("org.junit.jupiter.api"));
+    }
+
+    @Override
+    public String getSurefireProviderClassName() {
+        return "org.junit.platform.surefire.provider.JUnitPlatformProvider";
+    }
+
+    @Override
+    public Version getVersion() {
+        return VERSION;
+    }
+
+    @Override
+    public List<Dependency> getRequiredBundles() {
+        return singletonList(newDependency("org.eclipse.tycho", "org.eclipse.tycho.surefire.junit5"));
+    }
+
+    @Override
+    protected VersionRange getJUnitVersionRange() {
+        return new VersionRange("[5,6)");
+    }
+
+}
