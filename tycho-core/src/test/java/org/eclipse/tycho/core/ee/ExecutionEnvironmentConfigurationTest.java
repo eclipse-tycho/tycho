fba14ff726f938c13df6b130521b5f85a16d4857
Mickael Istria <mistria@redhat.com>
2018-04-18 18:23:00 +0200

    Bug 385703 - Update default EE to JavaSE-9
    Artifacts that don't have a EE (feature, repository...) should be
    default use the latest known EE, which is usually capable of the more
    compatibility.
    When building a feature or a repo, it's acceptable to get artifacts
    resolved against a too recent EE, as we expect individual bundles were
    already validated against their desired EE.
    
    Change-Id: Iaf12278001ccc58f08adccbc9a897816c783c2f7
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- tycho-core/src/test/java/org/eclipse/tycho/core/ee/ExecutionEnvironmentConfigurationTest.java
+++ tycho-core/src/test/java/org/eclipse/tycho/core/ee/ExecutionEnvironmentConfigurationTest.java
@@ -45,9 +45,9 @@ public class ExecutionEnvironmentConfigurationTest {
 
     @Test
     public void testDefaults() {
-        assertThat(subject.getProfileName(), is("JavaSE-1.7"));
+        assertThat(subject.getProfileName(), is("JavaSE-9"));
         assertThat(subject.isCustomProfile(), is(false));
-        assertThat(subject.getFullSpecification().getProfileName(), is("JavaSE-1.7"));
+        assertThat(subject.getFullSpecification().getProfileName(), is("JavaSE-9"));
     }
 
     @Test
