e6fdc51127b4ec056d233c1346218700a05f7083
Jan Sievers <jan.sievers@sap.com>
2018-11-23 17:08:10 +0100

    Bug 541403: WIP
    first hack to hardcode EE of
    test bundles to JavaSE-11
    lots of TODOs, this just shows
    where in the code we could start
    implementing a solution
    
    Change-Id: Ifbe0cd9dc6bdf8ae16a2ad2af9aba955be457d9a

--- tycho-core/src/main/java/org/eclipse/tycho/core/osgitools/OsgiTestBundleProject.java
+++ tycho-core/src/main/java/org/eclipse/tycho/core/osgitools/OsgiTestBundleProject.java
@@ -10,10 +10,27 @@
  *******************************************************************************/
 package org.eclipse.tycho.core.osgitools;
 
+import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.component.annotations.Component;
 import org.eclipse.tycho.PackagingType;
 import org.eclipse.tycho.core.TychoProject;
+import org.eclipse.tycho.core.ee.shared.ExecutionEnvironmentConfiguration;
 
 @Component(role = TychoProject.class, hint = PackagingType.TYPE_ECLIPSE_TEST_PLUGIN)
 public class OsgiTestBundleProject extends OsgiBundleProject {
+
+    @Override
+    public void readExecutionEnvironmentConfiguration(MavenProject project, ExecutionEnvironmentConfiguration sink) {
+        super.readExecutionEnvironmentConfiguration(project, sink);
+        // for test plugins, use the execution environment of the currently running JDK
+        // TODO we should probably have a switch to go back to the old behaviour
+        // TODO currently running profile should not be used in case useJDK=BREE in TestMojo
+        sink.setProfileConfiguration(calculateCurrentProfileName(), "currently running JDK");
+    }
+
+    private String calculateCurrentProfileName() {
+        // HACK hardcoded. we need to find a way get the EE profile name of the currently running JDK
+        return "JavaSE-11";
+    }
+
 }
