5cc2dbf6d50d884de5ac0eb444f568a78f5ed38f
Guillaume Dufour <guillaume.duff@gmail.com>
2018-11-30 01:42:03 +0100

    Bug 485926 : use contextfinder as classloader
    Change-Id: I6f6fd517cadc1bb9564badb776d61ae69e85ca29
    Signed-off-by: Guillaume Dufour <guillaume.duff@gmail.com>

---
+++ tycho-its/projects/surefire.requireBundle/surefire.requireBundle.bundle/src/org/eclipse/tycho/surefire/requirebundle/bundle/HelloWorld.java
@@ -0,0 +1,20 @@
+package org.eclipse.tycho.surefire.requirebundle.bundle;
+
+import javax.xml.validation.SchemaFactory;
+
+/**
+ * @author schulz
+ */
+public class HelloWorld
+{
+
+    public String test()
+    {
+        final SchemaFactory sf = SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.0"); //$NON-NLS-1$
+        if (sf == null) {
+            // fail
+        }
+        return "Hello World";
+    }
+
+}
