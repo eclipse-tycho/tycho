de5cc03c69cc0c51c1d665a36ceeedf7f27f10d4
Guillaume Dufour <guillaume.duff@gmail.com>
2018-11-30 01:42:03 +0100

    Bug 485926 : use contextfinder as classloader
    Change-Id: I6f6fd517cadc1bb9564badb776d61ae69e85ca29
    Signed-off-by: Guillaume Dufour <guillaume.duff@gmail.com>

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/surefire/RequireBundleTest.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2012 Sonatype Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Sonatype Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.test.surefire;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class RequireBundleTest extends AbstractTychoIntegrationTest {
+
+    // requested in bug 485926
+    @Test
+    public void loadResourceFromRequireBundle() throws Exception {
+        Verifier verifier = getVerifier("/surefire.requireBundle");
+        verifier.executeGoal("integration-test");
+        verifier.verifyErrorFreeLog();
+    }
+
+}
