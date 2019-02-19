a1696f610ee12da6c9b70d0992fe4f50d9edd1f9
Mat Booth <mat.booth@redhat.com>
2019-02-19 11:11:10 +0000

    Bug 547597 - Porting tests to mockito 2.x
    Switch to the latest versions of mockito 2 and junit 4 that are
    available in Orbit.
    
    Change-Id: Ie0f6189cc66a8f13d952fa6e9d5c7bf1d39535b5
    Signed-off-by: Mat Booth <mat.booth@redhat.com>

--- tycho-bundles/org.eclipse.tycho.p2.maven.repository.tests/src/test/java/org/eclipse/tycho/repository/p2base/artifact/repository/RepositoryArtifactProviderTest.java
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository.tests/src/test/java/org/eclipse/tycho/repository/p2base/artifact/repository/RepositoryArtifactProviderTest.java
@@ -26,8 +26,8 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.argThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.hamcrest.MockitoHamcrest.argThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
