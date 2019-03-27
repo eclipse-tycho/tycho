8f4aef250da24e72c7282da846b56bcc65f5e5d3
Robert Seidel <robert.seidel@aeb.com>
2019-03-27 12:34:39 +0100

    Bug 544016: allow multiple --add-exports compiler arguments
    Change-Id: I5cadad80cd3d41093d5743eb147a39e300136688
    Signed-off-by: Robert Seidel <robert.seidel@aeb.com>

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/compiler/CompilerExtraExports.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.tycho.test.compiler;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class CompilerExtraExports extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void testExtraExports() throws Exception {
+        Verifier verifier = getVerifier("compiler.extraExports", false);
+        verifier.executeGoal("compile");
+    }
+
+}
