e6ad5e67a3966dc96fd1dc9b8e2faec61e20681f
Guillaume Dufour <guillaume.duff@gmail.com>
2019-03-28 19:34:40 +0100

    Bug 453708: Support for site/repository-reference/@location in eclipse-repository
    Change-Id: I10957947bc927378e636f061f66ff252dd254ea7
    Signed-off-by: Guillaume Dufour <guillaume.duff@gmail.com>

--- tycho-bundles/org.eclipse.tycho.p2.tools.impl/src/main/java/org/eclipse/tycho/p2/tools/mirroring/MirrorApplication.java
+++ tycho-bundles/org.eclipse.tycho.p2.tools.impl/src/main/java/org/eclipse/tycho/p2/tools/mirroring/MirrorApplication.java
@@ -10,7 +10,10 @@
  *******************************************************************************/
 package org.eclipse.tycho.p2.tools.mirroring;
 
+import java.net.URI;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.equinox.p2.core.IProvisioningAgent;
@@ -19,21 +22,28 @@ import org.eclipse.equinox.p2.internal.repository.mirroring.Mirroring;
 import org.eclipse.equinox.p2.internal.repository.tools.RepositoryDescriptor;
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.query.IQueryable;
+import org.eclipse.equinox.p2.repository.IRepository;
+import org.eclipse.equinox.p2.repository.IRepositoryReference;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactRepository;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactRepositoryManager;
+import org.eclipse.equinox.p2.repository.metadata.IMetadataRepository;
+import org.eclipse.equinox.p2.repository.metadata.IMetadataRepositoryManager;
+import org.eclipse.tycho.p2.tools.RepositoryReference;
 
 @SuppressWarnings("restriction")
 public class MirrorApplication extends org.eclipse.equinox.p2.internal.repository.tools.MirrorApplication {
 
     private final boolean includePackedArtifacts;
-    private Map<String, String> extraArtifactRepositoryProperties;
+    private final Map<String, String> extraArtifactRepositoryProperties;
+    private final List<RepositoryReference> repositoryReferences;
 
     public MirrorApplication(IProvisioningAgent agent, boolean includePackedArtifacts,
-            Map<String, String> extraArtifactRepositoryProperties) {
+            Map<String, String> extraArtifactRepositoryProperties, List<RepositoryReference> repositoryReferences) {
         super();
         this.agent = agent;
         this.includePackedArtifacts = includePackedArtifacts;
         this.extraArtifactRepositoryProperties = extraArtifactRepositoryProperties;
+        this.repositoryReferences = repositoryReferences;
         this.removeAddedRepositories = false;
     }
 
@@ -46,6 +56,22 @@ public class MirrorApplication extends org.eclipse.equinox.p2.internal.repositor
         extraArtifactRepositoryProperties.entrySet()
                 .forEach(entry -> result.setProperty(entry.getKey(), entry.getValue()));
         return result;
+    }
+
+    @Override
+    protected IMetadataRepository initializeDestination(RepositoryDescriptor toInit, IMetadataRepositoryManager mgr)
+            throws ProvisionException {
+        IMetadataRepository result = super.initializeDestination(toInit, mgr);
+        List<? extends IRepositoryReference> iRepoRefs = repositoryReferences.stream()
+                .map(MirrorApplication::toSpiRepositoryReference).collect(Collectors.toList());
+        result.addReferences(iRepoRefs);
+        return result;
+    }
+
+    private static org.eclipse.equinox.p2.repository.spi.RepositoryReference toSpiRepositoryReference(
+            RepositoryReference rr) {
+        return new org.eclipse.equinox.p2.repository.spi.RepositoryReference(URI.create(rr.getLocation()), rr.getName(),
+                IRepository.TYPE_METADATA, rr.isEnable() ? IRepository.ENABLED : IRepository.NONE);
     }
 
     @Override
