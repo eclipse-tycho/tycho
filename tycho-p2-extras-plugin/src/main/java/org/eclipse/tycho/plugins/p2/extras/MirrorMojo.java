5ac3d2d32ec7049f646c50c834c2d511259462d8
Alexander Kurtakov <akurtako@redhat.com>
2019-05-09 14:45:46 +0300

    Bug 453708 - Support for site/repository-reference/@location in eclipse-repository (edit)
    Fix MirrorMojo compile after DestinationRepositoryDescriptor constructor
    change.
    
    Change-Id: I5dbaa192ecc209c8962824111e693ac256fc70c1
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tycho-p2-extras-plugin/src/main/java/org/eclipse/tycho/plugins/p2/extras/MirrorMojo.java
+++ tycho-p2-extras-plugin/src/main/java/org/eclipse/tycho/plugins/p2/extras/MirrorMojo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011-2017 SAP AG and others.
+ * Copyright (c) 2011, 2019 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -249,7 +249,8 @@ public class MirrorMojo extends AbstractMojo {
             name = "";
         }
         final DestinationRepositoryDescriptor destinationDescriptor = new DestinationRepositoryDescriptor(destination,
-                name, compress, xzCompress, keepNonXzIndexFiles, mirrorMetadataOnly, append, Collections.emptyMap());
+                name, compress, xzCompress, keepNonXzIndexFiles, mirrorMetadataOnly, append, Collections.emptyMap(),
+                Collections.emptyList());
         getLog().info("Mirroring to " + destination + "...");
         try {
             mirrorService.mirrorStandalone(sourceDescriptor, destinationDescriptor, createIUDescriptions(),
