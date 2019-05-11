5769851b9ebd4501d17a5d0d7b61e231583c939e
Mykola Nikishov <mn@mn.com.ua>
2019-05-11 12:58:39 +0300

    Reduce cognitive complexity of LocalDependencyResolver's addDependencies
    Replace if statement with guard condition to return immediately if POM
    dependencies should not be considered.
    
    Change-Id: I674f340af40e5dafba43e2b5243693a3b377c788
    Signed-off-by: Mykola Nikishov <mn@mn.com.ua>

--- tycho-core/src/main/java/org/eclipse/tycho/core/osgitools/targetplatform/LocalDependencyResolver.java
+++ tycho-core/src/main/java/org/eclipse/tycho/core/osgitools/targetplatform/LocalDependencyResolver.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.tycho.core.osgitools.targetplatform;
 
+import static java.util.Optional.ofNullable;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -148,71 +150,74 @@ public class LocalDependencyResolver extends AbstractLogEnabled implements Depen
         TargetPlatformConfiguration configuration = (TargetPlatformConfiguration) project
                 .getContextValue(TychoConstants.CTX_TARGET_PLATFORM_CONFIGURATION);
 
-        if (configuration != null
-                && TargetPlatformConfiguration.POM_DEPENDENCIES_CONSIDER.equals(configuration.getPomDependencies())) {
-            Map<String, MavenProject> projectIds = new HashMap<>(session.getProjects().size() * 2);
-            // make a list of reactor projects
-            for (MavenProject p : session.getProjects()) {
-                String key = ArtifactUtils.key(p.getGroupId(), p.getArtifactId(), p.getVersion());
-                projectIds.put(key, p);
-            }
-            // handle dependencies that are in reactor
-            for (Dependency dependency : project.getDependencies()) {
-                if (Artifact.SCOPE_COMPILE.equals(dependency.getScope())) {
-                    String key = ArtifactUtils.key(dependency.getGroupId(), dependency.getArtifactId(),
-                            dependency.getVersion());
-                    if (projectIds.containsKey(key)) {
-                        MavenProject dependent = projectIds.get(key);
-                        ArtifactKey artifactKey = getArtifactKey(session, dependent);
-                        if (artifactKey != null) {
-                            platform.removeAll(artifactKey.getType(), artifactKey.getId());
-                            ReactorProject projectProxy = DefaultReactorProject.adapt(dependent);
-                            platform.addReactorArtifact(artifactKey, projectProxy, null, null);
-                            if (getLogger().isDebugEnabled()) {
-                                getLogger().debug("Add Maven project " + artifactKey);
-                            }
+        boolean considerPomDependencies = ofNullable(configuration)//
+                .map(TargetPlatformConfiguration::getPomDependencies)
+                .map(TargetPlatformConfiguration.POM_DEPENDENCIES_CONSIDER::equals)//
+                .orElse(false);
+        if (!considerPomDependencies)
+            return;
+
+        Map<String, MavenProject> projectIds = new HashMap<>(session.getProjects().size() * 2);
+        // make a list of reactor projects
+        for (MavenProject p : session.getProjects()) {
+            String key = ArtifactUtils.key(p.getGroupId(), p.getArtifactId(), p.getVersion());
+            projectIds.put(key, p);
+        }
+        // handle dependencies that are in reactor
+        for (Dependency dependency : project.getDependencies()) {
+            if (Artifact.SCOPE_COMPILE.equals(dependency.getScope())) {
+                String key = ArtifactUtils.key(dependency.getGroupId(), dependency.getArtifactId(),
+                        dependency.getVersion());
+                if (projectIds.containsKey(key)) {
+                    MavenProject dependent = projectIds.get(key);
+                    ArtifactKey artifactKey = getArtifactKey(session, dependent);
+                    if (artifactKey != null) {
+                        platform.removeAll(artifactKey.getType(), artifactKey.getId());
+                        ReactorProject projectProxy = DefaultReactorProject.adapt(dependent);
+                        platform.addReactorArtifact(artifactKey, projectProxy, null, null);
+                        if (getLogger().isDebugEnabled()) {
+                            getLogger().debug("Add Maven project " + artifactKey);
                         }
                     }
                 }
             }
-            // handle rest of dependencies
-            ArrayList<String> scopes = new ArrayList<>();
-            scopes.add(Artifact.SCOPE_COMPILE);
-            Collection<Artifact> artifacts;
-            try {
-                artifacts = projectDependenciesResolver.resolve(project, scopes, session);
-            } catch (MultipleArtifactsNotFoundException e) {
-                Collection<Artifact> missing = new HashSet<>(e.getMissingArtifacts());
+        }
+        // handle rest of dependencies
+        ArrayList<String> scopes = new ArrayList<>();
+        scopes.add(Artifact.SCOPE_COMPILE);
+        Collection<Artifact> artifacts;
+        try {
+            artifacts = projectDependenciesResolver.resolve(project, scopes, session);
+        } catch (MultipleArtifactsNotFoundException e) {
+            Collection<Artifact> missing = new HashSet<>(e.getMissingArtifacts());
 
-                for (Iterator<Artifact> it = missing.iterator(); it.hasNext();) {
-                    Artifact a = it.next();
-                    String key = ArtifactUtils.key(a.getGroupId(), a.getArtifactId(), a.getBaseVersion());
-                    if (projectIds.containsKey(key)) {
-                        it.remove();
-                    }
+            for (Iterator<Artifact> it = missing.iterator(); it.hasNext();) {
+                Artifact a = it.next();
+                String key = ArtifactUtils.key(a.getGroupId(), a.getArtifactId(), a.getBaseVersion());
+                if (projectIds.containsKey(key)) {
+                    it.remove();
                 }
+            }
 
-                if (!missing.isEmpty()) {
-                    throw new RuntimeException("Could not resolve project dependencies", e);
-                }
-
-                artifacts = e.getResolvedArtifacts();
-                artifacts.removeAll(e.getMissingArtifacts());
-            } catch (AbstractArtifactResolutionException e) {
+            if (!missing.isEmpty()) {
                 throw new RuntimeException("Could not resolve project dependencies", e);
             }
-            for (Artifact artifact : artifacts) {
-                String key = ArtifactUtils.key(artifact.getGroupId(), artifact.getArtifactId(),
-                        artifact.getBaseVersion());
-                if (!projectIds.containsKey(key)) {
-                    File plugin = artifact.getFile();
-                    ArtifactKey artifactKey = getArtifactKey(session, plugin);
 
-                    if (artifactKey != null) {
-                        platform.addArtifactFile(artifactKey, plugin, null);
-                        if (getLogger().isDebugEnabled()) {
-                            getLogger().debug("Add Maven artifact " + artifactKey);
-                        }
+            artifacts = e.getResolvedArtifacts();
+            artifacts.removeAll(e.getMissingArtifacts());
+        } catch (AbstractArtifactResolutionException e) {
+            throw new RuntimeException("Could not resolve project dependencies", e);
+        }
+        for (Artifact artifact : artifacts) {
+            String key = ArtifactUtils.key(artifact.getGroupId(), artifact.getArtifactId(), artifact.getBaseVersion());
+            if (!projectIds.containsKey(key)) {
+                File plugin = artifact.getFile();
+                ArtifactKey artifactKey = getArtifactKey(session, plugin);
+
+                if (artifactKey != null) {
+                    platform.addArtifactFile(artifactKey, plugin, null);
+                    if (getLogger().isDebugEnabled()) {
+                        getLogger().debug("Add Maven artifact " + artifactKey);
                     }
                 }
             }
