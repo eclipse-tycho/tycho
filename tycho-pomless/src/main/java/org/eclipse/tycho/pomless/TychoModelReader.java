mizdebsk/tycho-pomless

Set location for pomless models
Core Maven model reader sets location and some tools depend on these
values.  These tools won't work with tycho-pomless unless it also sets
the location.

Signed-off-by: Mikolaj Izdebski <mizdebsk@redhat.com>
Change-Id: I4da47c995c1333db0bb0ddb31f7309b08bf5ff39

tycho-pomless/src/main/java/org/eclipse/tycho/pomless/TychoModelReader.java
@@ -24,6 +24,8 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.maven.model.InputLocation;
+import org.apache.maven.model.InputSource;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.Parent;
 import org.apache.maven.model.building.ModelProcessor;
@@ -80,6 +82,7 @@ public class TychoModelReader extends ModelReaderSupport {
         String bundleVersion = getRequiredHeaderValue("Bundle-Version", headers, manifestFile);
         model.setVersion(getPomVersion(bundleVersion));
         model.setPackaging(getPackagingType(bundleSymbolicName));
+        setLocation(model, manifestFile);
         return model;
     }
 
@@ -108,6 +111,7 @@ public class TychoModelReader extends ModelReaderSupport {
         model.setVersion(getPomVersion(featureVersionNode.getValue()));
         model.setPackaging("eclipse-feature");
         // groupId is inherited from parent pom
+        setLocation(model, featureXml);
         return model;
     }
 
@@ -190,5 +194,12 @@ public class TychoModelReader extends ModelReaderSupport {
         }
         parentReference.setVersion(version);
         return parentReference;
+    }
+
+    private void setLocation(Model model, File modelSource) {
+        InputSource inputSource = new InputSource();
+        inputSource.setLocation(modelSource.toString());
+        inputSource.setModelId(model.getParent().getGroupId() + ":" + model.getArtifactId() + ":" + model.getVersion());
+        model.setLocation("", new InputLocation(0, 0, inputSource));
     }
 }

eclipse-tycho/tycho

tycho-pomless/src/main/java/org/eclipse/tycho/pomless/TychoModelReader.java
@@ -26,6 +26,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.maven.model.Build;
+import org.apache.maven.model.InputLocation;
+import org.apache.maven.model.InputSource;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.Parent;
 import org.apache.maven.model.Plugin;
@@ -89,6 +91,7 @@ public class TychoModelReader extends ModelReaderSupport {
         String bundleVersion = getRequiredHeaderValue("Bundle-Version", headers, manifestFile);
         model.setVersion(getPomVersion(bundleVersion));
         model.setPackaging(getPackagingType(bundleSymbolicName));
+        setLocation(model, manifestFile);
         return model;
     }
 
@@ -149,6 +152,7 @@ public class TychoModelReader extends ModelReaderSupport {
         }
         model.setVersion(getPomVersion(versionNode.getValue()));
         // groupId is inherited from parent pom
+        setLocation(model, xmlFile);
         return model;
     }
 
@@ -236,5 +240,12 @@ public class TychoModelReader extends ModelReaderSupport {
         }
         parentReference.setVersion(version);
         return parentReference;
+    }
+
+    private void setLocation(Model model, File modelSource) {
+        InputSource inputSource = new InputSource();
+        inputSource.setLocation(modelSource.toString());
+        inputSource.setModelId(model.getParent().getGroupId() + ":" + model.getArtifactId() + ":" + model.getVersion());
+        model.setLocation("", new InputLocation(0, 0, inputSource));
     }
 }