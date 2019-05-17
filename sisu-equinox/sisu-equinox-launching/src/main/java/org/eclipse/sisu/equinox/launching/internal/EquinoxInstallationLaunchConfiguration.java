8bd7ca213df4c85adc8eef68a520bf58315e04a0
Alexander Kurtakov <akurtako@redhat.com>
2019-05-17 21:30:40 +0300

    Convert FilenameFilter to lambda.
    Change-Id: Ie7288b5092f8f00be9c1702004649247df64b4ae
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- sisu-equinox/sisu-equinox-launching/src/main/java/org/eclipse/sisu/equinox/launching/internal/EquinoxInstallationLaunchConfiguration.java
+++ sisu-equinox/sisu-equinox-launching/src/main/java/org/eclipse/sisu/equinox/launching/internal/EquinoxInstallationLaunchConfiguration.java
@@ -35,13 +35,8 @@ public class EquinoxInstallationLaunchConfiguration implements LaunchConfigurati
 
     public static File findLauncherJar(File equinoxDirectory) {
         File pluginsDir = new File(equinoxDirectory, "plugins");
-        File[] launchers = pluginsDir.listFiles(new FilenameFilter() {
-
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.startsWith("org.eclipse.equinox.launcher_");
-            }
-        });
+        File[] launchers = pluginsDir
+                .listFiles((FilenameFilter) (dir, name) -> name.startsWith("org.eclipse.equinox.launcher_"));
 
         if (launchers == null || launchers.length == 0)
             throw new IllegalArgumentException("No launcher bundle found in " + pluginsDir);
