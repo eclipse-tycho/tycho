e87b464cc6bece2196b9a9f36f79e45449593754
Alexander Kurtakov <akurtako@redhat.com>
2019-09-25 09:21:40 +0300

    Bug 526008 - tycho-surefire-plugin:test goald should enable assertions by default
    Add enableAssertions user property but default to false for now.
    
    Change-Id: I3e7bbecf2fa4dbc07d1422503de503ccc3617eb6
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/surefire/EnableAssertionsTest.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.test.surefire;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class EnableAssertionsTest extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void testEnableAssertions() throws Exception {
+        Verifier verifier = getVerifier("surefire.enableAssertions");
+        verifier.executeGoal("verify");
+        verifier.verifyTextInLog("java.lang.AssertionError");
+    }
+}
