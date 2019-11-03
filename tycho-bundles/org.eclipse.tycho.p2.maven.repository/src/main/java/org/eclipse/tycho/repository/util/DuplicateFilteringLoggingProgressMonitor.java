ad5e919442e26739c2d345fcf4594af9a3b0908c
Michael Keppler <Michael.Keppler@gmx.de>
2019-11-03 08:33:42 +0100

    Bug 548655 - Avoid duplicate download progress messages
    The previous implementation suppressed _only_ the second duplicate of
    potentially more than 2 duplicate lines (see line 52 of previous
    revision), and only if it reported 0 Bytes/sec.
    
    Simplify the logic by not parsing the log output at all, and just
    suppress _all_ duplicates of a previous line that fits the "Fetching..."
    pattern.
    
    Change-Id: Ida3733938e6f37540b7fc7a5ed21883d5b9dd09a
    Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

--- tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/repository/util/DuplicateFilteringLoggingProgressMonitor.java
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/repository/util/DuplicateFilteringLoggingProgressMonitor.java
@@ -24,11 +24,9 @@ import org.eclipse.tycho.core.shared.MavenLogger;
  */
 public final class DuplicateFilteringLoggingProgressMonitor extends LoggingProgressMonitor {
 
-    private final Pattern PROGRESS_WITH_UNKNOWN_SPEED = Pattern.compile("\\(.* at 0B/s\\)");
-
     private static final String NON_MATCHING_LINE = "";
-    private String lastLoggedFile = NON_MATCHING_LINE;
-    private boolean lastLoggedFileFiltered = false;
+    private String lastLine = NON_MATCHING_LINE;
+    private static final Pattern PATTERN_FETCHING = Pattern.compile("Fetching \\S+ from");
 
     public DuplicateFilteringLoggingProgressMonitor(MavenLogger logger) {
         super(logger);
@@ -45,30 +43,14 @@ public final class DuplicateFilteringLoggingProgressMonitor extends LoggingProgr
     }
 
     private boolean checkIfDuplicateOfLastOutput(String message) {
-        // special handling for "Fetching %file from %url (%bytes [of %total ]at %speed)" lines
-        int startOfByteProgress = message.indexOf('(');
-        if (startOfByteProgress > 0) {
-            if (startOfByteProgress == lastLoggedFile.length() && message.startsWith(lastLoggedFile)) {
-                if (!lastLoggedFileFiltered
-                        && PROGRESS_WITH_UNKNOWN_SPEED.matcher(message.substring(startOfByteProgress)).matches()) {
-                    /*
-                     * p2 (or more precisely: ECF) always seems to print at least two "Fetching ..."
-                     * lines for the same file. Remove the second one, which doesn't really add
-                     * anything. Later lines may be interesting because they include the download
-                     * speed.
-                     */
-                    lastLoggedFileFiltered = true;
-                    return true;
-                }
-            } else {
-                lastLoggedFile = message.substring(0, startOfByteProgress);
-                lastLoggedFileFiltered = false;
-            }
-        } else {
-            lastLoggedFile = NON_MATCHING_LINE;
-            lastLoggedFileFiltered = false;
+        // special handling for "Fetching %file from %url ..." lines
+        if (!PATTERN_FETCHING.matcher(message).find()) {
+            lastLine = NON_MATCHING_LINE;
+            return false;
         }
-        return false;
+        boolean duplicate = message.equals(lastLine);
+        lastLine = message;
+        return duplicate;
     }
 
 }
