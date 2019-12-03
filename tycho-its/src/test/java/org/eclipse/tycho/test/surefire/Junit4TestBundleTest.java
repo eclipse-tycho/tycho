94fb002b813eae817c3c3628b24025fdce871921
Alexander Kurtakov <akurtako@redhat.com>
2019-12-04 00:03:02 +0200

    Drop eclipse32 ignores in tests.
    Change-Id: I29c3b537b1c72371dd4930c60ebd21a9478dfa5b
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tycho-its/src/test/java/org/eclipse/tycho/test/surefire/Junit4TestBundleTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/surefire/Junit4TestBundleTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 Sonatype Inc. and others.
+ * Copyright (c) 2008, 2019 Sonatype Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.tycho.test.surefire;
 
-import static org.eclipse.tycho.test.util.EnvironmentUtil.isEclipse32Platform;
 import static org.eclipse.tycho.test.util.SurefireUtil.testResultFile;
 import static org.eclipse.tycho.test.util.TychoMatchers.exists;
 import static org.junit.Assert.assertThat;
@@ -23,11 +22,6 @@ public class Junit4TestBundleTest extends AbstractTychoIntegrationTest {
 
     @Test
     public void test() throws Exception {
-
-        if (isEclipse32Platform()) {
-            // there is no JUnit 4 support in Eclipse 3.2
-            return;
-        }
 
         // a eclipse-test-plugin using JUnit 4 -> supported since MNGECLIPSE-1031
         Verifier verifier = getVerifier("surefire.junit4/bundle.test");
