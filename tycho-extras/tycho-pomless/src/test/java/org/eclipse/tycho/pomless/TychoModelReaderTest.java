6969b0ccc76420c0b87a6c031dc4c02a817caef2
Alexander Kurtakov <akurtako@redhat.com>
2020-02-26 22:50:27 +0200

    Bug 553445 - [pomless] Upgrade to [takari/polyglot-maven] Release 0.4.4
    Change-Id: I1f905c24115539952e810c5183d36cf97a0696da
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tycho-extras/tycho-pomless/src/test/java/org/eclipse/tycho/pomless/TychoModelReaderTest.java
+++ tycho-extras/tycho-pomless/src/test/java/org/eclipse/tycho/pomless/TychoModelReaderTest.java
@@ -12,8 +12,10 @@ package org.eclipse.tycho.pomless;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThrows;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -183,13 +185,10 @@ public class TychoModelReaderTest extends PlexusTestCase {
     public void testNoParent() throws Exception {
         File buildProperties = new File(getTestResourcesDir(),
                 "modelreader/noParent/bundle/" + TychoBundleMapping.MANIFEST_MF_MARKER);
-        try {
-            getTychoModelReader(TychoBundleMapping.PACKAGING).read(buildProperties,
-                    createReaderOptions(buildProperties));
-            fail();
-        } catch (IOException e) {
-            assertThat(e.getMessage(), containsString("No parent pom file found in"));
-        }
+        FileNotFoundException e = assertThrows(FileNotFoundException.class,
+                () -> getTychoModelReader(TychoBundleMapping.PACKAGING).read(buildProperties,
+                        createReaderOptions(buildProperties)));
+        assertThat(e.getMessage(), containsString("No parent pom file found in"));
     }
 
     @Test
