f5adce19f80113a4005b0fe0b1ecc2835e1a12c5
Alexander Aumann <a.aumann@mailbox.org>
2020-06-20 13:04:01 +0200

    Bug: 564423 Fix NumberFormatException with BREE JavaSE-1.8
    Use versionToJdkLevel instead of releaseToJDKLevel when checking whether
    '-bootclasspath' should be passed to JDT (<= Java 1.8).
    
    This fixes the -bootclasspath handling originally introduced in  cbfacbc736b
    
    Formatting changes unrelated -- the new formatting looks correct, though.
    
    Change-Id: I60dbd66be15327a2269a35c95867e5cd7889efe2
    Signed-off-by: Alexander Aumann <a.aumann@mailbox.org>

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/compiler/JdtBreeJava8Test.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Alexander Aumann and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Aumann - bug 564423
+ *******************************************************************************/
+
+package org.eclipse.tycho.test.compiler;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class JdtBreeJava8Test extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void testCompilerJdtBreeJava8() throws Exception {
+        Verifier verifier = getVerifier("compiler.jdt.bree.java8", false);
+        verifier.executeGoal("compile");
+    }
+
+}
