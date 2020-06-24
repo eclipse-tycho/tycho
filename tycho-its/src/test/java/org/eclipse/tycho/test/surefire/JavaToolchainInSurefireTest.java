cbbc603fd33b76a3ad2c5bd49e56dbb66ee4307b
Mickael Istria <mistria@redhat.com>
2020-06-24 16:31:25 +0200

    Use propagated ExecutionEnvironment for resolution
    Instead of recomputing new ones that may be missing or have glitches, we
    reuse the ones made available in the eeConfiguration (computed with
    profiles and other settings).
    This better separate concerns, remove some duplication, improve
    factorization.
    + Move some tests to newer EE (via BREE in MANIFEST.MF or via
    target-platform-configuration/executionEnvironment configution
    
    Some irrelevant EE tests have been removed as they should be moved to
    tycho-core (with actual EE registry handling) instead of resolver.
    Some tests were removed for cases of product hardcoding dependency on
    `a.jre` as this case is deprecated and not supported any more.
    
    Change-Id: I37244f1d789c483f439ebebfd096b0d972d55b3e
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- tycho-its/src/test/java/org/eclipse/tycho/test/surefire/JavaToolchainInSurefireTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/surefire/JavaToolchainInSurefireTest.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SAP AG. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.tycho.test.surefire;
 
 import java.io.File;
