c16113c82b1db6dce256c1f783c2c711cab1469b
Sebastian Ratz <sebastian.ratz@sap.com>
2020-07-16 18:12:54 +0200

    Bug 565281 - tycho-eclipserun-plugin does not set -data directory correctly
    Change-Id: Ia1e8fa18a04df8ce2b518151f7d81c39207dd0f7

--- tycho-extras/tycho-eclipserun-plugin/src/main/java/org/eclipse/tycho/extras/eclipserun/EclipseRunMojo.java
+++ tycho-extras/tycho-eclipserun-plugin/src/main/java/org/eclipse/tycho/extras/eclipserun/EclipseRunMojo.java
@@ -262,10 +262,7 @@ public class EclipseRunMojo extends AbstractMojo {
 
     private void runEclipse(EquinoxInstallation runtime) throws MojoExecutionException, MojoFailureException {
         try {
-            File workspace = new File(work, "data").getAbsoluteFile();
-            FileUtils.deleteDirectory(workspace);
             LaunchConfiguration cli = createCommandLine(runtime);
-            getLog().info("Expected eclipse log file: " + new File(workspace, ".metadata/.log").getCanonicalPath());
             int returnCode = launcher.execute(cli, forkedProcessTimeoutInSeconds);
             if (returnCode != 0) {
                 throw new MojoExecutionException("Error while executing platform (return code: " + returnCode + ")");
@@ -295,6 +292,10 @@ public class EclipseRunMojo extends AbstractMojo {
 
         addProgramArgs(cli, "-install", runtime.getLocation().getAbsolutePath(), "-configuration",
                 new File(work, "configuration").getAbsolutePath());
+
+        File workspace = new File(work, "data").getAbsolutePath();
+        addProgramArgs("-data", workspace);
+        getLog().info("Expected eclipse log file: " + new File(workspace, ".metadata/.log").getCanonicalPath());
 
         cli.addProgramArguments(splitArgLine(appArgLine));
         if (applicationsArgs != null) {
