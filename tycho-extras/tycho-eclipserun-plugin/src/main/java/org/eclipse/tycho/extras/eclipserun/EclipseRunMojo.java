eclipse-tycho/tycho

commit 0eeb95d1e58f3c93ced88dfb0f6a76da33d9074d

Make eclipserun mojo use actual EE definition
This prevents from resolution to fail or succeed erroneously, at the
risk of causing issues at execution.

Change-Id: I3ae439a8e42eb3a14bf58cc365d4275ebe241026
Signed-off-by: Mickael Istria <mistria@redhat.com>

tycho-extras/tycho-eclipserun-plugin/src/main/java/org/eclipse/tycho/extras/eclipserun/EclipseRunMojo.java
@@ -27,6 +27,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.toolchain.Toolchain;
+import org.apache.maven.toolchain.ToolchainManager;
 import org.codehaus.plexus.logging.Logger;
 import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.cli.CommandLineUtils;
@@ -41,7 +42,8 @@ import org.eclipse.tycho.ArtifactType;
 import org.eclipse.tycho.DefaultArtifactKey;
 import org.eclipse.tycho.artifacts.IllegalArtifactReferenceException;
 import org.eclipse.tycho.artifacts.TargetPlatform;
-import org.eclipse.tycho.core.ee.shared.ExecutionEnvironmentConfigurationStub;
+import org.eclipse.tycho.core.ee.ExecutionEnvironmentConfigurationImpl;
+import org.eclipse.tycho.core.ee.shared.ExecutionEnvironmentConfiguration;
 import org.eclipse.tycho.core.maven.ToolchainProvider;
 import org.eclipse.tycho.core.resolver.shared.MavenRepositoryLocation;
 import org.eclipse.tycho.launching.LaunchConfiguration;
@@ -193,6 +195,9 @@ public class EclipseRunMojo extends AbstractMojo {
     @Component
     private Logger logger;
 
+    @Component
+    private ToolchainManager toolchainManager;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         if (skip) {
@@ -228,8 +233,10 @@ public class EclipseRunMojo extends AbstractMojo {
         for (Repository repository : repositories) {
             tpConfiguration.addP2Repository(new MavenRepositoryLocation(repository.getId(), repository.getLocation()));
         }
+        ExecutionEnvironmentConfiguration eeConfiguration = new ExecutionEnvironmentConfigurationImpl(logger, true,
+                toolchainManager, session);
         TargetPlatform targetPlatform = resolverFactory.getTargetPlatformFactory().createTargetPlatform(tpConfiguration,
-                new ExecutionEnvironmentConfigurationStub(executionEnvironment), null, null);
+                eeConfiguration, null, null);
         P2Resolver resolver = resolverFactory.createResolver(new MavenLoggerAdapter(logger, false));
         for (Dependency dependency : dependencies) {
             try {

tycho-bundles/org.eclipse.tycho.core.shared/src/main/java/org/eclipse/tycho/core/ee/shared/ExecutionEnvironment.java
@@ -11,6 +11,7 @@
 package org.eclipse.tycho.core.ee.shared;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Properties;
 
 public interface ExecutionEnvironment {
@@ -34,6 +35,25 @@ public interface ExecutionEnvironment {
             } else {
                 return packageName;
             }
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(packageName, version);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || !(o instanceof SystemPackageEntry)) {
+                return false;
+            }
+            SystemPackageEntry other = (SystemPackageEntry) o;
+            return Objects.equals(this.packageName, other.packageName) && Objects.equals(this.version, other.version);
+        }
+
+        @Override
+        public String toString() {
+            return getClass().getSimpleName() + '[' + packageName + '/' + version + ']';
         }
     }
 

tycho-bundles/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/target/ee/AllKnownEEsResolutionHints.java
@@ -10,14 +10,10 @@
  *******************************************************************************/
 package org.eclipse.tycho.p2.target.ee;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Properties;
 
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.metadata.IRequirement;
@@ -62,26 +58,6 @@ public class AllKnownEEsResolutionHints implements ExecutionEnvironmentResolutio
     @Override
     public Collection<IInstallableUnit> getTemporaryAdditions() {
         return temporaryUnits.values();
-    }
-
-    private static Properties readProperties(final URL url) {
-        Properties listProps = new Properties();
-        InputStream stream = null;
-        try {
-            stream = url.openStream();
-            listProps.load(stream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (stream != null) {
-                    stream.close();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return listProps;
     }
 
 }

commit 421411553793d41f58681c5bd614a8a6c5408c69

Resolve agaisnt EE for eclipse-run
0eeb95d1e58f3c93ced88dfb0f6a76da33d9074d introduced a (minor) regression
that all EEs are resolved (eeConfiguration.ignoredByResolver changed
from false to true). Restore to previous state.

Change-Id: I93c778398731e98aab51c2b841523abbd7a737d6
Signed-off-by: Mickael Istria <mistria@redhat.com>

tycho-extras/tycho-eclipserun-plugin/src/main/java/org/eclipse/tycho/extras/eclipserun/EclipseRunMojo.java
@@ -233,7 +233,7 @@ public class EclipseRunMojo extends AbstractMojo {
         for (Repository repository : repositories) {
             tpConfiguration.addP2Repository(new MavenRepositoryLocation(repository.getId(), repository.getLocation()));
         }
-        ExecutionEnvironmentConfiguration eeConfiguration = new ExecutionEnvironmentConfigurationImpl(logger, true,
+        ExecutionEnvironmentConfiguration eeConfiguration = new ExecutionEnvironmentConfigurationImpl(logger, false,
                 toolchainManager, session);
         TargetPlatform targetPlatform = resolverFactory.getTargetPlatformFactory().createTargetPlatform(tpConfiguration,
                 eeConfiguration, null, null);