163adc27c3a97374ee38e78d6cf0a30e7ca2d266
Alexander Kurtakov <akurtako@redhat.com>
2020-09-18 08:51:09 +0300

    Update to Platform 4.17.
    Change-Id: I57d35f3c74de8aa83999945251ed6aaf26a04825
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tycho-core/src/main/java/org/eclipse/tycho/core/locking/FileLockServiceImpl.java
+++ tycho-core/src/main/java/org/eclipse/tycho/core/locking/FileLockServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2018 SAP AG and others.
+ * Copyright (c) 2011, 2020 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -29,15 +29,11 @@ public class FileLockServiceImpl implements FileLockService {
     private Location anyLocation;
 
     public FileLockServiceImpl() {
-        anyLocation = new BasicLocation(null, null, false, null, new ConfigValues(new HashMap<String, String>(), new HashMap<>()),
-                new EquinoxContainer(null), new AtomicBoolean(false));
+        anyLocation = new BasicLocation(null, null, false, null,
+                new ConfigValues(new HashMap<String, String>(), new HashMap<>()), new EquinoxContainer(null, null),
+                new AtomicBoolean(false));
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.tycho.p2.repository.FileLockService#getFileLocker(java.io.File)
-     */
     @Override
     public synchronized FileLocker getFileLocker(File file) {
         return new FileLockerImpl(file, anyLocation);
