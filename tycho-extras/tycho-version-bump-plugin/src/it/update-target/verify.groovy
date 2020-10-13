cebdf6dc3492fd007c1ac1e377086ab79a190daf
Mickael Istria <mistria@redhat.com>
2020-10-13 12:26:09 +0200

    Bug 567835 - Module-specific integration tests not run in CI
    Change-Id: Ibbbbd40a7b44cbe542a332a4cce9e0b6a517644b
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- tycho-extras/tycho-version-bump-plugin/src/it/update-target/verify.groovy
+++ tycho-extras/tycho-version-bump-plugin/src/it/update-target/verify.groovy
@@ -3,7 +3,7 @@ import java.io.*;
 def target = new XmlSlurper().parse(new File(basedir, "update-target.target"));
 
 assert target.locations.location.unit.size() == 4;
-assert target.locations.location.unit.find{ it.@id == "org.eclipse.equinox.executable.feature.group" }.@version == "3.5.1.v20111216-1653-7P7NFUIFIbaUcU77s0KQWHw5HZTZ";
-assert target.locations.location.unit.find{ it.@id == "org.eclipse.jdt.feature.group"                }.@version == "3.7.2.v20120120-1414-7z8gFcuFMP7BW5XTz0jLTnz0l9B1";
-assert target.locations.location.unit.find{ it.@id == "org.eclipse.platform.ide"                     }.@version == "3.7.2.M20120208-0800";
-assert target.locations.location.unit.find{ it.@id == "org.eclipse.pde.feature.group"                }.@version == "3.7.2.v20120120-1420-7b7rFUOFEx2Xnqafnpz0E--0";
+assert target.locations.location.unit.find{ it.@id == "org.eclipse.equinox.executable.feature.group" }.@version == "3.8.900.v20200819-0940";
+assert target.locations.location.unit.find{ it.@id == "org.eclipse.jdt.feature.group"                }.@version == "3.18.500.v20200902-1800";
+assert target.locations.location.unit.find{ it.@id == "org.eclipse.platform.ide"                     }.@version == "4.17.0.I20200902-1800";
+assert target.locations.location.unit.find{ it.@id == "org.eclipse.pde.feature.group"                }.@version == "3.14.500.v20200902-1800";
