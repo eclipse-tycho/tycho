59511cbbf3c466e3ed20cdfeee92f9066e92fee9
Christoph Läubrich <laeubi@laeubi-soft.de>
2020-11-23 20:38:47 +0100

    Bug 568070 - add a unit-test for automatic source generation
    Change-Id: Ice9f04f7de0ae10c3e24f2f14d7087345a5d0323
    Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/sourceBundle/AutoGenerateSourceArtifactsTest.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Christoph Läubrich and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.test.sourceBundle;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class AutoGenerateSourceArtifactsTest extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void testSourceGeneration() throws Exception {
+        Verifier verifier = getVerifier("/automaticSourceGeneration", false, true);
+        verifier.executeGoal("package");
+        verifier.displayStreamBuffers();
+        verifier.verifyErrorFreeLog();
+    }
+}
