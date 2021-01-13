9f85f197f5547e17b832e96454fcefd3dcffcc82
Alexander Kurtakov <akurtako@redhat.com>
2021-01-13 17:07:29 +0200

    Fix warnings in BuildPropertiesImplTest
    * Silence serial version warnings, they make no sense in tests
    * Fix generics warnings.
    
    Change-Id: I3b52333b9b335c991024c95b9dee8a364c6e8388
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tycho-bundles/org.eclipse.tycho.core.shared.tests/src/test/java/org/eclipse/tycho/core/shared/BuildPropertiesImplTest.java
+++ tycho-bundles/org.eclipse.tycho.core.shared.tests/src/test/java/org/eclipse/tycho/core/shared/BuildPropertiesImplTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 SAP AG and others.
+ * Copyright (c) 2012, 2021 SAP AG and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -47,13 +47,13 @@ public class BuildPropertiesImplTest {
         }
 
         @Override
-        public Set keySet() {
-            List sortedList = new ArrayList(super.keySet());
-            Collections.sort(sortedList);
+        public Set<Object> keySet() {
+            List<Object> sortedList = new ArrayList<>(super.keySet());
+            sortedList.sort(null);
             if (reverse) {
                 Collections.reverse(sortedList);
             }
-            return new LinkedHashSet(sortedList);
+            return new LinkedHashSet<>(sortedList);
         }
 
         @Override
