83ef6093a93ab17724e88e0cb06b39a3e125233b
Mickael Istria <mistria@redhat.com>
2021-01-14 16:57:48 +0100

    Bug 570189 - EquinoxResolver leads to StackOverflowError in some case: need to move away from ResolverImpl
    Change-Id: Ibae4d0f44f8d53062625f3dd4c6e844a6b0d8e79
    Signed-off-by: Mickael Istria <mistria@redhat.com>

--- tycho-core/src/test/java/org/eclipse/tycho/core/osgitools/EquinoxResolverTest.java
+++ tycho-core/src/test/java/org/eclipse/tycho/core/osgitools/EquinoxResolverTest.java
@@ -17,8 +17,8 @@ import java.util.Properties;
 
 import org.apache.maven.plugin.testing.SilentLog;
 import org.apache.maven.project.MavenProject;
-import org.eclipse.osgi.service.resolver.BundleDescription;
-import org.eclipse.osgi.service.resolver.State;
+import org.eclipse.osgi.container.Module;
+import org.eclipse.osgi.container.ModuleContainer;
 import org.eclipse.tycho.ReactorProject;
 import org.eclipse.tycho.core.ee.ExecutionEnvironmentUtils;
 import org.eclipse.tycho.core.ee.shared.ExecutionEnvironment;
@@ -27,6 +27,8 @@ import org.eclipse.tycho.core.utils.TychoProjectUtils;
 import org.eclipse.tycho.core.utils.TychoVersion;
 import org.eclipse.tycho.testing.AbstractTychoMojoTestCase;
 import org.osgi.framework.BundleException;
+import org.osgi.framework.Constants;
+import org.osgi.framework.wiring.BundleRevision;
 
 public class EquinoxResolverTest extends AbstractTychoMojoTestCase {
     private static final ExecutionEnvironment DUMMY_EE = ExecutionEnvironmentUtils.getExecutionEnvironment("J2SE-1.5",
@@ -49,12 +51,10 @@ public class EquinoxResolverTest extends AbstractTychoMojoTestCase {
     }
 
     public void test_noSystemBundle() throws BundleException {
-        Properties properties = subject.getPlatformProperties(new Properties(), null, DUMMY_EE);
-        State state = subject.newState(new DefaultDependencyArtifacts(), properties, false, null);
-
-        BundleDescription[] bundles = state.getBundles("system.bundle");
-
-        assertEquals(1, bundles.length);
+        Properties properties = subject.getPlatformProperties(new Properties(), null, null, DUMMY_EE);
+        ModuleContainer container = subject.newState(new DefaultDependencyArtifacts(), properties, null);
+        assertEquals(1, container.getModules().stream().map(Module::getCurrentRevision)
+                .map(BundleRevision::getSymbolicName).filter(Constants.SYSTEM_BUNDLE_SYMBOLICNAME::equals).count());
     }
 
     public void testBREEJavaSE11() throws Exception {
@@ -69,13 +69,12 @@ public class EquinoxResolverTest extends AbstractTychoMojoTestCase {
         ExecutionEnvironment ee = TychoProjectUtils.getExecutionEnvironmentConfiguration(reactorProject)
                 .getFullSpecification();
         assertEquals("JavaSE-11", ee.getProfileName());
-        Properties platformProperties = subject.getPlatformProperties(reactorProject, new DefaultDependencyArtifacts(),
-                ee);
+        Properties platformProperties = subject.getPlatformProperties(reactorProject, null,
+                new DefaultDependencyArtifacts(), ee);
         String executionEnvironments = platformProperties.getProperty("org.osgi.framework.executionenvironment");
         assertTrue(executionEnvironments.contains("JavaSE-10"));
         String capabilities = platformProperties.getProperty("org.osgi.framework.system.capabilities");
         assertTrue(capabilities.contains(
                 "osgi.ee=\"JavaSE\"; version:List<Version>=\"1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 9.0, 10.0, 11.0\""));
     }
-
 }
