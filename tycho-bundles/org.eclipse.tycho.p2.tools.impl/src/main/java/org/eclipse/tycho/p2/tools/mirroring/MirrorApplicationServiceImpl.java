1ef4824d93e1940756db3799a61a0368809f04f8
Christoph Läubrich <laeubi@laeubi-soft.de>
2021-01-15 09:20:30 +0100

    Bug 570191 - Let tycho sign all artifacts/plugins when making a p2 repository
    create a new mojo for explicitly updating the meta-data so it oculd be
    reused in other phases
    
    Change-Id: I1a7e8e9bf21185025e9b85aa2376765b9f1ac8d5
    Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>

--- tycho-bundles/org.eclipse.tycho.p2.tools.impl/src/main/java/org/eclipse/tycho/p2/tools/mirroring/MirrorApplicationServiceImpl.java
+++ tycho-bundles/org.eclipse.tycho.p2.tools.impl/src/main/java/org/eclipse/tycho/p2/tools/mirroring/MirrorApplicationServiceImpl.java
@@ -168,7 +168,6 @@ public class MirrorApplicationServiceImpl implements MirrorApplicationService {
                 }
             }
             recreateArtifactRepository(destination);
-            xzCompress(destination);
         } finally {
             agent.stop();
         }
@@ -188,7 +187,7 @@ public class MirrorApplicationServiceImpl implements MirrorApplicationService {
         }
     }
 
-    private void recreateArtifactRepository(DestinationRepositoryDescriptor destination) throws FacadeException {
+    public void recreateArtifactRepository(DestinationRepositoryDescriptor destination) throws FacadeException {
         // bug 357513 - force artifact repo recreation which will
         // create the missing md5 checksums
         if (destination.isMetaDataOnly()) {
@@ -207,6 +206,7 @@ public class MirrorApplicationServiceImpl implements MirrorApplicationService {
         } catch (ProvisionException e) {
             throw new FacadeException("Recreate artifact repository failed", e);
         }
+        xzCompress(destination);
     }
 
     private static MirrorApplication createMirrorApplication(RepositoryReferences sources,
