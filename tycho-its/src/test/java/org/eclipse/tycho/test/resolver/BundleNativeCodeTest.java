8bb62c3900522418d64e45d2551b5758be891f83
Mickael Istria <mistria@redhat.com>
2021-02-06 19:19:15 +0100

    Add Test for Bundle-NativeCode resolution
    Change-Id: If0425a29c422fa60673d17e6aace4bf237c5dfdc
    Signed-off-by: Mickael Istria <mistria@redhat.com>

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/resolver/BundleNativeCodeTest.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Red Hat Inc. and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.tycho.test.resolver;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class BundleNativeCodeTest extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void testBundleNativeCode() throws Exception {
+        Verifier verifier = getVerifier("/resolver.nativeCode");
+        verifier.executeGoal("compile");
+        verifier.verifyErrorFreeLog();
+    }
+}
