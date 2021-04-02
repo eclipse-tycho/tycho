c802ba13b81baebc2372d6669223afd113267a28
Christoph Läubrich <laeubi@laeubi-soft.de>
2021-04-02 16:22:46 +0200

    Bug 572416 - Compile all source folders contained in .classpath
    - compile test- source folders in compile-test phase
    
    Change-Id: Ia4eaab2fe3a4bc9c58f203384e70e31ddd33ee41
    Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>

--- tycho-compiler-plugin/src/main/java/org/eclipse/tycho/compiler/OsgiCompilerMojo.java
+++ tycho-compiler-plugin/src/main/java/org/eclipse/tycho/compiler/OsgiCompilerMojo.java
@@ -16,14 +16,72 @@
 
 package org.eclipse.tycho.compiler;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.eclipse.tycho.classpath.SourcepathEntry;
+import org.eclipse.tycho.core.osgitools.project.BuildOutputJar;
 
 /**
  * Compiles application sources with eclipse plugin dependencies
  */
 @Mojo(name = "compile", defaultPhase = LifecyclePhase.COMPILE, requiresDependencyResolution = ResolutionScope.COMPILE, threadSafe = true)
 public class OsgiCompilerMojo extends AbstractOsgiCompilerMojo {
+
+    private final class BuildOutputJarSourcepathEntry implements SourcepathEntry {
+        private final File sourcesRoot;
+        private final BuildOutputJar jar;
+
+        private BuildOutputJarSourcepathEntry(File sourcesRoot, BuildOutputJar jar) {
+            this.sourcesRoot = sourcesRoot;
+            this.jar = jar;
+        }
+
+        @Override
+        public File getSourcesRoot() {
+            return sourcesRoot;
+        }
+
+        @Override
+        public File getOutputDirectory() {
+            return jar.getOutputDirectory();
+        }
+
+        @Override
+        public List<String> getIncludes() {
+            return null;
+        }
+
+        @Override
+        public List<String> getExcludes() {
+            return jar.getFilesToExclude();
+        }
+    }
+
+    protected void doCompile() throws MojoExecutionException, MojoFailureException {
+        super.doCompile();
+        BuildOutputJar dotOutputJar = getEclipsePluginProject().getDotOutputJar();
+        if (dotOutputJar != null) {
+            project.getArtifact().setFile(dotOutputJar.getOutputDirectory());
+        }
+    }
+
+    @Override
+    public List<SourcepathEntry> getSourcepath() throws MojoExecutionException {
+        ArrayList<SourcepathEntry> entries = new ArrayList<>();
+        for (BuildOutputJar jar : getEclipsePluginProject().getOutputJars()) {
+            for (final File sourcesRoot : jar.getSourceFolders()) {
+                SourcepathEntry entry = new BuildOutputJarSourcepathEntry(sourcesRoot, jar);
+                entries.add(entry);
+            }
+        }
+        return entries;
+    }
 
 }
