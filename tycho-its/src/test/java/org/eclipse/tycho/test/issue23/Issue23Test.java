12fa4389c6d8ffd5fe99c6b9622b70dee70f4cf6
Christian Dietrich <christian.dietrich@itemis.de>
2021-04-09 20:06:06 +0200

    test case for #23
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/issue23/Issue23Test.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Sonatype Inc. and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Sonatype Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.test.issue23;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class Issue23Test extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void test() throws Exception {
+        Verifier verifier = getVerifier("issue23");
+
+        verifier.executeGoal("integration-test");
+        verifier.verifyErrorFreeLog();
+    }
+}
