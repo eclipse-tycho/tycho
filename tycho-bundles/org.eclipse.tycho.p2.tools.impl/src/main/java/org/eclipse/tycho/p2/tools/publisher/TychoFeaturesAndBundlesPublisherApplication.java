b890d8189d6a918a83996fc2cc44cc74c4ddb210
Christoph Läubrich <laeubi@laeubi-soft.de>
2021-06-09 07:39:12 +0200

    Support for 'assemble-p2-maven-site' mojo (#121)
    - add a PublisherApplication
    - Replace the rules and set repository names
    - package and attach update-site
    - Support inclusion of managed dependencies 
    - Filter duplicates
    - Add documentation and providedCapabilities
    - Add integration test

---
+++ tycho-bundles/org.eclipse.tycho.p2.tools.impl/src/main/java/org/eclipse/tycho/p2/tools/publisher/TychoFeaturesAndBundlesPublisherApplication.java
@@ -0,0 +1,224 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Christoph Läubrich and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.p2.tools.publisher;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.URIUtil;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.internal.p2.artifact.repository.simple.SimpleArtifactRepository;
+import org.eclipse.equinox.internal.p2.artifact.repository.simple.SimpleArtifactRepositoryFactory;
+import org.eclipse.equinox.internal.p2.updatesite.CategoryXMLAction;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
+import org.eclipse.equinox.p2.metadata.MetadataFactory.InstallableUnitDescription;
+import org.eclipse.equinox.p2.publisher.AbstractPublisherAction;
+import org.eclipse.equinox.p2.publisher.AbstractPublisherApplication;
+import org.eclipse.equinox.p2.publisher.AdviceFileAdvice;
+import org.eclipse.equinox.p2.publisher.IPublisherAction;
+import org.eclipse.equinox.p2.publisher.IPublisherInfo;
+import org.eclipse.equinox.p2.publisher.IPublisherResult;
+import org.eclipse.equinox.p2.publisher.PublisherInfo;
+import org.eclipse.equinox.p2.publisher.eclipse.BundlesAction;
+import org.eclipse.equinox.p2.repository.IRepositoryManager;
+import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
+import org.eclipse.equinox.p2.repository.artifact.spi.ArtifactDescriptor;
+import org.eclipse.equinox.spi.p2.publisher.PublisherHelper;
+import org.eclipse.osgi.service.resolver.BundleDescription;
+import org.osgi.framework.BundleException;
+
+@SuppressWarnings("restriction")
+public class TychoFeaturesAndBundlesPublisherApplication extends AbstractPublisherApplication {
+
+    private static final String MAVEN_PREFIX = "maven-";
+    private BundleDescription[] bundles;
+    private File[] advices;
+    private URI categoryDefinition;
+    private String[] rules;
+
+    @Override
+    public Object run(PublisherInfo publisherInfo) throws Exception {
+        Object run = super.run(publisherInfo);
+        if (run == IApplication.EXIT_OK) {
+            if (rules != null && artifactLocation != null) {
+                String[][] newRules = new String[rules.length][];
+                for (int i = 0; i < rules.length; i++) {
+                    newRules[i] = rules[i].split(";", 2);
+                }
+
+                SimpleArtifactRepositoryFactory repoFactory = new SimpleArtifactRepositoryFactory();
+                SimpleArtifactRepository repo = (SimpleArtifactRepository) repoFactory.load(artifactLocation,
+                        IRepositoryManager.REPOSITORY_HINT_MODIFIABLE, null);
+                repo.setRules(newRules);
+                repo.save();
+            }
+        }
+        return run;
+    }
+
+    @Override
+    protected void processParameter(String arg, String parameter, PublisherInfo publisherInfo)
+            throws URISyntaxException {
+        super.processParameter(arg, parameter, publisherInfo);
+        if (arg.equalsIgnoreCase("-bundles")) {
+            bundles = Arrays.stream(AbstractPublisherAction.getArrayFromString(parameter, ",")).map(File::new)
+                    .map(t -> {
+                        try {
+                            return BundlesAction.createBundleDescription(t);
+                        } catch (IOException | BundleException e) {
+                            //ignoring files that are "not bundles" they will be skipped on the later steps
+                            return null;
+                        }
+                    }).toArray(BundleDescription[]::new);
+        }
+        if (arg.equalsIgnoreCase("-bundlesFile")) {
+            bundles = Arrays.stream(getArrayFromFile(parameter)).map(File::new).map(t -> {
+                try {
+                    return BundlesAction.createBundleDescription(t);
+                } catch (IOException | BundleException e) {
+                    //ignoring files that are "not bundles" they will be skipped on the later steps
+                    return null;
+                }
+            }).toArray(BundleDescription[]::new);
+        }
+        if (arg.equalsIgnoreCase("-advices")) {
+            advices = Arrays.stream(AbstractPublisherAction.getArrayFromString(parameter, ","))
+                    .map(str -> str.isBlank() ? null : new File(str)).toArray(File[]::new);
+        }
+        if (arg.equalsIgnoreCase("-advicesFile")) {
+            advices = Arrays.stream(getArrayFromFile(parameter)).map(str -> str.isBlank() ? null : new File(str))
+                    .toArray(File[]::new);
+        }
+        if (arg.equalsIgnoreCase("-categoryDefinition")) {
+            categoryDefinition = URIUtil.fromString(parameter);
+        }
+        if (arg.equalsIgnoreCase("-rules")) {
+            rules = AbstractPublisherAction.getArrayFromString(parameter, ",");
+        }
+    }
+
+    private String[] getArrayFromFile(String parameter) {
+        File file = new File(parameter);
+        try {
+            return FileUtils.readLines(file, StandardCharsets.UTF_8).toArray(String[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException("reading file parameter failed", e);
+        }
+    }
+
+    @Override
+    protected IPublisherAction[] createActions() {
+        List<IPublisherAction> result = new ArrayList<>();
+        if (advices != null) {
+            List<AdviceFileAdvice> advicesList = new ArrayList<>();
+            for (int i = 0; i < advices.length; i++) {
+                File adviceFile = advices[i];
+                BundleDescription bundleDescription;
+                if (i >= bundles.length - 1 || (bundleDescription = bundles[i]) == null) {
+                    continue;
+                }
+                String symbolicName = bundleDescription.getSymbolicName();
+                if (symbolicName == null) {
+                    //not a bundle... no advice...
+                    continue;
+                }
+                advicesList.add(new AdviceFileAdvice(symbolicName,
+                        PublisherHelper.fromOSGiVersion(bundleDescription.getVersion()),
+                        new Path(adviceFile.getParentFile().getAbsolutePath()), new Path(adviceFile.getName())) {
+
+                    @Override
+                    public Map<String, String> getArtifactProperties(IInstallableUnit iu,
+                            IArtifactDescriptor descriptor) {
+
+                        // workaround Bug 539672
+                        Map<String, String> properties = super.getInstallableUnitProperties(null);
+                        if (properties != null) {
+                            if (descriptor instanceof ArtifactDescriptor) {
+                                ArtifactDescriptor artifactDescriptor = (ArtifactDescriptor) descriptor;
+                                for (Map.Entry<String, String> entry : properties.entrySet()) {
+                                    String key = entry.getKey();
+                                    String value = entry.getValue();
+                                    if (key.startsWith(MAVEN_PREFIX)) {
+                                        String key2 = "maven." + key.substring(MAVEN_PREFIX.length());
+                                        artifactDescriptor.setProperty(key2, value);
+                                    }
+                                }
+                            }
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    public Map<String, String> getInstallableUnitProperties(InstallableUnitDescription iu) {
+                        Map<String, String> properties = super.getInstallableUnitProperties(iu);
+                        if (properties == null) {
+                            return null;
+                        }
+                        Map<String, String> installableUnitProperties = new LinkedHashMap<>(properties);
+                        Set<String> keySet = installableUnitProperties.keySet();
+                        for (Iterator<String> iterator = keySet.iterator(); iterator.hasNext();) {
+                            String string = iterator.next();
+                            if (string.startsWith(MAVEN_PREFIX)) {
+                                iterator.remove();
+                            }
+                        }
+                        return installableUnitProperties.isEmpty() ? null : installableUnitProperties;
+                    }
+
+                });
+                result.add(new AdviceFilePublisherAction(advicesList));
+            }
+        }
+        if (bundles != null) {
+            result.add(new BundlesAction(bundles));
+        }
+        if (categoryDefinition != null) {
+            result.add(new CategoryXMLAction(categoryDefinition, "category"));
+        }
+        return result.toArray(IPublisherAction[]::new);
+    }
+
+    private static final class AdviceFilePublisherAction extends AbstractPublisherAction {
+
+        private List<AdviceFileAdvice> advicesList;
+
+        public AdviceFilePublisherAction(List<AdviceFileAdvice> advicesList) {
+            this.advicesList = advicesList;
+        }
+
+        @Override
+        public IStatus perform(IPublisherInfo publisherInfo, IPublisherResult results, IProgressMonitor monitor) {
+            for (AdviceFileAdvice advice : advicesList) {
+                publisherInfo.addAdvice(advice);
+            }
+            return Status.OK_STATUS;
+        }
+
+    }
+
+}
---
+++ tycho-p2/tycho-p2-repository-plugin/src/main/java/org/eclipse/tycho/plugins/p2/repository/MavenP2SiteMojo.java
@@ -0,0 +1,373 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Christoph Läubrich and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.plugins.p2.repository;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.handler.ArtifactHandler;
+import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
+import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+import org.apache.maven.repository.RepositorySystem;
+import org.codehaus.plexus.archiver.util.DefaultFileSet;
+import org.codehaus.plexus.archiver.zip.ZipArchiver;
+import org.codehaus.plexus.logging.Logger;
+import org.eclipse.sisu.equinox.EquinoxServiceFactory;
+import org.eclipse.sisu.equinox.launching.internal.P2ApplicationLauncher;
+import org.eclipse.tycho.p2.repository.RepositoryLayoutHelper;
+
+/**
+ * <p>
+ * This goals produces a "p2-maven-site" from the projects declared &lt;dependencies&gt; (and
+ * &lt;dependencyManagement&gt; if desired). A p2-maven-site is completely manageable by standard
+ * maven tools and has the following properties:
+ * <ul>
+ * <li>The artifacts are not stored in the site itself but referenced as maven-coordinates, that
+ * means you don't have to upload your artifacts to a dedicated place, everything is fetched from
+ * the maven repository</li>
+ * <li>The metadata of the page is attached to the current project with type=zip and
+ * classifier=p2site and could be deployed using standard maven techniques</li>
+ * </ul>
+ * 
+ * <b>Please note:</b> Only valid OSGi bundles are included, there is no way to automatically wrap
+ * plain jars and they are silently ignored. This is intentional, as the goal of a p2-maven-site is
+ * to use exactly the same artifact that is deployed in the maven repository.
+ * </p>
+ * <p>
+ * The produced p2-maven-site can then be consumed by Tycho or PDE targets (m2eclipse is required
+ * for this), in the following way: A tycho-repository section:
+ * 
+ * <pre>
+    &lt;repository>
+    &lt;id>my-p2-maven-site</id>
+        &lt;url>mvn:[grouId]:[artifactId]:[version]:zip:p2site</url>
+        &lt;layout>p2</layout>
+    &lt;/repository>
+ * </pre>
+ * 
+ * A target location of type software-site:
+ * 
+ * <pre>
+ *  &lt;location includeAllPlatforms="false" includeConfigurePhase="true" includeMode="planner" includeSource="true" type="InstallableUnit">
+        &lt;repository location="mvn:[grouId]:[artifactId]:[version]:zip:p2site"/>
+        -- list desired units here --
+    &lt;/location>
+ * </pre>
+ * </p>
+ *
+ */
+@Mojo(name = "assemble-p2-maven-site", requiresDependencyResolution = ResolutionScope.COMPILE)
+public class MavenP2SiteMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject project;
+
+    @Parameter(defaultValue = "${session}", readonly = true, required = true)
+    private MavenSession session;
+
+    /**
+     * Flag whether dependencies of the projects declared &lt;dependencies&gt; (and
+     * &lt;dependencyManagement&gt; if desired) should be included. If enabled this creates the
+     * maven equivalent of a self-contained P2 site
+     */
+    @Parameter(defaultValue = "false")
+    private boolean includeDependencies;
+
+    /**
+     * Flag that controls if &lt;dependencyManagement&gt; managed dependencies should be included,
+     * this is useful if your are using a BOM and like to include all materials in the update-site
+     * regardless of if they are explicitly included in the &lt;dependencies&gt; section
+     */
+    @Parameter(defaultValue = "false")
+    private boolean includeManaged;
+
+    @Parameter(defaultValue = "300")
+    private int timeoutInSeconds = 300;
+
+    /**
+     * Location of the category definition. If the file does not exits, a generic category
+     * definition is generated including all bundles under one category
+     */
+    @Parameter(defaultValue = "${project.basedir}/category.xml")
+    private File categoryFile;
+    /**
+     * Name for the automatically generated category
+     */
+    @Parameter(defaultValue = "Bundles")
+    private String categoryName;
+
+    @Parameter(defaultValue = "${project.build.directory}/repository")
+    private File destination;
+
+    @Component
+    private EquinoxServiceFactory equinox;
+
+    @Component
+    private Logger logger;
+    @Component
+    private RepositorySystem repositorySystem;
+
+    @Component
+    private P2ApplicationLauncher launcher;
+
+    @Component
+    private MavenProjectHelper projectHelper;
+
+    /**
+     * The output directory of the jar file
+     * 
+     * By default this is the Maven "target/" directory.
+     */
+    @Parameter(property = "project.build.directory", required = true)
+    protected File buildDirectory;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        logger.debug("categoryName =        " + categoryName);
+        logger.debug("includeManaged =      " + includeManaged);
+        logger.debug("includeDependencies = " + includeDependencies);
+        Set<String> filesAdded = new HashSet<>();
+        List<Dependency> dependencies = project.getDependencies();
+        List<File> bundles = new ArrayList<>();
+        List<File> advices = new ArrayList<>();
+        resolve(dependencies, bundles, advices, filesAdded);
+        if (includeManaged) {
+            resolve(project.getDependencyManagement().getDependencies(), bundles, advices, filesAdded);
+        }
+        String categoryURI;
+        if (categoryFile.exists()) {
+            categoryURI = categoryFile.toURI().toASCIIString();
+        } else {
+            try {
+                File categoryGenFile = File.createTempFile("category", ".xml");
+
+                try (PrintWriter writer = new PrintWriter(
+                        new OutputStreamWriter(new FileOutputStream(categoryGenFile), StandardCharsets.UTF_8))) {
+                    writer.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+                    writer.println("<site>");
+                    writer.println("<category-def name=\"bundles\" label=\"" + categoryName + "\"/>");
+                    writer.println("<iu>");
+                    writer.println("<category name=\"bundles\"/>");
+                    writer.println(
+                            " <query><expression type=\"match\">providedCapabilities.exists(p | p.namespace == 'osgi.bundle')</expression></query>");
+                    writer.println("</iu>");
+                    writer.println("</site>");
+                }
+                categoryGenFile.deleteOnExit();
+                categoryURI = categoryGenFile.toURI().toASCIIString();
+            } catch (IOException e) {
+                throw new MojoExecutionException("failed to generate category.xml", e);
+            }
+        }
+
+        File bundlesFile;
+        try {
+            bundlesFile = File.createTempFile("bundles", ".txt");
+            FileUtils.writeLines(bundlesFile, StandardCharsets.UTF_8.name(),
+                    bundles.stream().map(File::getAbsolutePath).collect(Collectors.toList()));
+        } catch (IOException e) {
+            throw new MojoExecutionException("failed to generate bundles list", e);
+        }
+        File advicesFile;
+        try {
+            advicesFile = File.createTempFile("advices", ".txt");
+            FileUtils.writeLines(advicesFile, StandardCharsets.UTF_8.name(),
+                    advices.stream().map(File::getAbsolutePath).collect(Collectors.toList()));
+        } catch (IOException e) {
+            throw new MojoExecutionException("failed to generate bundles list", e);
+        }
+
+        destination.mkdirs();
+        launcher.setWorkingDirectory(destination);
+        launcher.setApplicationName("org.eclipse.tycho.p2.tools.publisher.TychoFeaturesAndBundlesPublisher");
+        launcher.addArguments("-artifactRepository", destination.toURI().toString(), //
+                "-metadataRepository", destination.toURI().toString(), //
+                "-bundlesFile", //
+                bundlesFile.getAbsolutePath(), //
+                "-advicesFile", //
+                advicesFile.getAbsolutePath(), //
+                "-categoryDefinition", categoryURI, //
+                "-artifactRepositoryName", //
+                project.getName(), //
+                "-metadataRepositoryName", //
+                project.getName(), //
+                "-rules", //
+                "(&(classifier=osgi.bundle));mvn:${maven.groupId}:${maven.artifactId}:${maven.version}:${maven.extension}:${maven.classifier}");
+        int result = launcher.execute(timeoutInSeconds);
+        for (File file : advices) {
+            file.delete();
+        }
+        bundlesFile.delete();
+        if (result != 0) {
+            throw new MojoFailureException("P2 publisher return code was " + result);
+        }
+        ZipArchiver archiver = new ZipArchiver();
+        File destFile = new File(buildDirectory, "p2-site.zip");
+        archiver.setDestFile(destFile);
+        archiver.addFileSet(new DefaultFileSet(destination));
+        try {
+            archiver.createArchive();
+        } catch (IOException e) {
+            throw new MojoExecutionException("failed to createa archive", e);
+        }
+        projectHelper.attachArtifact(project, "zip", "p2site", destFile);
+    }
+
+    protected void resolve(List<Dependency> dependencies, List<File> bundles, List<File> advices,
+            Set<String> filesAdded) throws MojoExecutionException {
+        for (Dependency dependency : dependencies) {
+            logger.debug("resolving " + dependency.getGroupId() + "::" + dependency.getArtifactId() + "::"
+                    + dependency.getVersion() + "::" + dependency.getClassifier());
+            Artifact artifact = repositorySystem.createArtifactWithClassifier(dependency.getGroupId(),
+                    dependency.getArtifactId(), dependency.getVersion(), dependency.getType(),
+                    dependency.getClassifier());
+            Set<Artifact> artifacts = resolveArtifact(artifact, includeDependencies);
+            for (Artifact resolvedArtifact : artifacts) {
+                logger.debug("    resolved " + resolvedArtifact.getGroupId() + "::" + resolvedArtifact.getArtifactId()
+                        + "::" + resolvedArtifact.getVersion() + "::" + resolvedArtifact.getClassifier());
+                File file = resolvedArtifact.getFile();
+                if (filesAdded.add(file.getAbsolutePath())) {
+                    bundles.add(file);
+                    try {
+                        int cnt = 0;
+                        File p2 = File.createTempFile("p2properties", ".inf");
+                        p2.deleteOnExit();
+                        Properties properties = new Properties();
+                        addProvidesAndProperty(properties, RepositoryLayoutHelper.PROP_GROUP_ID,
+                                resolvedArtifact.getGroupId(), cnt++);
+                        addProvidesAndProperty(properties, RepositoryLayoutHelper.PROP_ARTIFACT_ID,
+                                resolvedArtifact.getArtifactId(), cnt++);
+                        addProvidesAndProperty(properties, RepositoryLayoutHelper.PROP_VERSION,
+                                resolvedArtifact.getVersion(), cnt++);
+                        addProvidesAndProperty(properties, RepositoryLayoutHelper.PROP_EXTENSION,
+                                resolvedArtifact.getType(), cnt++);
+                        addProvidesAndProperty(properties, RepositoryLayoutHelper.PROP_CLASSIFIER,
+                                resolvedArtifact.getClassifier(), cnt++);
+                        addProvidesAndProperty(properties, "maven-scope", resolvedArtifact.getScope(), cnt++);
+                        //TODO pgp.signatures --> getSignatureFile(resolvedArtifact)
+                        properties.store(new FileOutputStream(p2), null);
+                        advices.add(p2);
+                    } catch (IOException e) {
+                        throw new MojoExecutionException("failed to generate p2.inf", e);
+                    }
+                }
+            }
+        }
+    }
+
+    private void addProvidesAndProperty(Properties properties, String key, String value, int i) {
+        //see https://help.eclipse.org/2021-03/index.jsp?topic=%2Forg.eclipse.platform.doc.isv%2Fguide%2Fp2_customizing_metadata.html
+        addProvides(properties, key.replace('-', '.'), value, null, i);
+        addProperty(properties, key, value, i);
+    }
+
+    private void addProvides(Properties properties, String namespace, String name, String version, int i) {
+        if (name != null && !name.isBlank()) {
+            properties.setProperty("provides." + i + ".namespace", namespace);
+            properties.setProperty("provides." + i + ".name", name);
+            if (version != null && !version.isBlank()) {
+                properties.setProperty("provides." + i + ".version", version);
+            }
+        }
+    }
+
+    private void addProperty(Properties properties, String name, String value, int i) {
+        if (value != null && !value.isBlank()) {
+            properties.setProperty("properties." + i + ".name", name);
+            properties.setProperty("properties." + i + ".value", value);
+        }
+
+    }
+
+    protected Set<Artifact> resolveArtifact(Artifact artifact, boolean resolveTransitively) {
+        ArtifactResolutionRequest request = new ArtifactResolutionRequest();
+        request.setArtifact(artifact);
+        request.setOffline(session.isOffline());
+        request.setLocalRepository(session.getLocalRepository());
+        request.setResolveTransitively(resolveTransitively);
+        request.setRemoteRepositories(session.getCurrentProject().getRemoteArtifactRepositories());
+        ArtifactResolutionResult result = repositorySystem.resolve(request);
+        return result.getArtifacts();
+    }
+
+    private File getSignatureFile(Artifact artifact) {
+        final Artifact signatureArtifact = repositorySystem.createArtifactWithClassifier(artifact.getGroupId(),
+                artifact.getArtifactId(), artifact.getVersion(), artifact.getType(), artifact.getClassifier());
+
+        signatureArtifact.setArtifactHandler(new ArtifactHandler() {
+
+            @Override
+            public boolean isIncludesDependencies() {
+                return artifact.getArtifactHandler().isIncludesDependencies();
+            }
+
+            @Override
+            public boolean isAddedToClasspath() {
+                return artifact.getArtifactHandler().isAddedToClasspath();
+            }
+
+            @Override
+            public String getPackaging() {
+                return artifact.getArtifactHandler().getPackaging();
+            }
+
+            @Override
+            public String getLanguage() {
+                return artifact.getArtifactHandler().getLanguage();
+            }
+
+            @Override
+            public String getExtension() {
+                return artifact.getArtifactHandler().getExtension() + ".asc";
+            }
+
+            @Override
+            public String getDirectory() {
+                return artifact.getArtifactHandler().getDirectory();
+            }
+
+            @Override
+            public String getClassifier() {
+                return artifact.getArtifactHandler().getClassifier();
+            }
+        });
+        for (Artifact signature : resolveArtifact(signatureArtifact, false)) {
+            return signature.getFile();
+        }
+        return null;
+    }
+
+}
