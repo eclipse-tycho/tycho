6841107308e79c92d28aee2efca29741384b1566
Christoph Läubrich <laeubi@laeubi-soft.de>
2021-07-03 07:42:00 +0200

    Fix #170 - AllKnownEEsResolutionHints is missing equals/hashcode causing target resolution for every project
    Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>

--- tycho-bundles/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/target/ee/AllKnownEEsResolutionHints.java
+++ tycho-bundles/org.eclipse.tycho.p2.resolver.impl/src/main/java/org/eclipse/tycho/p2/target/ee/AllKnownEEsResolutionHints.java
@@ -16,6 +16,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.metadata.IRequirement;
@@ -60,6 +61,23 @@ public class AllKnownEEsResolutionHints implements ExecutionEnvironmentResolutio
     @Override
     public Collection<IInstallableUnit> getTemporaryAdditions() {
         return temporaryUnits.values();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(temporaryUnits);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AllKnownEEsResolutionHints other = (AllKnownEEsResolutionHints) obj;
+        return Objects.equals(temporaryUnits, other.temporaryUnits);
     }
 
 }
