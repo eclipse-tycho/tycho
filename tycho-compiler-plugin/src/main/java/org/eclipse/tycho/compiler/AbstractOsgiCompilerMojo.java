2839a98639f7627258bfc7a2840c5f24c5b1f03c
Christoph Läubrich <laeubi@laeubi-soft.de>
2021-07-31 07:26:40 +0200

    #37 support M2_REPO Classpath Variable (#207)
    #37 support M2_REPO Classpath Variable

--- tycho-compiler-plugin/src/main/java/org/eclipse/tycho/compiler/AbstractOsgiCompilerMojo.java
+++ tycho-compiler-plugin/src/main/java/org/eclipse/tycho/compiler/AbstractOsgiCompilerMojo.java
@@ -71,6 +71,7 @@ import org.eclipse.tycho.core.BundleProject;
 import org.eclipse.tycho.core.TychoConstants;
 import org.eclipse.tycho.core.TychoProject;
 import org.eclipse.tycho.core.dotClasspath.JREClasspathEntry;
+import org.eclipse.tycho.core.dotClasspath.M2ClasspathVariable;
 import org.eclipse.tycho.core.dotClasspath.ProjectClasspathEntry;
 import org.eclipse.tycho.core.ee.ExecutionEnvironmentUtils;
 import org.eclipse.tycho.core.ee.StandardExecutionEnvironment;
@@ -470,6 +471,19 @@ public abstract class AbstractOsgiCompilerMojo extends AbstractCompilerMojo
         for (ClasspathEntry cpe : getClasspath()) {
             for (File location : cpe.getLocations()) {
                 classpath.add(location.getAbsolutePath() + toString(cpe.getAccessRules()));
+            }
+        }
+        if (session != null) {
+            ArtifactRepository repository = session.getLocalRepository();
+            if (repository != null) {
+                String basedir = repository.getBasedir();
+                Collection<ProjectClasspathEntry> classpathEntries = getEclipsePluginProject().getClasspathEntries();
+                for (ProjectClasspathEntry cpe : classpathEntries) {
+                    if (cpe instanceof M2ClasspathVariable) {
+                        M2ClasspathVariable cpv = (M2ClasspathVariable) cpe;
+                        classpath.add(new File(basedir, cpv.getRepositoryPath()).getAbsolutePath());
+                    }
+                }
             }
         }
         return classpath;
