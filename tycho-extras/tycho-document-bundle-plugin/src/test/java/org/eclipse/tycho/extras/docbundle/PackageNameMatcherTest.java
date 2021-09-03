236fcdb4f952f989ec95ea3f1f350e7b14e3af33
Alexander Kurtakov <akurtako@redhat.com>
2021-09-03 16:12:40 +0300

    Modernize tycho-extras codebase
    * Missing annotations
    * try-with-resources
    * useless throws declarations
    * etc.

--- tycho-extras/tycho-document-bundle-plugin/src/test/java/org/eclipse/tycho/extras/docbundle/PackageNameMatcherTest.java
+++ tycho-extras/tycho-document-bundle-plugin/src/test/java/org/eclipse/tycho/extras/docbundle/PackageNameMatcherTest.java
@@ -23,80 +23,80 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-public class PackageNameMatcherTest {
+class PackageNameMatcherTest {
 
-    @Test
-    public void testNoPattern() {
-        assertNotNull(PackageNameMatcher.compile(new ArrayList<String>()));
-    }
+	@Test
+	void testNoPattern() {
+		assertNotNull(PackageNameMatcher.compile(new ArrayList<String>()));
+	}
 
-    @Test
-    public void testPattern01() {
-        PackageNameMatcher matcher = PackageNameMatcher.compile(asList("com.example"));
-        assertNotNull(matcher);
-        assertTrue(matcher.matches("com.example"));
-        assertFalse(matcher.matches("com.example.child"));
-        assertFalse(matcher.matches("parent.com.example"));
-    }
+	@Test
+	void testPattern01() {
+		PackageNameMatcher matcher = PackageNameMatcher.compile(asList("com.example"));
+		assertNotNull(matcher);
+		assertTrue(matcher.matches("com.example"));
+		assertFalse(matcher.matches("com.example.child"));
+		assertFalse(matcher.matches("parent.com.example"));
+	}
 
-    @Test
-    public void testPattern02() {
-        PackageNameMatcher matcher = PackageNameMatcher.compile(asList("com.example.*"));
-        assertFalse(matcher.matches("com.example"));
-        assertTrue(matcher.matches("com.example.child"));
-        assertTrue(matcher.matches("com.example.child.granchild"));
-        assertFalse(matcher.matches("parent.com.example"));
-    }
+	@Test
+	void testPattern02() {
+		PackageNameMatcher matcher = PackageNameMatcher.compile(asList("com.example.*"));
+		assertFalse(matcher.matches("com.example"));
+		assertTrue(matcher.matches("com.example.child"));
+		assertTrue(matcher.matches("com.example.child.granchild"));
+		assertFalse(matcher.matches("parent.com.example"));
+	}
 
-    @Test
-    public void testPattern03() {
-        PackageNameMatcher matcher = PackageNameMatcher.compile(asList("*.example"));
-        assertTrue(matcher.matches("com.example"));
-        assertTrue(matcher.matches("org.example"));
-        assertTrue(matcher.matches("org.internal.example"));
-        assertFalse(matcher.matches("com.example.child"));
-    }
+	@Test
+	void testPattern03() {
+		PackageNameMatcher matcher = PackageNameMatcher.compile(asList("*.example"));
+		assertTrue(matcher.matches("com.example"));
+		assertTrue(matcher.matches("org.example"));
+		assertTrue(matcher.matches("org.internal.example"));
+		assertFalse(matcher.matches("com.example.child"));
+	}
 
-    @Test
-    public void testPattern04() {
-        PackageNameMatcher matcher = PackageNameMatcher.compile(asList("*.example*"));
-        assertTrue(matcher.matches("com.example"));
-        assertTrue(matcher.matches("org.example.pkg"));
-        assertTrue(matcher.matches("org.internal.examples"));
-        assertFalse(matcher.matches("example.child"));
-    }
+	@Test
+	void testPattern04() {
+		PackageNameMatcher matcher = PackageNameMatcher.compile(asList("*.example*"));
+		assertTrue(matcher.matches("com.example"));
+		assertTrue(matcher.matches("org.example.pkg"));
+		assertTrue(matcher.matches("org.internal.examples"));
+		assertFalse(matcher.matches("example.child"));
+	}
 
-    @Test
-    public void testPattern05() {
-        final List<String> specs = List.of("com.example.*", "org.example.*");
-        PackageNameMatcher matcher = PackageNameMatcher.compile(specs);
-        assertFalse(matcher.matches("com.example"));
-        assertTrue(matcher.matches("com.example.pkg"));
-        assertTrue(matcher.matches("com.example.pkg.sub"));
-        assertFalse(matcher.matches("org.example"));
-        assertTrue(matcher.matches("org.example.pkg"));
-        assertTrue(matcher.matches("org.example.pkg.sub"));
-    }
+	@Test
+	void testPattern05() {
+		final List<String> specs = List.of("com.example.*", "org.example.*");
+		PackageNameMatcher matcher = PackageNameMatcher.compile(specs);
+		assertFalse(matcher.matches("com.example"));
+		assertTrue(matcher.matches("com.example.pkg"));
+		assertTrue(matcher.matches("com.example.pkg.sub"));
+		assertFalse(matcher.matches("org.example"));
+		assertTrue(matcher.matches("org.example.pkg"));
+		assertTrue(matcher.matches("org.example.pkg.sub"));
+	}
 
-    @Test
-    public void testPattern06() {
-        assertThrows(IllegalArgumentException.class, () -> PackageNameMatcher.compile(asList("com.example.!")));
-    }
+	@Test
+	void testPattern06() {
+		assertThrows(IllegalArgumentException.class, () -> PackageNameMatcher.compile(asList("com.example.!")));
+	}
 
-    @Test
-    public void testPattern07() {
-        final List<String> specs = List.of("com.example.*", "org.example.!");
-        assertThrows(IllegalArgumentException.class, () -> PackageNameMatcher.compile(specs));
-    }
+	@Test
+	void testPattern07() {
+		final List<String> specs = List.of("com.example.*", "org.example.!");
+		assertThrows(IllegalArgumentException.class, () -> PackageNameMatcher.compile(specs));
+	}
 
-    @Test
-    public void testSpacesPattern01() {
-        assertThrows(IllegalArgumentException.class, () -> PackageNameMatcher.compile(asList("  ")));
-    }
+	@Test
+	void testSpacesPattern01() {
+		assertThrows(IllegalArgumentException.class, () -> PackageNameMatcher.compile(asList("  ")));
+	}
 
-    @Test
-    public void testSpacesPattern02() {
-        assertThrows(IllegalArgumentException.class, () -> PackageNameMatcher.compile(asList("\t\t")));
-    }
+	@Test
+	void testSpacesPattern02() {
+		assertThrows(IllegalArgumentException.class, () -> PackageNameMatcher.compile(asList("\t\t")));
+	}
 
 }
