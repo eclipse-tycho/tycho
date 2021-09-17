e0f17213a686457f55252f019697e14c29ed722d
Mickael Istria <mistria@redhat.com>
2021-09-17 22:48:55 +0200

    Directly publish EE units from Tycho, bypassing JREAction
    JREAction can easily miss some context that Tycho has access to (eg
    toolchains) and generate extra units that aren't necessary in Tycho; so
    we replace it by a local action that generates the exact same EE units
    that are used for dependency/target-platform resolution.

---
+++ tycho-bundles/org.eclipse.tycho.p2.tools.impl/src/main/java/org/eclipse/tycho/p2/tools/publisher/ExecutionEnvironmentAction.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Red Hat Inc. and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.tycho.p2.tools.publisher;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.equinox.p2.publisher.IPublisherAction;
+import org.eclipse.equinox.p2.publisher.IPublisherInfo;
+import org.eclipse.equinox.p2.publisher.IPublisherResult;
+import org.eclipse.tycho.core.ee.shared.ExecutionEnvironment;
+import org.eclipse.tycho.p2.target.ee.StandardEEResolutionHints;
+import org.eclipse.tycho.p2.util.resolution.ExecutionEnvironmentResolutionHints;
+
+public class ExecutionEnvironmentAction implements IPublisherAction {
+
+    private ExecutionEnvironmentResolutionHints ee;
+
+    public ExecutionEnvironmentAction(ExecutionEnvironment ee) {
+        this.ee = new StandardEEResolutionHints(ee);
+    }
+
+    @Override
+    public IStatus perform(IPublisherInfo info, IPublisherResult results, IProgressMonitor monitor) {
+        results.addIUs(ee.getMandatoryUnits(), IPublisherResult.NON_ROOT);
+        return Status.OK_STATUS;
+    }
+
+}
