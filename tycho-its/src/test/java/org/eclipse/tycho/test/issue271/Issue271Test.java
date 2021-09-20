c3d1f2f6eeba5b04b9a5bafea01cc6d9af62d8f6
András Péteri <apeteri@b2international.com>
2021-09-20 13:09:49 +0200

    Add test case for #271 (#279)

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/issue271/Issue271Test.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Sonatype Inc. and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Sonatype Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.test.issue271;
+
+import java.util.List;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class Issue271Test extends AbstractTychoIntegrationTest {
+
+	@Test
+	public void test() throws Exception {
+		Verifier verifier = getVerifier("issue271");
+
+		verifier.executeGoals(List.of("clean", "verify"));
+		verifier.verifyErrorFreeLog();
+	}
+}
