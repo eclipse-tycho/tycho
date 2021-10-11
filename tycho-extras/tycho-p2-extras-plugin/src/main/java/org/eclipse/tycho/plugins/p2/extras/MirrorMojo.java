d0aec22bdf5eb9f9b4de9af91332a07faed3fec9
Alexander Kurtakov <akurtako@redhat.com>
2021-10-11 12:53:16 +0300

    Do not mirror pack200 artifacts by default

--- tycho-extras/tycho-p2-extras-plugin/src/main/java/org/eclipse/tycho/plugins/p2/extras/MirrorMojo.java
+++ tycho-extras/tycho-p2-extras-plugin/src/main/java/org/eclipse/tycho/plugins/p2/extras/MirrorMojo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2020 SAP AG and others.
+ * Copyright (c) 2011, 2021 SAP AG and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -113,7 +113,7 @@ public class MirrorMojo extends AbstractMojo {
     /**
      * Whether or not to include pack200 artifacts.
      */
-    @Parameter(defaultValue = "true")
+    @Parameter(defaultValue = "false")
     private boolean includePacked;
 
     /**
