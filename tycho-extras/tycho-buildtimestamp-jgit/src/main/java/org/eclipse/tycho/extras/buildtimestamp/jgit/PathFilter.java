d9f3a02837aa257cd71b8a45a0a83c6c7787e765
Christoph Läubrich <laeubi@laeubi-soft.de>
2022-02-02 09:25:29 +0100

    Fix #613 - Add default excludes to JGitTimestampProvider
    Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>

--- tycho-extras/tycho-buildtimestamp-jgit/src/main/java/org/eclipse/tycho/extras/buildtimestamp/jgit/PathFilter.java
+++ tycho-extras/tycho-buildtimestamp-jgit/src/main/java/org/eclipse/tycho/extras/buildtimestamp/jgit/PathFilter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Sonatype Inc. and others.
+ * Copyright (c) 2012, 2022 Sonatype Inc. and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *    Sonatype Inc. - initial API and implementation
+ *    Christoph Läubrich - Add default excludes to JGitTimestampProvider #613 
  *******************************************************************************/
 package org.eclipse.tycho.extras.buildtimestamp.jgit;
 
@@ -27,55 +28,59 @@ import org.eclipse.jgit.treewalk.filter.TreeFilter;
 
 public class PathFilter extends TreeFilter {
 
-    private final byte[] basedir;
+	private final byte[] basedir;
 
-    private final List<FastIgnoreRule> rules;
+	private final List<FastIgnoreRule> rules;
 
-    public PathFilter(String basedir, String filters) {
-        this.basedir = Constants.encode(basedir);
+	public PathFilter(String basedir, String filters) {
+		this.basedir = Constants.encode(basedir);
 
-        if (filters != null) {
-            StringTokenizer st = new StringTokenizer(filters, "\n\r\f", false);
-            List<FastIgnoreRule> rules = new ArrayList<>();
-            while (st.hasMoreTokens()) {
-                String trimmed = st.nextToken().trim();
-                if (!trimmed.isEmpty()) {
-                    rules.add(new FastIgnoreRule(trimmed));
-                }
-            }
-            this.rules = Collections.unmodifiableList(rules);
-        } else {
-            this.rules = null;
-        }
-    }
+		List<FastIgnoreRule> rules = new ArrayList<>();
+		// the consumer pom generated ba the UpdateConsumerPomMojo
+		rules.add(new FastIgnoreRule(".tycho-consumer-pom.xml"));
+		// polyglot files generated during pomless builds
+		rules.add(new FastIgnoreRule(".polyglot.*"));
+		// connector poms generated during pomless builds
+		rules.add(new FastIgnoreRule("pom.tycho"));
+		if (filters != null) {
+			StringTokenizer st = new StringTokenizer(filters, "\n\r\f", false);
+			while (st.hasMoreTokens()) {
+				String trimmed = st.nextToken().trim();
+				if (!trimmed.isEmpty()) {
+					rules.add(new FastIgnoreRule(trimmed));
+				}
+			}
+		}
+		this.rules = Collections.unmodifiableList(rules);
+	}
 
-    @Override
-    public boolean include(TreeWalk tw) throws MissingObjectException, IncorrectObjectTypeException, IOException {
-        if (tw.isPathPrefix(basedir, basedir.length) != 0) {
-            // not under basedir, not interesting
-            return false;
-        }
+	@Override
+	public boolean include(TreeWalk tw) throws MissingObjectException, IncorrectObjectTypeException, IOException {
+		if (tw.isPathPrefix(basedir, basedir.length) != 0) {
+			// not under basedir, not interesting
+			return false;
+		}
 
-        if (!tw.isSubtree() && rules != null) {
-            String path = tw.getPathString();
-            for (FastIgnoreRule rule : rules) {
-                if (rule.isMatch(path, tw.isSubtree())) {
-                    return !rule.getResult();
-                }
-            }
-        }
+		if (!tw.isSubtree()) {
+			String path = tw.getPathString();
+			for (FastIgnoreRule rule : rules) {
+				if (rule.isMatch(path, tw.isSubtree())) {
+					return !rule.getResult();
+				}
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public boolean shouldBeRecursive() {
-        return true;
-    }
+	@Override
+	public boolean shouldBeRecursive() {
+		return true;
+	}
 
-    @Override
-    public TreeFilter clone() {
-        return this;
-    }
+	@Override
+	public TreeFilter clone() {
+		return this;
+	}
 
 }
