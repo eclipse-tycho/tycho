a1257f5d9a659768fcd16b6771892203844ea2ca
Johannes Bechberger <johannes.bechberger@sap.com>
2022-02-08 15:43:45 +0100

    Improve compiler test projects

--- tycho-its/projects/compiler.extraExports.java17/src/main/java/Foo.java
+++ tycho-its/projects/compiler.extraExports.java17/src/main/java/Foo.java
@@ -2,5 +2,5 @@ import com.sun.imageio.plugins.png.PNGImageReader;
 import com.sun.imageio.plugins.png.PNGImageReaderSpi;
 
 public class Foo {
-		private String unused;
+		private PNGImageReader reader = new PNGImageReader(new PNGImageReaderSpi());
 }
---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/compiler/CompilerExtraExportsTest.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat Inc. and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.tycho.test.compiler;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class CompilerExtraExportsTest extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void testExtraExports() throws Exception {
+        Verifier verifier = getVerifier("compiler.extraExports", false);
+        verifier.executeGoal("compile");
+    }
+
+}
