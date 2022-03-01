72d227ce2afbc7389b25e87813f611a3279f8875
Mickael Istria <mistria@redhat.com>
2022-03-01 17:54:17 +0100

    Issue #707 - fix logic detecting custom vs none EE profile
    Fix some methods and conditions to better distinguish cases of ee=none
    vs custom EE.
    This allows further invocations of
    TargetPlatformFactoryImpl.createTargetPlatform to work with ee=none.

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/compare/CompareWithBaselineTest.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Red Hat, Inc. and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.tycho.test.compare;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class CompareWithBaselineTest extends AbstractTychoIntegrationTest {
+
+	@Test
+	public void testEENone() throws Exception {
+		Verifier verifier = getVerifier("eeProfile.none/feature", false);
+		verifier.executeGoal("verify");
+		verifier.verifyErrorFreeLog();
+	}
+}
