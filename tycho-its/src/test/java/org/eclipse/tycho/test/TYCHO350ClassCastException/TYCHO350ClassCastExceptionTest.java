2bbd954c71ca0a0d31f5429d6c1fe1325b056e16
Jonah Graham <jonah@kichwacoders.com>
2022-03-30 12:25:51 -0400

    issue #350 reproducer
    A ClassCastException is thrown when tycho-p2-repository-plugin is run
    
    Change-Id: I1e1d69c2e4b4d7f59947a6ab003eaa10502ba44a

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/TYCHO350ClassCastException/TYCHO350ClassCastExceptionTest.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Kichwa Coders Canada Inc. and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.tycho.test.TYCHO350ClassCastException;
+
+import static java.util.Arrays.asList;
+
+import java.io.File;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class TYCHO350ClassCastExceptionTest extends AbstractTychoIntegrationTest {
+    @Test
+    public void test() throws Exception {
+        Verifier verifier = getVerifier("/TYCHO350ClassCastException", false);
+        verifier.executeGoals(asList("verify"));
+        verifier.verifyErrorFreeLog();
+    }
+
+}
