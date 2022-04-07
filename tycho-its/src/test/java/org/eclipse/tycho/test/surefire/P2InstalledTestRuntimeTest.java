2fd6c4d9f470cbb1c1e60d4a7000c7bee7284d52
Alexander Kurtakov <akurtako@redhat.com>
2022-04-07 13:19:37 +0300

    Speed up P2InstalledRuntimeTest.
    It's the slowest test in our builds as of today. These changes combined
    cut 20+ sec on my machine (from almost 80 sec to a bit under 60).

--- tycho-its/src/test/java/org/eclipse/tycho/test/surefire/P2InstalledTestRuntimeTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/surefire/P2InstalledTestRuntimeTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2018 Red Hat Inc. and others
+ * Copyright (c) 2013, 2021 Red Hat Inc. and others
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -12,8 +12,6 @@
  *******************************************************************************/
 package org.eclipse.tycho.test.surefire;
 
-import static java.util.Arrays.asList;
-
 import java.util.List;
 
 import org.apache.maven.it.Verifier;
@@ -24,42 +22,39 @@ import org.junit.Test;
 
 public class P2InstalledTestRuntimeTest extends AbstractTychoIntegrationTest {
 
-    @Test
-    public void testProvisionAppAndRunTest() throws Exception {
-        Verifier verifier = getVerifier("surefire.p2InstalledRuntime", false);
-        List<String> options = verifier.getCliOptions();
-        options.add("-Dp2.repo.url=" + ResourceUtil.P2Repositories.ECLIPSE_OXYGEN.toString());
-        options.add("-PprovisionProduct");
-        verifier.executeGoals(asList("clean", "integration-test"));
-        verifier.verifyErrorFreeLog();
-    }
+	@Test
+	public void testProvisionAppAndRunTest() throws Exception {
+		Verifier verifier = getVerifier("surefire.p2InstalledRuntime", false);
+		verifier.addCliOption("-Dp2.repo.url=" + ResourceUtil.P2Repositories.ECLIPSE_LATEST.toString());
+		verifier.addCliOption("-PprovisionProduct");
+		verifier.executeGoals(List.of("clean", "integration-test"));
+		verifier.verifyErrorFreeLog();
+	}
 
-    @Test
-    public void testRunTestOnProvisionedApp() throws Exception {
-        Verifier verifier = getVerifier("surefire.p2InstalledRuntime", false);
-        List<String> options = verifier.getCliOptions();
-        options.add("-Dp2.repo.url=" + ResourceUtil.P2Repositories.ECLIPSE_OXYGEN.toString());
-        options.add("-PuseProvisionedProduct");
-        options.add("-DproductClassifier=" + getProductClassifier());
-        verifier.executeGoals(asList("clean", "integration-test"));
-        verifier.verifyErrorFreeLog();
-    }
+	@Test
+	public void testRunTestOnProvisionedApp() throws Exception {
+		Verifier verifier = getVerifier("surefire.p2InstalledRuntime", false);
+		verifier.addCliOption("-Dp2.repo.url=" + ResourceUtil.P2Repositories.ECLIPSE_LATEST.toString());
+		verifier.addCliOption("-PuseProvisionedProduct");
+		verifier.addCliOption("-DproductClassifier=" + getProductClassifier());
+		verifier.executeGoals(List.of("clean", "integration-test"));
+		verifier.verifyErrorFreeLog();
+	}
 
-    @Test
-    public void testDifferentHarnessVersions() throws Exception {
-        Verifier verifier = getVerifier("surefire.p2InstalledRuntime", false);
-        List<String> options = verifier.getCliOptions();
-        options.add("-Dp2.repo.url=" + ResourceUtil.P2Repositories.ECLIPSE_OXYGEN.toString());
-        // Use different TP for test bundle and product under test
-        options.add("-Dother.p2.repo.url=" + ResourceUtil.P2Repositories.ECLIPSE_LATEST.toString());
-        options.add("-PuseProvisionedProduct");
-        options.add("-DproductClassifier=" + getProductClassifier());
-        verifier.executeGoals(asList("clean", "integration-test"));
-        verifier.verifyErrorFreeLog();
-    }
+	@Test
+	public void testDifferentHarnessVersions() throws Exception {
+		Verifier verifier = getVerifier("surefire.p2InstalledRuntime", false);
+		verifier.addCliOption("-Dp2.repo.url=" + ResourceUtil.P2Repositories.ECLIPSE_OXYGEN.toString());
+		// Use different TP for test bundle and product under test
+		verifier.addCliOption("-Dother.p2.repo.url=" + ResourceUtil.P2Repositories.ECLIPSE_LATEST.toString());
+		verifier.addCliOption("-PuseProvisionedProduct");
+		verifier.addCliOption("-DproductClassifier=" + getProductClassifier());
+		verifier.executeGoals(List.of("clean", "integration-test"));
+		verifier.verifyErrorFreeLog();
+	}
 
-    private static String getProductClassifier() {
-        TargetEnvironment currentEnv = TargetEnvironment.getRunningEnvironment();
-        return currentEnv.getOs() + "." + currentEnv.getWs() + "." + currentEnv.getArch();
-    }
+	private static String getProductClassifier() {
+		TargetEnvironment currentEnv = TargetEnvironment.getRunningEnvironment();
+		return String.join(".", currentEnv.getOs(), currentEnv.getWs(), currentEnv.getArch());
+	}
 }
