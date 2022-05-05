4819f9008bbf273f98e20576506bc39cd8e6007e
Christian Dietrich <christian.dietrich@itemis.de>
2022-05-05 08:53:11 +0200

    [eclipse/tycho#937] reproducer for 'does not have an expanded version' issue
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/tycho937/Tycho937Test.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2022 itemis AG and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     itemis AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.test.tycho937;
+
+import java.io.File;
+import java.util.List;
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Tycho937Test extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void testCompilerSourceTargetConfigurationViaManifest() throws Exception {
+        Verifier verifier = getVerifier("tycho937", false);
+        verifier.executeGoals(List.of("clean", "javadoc:aggregate-jar", "verify"));
+        verifier.verifyErrorFreeLog();
+    }
+
+}
