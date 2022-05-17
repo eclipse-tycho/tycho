cc1a58bfd910f51587724f2fd171cbd11ef8ff20
Alexander Kurtakov <akurtako@redhat.com>
2022-05-17 22:05:30 +0300

    Immediately return value.

--- tycho-core/src/main/java/org/eclipse/tycho/core/osgitools/targetplatform/LocalDependencyResolver.java
+++ tycho-core/src/main/java/org/eclipse/tycho/core/osgitools/targetplatform/LocalDependencyResolver.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2021 Sonatype Inc. and others.
+ * Copyright (c) 2008, 2022 Sonatype Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -252,8 +252,7 @@ public class LocalDependencyResolver extends AbstractLogEnabled implements Depen
 
     public ArtifactKey getArtifactKey(File plugin) {
         OsgiManifest mf = manifestReader.loadManifest(plugin);
-        ArtifactKey key = mf.toArtifactKey();
-        return key;
+        return mf.toArtifactKey();
     }
 
     public void setLocation(File location) {
