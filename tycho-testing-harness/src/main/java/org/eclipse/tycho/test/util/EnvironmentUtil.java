e6a4ecd39029c6b233f9890c7556106e3149ea94
Alexander Kurtakov <akurtako@redhat.com>
2022-06-18 18:13:52 +0300

    Run tests against 2022-06

--- tycho-testing-harness/src/main/java/org/eclipse/tycho/test/util/EnvironmentUtil.java
+++ tycho-testing-harness/src/main/java/org/eclipse/tycho/test/util/EnvironmentUtil.java
@@ -26,7 +26,7 @@ import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
  */
 public class EnvironmentUtil {
 
-    public static final String ECLIPSE_LATEST = "https:////download.eclipse.org/releases/2022-03/";
+    public static final String ECLIPSE_LATEST = "https:////download.eclipse.org/releases/2022-06/";
 
     private static final Properties props;
 
