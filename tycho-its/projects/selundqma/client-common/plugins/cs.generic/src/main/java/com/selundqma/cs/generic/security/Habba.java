170483e135427df53988da3ea5c6689eecfb1776
Mats Lundquist <Mats.Lundquist@sony.com>
2022-06-21 10:44:37 +0200

    Cannot build sub module separately with 2.7.3

---
+++ tycho-its/projects/selundqma/client-common/plugins/cs.generic/src/main/java/com/selundqma/cs/generic/security/Habba.java
@@ -0,0 +1,15 @@
+package com.selundqma.cs.generic.security;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Habba
+{
+   public Habba(File pKeyFile)
+   {
+		pKeyFile.toString();
+   }
+}
