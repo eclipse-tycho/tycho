710c2faaf7a4d55e8ddcd757950d2b25c15d686e
selundqma <102162435+selundqma@users.noreply.github.com>
2022-06-27 13:10:33 +0200

    Add a testcase for "Cannot build sub module separately with 2.7.3"

---
+++ tycho-its/projects/selundqma/clients/Analytics/plugins/selundqma.ub.ga.impl/src/main/java/com/selundqma/ub/ga/impl/Activator.java
@@ -0,0 +1,19 @@
+package com.selundqma.ub.ga.impl;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator
+{
+   @Override
+   public void start(final BundleContext pContext) throws Exception
+   {
+
+   }
+
+   @Override
+   public void stop(final BundleContext pContext) throws Exception
+   {
+   }
+
+}
---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/selundqma/SeLundqmaTest.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2021 Sonatype Inc. and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Sonatype Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.test.SeLundqma;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class SeLundqmaTest extends AbstractTychoIntegrationTest {
+
+	@Test
+	public void test() throws Exception {
+		final boolean ignoreLocallyInstalledArtifacts = false;
+
+		// build01
+		Verifier v01 = getVerifier("selundqma", true, ignoreLocallyInstalledArtifacts);
+		v01.executeGoal("install");
+		v01.verifyErrorFreeLog();
+
+		// build02
+		Verifier v02 = getVerifier("selundqma/clients", true, ignoreLocallyInstalledArtifacts);
+		v02.executeGoal("install");
+		v02.verifyErrorFreeLog();
+	}
+
+}
