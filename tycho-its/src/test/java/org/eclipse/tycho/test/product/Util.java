9bf6666beaa53115cbbeff20467597c6b6b642ea
Александър Куртаков <akurtakov@gmail.com>
2022-07-11 07:29:24 +0300

    Use try-with-resources

--- tycho-its/src/test/java/org/eclipse/tycho/test/product/Util.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/product/Util.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 SAP AG and others.
+ * Copyright (c) 2010, 2022 SAP AG and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -31,86 +31,83 @@ import de.pdark.decentxml.XMLParser;
  */
 @Deprecated
 class Util {
-    public static Document openXmlFromZip(File zipFile, String xmlFile) throws IOException, ZipException {
-        XMLParser parser = new XMLParser();
-        ZipFile zip = new ZipFile(zipFile);
-        try {
-            ZipEntry contentXmlEntry = zip.getEntry(xmlFile);
-            InputStream entryStream = zip.getInputStream(contentXmlEntry);
-            return parser.parse(new XMLIOSource(entryStream));
-        } finally {
-            zip.close();
-        }
-    }
+	public static Document openXmlFromZip(File zipFile, String xmlFile) throws IOException, ZipException {
+		XMLParser parser = new XMLParser();
+		try (ZipFile zip = new ZipFile(zipFile)) {
+			ZipEntry contentXmlEntry = zip.getEntry(xmlFile);
+			InputStream entryStream = zip.getInputStream(contentXmlEntry);
+			return parser.parse(new XMLIOSource(entryStream));
+		}
+	}
 
-    static public boolean containsIUWithProperty(Document contentXML, String iuId, String propName, String propValue) {
-        Set<Element> ius = findIU(contentXML, iuId);
-        for (Element unitElement : ius) {
-            if (iuHasProperty(unitElement, propName, propValue))
-                return true;
-        }
-        return false;
-    }
+	static public boolean containsIUWithProperty(Document contentXML, String iuId, String propName, String propValue) {
+		Set<Element> ius = findIU(contentXML, iuId);
+		for (Element unitElement : ius) {
+			if (iuHasProperty(unitElement, propName, propValue))
+				return true;
+		}
+		return false;
+	}
 
-    static public Set<Element> findIU(Document contentXML, String iuId) {
-        Set<Element> foundIUs = new HashSet<>();
+	static public Set<Element> findIU(Document contentXML, String iuId) {
+		Set<Element> foundIUs = new HashSet<>();
 
-        Element repository = contentXML.getRootElement();
-        for (Element unit : repository.getChild("units").getChildren("unit")) {
-            if (iuId.equals(unit.getAttributeValue("id"))) {
-                foundIUs.add(unit);
-            }
-        }
-        return foundIUs;
-    }
+		Element repository = contentXML.getRootElement();
+		for (Element unit : repository.getChild("units").getChildren("unit")) {
+			if (iuId.equals(unit.getAttributeValue("id"))) {
+				foundIUs.add(unit);
+			}
+		}
+		return foundIUs;
+	}
 
-    static public boolean iuHasProperty(Element unit, String propName, String propValue) {
-        boolean foundIU = false;
+	static public boolean iuHasProperty(Element unit, String propName, String propValue) {
+		boolean foundIU = false;
 
-        if (propName != null) {
-            for (Element property : unit.getChild("properties").getChildren("property")) {
-                if (propName.equals(property.getAttributeValue("name"))
-                        && propValue.equals((property.getAttributeValue("value")))) {
-                    foundIU = true;
-                    break;
-                }
-            }
-        } else {
-            foundIU = true;
-        }
-        return foundIU;
-    }
+		if (propName != null) {
+			for (Element property : unit.getChild("properties").getChildren("property")) {
+				if (propName.equals(property.getAttributeValue("name"))
+						&& propValue.equals((property.getAttributeValue("value")))) {
+					foundIU = true;
+					break;
+				}
+			}
+		} else {
+			foundIU = true;
+		}
+		return foundIU;
+	}
 
-    static public boolean iuHasAllRequirements(Element unit, String... requiredIus) {
-        boolean hasAllRequirements = true;
-        for (String requiredIu : requiredIus) {
-            boolean foundIU = false;
-            for (Element property : unit.getChild("requires").getChildren("required")) {
-                if (requiredIu.equals(property.getAttributeValue("name"))) {
-                    foundIU = true;
-                    break;
-                }
-            }
-            if (!foundIU) {
-                hasAllRequirements = false;
-                break;
-            }
-        }
-        return hasAllRequirements;
-    }
+	static public boolean iuHasAllRequirements(Element unit, String... requiredIus) {
+		boolean hasAllRequirements = true;
+		for (String requiredIu : requiredIus) {
+			boolean foundIU = false;
+			for (Element property : unit.getChild("requires").getChildren("required")) {
+				if (requiredIu.equals(property.getAttributeValue("name"))) {
+					foundIU = true;
+					break;
+				}
+			}
+			if (!foundIU) {
+				hasAllRequirements = false;
+				break;
+			}
+		}
+		return hasAllRequirements;
+	}
 
-    static public boolean iuHasTouchpointDataInstruction(Element unit, String instructionTrimmedText) {
-        Element touchpointDataElem = unit.getChild("touchpointData");
+	static public boolean iuHasTouchpointDataInstruction(Element unit, String instructionTrimmedText) {
+		Element touchpointDataElem = unit.getChild("touchpointData");
 
-        if (touchpointDataElem != null) {
-            for (Element instructions : touchpointDataElem.getChildren("instructions")) {
-                for (Element instruction : instructions.getChildren("instruction")) {
-                    if (instructionTrimmedText.equals(instruction.getTrimmedText())) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
+		if (touchpointDataElem != null) {
+			for (Element instructions : touchpointDataElem.getChildren("instructions")) {
+				for (Element instruction : instructions.getChildren("instruction")) {
+					if (instructionTrimmedText.equals(instruction.getTrimmedText())) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
