94168f5f8428045fbb2e2a3f4acfee6cc0034544
Patrick Ziegler <Patrick.Ziegler@dsa.de>
2022-07-12 08:42:41 +0200

    Reproducer for issue #1124

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/resolver/CycleTest.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Patrick Ziegler and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Patrick Ziegler - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.test.resolver;
+
+import java.util.List;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class CycleTest extends AbstractTychoIntegrationTest {
+
+	@Test
+	public void testDependencyCycle() throws Exception {
+		Verifier verifier = getVerifier("/resolver.cycles");
+		verifier.executeGoals(List.of("clean", "verify"));
+		verifier.verifyErrorFreeLog();
+	}
+
+}
