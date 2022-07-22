1af5c5bfbb89ffde04ccbb262636d7b762b36fda
Christoph Läubrich <laeubi@laeubi-soft.de>
2022-07-22 11:38:19 +0200

    Improve sisu-connect for usage by multiple relams
    - Export realms into the framework and core-exports as well
    - Use the realm itself as a classloader for the bundles(s)
    - Support for importing foreign frameworks into the current realm
    - improved debugging

--- sisu-osgi/sisu-osgi-connect/src/main/java/org/eclipse/sisu/osgi/connect/PlexusFrameworkUtilHelper.java
+++ sisu-osgi/sisu-osgi-connect/src/main/java/org/eclipse/sisu/osgi/connect/PlexusFrameworkUtilHelper.java
@@ -12,11 +12,16 @@
  *******************************************************************************/
 package org.eclipse.sisu.osgi.connect;
 
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
 import java.util.Optional;
 
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.connect.FrameworkUtilHelper;
-import org.osgi.framework.launch.Framework;
 
 /**
  * Implementation of the {@link FrameworkUtilHelper} to connect classes to
@@ -26,23 +31,66 @@ public class PlexusFrameworkUtilHelper implements FrameworkUtilHelper {
 
 	@Override
 	public Optional<Bundle> getBundle(Class<?> classFromBundle) {
-		PlexusFrameworkConnectServiceFactory factory = PlexusFrameworkConnectServiceFactory.instance;
-		if (factory != null) {
-			Framework framework = factory.frameworkMap.get(classFromBundle.getClassLoader());
-			if (framework != null) {
-				String location = classFromBundle.getProtectionDomain().getCodeSource().getLocation().toString();
-				for (Bundle bundle : framework.getBundleContext().getBundles()) {
+		ClassLoader loader = classFromBundle.getClassLoader();
+		if (loader instanceof ClassRealm) {
+			@SuppressWarnings("resource")
+			ClassRealm realm = (ClassRealm) loader;
+			Class<?> thisHelper = PlexusFrameworkUtilHelper.class;
+			Class<?> realmHelper = realm.loadClassFromSelf(thisHelper.getName());
+			if (realmHelper != null && thisHelper != realmHelper) {
+				try {
+					Object object = realmHelper.getConstructor().newInstance();
+					if (object instanceof FrameworkUtilHelper) {
+						FrameworkUtilHelper helper = (FrameworkUtilHelper) object;
+						Optional<Bundle> bundle = helper.getBundle(classFromBundle);
+						return bundle;
+					}
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+				}
+			}
+		}
+		String location = getLocationFromClass(classFromBundle);
+		if (location != null) {
+			ClassLoader classLoader = classFromBundle.getClassLoader();
+			PlexusConnectFramework connect = PlexusFrameworkConnectServiceFactory.frameworkMap.get(classLoader);
+			if (connect != null) {
+				connect.debug(" Use framework" + connect.getName());
+				BundleContext bundleContext = connect.getFramework().getBundleContext();
+				Bundle[] bundles = bundleContext.getBundles();
+				for (Bundle bundle : bundles) {
 					String bundleLocation = bundle.getLocation();
 					if (locationsMatch(location, bundleLocation)) {
+						connect.debug("Return bundle " + bundle.getSymbolicName() + " for location " + location);
 						return Optional.of(bundle);
 					}
 				}
+				if (classLoader == BundleContext.class.getClassLoader()) {
+					return Optional.of(bundleContext.getBundle(0));
+				}
+				connect.debug("No bundle matched " + location);
 			}
 		}
 		return Optional.empty();
 	}
 
-	private static boolean locationsMatch(String classLocation, String bundleLocation) {
+	static String getLocationFromClass(Class<?> classFromBundle) {
+		ProtectionDomain domain = classFromBundle.getProtectionDomain();
+		if (domain == null) {
+			return null;
+		}
+		CodeSource codeSource = domain.getCodeSource();
+		if (codeSource == null) {
+			return null;
+		}
+		URL url = codeSource.getLocation();
+		if (url == null) {
+			return null;
+		}
+		return url.toString();
+	}
+
+	static boolean locationsMatch(String classLocation, String bundleLocation) {
 		return classLocation.endsWith(bundleLocation);
 	}
 
