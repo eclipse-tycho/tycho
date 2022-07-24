9461f65756b1711379fde3ad39309bbeb866f261
Christoph Läubrich <laeubi@laeubi-soft.de>
2022-07-24 08:44:49 +0200

    Some improvements in service handling
    Currently there are some places where service handling is not aware of
    dynamics and/or missing services and simply run into NPE, also the Tycho
    packages are not exported with a version.

--- tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/p2/maven/repository/Activator.java
+++ tycho-bundles/org.eclipse.tycho.p2.maven.repository/src/main/java/org/eclipse/tycho/p2/maven/repository/Activator.java
@@ -13,26 +13,20 @@
 package org.eclipse.tycho.p2.maven.repository;
 
 import org.eclipse.equinox.p2.core.IProvisioningAgent;
-import org.eclipse.equinox.p2.core.IProvisioningAgentProvider;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
 
 public class Activator implements BundleActivator {
     private static BundleContext context;
 
-    private static IProvisioningAgent agent;
+    private static ServiceTracker<IProvisioningAgent, IProvisioningAgent> serviceTracker;
 
     @Override
     public void start(BundleContext context) throws Exception {
         Activator.context = context;
-
-        ServiceReference<IProvisioningAgentProvider> providerRef = context
-                .getServiceReference(IProvisioningAgentProvider.class);
-        IProvisioningAgentProvider provider = context.getService(providerRef);
-        // TODO this doesn't return the running agent; is this intended?
-        agent = provider.createAgent(null); // null == currently running system
-        context.ungetService(providerRef);
+        serviceTracker = new ServiceTracker<>(context, IProvisioningAgent.class, null);
+        serviceTracker.open();
     }
 
     @Override
@@ -46,6 +40,10 @@ public class Activator implements BundleActivator {
 
     // TODO repositories should not make assumptions on the agent they are loaded by (see callers)
     public static IProvisioningAgent getProvisioningAgent() {
-        return agent;
+        if (serviceTracker == null) {
+            return null;
+        }
+        IProvisioningAgent service = serviceTracker.getService();
+        return service;
     }
 }
