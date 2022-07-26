dea1df86c939f39c648b779b308a9bf1fbbddd14
Holger Voormann <eclipse@voormann.de>
2022-07-26 08:48:59 +0200

    Reproducer for issue 1093: misleading error message
    Plugin without Java code with a "build.properties" file containing:
    bin.includes = META-INF/,\
                   non-existing-file.txt
    
    Actual:
    Build fails with the error message
    ".../target/classes does not exist"
    
    Expected:
    Build fails with an error message like
    ".../non-existing-file.txt does not exist"

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/issue1093/Issue1093Test.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Holger Voormann and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.tycho.test.issue1093;
+
+import java.util.List;
+
+import static org.junit.Assert.fail;
+
+import org.apache.maven.it.VerificationException;
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+public class Issue1093Test extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void test() throws Exception {
+        Verifier verifier = getVerifier("issue1093", false);
+        try {
+        	verifier.executeGoals(List.of("clean", "verify"));
+            fail();
+        } catch (VerificationException e) {
+            // expected
+            verifier.verifyTextInLog("non-existing-file.txt does not exist");
+        }
+    }
+}
