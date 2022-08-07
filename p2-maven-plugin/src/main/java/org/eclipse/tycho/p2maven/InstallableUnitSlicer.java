7f1053fd3786694838c7483eca4899bfe0a5d282
Christoph Läubrich <laeubi@laeubi-soft.de>
2022-08-07 19:01:32 +0200

    Only declare direct project dependencies for the Dependency Closure
    Currently the ProjectDependencyClosure#getProjectDependecies returns
    also transitive dependencies of projects, but this is undesirable in
    certain use-cases. This changes the method in a way that it only returns
    direct dependencies of the given project.

--- p2-maven-plugin/src/main/java/org/eclipse/tycho/p2maven/InstallableUnitSlicer.java
+++ p2-maven-plugin/src/main/java/org/eclipse/tycho/p2maven/InstallableUnitSlicer.java
@@ -12,6 +12,9 @@ package org.eclipse.tycho.p2maven;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
@@ -20,8 +23,10 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.equinox.internal.p2.director.PermissiveSlicer;
+import org.eclipse.equinox.internal.p2.metadata.InstallableUnit;
 import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.p2.metadata.IRequirement;
+import org.eclipse.equinox.p2.query.CollectionResult;
 import org.eclipse.equinox.p2.query.IQueryResult;
 import org.eclipse.equinox.p2.query.IQueryable;
 import org.eclipse.equinox.p2.query.QueryUtil;
@@ -45,7 +50,7 @@ public class InstallableUnitSlicer {
 	 * @param avaiableIUs the {@link IQueryable} of all units that could be used for
 	 *                    the slice
 	 * @return the result of the slicing
-	 * @throws CoreException if there is any
+	 * @throws CoreException if there is any error
 	 */
 	public IQueryResult<IInstallableUnit> computeDependencies(Collection<IInstallableUnit> rootIus,
 			IQueryable<IInstallableUnit> avaiableIUs) throws CoreException {
@@ -60,6 +65,42 @@ public class InstallableUnitSlicer {
 			log.debug("There are warnings from the slicer: " + sliceStatus);
 		}
 		return slice.query(QueryUtil.createIUAnyQuery(), monitor);
+	}
+
+	/**
+	 * Computes a "slice" that is the <b>direct</b> dependencies of the given
+	 * {@link IInstallableUnit}s in a way that the result contains any unit that
+	 * satisfies a requirement in for rootIus
+	 * 
+	 * @param rootIus     the root {@link InstallableUnit}s to take into account
+	 * @param avaiableIUs the {@link IQueryable} of all units that could be used for
+	 *                    fulfilling a requirement
+	 * @return the result of the slicing
+	 * @throws CoreException if there is any error
+	 */
+	public IQueryResult<IInstallableUnit> computeDirectDependencies(Collection<IInstallableUnit> rootIus,
+			IQueryable<IInstallableUnit> avaiableIUs) throws CoreException {
+		Collection<IInstallableUnit> result = new LinkedHashSet<>();
+		List<IRequirement> collect = rootIus.stream().flatMap(iu -> iu.getRequirements().stream()).filter(req -> {
+			for (IInstallableUnit unit : rootIus) {
+				if (unit.satisfies(req)) {
+					// self full filled requirement
+					return false;
+				}
+			}
+			return true;
+		}).collect(Collectors.toList());
+		for (IInstallableUnit iu : avaiableIUs.query(QueryUtil.ALL_UNITS, new NullProgressMonitor()).toSet()) {
+			for (IRequirement requirement : collect) {
+				if (iu.satisfies(requirement)) {
+					result.add(iu);
+					// TODO remove the requirement from the set so we only collect exactly one
+					// provider for a requirement?
+					break;
+				}
+			}
+		}
+		return new CollectionResult<IInstallableUnit>(result);
 	}
 
 	private final class TychoSlicer extends PermissiveSlicer {
