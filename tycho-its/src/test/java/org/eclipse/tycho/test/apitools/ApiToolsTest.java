672552edd92d21afe8c04afafd8441dd41793366
Hannes Wellmann <wellmann.hannes1@gmx.net>
2022-09-10 18:09:12 +0200

    Add integration test for tycho-apitools-plugin:verify mojo
    Co-authored-by: Christoph Läubrich <laeubi@laeubi-soft.de>

--- tycho-its/src/test/java/org/eclipse/tycho/test/apitools/ApiToolsTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/apitools/ApiToolsTest.java
@@ -1,13 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2022, 2023 Christoph Läubrich and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Christoph Läubrich - initial API and implementation
+ *     Hannes Wellman - add verify test case
+ *******************************************************************************/
 package org.eclipse.tycho.test.apitools;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.util.List;
 
+import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.eclipse.tycho.test.util.ResourceUtil;
 import org.junit.Test;
 
 import de.pdark.decentxml.Document;
@@ -19,10 +35,28 @@ public class ApiToolsTest extends AbstractTychoIntegrationTest {
 		Verifier verifier = getVerifier("api-tools", true, true);
 		verifier.executeGoals(List.of("clean", "package"));
 		verifier.verifyErrorFreeLog();
-		File descriptionFile = new File(verifier.getBasedir(), "bundle/target/.api_description");
+		File descriptionFile = new File(verifier.getBasedir(), "bundle1/target/.api_description");
 		assertTrue(descriptionFile.getAbsoluteFile() + " not found", descriptionFile.isFile());
 		Document document = XMLParser.parse(descriptionFile);
-		assertEquals("api-bundle_0.0.1-SNAPSHOT", document.getRootElement().getAttribute("name").getValue());
+		assertEquals("api-bundle-1_0.0.1-SNAPSHOT", document.getRootElement().getAttribute("name").getValue());
 		// TODO enhance project and assert more useful things...
+	}
+
+	@Test
+	public void testVerify() throws Exception {
+		Verifier verifier = getVerifier("api-tools", true, true);
+		File repo = ResourceUtil.resolveTestResource("repositories/api-tools");
+		verifier.addCliOption("-DbaselineRepo=" + repo.toURI());
+		try {
+			verifier.executeGoals(List.of("clean", "verify"));
+			fail("No API errors where detected!");
+		} catch (VerificationException e) {
+			verifier.verifyTextInLog("1 API ERRORS");
+			verifier.verifyTextInLog("0 API warnings");
+			verifier.verifyTextInLog("The type bundle.InterfaceA has been removed from api-bundle");
+			verifier.verifyTextInLog("The type bundle.ClassA has been removed from api-bundle");
+			// TODO: check with api-filter
+			// TODO: check with second plugin with BREE?
+		}
 	}
 }
