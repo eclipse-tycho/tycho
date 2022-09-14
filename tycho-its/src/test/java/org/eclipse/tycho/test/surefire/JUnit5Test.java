c98c61e75feafae4c75de2858bf9011309e4bebf
Mickael Istria <mistria@redhat.com>
2022-09-14 18:41:57 +0200

    Make ECLIPSE_LATEST reference 2022-09
    and use it in latest tests

--- tycho-its/src/test/java/org/eclipse/tycho/test/surefire/JUnit5Test.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/surefire/JUnit5Test.java
@@ -77,10 +77,10 @@ public class JUnit5Test extends AbstractTychoIntegrationTest {
 		assertNumberOfSuccessfulTests(projectBasedir, "bundle.test.JUnit56Test", 1);
 	}
 
-  @Test
+	@Test
 	public void testJUnit59Runner() throws Exception {
 		Verifier verifier = getVerifier("/surefire.junit59/bundle.test", false);
-		verifier.addCliOption("-Drepo-2020-03=" + "https:////download.eclipse.org/eclipse/updates/4.25-I-builds/");
+		verifier.addCliOption("-Drepo-2020-03=" + P2Repositories.ECLIPSE_LATEST.toString());
 		verifier.executeGoal("verify");
 		verifier.verifyErrorFreeLog();
 		String projectBasedir = verifier.getBasedir();
