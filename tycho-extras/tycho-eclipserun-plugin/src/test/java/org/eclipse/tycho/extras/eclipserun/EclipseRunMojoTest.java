649e530a23f9704e7de0378f2d67323321ee5114
Sebastian Ratz <sebastian.ratz@sap.com>
2022-09-26 15:35:34 +0200

    EclipseRunMojo: Add unit tests for <executionEnvironment>
    Follow-up on #1397.

--- tycho-extras/tycho-eclipserun-plugin/src/test/java/org/eclipse/tycho/extras/eclipserun/EclipseRunMojoTest.java
+++ tycho-extras/tycho-eclipserun-plugin/src/test/java/org/eclipse/tycho/extras/eclipserun/EclipseRunMojoTest.java
@@ -12,6 +12,7 @@
  ******************************************************************************/
 package org.eclipse.tycho.extras.eclipserun;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -19,16 +20,23 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.testing.SilentLog;
 import org.apache.maven.project.MavenProject;
+import org.eclipse.sisu.equinox.EquinoxServiceFactory;
 import org.eclipse.sisu.equinox.launching.EquinoxInstallation;
 import org.eclipse.sisu.equinox.launching.EquinoxLauncher;
 import org.eclipse.sisu.equinox.launching.LaunchConfiguration;
+import org.eclipse.tycho.TargetPlatform;
+import org.eclipse.tycho.core.ee.shared.ExecutionEnvironmentConfiguration;
 import org.eclipse.tycho.core.maven.ToolchainProvider;
 import org.eclipse.tycho.core.utils.TychoVersion;
+import org.eclipse.tycho.p2.resolver.facade.P2Resolver;
+import org.eclipse.tycho.p2.resolver.facade.P2ResolverFactory;
+import org.eclipse.tycho.p2.target.facade.TargetPlatformFactory;
 import org.eclipse.tycho.testing.AbstractTychoMojoTestCase;
 import org.junit.rules.TemporaryFolder;
 
@@ -38,6 +46,7 @@ public class EclipseRunMojoTest extends AbstractTychoMojoTestCase {
 	private EquinoxInstallation installation;
 	private TemporaryFolder temporaryFolder;
 	private File workFolder;
+	private ToolchainProvider toolchainProvider;
 
 	@Override
 	public void setUp() throws Exception {
@@ -54,11 +63,12 @@ public class EclipseRunMojoTest extends AbstractTychoMojoTestCase {
 		temporaryFolder.create();
 		MavenProject project = mock(MavenProject.class);
 		setVariableValueToObject(runMojo, "project", project);
-		ToolchainProvider toolchainProvider = mock(ToolchainProvider.class);
+		toolchainProvider = mock(ToolchainProvider.class);
 		setVariableValueToObject(runMojo, "toolchainProvider", toolchainProvider);
 		workFolder = new File(temporaryFolder.getRoot(), "work");
 		setVariableValueToObject(runMojo, "work", workFolder);
 		setVariableValueToObject(runMojo, "launcher", mock(EquinoxLauncher.class));
+		setVariableValueToObject(runMojo, "repositories", List.of());
 		when(installation.getLocation()).thenReturn(new File("installpath"));
 	}
 
@@ -80,8 +90,7 @@ public class EclipseRunMojoTest extends AbstractTychoMojoTestCase {
 	}
 
 	public void testCreateCommandlineWithApplicationArgs() throws IllegalAccessException, MojoExecutionException {
-		List<String> args = Arrays.asList("arg1", "literal arg with spaces",
-				"argument'with'literalquotes");
+		List<String> args = Arrays.asList("arg1", "literal arg with spaces", "argument'with'literalquotes");
 		setVariableValueToObject(runMojo, "applicationArgs", args);
 		LaunchConfiguration commandLine = runMojo.createCommandLine(installation);
 		List<String> programArgs = Arrays.asList(commandLine.getProgramArguments());
@@ -112,4 +121,47 @@ public class EclipseRunMojoTest extends AbstractTychoMojoTestCase {
 		runMojo.runEclipse(installation);
 		assertFalse(markerFile.exists());
 	}
+
+	public void testExecutionEnvironmentIsRespectedDuringDependencyResolution() throws Exception {
+		AtomicReference<ExecutionEnvironmentConfiguration> recordedExecutionEnvironmentConfiguration = new AtomicReference<>();
+
+		TargetPlatform mockTargetPlatform = mock(TargetPlatform.class);
+		TargetPlatformFactory mockTargetPlatformFactory = mock(TargetPlatformFactory.class);
+		when(mockTargetPlatformFactory.createTargetPlatform(any(), any(), any())).thenAnswer(invocation -> {
+			recordedExecutionEnvironmentConfiguration.set(invocation.getArgument(1));
+			return mockTargetPlatform;
+		});
+
+		P2Resolver mockP2Resolver = mock(P2Resolver.class);
+
+		P2ResolverFactory mockP2ResolverFactory = mock(P2ResolverFactory.class);
+		when(mockP2ResolverFactory.getTargetPlatformFactory()).thenReturn(mockTargetPlatformFactory);
+		when(mockP2ResolverFactory.createResolver(any())).thenReturn(mockP2Resolver);
+
+		EquinoxServiceFactory mockEquinoxServiceFactory = mock(EquinoxServiceFactory.class);
+		when(mockEquinoxServiceFactory.getService(P2ResolverFactory.class)).thenReturn(mockP2ResolverFactory);
+		setVariableValueToObject(runMojo, "equinox", mockEquinoxServiceFactory);
+
+		setVariableValueToObject(runMojo, "executionEnvironment", "custom-ee");
+
+		runMojo.execute();
+
+		assertEquals("custom-ee", recordedExecutionEnvironmentConfiguration.get().getProfileName());
+	}
+
+	public void testExecutionEnvironmentIsRespectedDuringEclipseExecution() throws Exception {
+		setVariableValueToObject(runMojo, "executionEnvironment", "custom-ee");
+		@SuppressWarnings("deprecation")
+		org.apache.maven.toolchain.java.DefaultJavaToolChain customToolchain = mock(
+				org.apache.maven.toolchain.java.DefaultJavaToolChain.class);
+		when(customToolchain.findTool("java")).thenReturn("/path/to/custom-ee-jdk/bin/java");
+		when(toolchainProvider.findMatchingJavaToolChain(any(), any())).thenAnswer(invocation -> {
+			return customToolchain;
+		});
+
+		LaunchConfiguration commandLine = runMojo.createCommandLine(installation);
+
+		assertEquals("/path/to/custom-ee-jdk/bin/java", commandLine.getJvmExecutable());
+	}
+
 }
