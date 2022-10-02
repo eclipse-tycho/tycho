62a85e7f523f43d41223d3aa50f78280ab3117c8
Christoph Läubrich <laeubi@laeubi-soft.de>
2022-10-02 19:53:35 +0200

    Extract the MavenContext Impl into a component

--- tycho-core/src/main/java/org/eclipse/tycho/osgi/configuration/MavenContextConfigurator.java
+++ tycho-core/src/main/java/org/eclipse/tycho/osgi/configuration/MavenContextConfigurator.java
@@ -15,111 +15,38 @@
  *******************************************************************************/
 package org.eclipse.tycho.osgi.configuration;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Properties;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import org.apache.maven.artifact.handler.ArtifactHandler;
-import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
-import org.apache.maven.execution.MavenExecutionRequest;
 import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.LegacySupport;
-import org.apache.maven.project.MavenProject;
 import org.apache.maven.settings.Profile;
 import org.apache.maven.settings.Settings;
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
-import org.codehaus.plexus.logging.Logger;
 import org.eclipse.sisu.equinox.embedder.EmbeddedEquinox;
 import org.eclipse.sisu.equinox.embedder.EquinoxLifecycleListener;
-import org.eclipse.tycho.MavenRepositoryLocation;
-import org.eclipse.tycho.core.osgitools.DefaultReactorProject;
 import org.eclipse.tycho.core.shared.MavenContext;
-import org.eclipse.tycho.core.shared.MavenContext.ChecksumPolicy;
-import org.eclipse.tycho.core.shared.MavenContextImpl;
-import org.eclipse.tycho.osgi.adapters.MavenLoggerAdapter;
-import org.eclipse.tycho.p2maven.repository.P2ArtifactRepositoryLayout;
 
 @Component(role = EquinoxLifecycleListener.class, hint = "MavenContextConfigurator")
 public class MavenContextConfigurator implements EquinoxLifecycleListener {
 
     @Requirement
-    private Logger logger;
-
-    @Requirement
-    private LegacySupport context;
-
-    @Requirement
-    private ArtifactHandlerManager artifactHandlerManager;
+    private MavenContext context;
 
     @Override
     public void afterFrameworkStarted(EmbeddedEquinox framework) {
-        MavenSession session = context.getSession();
-        if (session == null) {
-            return;
-        }
-        File localRepoRoot = new File(session.getLocalRepository().getBasedir());
-        MavenLoggerAdapter mavenLogger = new MavenLoggerAdapter(logger, false);
-        Properties globalProps = getGlobalProperties(session);
-        List<MavenRepositoryLocation> repositoryLocations = session.getProjects().stream()
-                .map(MavenProject::getRemoteArtifactRepositories).flatMap(Collection::stream)
-                .filter(r -> r.getLayout() instanceof P2ArtifactRepositoryLayout).map(r -> {
-                    try {
-                        return new MavenRepositoryLocation(r.getId(), new URL(r.getUrl()).toURI());
-                    } catch (MalformedURLException | URISyntaxException e) {
-                        return null;
-                    }
-                }).filter(Objects::nonNull).collect(Collectors.toUnmodifiableList());
-        ChecksumPolicy checksumPolicy;
-        if (MavenExecutionRequest.CHECKSUM_POLICY_FAIL.equals(session.getRequest().getGlobalChecksumPolicy())) {
-            checksumPolicy = ChecksumPolicy.STRICT;
-        } else {
-            checksumPolicy = ChecksumPolicy.LAX;
-        }
-        MavenContextImpl mavenContext = new MavenContextImpl(localRepoRoot, session.isOffline(), mavenLogger,
-                globalProps) {
-
-            @Override
-            public String getExtension(String artifactType) {
-                if (artifactType == null) {
-                    return "jar";
-                }
-                ArtifactHandler handler = artifactHandlerManager.getArtifactHandler(artifactType);
-                return handler.getExtension();
-            }
-
-            @Override
-            public boolean isUpdateSnapshots() {
-                return session.getRequest().isUpdateSnapshots();
-            }
-
-            @Override
-            public Stream<MavenRepositoryLocation> getMavenRepositoryLocations() {
-
-                return repositoryLocations.stream();
-            }
-
-            @Override
-            public ChecksumPolicy getChecksumsMode() {
-                return checksumPolicy;
-            }
-
-        };
-        for (MavenProject project : session.getProjects()) {
-            mavenContext.addProject(DefaultReactorProject.adapt(project));
-        }
-        framework.registerService(MavenContext.class, mavenContext);
+        //we call all methods here to init the data for sure inside the maven thread...
+        context.getChecksumsMode();
+        context.isUpdateSnapshots();
+        context.getSessionProperties();
+        context.getLocalRepositoryRoot();
+        context.getMavenRepositoryLocations();
+        context.getProjects();
+        context.isOffline();
+        framework.registerService(MavenContext.class, context);
     }
 
-    private Properties getGlobalProperties(MavenSession session) {
+    static Properties getGlobalProperties(MavenSession session) {
         Properties globalProps = new Properties();
         // 1. system
         globalProps.putAll(session.getSystemProperties());
