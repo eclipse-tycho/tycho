7565fc23930f301283930eb4b681e86cfd565c59
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-01-10 09:50:33 +0100

    Move the CustomBundlePlugin invoker test to the integration-test-suite

--- tycho-extras/tycho-custom-bundle-plugin/src/it/custom-bundle-parent/verify.groovy
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-
-File attached = new File(basedir, "custom.bundle.feature/target/site/plugins/custom.bundle.attached_1.0.0.123abc.jar"); 
-if (!attached.canRead()) {
-  throw new Exception( "Missing expected file " + attached );
-}
-
-return true;
