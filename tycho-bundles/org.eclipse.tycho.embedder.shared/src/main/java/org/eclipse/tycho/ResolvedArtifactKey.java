532379598af66a3b3203c13202665f8a3befe843
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-01-13 19:51:50 +0100

    Support generation of IUs from a Manifest object

--- tycho-bundles/org.eclipse.tycho.embedder.shared/src/main/java/org/eclipse/tycho/ResolvedArtifactKey.java
+++ tycho-bundles/org.eclipse.tycho.embedder.shared/src/main/java/org/eclipse/tycho/ResolvedArtifactKey.java
@@ -49,6 +49,11 @@ public interface ResolvedArtifactKey extends ArtifactKey {
             public File getLocation() {
                 return location;
             }
+
+            @Override
+            public String toString() {
+                return "[" + getType() + "] " + getId() + " " + getVersion() + " @ " + getLocation();
+            }
         };
     }
 }
