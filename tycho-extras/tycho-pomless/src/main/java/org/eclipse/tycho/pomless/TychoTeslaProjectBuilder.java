6615925c231531051e486c6016c1f0e3b190ee8a
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-02-14 19:35:15 +0100

    Workaround for NPE when running Tycho-Build-Extension on maven 3.9.0

--- tycho-extras/tycho-pomless/src/main/java/org/eclipse/tycho/pomless/TychoTeslaProjectBuilder.java
+++ tycho-extras/tycho-pomless/src/main/java/org/eclipse/tycho/pomless/TychoTeslaProjectBuilder.java
@@ -14,15 +14,36 @@
  *******************************************************************************/
 package org.eclipse.tycho.pomless;
 
+import java.lang.reflect.Field;
+
 import javax.annotation.Priority;
 
+import org.apache.maven.project.DefaultProjectBuilder;
 import org.apache.maven.project.ProjectBuilder;
 import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
 import org.sonatype.maven.polyglot.TeslaProjectBuilder;
 
 @Component(role = ProjectBuilder.class)
 @Priority(10)
-//Workaround for https://github.com/takari/polyglot-maven/pull/256
-public class TychoTeslaProjectBuilder extends TeslaProjectBuilder {
+public class TychoTeslaProjectBuilder extends TeslaProjectBuilder implements Initializable, ProjectBuilder {
 
+    @Override
+    public void initialize() throws InitializationException {
+        try {
+            // Workaround for https://github.com/takari/polyglot-maven/pull/256
+        	// and https://github.com/takari/polyglot-maven/pull/257
+            Field field = DefaultProjectBuilder.class.getDeclaredField("modelCacheFactory");
+            field.setAccessible(true);
+            Object value = field.get(this);
+            if (value == null) {
+                Class<?> clazz = getClass().getClassLoader()
+                        .loadClass("org.apache.maven.repository.internal.DefaultModelCacheFactory");
+                Object instance = clazz.getConstructor().newInstance();
+                field.set(this, instance);
+            }
+        } catch (Exception e) {
+        }
+    }
 }
