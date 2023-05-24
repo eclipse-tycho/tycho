434fbd9d705bc67b22796acd0c6041fa6a647c6d
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-05-24 12:51:27 +0200

    Adapt m2e testcases for target locations
    this is an adaption of the m2e target testcases to ensure we have equal
    feature / test coverage.

---
+++ tycho-core/src/test/java/org/eclipse/m2e/pde/target/tests/AbstractMavenTargetTest.java
@@ -0,0 +1,225 @@
+/*******************************************************************************
+ * Copyright (c) 2023, 2023 Hannes Wellmann and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Hannes Wellmann - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2e.pde.target.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+import java.util.ServiceLoader.Provider;
+import java.util.Set;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.URIUtil;
+import org.eclipse.equinox.frameworkadmin.BundleInfo;
+import org.eclipse.m2e.pde.target.tests.spi.TargetLocationLoader;
+import org.eclipse.pde.core.target.ITargetLocation;
+import org.eclipse.pde.core.target.NameVersionDescriptor;
+import org.eclipse.pde.core.target.TargetBundle;
+import org.eclipse.pde.core.target.TargetFeature;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+public abstract class AbstractMavenTargetTest {
+	static final String SOURCE_BUNDLE_SUFFIX = ".source";
+	static final TargetBundle[] EMPTY = {};
+
+	@Rule
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+	private static ServiceLoader<TargetLocationLoader> LOCATION_LOADER = ServiceLoader.load(TargetLocationLoader.class,
+			AbstractMavenTargetTest.class.getClassLoader());
+	private static TargetLocationLoader loader;
+
+	ITargetLocation resolveMavenTarget(String targetXML) throws Exception {
+		return getLoader().resolveMavenTarget(targetXML, temporaryFolder.newFolder());
+	}
+
+	private static TargetLocationLoader getLoader() {
+		if (loader == null) {
+			Provider<TargetLocationLoader> provider = LOCATION_LOADER.stream().sorted().findFirst().orElseThrow(
+					() -> new IllegalStateException("No TargetLocationLoader found on classpath of test!"));
+			loader = provider.get();
+		}
+		return loader;
+	}
+
+	protected static void assertStatusOk(IStatus status) {
+		if (!status.isOK()) {
+			throw new AssertionError(status.toString(), status.getException());
+		}
+	}
+
+	// --- common assertion utilities ---
+
+	private static <U extends ExpectedUnit, T> Map<U, T> assertTargetContent(List<U> expectedUnits, T[] allUnit,
+			BiPredicate<U, T> matcher, Function<T, URI> getLocation, Predicate<T> isSourceUnit,
+			Function<T, String> getSourceTarget, Function<T, String> toString) {
+
+		Map<U, T> units = new HashMap<>();
+		List<T> allElements = new ArrayList<>(Arrays.asList(allUnit));
+		for (U expectedUnit : expectedUnits) {
+			List<T> matchingUnits = allElements.stream().filter(u -> matcher.test(expectedUnit, u)).toList();
+
+			if (matchingUnits.isEmpty()) {
+				fail("Expected unit is missing: " + expectedUnit);
+			} else if (matchingUnits.size() == 1) {
+				T targetUnit = matchingUnits.get(0);
+				allElements.remove(targetUnit);
+
+				assertEquals("Unexpected 'original' state of " + targetUnit, expectedUnit.isOriginal(),
+						isOriginalArtifact(expectedUnit, targetUnit, getLocation));
+				assertEquals("Unexpected 'isSource' state of " + targetUnit, expectedUnit.isSourceBundle(),
+						isSourceUnit.test(targetUnit));
+				if (expectedUnit.isSourceBundle()) {
+					String expectedSourceTarget = expectedUnit.id().substring(0,
+							expectedUnit.id().length() - SOURCE_BUNDLE_SUFFIX.length());
+					assertEquals("Source target id", expectedSourceTarget, getSourceTarget.apply(targetUnit));
+				} else {
+					assertNull(getSourceTarget.apply(targetUnit));
+				}
+				units.put(expectedUnit, targetUnit);
+			} else {
+				fail("Expected bundle contaiend multiple times:" + expectedUnit);
+			}
+		}
+		if (!allElements.isEmpty()) {
+			String unepxectedBundlesList = allElements.stream().map(u -> "  " + toString.apply(u))
+					.collect(Collectors.joining("\n"));
+			fail("Encoutnered the following unexpected bundles:" + unepxectedBundlesList);
+		}
+		return units;
+	}
+
+	private static <T> boolean isOriginalArtifact(ExpectedUnit expectedUnit, T unit, Function<T, URI> getLocation) {
+		ArtifactKey key = expectedUnit.key();
+		if (key == null) {
+			return false;
+		}
+		URI location = getLocation.apply(unit);
+		String expectedPathSuffix = "/" + String.join("/", ".m2", "repository", key.groupId().replace('.', '/'),
+				key.artifactId(), key.version(), key.artifactId() + "-" + key.version() + ".jar");
+		return location.toASCIIString().endsWith(expectedPathSuffix);
+	}
+
+	// --- assertion utilities for Bundles in target ---
+
+	static ExpectedBundle originalOSGiBundle(String bsn, String version, String groupArtifact) {
+		return originalOSGiBundle(bsn, version, groupArtifact, version);
+	}
+
+	static ExpectedBundle originalOSGiBundle(String bsn, String version, String groupArtifact, String mavenVersion) {
+		return new ExpectedBundle(bsn, version, false, true,
+				ArtifactKey.fromPortableString(groupArtifact + ":" + mavenVersion + "::"));
+	}
+
+	static ExpectedBundle generatedBundle(String bsn, String version, String groupArtifact) {
+		return new ExpectedBundle(bsn, version, false, false,
+				ArtifactKey.fromPortableString(groupArtifact + ":" + version + "::"));
+	}
+
+	static List<ExpectedBundle> withSourceBundles(List<ExpectedBundle> mainBundles) {
+		return mainBundles.stream().<ExpectedBundle>mapMulti((unit, downStream) -> {
+			downStream.accept(unit);
+			String sourceId = unit.bsn() + SOURCE_BUNDLE_SUFFIX;
+			ExpectedBundle sourceUnit = new ExpectedBundle(sourceId, unit.version(), true, false, unit.key());
+			downStream.accept(sourceUnit);
+		}).toList();
+	}
+
+	static Attributes getManifestMainAttributes(TargetBundle targetBundle) throws IOException {
+		BundleInfo bundleInfo = targetBundle.getBundleInfo();
+		File file = URIUtil.toFile(bundleInfo.getLocation());
+		try (var jar = new JarFile(file)) {
+			return jar.getManifest().getMainAttributes();
+		}
+	}
+
+	static void assertTargetBundles(ITargetLocation target, List<ExpectedBundle> expectedUnits) {
+		assertTargetContent(expectedUnits, target.getBundles(), //
+				(expectedBundle, bundle) -> {
+					BundleInfo info = bundle.getBundleInfo();
+					return expectedBundle.bsn().equals(info.getSymbolicName())
+							&& expectedBundle.version().equals(info.getVersion());
+				}, //
+				tb -> tb.getBundleInfo().getLocation(), //
+				tb -> tb.isSourceBundle(),
+				tb -> tb.getSourceTarget() != null ? tb.getSourceTarget().getSymbolicName() : null,
+				tb -> tb.getBundleInfo().getSymbolicName() + ":" + tb.getBundleInfo().getVersion());
+	}
+
+	// --- assertion utilities for Features in a target ---
+
+	static ExpectedFeature originalFeature(String id, String version, String groupArtifact,
+			List<NameVersionDescriptor> containedPlugins) {
+		ArtifactKey key = ArtifactKey.fromPortableString(groupArtifact + ":" + version + "::");
+		return new ExpectedFeature(id, version, false, true, key, containedPlugins);
+	}
+
+	static ExpectedFeature generatedFeature(String id, String version, List<NameVersionDescriptor> containedPlugins) {
+		return new ExpectedFeature(id, version, false, false, null, containedPlugins);
+	}
+
+	static NameVersionDescriptor featurePlugin(String bsn, String version) {
+		return new NameVersionDescriptor(bsn, version);
+	}
+
+	static List<ExpectedFeature> withSourceFeatures(List<ExpectedFeature> mainFeatures) {
+		return mainFeatures.stream().<ExpectedFeature>mapMulti((feature, downStream) -> {
+			downStream.accept(feature);
+			String sourceId = feature.id() + SOURCE_BUNDLE_SUFFIX;
+			List<NameVersionDescriptor> sourcePlugins = feature.containedPlugins().stream()
+					.map(d -> featurePlugin(d.getId() + SOURCE_BUNDLE_SUFFIX, d.getVersion())).toList();
+			ExpectedFeature sourceUnit = new ExpectedFeature(sourceId, feature.version(), true, false, feature.key(),
+					sourcePlugins);
+			downStream.accept(sourceUnit);
+		}).toList();
+	}
+
+	static Map<ExpectedFeature, TargetFeature> assertTargetFeatures(ITargetLocation target,
+			List<ExpectedFeature> expectedFeatures) {
+		var encounteredFeatures = assertTargetContent(expectedFeatures, target.getFeatures(), //
+				(expectedFeature, feature) -> expectedFeature.id().equals(feature.getId())
+						&& expectedFeature.version().equals(feature.getVersion()), //
+				f -> Path.of(f.getLocation()).toUri(), //
+				f -> isSourceFeature(f), //
+				f -> isSourceFeature(f) ? f.getId().substring(0, f.getId().length() - SOURCE_BUNDLE_SUFFIX.length())
+						: null, //
+				f -> f.getId() + ":" + f.getVersion());
+		encounteredFeatures.forEach((expectedFeature, feature) -> {
+			assertEquals(Set.copyOf(expectedFeature.containedPlugins()), Set.of(feature.getPlugins()));
+		});
+		return encounteredFeatures;
+	}
+
+	private static boolean isSourceFeature(TargetFeature f) {
+		return f.getId().endsWith(SOURCE_BUNDLE_SUFFIX)
+				&& Arrays.stream(f.getPlugins()).allMatch(d -> d.getId().endsWith(SOURCE_BUNDLE_SUFFIX));
+	}
+}
---
+++ tycho-core/src/test/java/org/eclipse/m2e/pde/target/tests/spi/TychoTargetLocationLoader.java
@@ -0,0 +1,294 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Christoph Läubrich and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2e.pde.target.tests.spi;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.LegacyLocalRepositoryManager;
+import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
+import org.apache.maven.execution.DefaultMavenExecutionRequest;
+import org.apache.maven.execution.DefaultMavenExecutionResult;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.LegacySupport;
+import org.apache.maven.plugin.testing.stubs.StubArtifactRepository;
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.apache.maven.session.scope.internal.SessionScope;
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.equinox.p2.metadata.expression.IExpression;
+import org.eclipse.equinox.p2.query.Collector;
+import org.eclipse.equinox.p2.query.IQuery;
+import org.eclipse.equinox.p2.query.IQueryResult;
+import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
+import org.eclipse.pde.core.target.ITargetDefinition;
+import org.eclipse.pde.core.target.ITargetLocation;
+import org.eclipse.pde.core.target.TargetBundle;
+import org.eclipse.pde.core.target.TargetFeature;
+import org.eclipse.tycho.core.resolver.MavenTargetLocationFactory;
+import org.eclipse.tycho.core.resolver.shared.IncludeSourceMode;
+import org.eclipse.tycho.core.resolver.target.FileArtifactRepository;
+import org.eclipse.tycho.core.resolver.target.TargetDefinitionContent;
+import org.eclipse.tycho.targetplatform.TargetDefinition.Location;
+import org.eclipse.tycho.targetplatform.TargetDefinition.MavenGAVLocation;
+import org.eclipse.tycho.targetplatform.TargetDefinitionFile;
+import org.eclipse.tycho.testing.TychoPlexusTestCase;
+
+public class TychoTargetLocationLoader implements TargetLocationLoader {
+
+    @Override
+    public int getPriority() {
+        // use a higher priority to override m2e provided loader...
+        return 100;
+    }
+
+    @Override
+    public ITargetLocation resolveMavenTarget(String targetXML, File tempDir) throws Exception {
+        File targetFile = new File(tempDir, "test.target");
+        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(targetFile),
+                StandardCharsets.UTF_8)) {
+            writer.write("<target name=\"test-target-platform\"><locations>");
+            writer.write(targetXML);
+            writer.write("</locations></target>");
+        }
+        PlexusContainer container = new ContainerFactory().createContainer(tempDir);
+        MavenTargetLocationFactory locationFactory = container.lookup(MavenTargetLocationFactory.class);
+        TargetDefinitionFile targetDefinition = TargetDefinitionFile.read(targetFile);
+        for (Location loc : targetDefinition.getLocations()) {
+            if (loc instanceof MavenGAVLocation gav) {
+                File locationTempDir = new File(tempDir, "tmp");
+                locationTempDir.mkdirs();
+                return getLocationOf(gav, locationFactory, locationTempDir.toPath());
+            }
+        }
+        throw new IllegalArgumentException("Can't extract MavenGAVLocation from " + targetXML);
+    }
+
+    private ITargetLocation getLocationOf(MavenGAVLocation gav, MavenTargetLocationFactory locationFactory,
+            Path tmpFir) {
+        try {
+            return getLocationOf(locationFactory.resolveTargetDefinitionContent(gav, IncludeSourceMode.honor), tmpFir);
+        } catch (Exception e) {
+            return new ITargetLocation() {
+
+                @Override
+                public <T> T getAdapter(Class<T> adapter) {
+                    return null;
+                }
+
+                @Override
+                public String serialize() {
+                    throw new UnsupportedOperationException();
+                }
+
+                @Override
+                public IStatus resolve(ITargetDefinition definition, IProgressMonitor monitor) {
+                    throw new UnsupportedOperationException();
+                }
+
+                @Override
+                public boolean isResolved() {
+                    return true;
+                }
+
+                @Override
+                public String[] getVMArguments() {
+                    throw new UnsupportedOperationException();
+                }
+
+                @Override
+                public String getType() {
+                    return MAVEN_LOCATION_TYPE;
+                }
+
+                @Override
+                public IStatus getStatus() {
+                    return Status.error("Resolve target location failed: " + e, e);
+                }
+
+                @Override
+                public String getLocation(boolean resolve) throws CoreException {
+                    throw new UnsupportedOperationException();
+                }
+
+                @Override
+                public TargetFeature[] getFeatures() {
+                    return new TargetFeature[0];
+                }
+
+                @Override
+                public TargetBundle[] getBundles() {
+                    return new TargetBundle[0];
+                }
+            };
+        }
+    }
+
+    private ITargetLocation getLocationOf(TargetDefinitionContent content, Path tmpFir) throws Exception {
+        List<TargetBundle> bundles = new ArrayList<>();
+        List<TargetFeature> features = new ArrayList<>();
+        FileArtifactRepository resolvedItems = (FileArtifactRepository) content.getArtifactRepository();
+        IQuery<IArtifactDescriptor> query = new IQuery<>() {
+
+            @Override
+            public IQueryResult<IArtifactDescriptor> perform(Iterator<IArtifactDescriptor> iterator) {
+                Collector<IArtifactDescriptor> result = new Collector<>();
+                while (iterator.hasNext()) {
+                    IArtifactDescriptor candidate = iterator.next();
+                    if (candidate != null)
+                        if (!result.accept(candidate))
+                            break;
+                }
+                return result;
+            }
+
+            @Override
+            public IExpression getExpression() {
+                return null;
+            }
+        };
+        Set<IArtifactDescriptor> descriptors = resolvedItems.descriptorQueryable().query(query, null).toSet();
+        for (IArtifactDescriptor descriptor : descriptors) {
+            File artifactFile = resolvedItems.getArtifactFile(descriptor);
+            if ("osgi.bundle".equals(descriptor.getArtifactKey().getClassifier())) {
+                bundles.add(new TargetBundle(artifactFile));
+            } else if ("org.eclipse.update.feature".equals(descriptor.getArtifactKey().getClassifier())) {
+                Path feature;
+                try (JarFile file = new JarFile(artifactFile)) {
+                    Path tempDirectory = Files.createTempDirectory(tmpFir, "extract");
+                    ZipEntry entry = file.getEntry("feature.xml");
+                    InputStream stream = file.getInputStream(entry);
+                    feature = tempDirectory.resolve("feature.xml");
+                    Files.copy(stream, feature);
+                }
+                features.add(new TargetFeature(feature.toFile()));
+            }
+        }
+        return new ITargetLocation() {
+
+            @Override
+            public <T> T getAdapter(Class<T> adapter) {
+                return null;
+            }
+
+            @Override
+            public String serialize() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public IStatus resolve(ITargetDefinition definition, IProgressMonitor monitor) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public boolean isResolved() {
+                return true;
+            }
+
+            @Override
+            public String[] getVMArguments() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public String getType() {
+                return MAVEN_LOCATION_TYPE;
+            }
+
+            @Override
+            public IStatus getStatus() {
+                return Status.OK_STATUS;
+            }
+
+            @Override
+            public String getLocation(boolean resolve) throws CoreException {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public TargetFeature[] getFeatures() {
+                return features.toArray(TargetFeature[]::new);
+            }
+
+            @Override
+            public TargetBundle[] getBundles() {
+                return bundles.toArray(TargetBundle[]::new);
+            }
+        };
+    }
+
+    private static final class ContainerFactory extends TychoPlexusTestCase {
+
+        private boolean init;
+
+        public PlexusContainer createContainer(File tempDir) throws ComponentLookupException {
+            PlexusContainer container = super.getContainer();
+            if (!init) {
+                init = true;
+                LegacySupport legacySupport = lookup(LegacySupport.class);
+//                Settings settings = new Settings();
+                ArtifactRepository localRepository = new StubArtifactRepository(
+                        new File(tempDir, ".m2/repository").getAbsolutePath()) {
+                    DefaultRepositoryLayout layout = new DefaultRepositoryLayout();
+
+                    @Override
+                    public String pathOf(Artifact artifact) {
+                        return layout.pathOf(artifact);
+                    }
+                };
+                DefaultMavenExecutionRequest request = new DefaultMavenExecutionRequest();
+                request.setUserProperties(System.getProperties());
+                request.setLocalRepository(localRepository);
+                request.setGoals(List.of());
+                request.setBaseDirectory(new File(tempDir, "build"));
+                request.setStartTime(new Date());
+
+                RepositorySystemSession repositorySystemSession = LegacyLocalRepositoryManager.overlay(localRepository,
+                        MavenRepositorySystemUtils.newSession(), null);
+                MavenSession mavenSession = new MavenSession(container, repositorySystemSession, request,
+                        new DefaultMavenExecutionResult());
+//                MavenSession mavenSession = new MavenSession(container, settings, localRepository, null, null,
+//                        List.of(), new File(tempDir, "build").getAbsolutePath(), System.getProperties(),
+//                        System.getProperties(), new Date());
+                SessionScope sessionScope = container.lookup(SessionScope.class);
+                mavenSession.setProjects(Collections.emptyList());
+                sessionScope.enter();
+                sessionScope.seed(MavenSession.class, mavenSession);
+                legacySupport.setSession(mavenSession);
+            }
+            return container;
+        }
+    }
+
+}
