4adf043ff38ca0451cbf9d4bd59f3bf3d6a8053d
qtran <quang.tran@mailbox.org>
2023-06-01 17:04:55 +0200

    Updated header

--- tycho-its/src/test/java/org/eclipse/tycho/test/product/ProductProgramArgsTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/product/ProductProgramArgsTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Sonatype Inc. and others.
+ * Copyright (c) 2023 Sonatype Inc. and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *    Sonatype Inc. - initial API and implementation
+ *    Quang Tran - initial API and implementation
  *******************************************************************************/
 package org.eclipse.tycho.test.product;
 
