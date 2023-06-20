fedbd7dd6b32d80efd5f09e7bb26cc6eea465179
Ben Shelbourne <ben.shelbourne@arm.com>
2023-06-20 08:56:06 +0100

    Amended copyright header

--- tycho-its/src/test/java/org/eclipse/tycho/test/target/PasswordProtectedCompositeP2RepositoryTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/target/PasswordProtectedCompositeP2RepositoryTest.java
@@ -1,4 +1,15 @@
-// Copyright (C) 2023 Arm Limited (or its affiliates).
+/*******************************************************************************
+ * Copyright (c) 23 Arm Limited and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Arm Limited - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.tycho.test.target;
 
 import java.io.File;
