6e4ddc78849728e0b52522480b3d70fe225130c8
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-07-17 20:16:08 +0200

    Add support for a local packed osgi-repository
    (cherry picked from commit 25e742611af37529311a571683f86ed0b905adc8)

--- tycho-repository-plugin/src/main/java/org/eclipse/tycho/repository/plugin/PackageRepositoryMojo.java
+++ tycho-repository-plugin/src/main/java/org/eclipse/tycho/repository/plugin/PackageRepositoryMojo.java
@@ -16,6 +16,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.handler.DefaultArtifactHandler;
 import org.apache.maven.execution.MavenSession;
@@ -23,9 +25,12 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.archiver.Archiver;
+import org.codehaus.plexus.archiver.zip.ZipArchiver;
 import org.eclipse.tycho.ArtifactType;
 import org.eclipse.tycho.MavenArtifactNamespace;
 
@@ -37,8 +42,8 @@ import aQute.bnd.osgi.resource.ResourceBuilder;
  * Generates an OSGi repository from the current reactor projects
  *
  */
-@Mojo(name = "package-maven-repository")
-public class PackageMavenRepositoryMojo extends AbstractMojo {
+@Mojo(name = "package-repository")
+public class PackageRepositoryMojo extends AbstractMojo {
 
 	@Parameter(property = "session", readonly = true)
 	protected MavenSession session;
@@ -64,16 +69,47 @@ public class PackageMavenRepositoryMojo extends AbstractMojo {
 	@Parameter(property = "project", readonly = true)
 	private MavenProject project;
 
+	/**
+	 * Specify the used layout, possible values are:
+	 * <ul>
+	 * <li><code>maven</code> - all artifacts are referenced with the mvn protocol
+	 * and the result can be deployment to a maven repository (either local or
+	 * remote)</li>
+	 * <li><code>local</code> - all artifacts are copied into a folder and
+	 * referenced relative to this folder, the result can be</li>
+	 * </ul>
+	 */
+	@Parameter(defaultValue = "maven")
+	private RepositoryLayout repositoryLayout;
+
+	@Component(role = Archiver.class, hint = "zip")
+	private ZipArchiver zipArchiver;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		XMLResourceGenerator resourceGenerator = new XMLResourceGenerator();
 		resourceGenerator.name(repositoryName);
+		File folder;
+		if (repositoryLayout == RepositoryLayout.local) {
+			folder = new File(destination, FilenameUtils.getBaseName(repositoryFileName));
+			folder.mkdirs();
+			resourceGenerator.base(folder.toURI());
+		} else {
+			folder = null;
+		}
 		for (MavenProject project : session.getProjects()) {
 			if (isInteresting(project)) {
 				ResourceBuilder rb = new ResourceBuilder();
 				try {
-					if (rb.addFile(project.getArtifact().getFile(), new URI("mvn:" + project.getGroupId() + ":"
-							+ project.getArtifactId() + ":" + project.getVersion()))) {
+					URI uri;
+					File file = project.getArtifact().getFile();
+					if (folder == null) {
+						uri = new URI("mvn:" + project.getGroupId() + ":" + project.getArtifactId() + ":"
+								+ project.getVersion());
+					} else {
+						uri = new File(folder, file.getName()).toURI();
+					}
+					if (rb.addFile(project.getArtifact().getFile(), uri)) {
 						CapReqBuilder identity = new CapReqBuilder(MavenArtifactNamespace.MAVEN_ARTIFACT_NAMESPACE)
 								.addAttribute(MavenArtifactNamespace.CAPABILITY_GROUP_ATTRIBUTE, project.getGroupId())
 								.addAttribute(MavenArtifactNamespace.MAVEN_ARTIFACT_NAMESPACE, project.getArtifactId())
@@ -82,6 +118,9 @@ public class PackageMavenRepositoryMojo extends AbstractMojo {
 						rb.addCapability(identity);
 						resourceGenerator.resource(rb.build());
 						getLog().info("Adding " + project.getId());
+						if (folder != null) {
+							FileUtils.copyFileToDirectory(file, folder);
+						}
 					} else {
 						getLog().info("Skip " + project.getId() + ": Not a bundle");
 					}
@@ -92,11 +131,22 @@ public class PackageMavenRepositoryMojo extends AbstractMojo {
 			}
 		}
 		try {
-			File location = new File(destination, repositoryFileName);
-			resourceGenerator.save(location);
 			Artifact artifact = project.getArtifact();
-			artifact.setArtifactHandler(new DefaultArtifactHandler("xml"));
-			artifact.setFile(location);
+			if (folder != null) {
+				File location = new File(folder, repositoryFileName);
+				resourceGenerator.save(location);
+				File destFile = new File(destination, project.getArtifactId() + "-" + folder.getName() + ".zip");
+				zipArchiver.addDirectory(folder);
+				zipArchiver.setDestFile(destFile);
+				zipArchiver.createArchive();
+				artifact.setFile(destFile);
+				artifact.setArtifactHandler(new DefaultArtifactHandler("zip"));
+			} else {
+				File location = new File(destination, repositoryFileName);
+				resourceGenerator.save(location);
+				artifact.setArtifactHandler(new DefaultArtifactHandler("xml"));
+				artifact.setFile(location);
+			}
 		} catch (IOException e) {
 			throw new MojoExecutionException("Could not write OSGi Repository!", e);
 		}
