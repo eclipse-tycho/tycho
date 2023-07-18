0d47dafaec3e676501ff9d94f317ce414a9c7a9f
Titouan Vervack <titouan.vervack@sigasi.com>
2023-07-18 14:38:01 +0200

    Add integration test

---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/brokenp2data/BrokenP2DataTest.java
@@ -0,0 +1,19 @@
+package org.eclipse.tycho.test.brokenp2data;
+
+import java.util.List;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.junit.Test;
+
+// See #2625
+public class BrokenP2DataTest extends AbstractTychoIntegrationTest {
+
+	@Test
+	public void test() throws Exception {
+		Verifier verifier = getVerifier("brokenp2data");
+
+		verifier.executeGoals(List.of("clean", "verify", "-Dtycho-version=3.0.5"));
+		verifier.verifyErrorFreeLog();
+	}
+}
