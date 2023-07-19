486f6cf70b831fff575e42f1f6465f4ebbd93c8e
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-07-19 18:10:45 +0200

    Make the repository format pluggable
    Currently there needs to be a mojo for each format where the basics are
    quite common and just the serialization is special so it seems quite
    useful to reuse the basics.
    
    (cherry picked from commit 374e2b2542f98f2e110747d15a27310e7dbecf1c)

--- tycho-repository-plugin/src/main/java/org/eclipse/tycho/repository/plugin/TychoRepositoryPluginMavenLifecycleParticipant.java
+++ tycho-repository-plugin/src/main/java/org/eclipse/tycho/repository/plugin/TychoRepositoryPluginMavenLifecycleParticipant.java
@@ -12,42 +12,85 @@
  *******************************************************************************/
 package org.eclipse.tycho.repository.plugin;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.maven.AbstractMavenLifecycleParticipant;
 import org.apache.maven.MavenExecutionException;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Plugin;
+import org.apache.maven.model.PluginExecution;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.logging.Logger;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.eclipse.tycho.helper.PluginConfigurationHelper;
+import org.eclipse.tycho.helper.ProjectHelper;
+import org.eclipse.tycho.packaging.RepositoryGenerator;
 
 @Component(role = AbstractMavenLifecycleParticipant.class)
 public class TychoRepositoryPluginMavenLifecycleParticipant extends AbstractMavenLifecycleParticipant {
+
+	@Requirement
+	Map<String, RepositoryGenerator> generators;
+
+	@Requirement
+	PluginConfigurationHelper configurationHelper;
+
+	@Requirement
+	ProjectHelper projectHelper;
+
+	@Requirement
+	Logger logger;
 
 	@Override
 	public void afterProjectsRead(MavenSession session) throws MavenExecutionException {
 		List<MavenProject> projects = session.getProjects();
 		for (MavenProject project : projects) {
-			if ("repository".equals(project.getPackaging()) && project.getPlugin("org.eclipse.tycho:tycho-repository-plugin") != null) {
-				addInterestingProjects(project, projects);
+			Plugin plugin = project.getPlugin("org.eclipse.tycho:tycho-repository-plugin");
+			if ("repository".equals(project.getPackaging()) && plugin != null) {
+				Set<MavenProject> added = new HashSet<MavenProject>();
+				for (PluginExecution execution : plugin.getExecutions()) {
+					for (String goal : execution.getGoals()) {
+						addInterestingProjects(project, projects, session, goal, added);
+					}
+				}
 			}
 		}
 	}
 
-	private void addInterestingProjects(MavenProject project, List<MavenProject> projects) {
+	private void addInterestingProjects(MavenProject project, List<MavenProject> projects, MavenSession session,
+			String goal, Set<MavenProject> added) {
+		Xpp3Dom configuration = projectHelper.getPluginConfiguration("org.eclipse.tycho", "tycho-repository-plugin",
+				goal, project, session);
+		String repoType = configurationHelper.getConfiguration(configuration)
+				.getString(PackageRepositoryMojo.PARAMETER_REPOSITORY_TYPE)
+				.orElse(PackageRepositoryMojo.DEFAULT_REPOSITORY_TYPE);
+		RepositoryGenerator generator = generators.get(repoType);
+		if (generator == null) {
+			logger.warn(
+					"Can't determine projects that should be declared as automatic discovered dependencies because RepositoryGenerator of type '"
+							+ repoType + "' was not found!");
+			return;
+		}
 		for (MavenProject other : projects) {
-			if (other == project) {
+			if (other == project || added.contains(other)) {
 				continue;
 			}
-			if (PackageRepositoryMojo.isInteresting(other)) {
+			if (generator.isInteresting(other)) {
 				Dependency dependency = new Dependency();
 				dependency.setGroupId(other.getGroupId());
 				dependency.setArtifactId(other.getArtifactId());
 				dependency.setVersion(other.getVersion());
 				project.getModel().addDependency(dependency);
+				added.add(other);
 			}
 		}
+
 	}
 
 }
