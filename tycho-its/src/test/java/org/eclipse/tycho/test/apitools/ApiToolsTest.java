58051149efe4e3f47f8dd5339e09a68ac7ec1654
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-10-08 13:25:41 +0200

    Workaround for eclipse-pde/eclipse.pde#789
    and eclipse-pde/eclipse.pde#791

--- tycho-its/src/test/java/org/eclipse/tycho/test/apitools/ApiToolsTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/apitools/ApiToolsTest.java
@@ -14,11 +14,12 @@
 package org.eclipse.tycho.test.apitools;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
@@ -48,17 +49,36 @@ public class ApiToolsTest extends AbstractTychoIntegrationTest {
 		File repo = ResourceUtil.resolveTestResource("repositories/api-tools");
 		verifier.addCliOption("-DbaselineRepo=" + repo.toURI());
 
-		assertThrows("No API errors where detected!", VerificationException.class,
-				() -> verifier.executeGoals(List.of("clean", "verify")));
-
+		assertThrows(VerificationException.class, () -> verifier.executeGoals(List.of("clean", "verify")), () -> {
+			String msg = "No API errors where detected!";
+			try {
+				return msg + System.lineSeparator()
+						+ verifier.loadFile(verifier.getBasedir(), verifier.getLogFileName(), false).stream()
+								.collect(Collectors.joining(System.lineSeparator()));
+			} catch (VerificationException e) {
+				return msg;
+			}
+		});
+		// check summary output
 		verifier.verifyTextInLog("4 API ERRORS");
 		verifier.verifyTextInLog("0 API warnings");
-		verifier.verifyTextInLog("The type bundle.ApiInterface has been removed from api-bundle");
-		verifier.verifyTextInLog("The type bundle.InterfaceA has been removed from api-bundle");
-		verifier.verifyTextInLog("The type bundle.ClassA has been removed from api-bundle");
+		// check error output has source references and lines
 		verifier.verifyTextInLog(
-				"The major version should be incremented in version 0.0.1, since API breakage occurred since version 0.0.1");
-
+				"File ApiInterface.java at line 2: The type bundle.ApiInterface has been removed from api-bundle");
+		verifier.verifyTextInLog("File ClassA.java at line 5: The type bundle.ClassA has been removed from api-bundle");
+		verifier.verifyTextInLog(
+				"File MANIFEST.MF at line 0: The type bundle.InterfaceA has been removed from api-bundle");
+		verifier.verifyTextInLog(
+				"File MANIFEST.MF at line 5: The major version should be incremented in version 0.0.1, since API breakage occurred since version 0.0.1");
+		// now check for the build error output
+		verifier.verifyTextInLog("on project api-bundle-1: There are API errors:");
+		verifier.verifyTextInLog(
+				"src/bundle/ApiInterface.java:2 The type bundle.ApiInterface has been removed from api-bundle");
+		verifier.verifyTextInLog(
+				"src/bundle/ClassA.java:5 The type bundle.ClassA has been removed from api-bundle-1_0.0.1");
+		verifier.verifyTextInLog("META-INF/MANIFEST.MF:0 The type bundle.InterfaceA has been removed from api-bundle");
+		verifier.verifyTextInLog(
+				"META-INF/MANIFEST.MF:5 The major version should be incremented in version 0.0.1, since API breakage occurred since version 0.0.1");
 		// TODO: check with api-filter
 		// TODO: check with second plugin with BREE?
 	}
