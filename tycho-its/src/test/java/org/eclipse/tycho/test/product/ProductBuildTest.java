954c96e35f3bd66f4fc9e1c94b2d47e670176596
Hannes Wellmann <wellmann.hannes1@gmx.net>
2023-10-15 10:45:02 +0200

    Add support for Product Update-site names and a test-case for that
    This a work-around to have
    https://github.com/eclipse-equinox/p2/pull/353 available now and can be
    reverted once the mentioned change in P2 is available in Tycho.

--- tycho-its/src/test/java/org/eclipse/tycho/test/product/ProductBuildTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/product/ProductBuildTest.java
@@ -9,11 +9,15 @@
  *******************************************************************************/
 package org.eclipse.tycho.test.product;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +25,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.TargetEnvironment;
 import org.eclipse.tycho.TychoConstants;
 import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
 import org.junit.Test;
@@ -66,6 +71,54 @@ public class ProductBuildTest extends AbstractTychoIntegrationTest {
 			assertTrue("property " + property + " is missing", properties.containsKey(property));
 			assertFalse("property " + property + " is present but empty", properties.get(property).isBlank());
 
+		}
+	}
+
+	@Test
+	public void testAdditionOfUpdateRepositories() throws Exception {
+		Verifier verifier = getVerifier("product.update_repository", true);
+		verifier.executeGoals(List.of("clean", "verify"));
+		verifier.verifyErrorFreeLog();
+		TargetEnvironment env = TargetEnvironment.getRunningEnvironment();
+		Path baseDir = Path.of(verifier.getBasedir());
+		Path productPath = baseDir.resolve("target/products/aProduct");
+		Path osPlatformPath = productPath.resolve(Path.of(env.getOs(), env.getWs(), env.getArch()));
+		if (env.getOs().equals("macosx")) {
+			osPlatformPath = osPlatformPath.resolve("Eclipse.app/Contents/Eclipse");
+		}
+		Path p2EnginePath = osPlatformPath.resolve("p2/org.eclipse.equinox.p2.engine");
+
+		List<UpdateSiteReference> expectedReferences = List.of(
+				new UpdateSiteReference("https://foo.bar.org", null, true),
+				new UpdateSiteReference("https://foo.bar.org/releases", "Latest release", true),
+				new UpdateSiteReference("https://foo.bar.org/snapshots", "Latest snapshot", false));
+
+		assertUpdateRepositoryReferences(expectedReferences,
+				p2EnginePath.resolve(".settings/org.eclipse.equinox.p2.artifact.repository.prefs"));
+		assertUpdateRepositoryReferences(expectedReferences,
+				p2EnginePath.resolve(".settings/org.eclipse.equinox.p2.metadata.repository.prefs"));
+
+		assertUpdateRepositoryReferences(expectedReferences, p2EnginePath.resolve(
+				"profileRegistry/DefaultProfile.profile/.data/.settings/org.eclipse.equinox.p2.artifact.repository.prefs"));
+		assertUpdateRepositoryReferences(expectedReferences, p2EnginePath.resolve(
+				"profileRegistry/DefaultProfile.profile/.data/.settings/org.eclipse.equinox.p2.metadata.repository.prefs"));
+	}
+
+	private record UpdateSiteReference(String uri, String name, boolean enabled) {
+	}
+
+	private static void assertUpdateRepositoryReferences(List<UpdateSiteReference> expectedReferences, Path path)
+			throws IOException {
+		List<String> prefLines = Files.readAllLines(path);
+		for (UpdateSiteReference reference : expectedReferences) {
+			String preferencePrefix = "repositories/" + reference.uri.replace(":", "\\:").replace("/", "_");
+			assertThat(prefLines, hasItem(preferencePrefix + "/uri=" + reference.uri.replace(":", "\\:")));
+			assertThat(prefLines, hasItem(preferencePrefix + "/enabled=" + reference.enabled));
+			if (reference.name != null) {
+				assertThat(prefLines, hasItem(preferencePrefix + "/nickname=" + reference.name));
+			} else {
+				assertFalse(prefLines.stream().anyMatch(l -> l.startsWith(preferencePrefix + "/nickname=")));
+			}
 		}
 	}
 }
