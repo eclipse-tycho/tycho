3b420369d72e02a61e79b3ceda243853087d51d5
Marco Lehmann-Mörz <mlehmannm@googlemail.com>
2023-10-28 16:08:22 +0200

    fix #2877 (#2878)
    Retain .qualifier/-SNAPSHOT on version bump
    
    fix #2877

--- tycho-versions-plugin/src/main/java/org/eclipse/tycho/versions/engine/Versions.java
+++ tycho-versions-plugin/src/main/java/org/eclipse/tycho/versions/engine/Versions.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Sonatype Inc. and others.
+ * Copyright (c) 2011, 2023 Sonatype Inc. and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -9,39 +9,38 @@
  *
  * Contributors:
  *    Sonatype Inc. - initial API and implementation
+ *    Marco Lehmann-Mörz - issue #2877 - tycho-versions-plugin:bump-versions does not honor SNAPSHOT suffix
  *******************************************************************************/
 package org.eclipse.tycho.versions.engine;
 
 import java.io.File;
 
+import org.eclipse.tycho.TychoConstants;
 import org.osgi.framework.Version;
 
 public class Versions {
-    private static final String SUFFIX_QUALIFIER = ".qualifier";
-
-    private static final String SUFFIX_SNAPSHOT = "-SNAPSHOT";
-
     public static String toCanonicalVersion(String version) {
         if (version == null) {
             return null;
         }
 
-        if (version.endsWith(SUFFIX_SNAPSHOT)) {
-            return version.substring(0, version.length() - SUFFIX_SNAPSHOT.length()) + SUFFIX_QUALIFIER;
+        if (version.endsWith(TychoConstants.SUFFIX_SNAPSHOT)) {
+            return version.substring(0, version.length() - TychoConstants.SUFFIX_SNAPSHOT.length())
+                    + TychoConstants.SUFFIX_QUALIFIER;
         }
 
         return version;
     }
 
     public static String incrementVersion(String version, int increment) {
-        boolean isSnapshot = version.endsWith(SUFFIX_SNAPSHOT);
+        boolean isSnapshot = version.endsWith(TychoConstants.SUFFIX_SNAPSHOT);
         if (isSnapshot) {
-            version = version.substring(0, version.length() - SUFFIX_SNAPSHOT.length());
+            version = version.substring(0, version.length() - TychoConstants.SUFFIX_SNAPSHOT.length());
         }
         Version osgi = new Version(version);
         String incremented = osgi.getMajor() + "." + osgi.getMinor() + "." + (osgi.getMicro() + increment);
         if (isSnapshot) {
-            incremented += SUFFIX_SNAPSHOT;
+            incremented += TychoConstants.SUFFIX_SNAPSHOT;
         }
         return incremented;
     }
@@ -54,11 +53,11 @@ public class Versions {
             return null;
         }
 
-        if (version.endsWith(SUFFIX_SNAPSHOT)) {
-            return version.substring(0, version.length() - SUFFIX_SNAPSHOT.length());
+        if (version.endsWith(TychoConstants.SUFFIX_SNAPSHOT)) {
+            return version.substring(0, version.length() - TychoConstants.SUFFIX_SNAPSHOT.length());
         }
-        if (version.endsWith(SUFFIX_QUALIFIER)) {
-            return version.substring(0, version.length() - SUFFIX_QUALIFIER.length());
+        if (version.endsWith(TychoConstants.SUFFIX_QUALIFIER)) {
+            return version.substring(0, version.length() - TychoConstants.SUFFIX_QUALIFIER.length());
         }
 
         return version;
@@ -74,8 +73,9 @@ public class Versions {
             return null;
         }
 
-        if (version.endsWith(SUFFIX_QUALIFIER)) {
-            return version.substring(0, version.length() - SUFFIX_QUALIFIER.length()) + SUFFIX_SNAPSHOT;
+        if (version.endsWith(TychoConstants.SUFFIX_QUALIFIER)) {
+            return version.substring(0, version.length() - TychoConstants.SUFFIX_QUALIFIER.length())
+                    + TychoConstants.SUFFIX_SNAPSHOT;
         }
 
         return version;
