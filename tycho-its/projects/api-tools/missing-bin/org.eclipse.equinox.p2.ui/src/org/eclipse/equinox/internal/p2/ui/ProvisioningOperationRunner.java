eae3e25d6e566c38d26fb3343d7aaea52bed358b
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-11-08 16:02:45 +0100

    Add testcase to reproduce failing behavior
    See https://github.com/eclipse-tycho/tycho/issues/3019

---
+++ tycho-its/projects/api-tools/missing-bin/org.eclipse.equinox.p2.ui/src/org/eclipse/equinox/internal/p2/ui/ProvisioningOperationRunner.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ *  Copyright (c) 2007, 2018 IBM Corporation and others.
+ *
+ *  This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License 2.0
+ *  which accompanies this distribution, and is available at
+ *  https://www.eclipse.org/legal/epl-2.0/
+ *
+ *  SPDX-License-Identifier: EPL-2.0
+ *
+ *  Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Red Hat Inc. - Bug 460967
+ *******************************************************************************/
+package org.eclipse.equinox.internal.p2.ui;
+
+/**
+ * Utility methods for running provisioning operations. Operations can either be
+ * run synchronously or in a job. When scheduled as a job, the operation
+ * determines whether the job is run in the background or in the UI.
+ *
+ * @since 3.4
+ */
+public class ProvisioningOperationRunner {
+
+}
