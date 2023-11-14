a518838eaa1e509bf2af202616f8882982201f57
Martin D'Aloia <martindaloia@gmail.com>
2023-11-13 22:19:28 -0300

    Add a reproducer to show that PR #2978 was a partial fix
    The fix of PR #2978 was a partial fix for issue #2977. It fixed the case
    of a single bundled but when the bundle providing/requiring the virtual
    IU is required by another bundle it fails with the same error as before
    even with Tycho 4.0.4:
    
    ```
    [ERROR] Cannot resolve project dependencies:
    [ERROR]   Software being installed: pvumb.bundle2 1.0.0.qualifier
    [ERROR]   Missing requirement: pvumb.bundle1 1.0.0.qualifier requires
    'org.eclipse.equinox.p2.iu; configure.pvumb.bundle1 0.0.0' but it could
    not be found
    [ERROR]   Cannot satisfy dependency: pvumb.bundle2 1.0.0.qualifier
    depends on: osgi.bundle; pvumb.bundle1 0.0.0
    ```
    
    A workaround is to use:
    
    ```
    requires.0.optional=true
    requires.0.greedy=true
    ```

--- tycho-its/src/test/java/org/eclipse/tycho/test/p2Inf/VirtualUnitTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/p2Inf/VirtualUnitTest.java
@@ -10,8 +10,13 @@
 package org.eclipse.tycho.test.p2Inf;
 
 import static java.util.Arrays.asList;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.apache.maven.it.Verifier;
 import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
@@ -21,24 +26,22 @@ import de.pdark.decentxml.Document;
 import de.pdark.decentxml.Element;
 import de.pdark.decentxml.XMLParser;
 
-import java.io.File;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
-
 /**
- * Test that a virtual IU created just with metadata in a p2.inf file can be required
- * in the same p2.inf file.
+ * Test that a virtual IU created just with metadata in a p2.inf file can be
+ * required in the same p2.inf file.
  * <p>
- * It used to work until 3.0.5 (using <code><pomDependencies>consider</pomDependencies></code>)
- * but since 4.0.0 failed with:
+ * It used to work until 3.0.5 (using
+ * <code><pomDependencies>consider</pomDependencies></code>) but since 4.0.0
+ * failed with:
+ * 
  * <pre>
  * Cannot resolve project dependencies:
  *   Software being installed: pvu.bundle 1.0.0.qualifier
  *   Missing requirement: pvu.bundle 1.0.0.qualifier requires 'org.eclipse.equinox.p2.iu; configure.pvu.bundle 0.0.0' but it could not be found
  * </pre>
  *
- * See https://github.com/eclipse-tycho/tycho/blob/master/RELEASE_NOTES.md#mixed-reactor-setups-require-the-new-resolver-now
+ * See
+ * https://github.com/eclipse-tycho/tycho/blob/master/RELEASE_NOTES.md#mixed-reactor-setups-require-the-new-resolver-now
  * and https://github.com/eclipse-tycho/tycho/issues/2977
  */
 public class VirtualUnitTest extends AbstractTychoIntegrationTest {
@@ -52,10 +55,7 @@ public class VirtualUnitTest extends AbstractTychoIntegrationTest {
 		String hostUnitId = "pvu.bundle";
 		String configureUnitId = "configure.pvu.bundle";
 
-		File p2Content = new File(verifier.getBasedir(), "bundle/target/p2content.xml");
-		Document doc = XMLParser.parse(p2Content);
-
-		List<Element> units = doc.getChild("units").getChildren("unit");
+		List<Element> units = getUnits(verifier.getBasedir(), "bundle/target/p2content.xml");
 		Optional<Element> hostUnit = findUnit(units, hostUnitId);
 		Optional<Element> configureUnit = findUnit(units, configureUnitId);
 
@@ -67,10 +67,49 @@ public class VirtualUnitTest extends AbstractTychoIntegrationTest {
 				hostUnitRequirements.anyMatch(elem -> configureUnitId.equals(elem.getAttributeValue("name"))));
 	}
 
+	@Test
+	public void testVirtualUnitMultiBundleWithRequirementDoesNotFailBuild() throws Exception {
+		Verifier verifier = getVerifier("/p2Inf.virtualUnit.multiBundle", false);
+		verifier.executeGoals(asList("verify"));
+		verifier.verifyErrorFreeLog();
+
+		// Host bundle and virtual IU assertions
+		String hostUnitId = "pvumb.bundle1";
+		String configureUnitId = "configure.pvumb.bundle1";
+
+		List<Element> units = getUnits(verifier.getBasedir(), "bundle1/target/p2content.xml");
+		Optional<Element> hostUnit = findUnit(units, hostUnitId);
+		Optional<Element> configureUnit = findUnit(units, configureUnitId);
+
+		Stream<Element> hostUnitRequirements = findRequirements(hostUnit);
+
+		assertTrue("Host IU " + hostUnitId + " not found", hostUnit.isPresent());
+		assertTrue("Configure IU " + configureUnitId + " not found", configureUnit.isPresent());
+		assertTrue("Requirement of IU " + configureUnitId + " not found in IU " + hostUnitId,
+				hostUnitRequirements.anyMatch(elem -> configureUnitId.equals(elem.getAttributeValue("name"))));
+
+		// Client bundle assertions
+		String clientUnitId = "pvumb.bundle2";
+
+		units = getUnits(verifier.getBasedir(), "bundle2/target/p2content.xml");
+		Optional<Element> clientUnit = findUnit(units, clientUnitId);
+
+		Stream<Element> clientUnitRequirements = findRequirements(clientUnit);
+
+		assertTrue("Client IU " + clientUnitId + " not found", clientUnit.isPresent());
+		assertTrue("Requirement of IU " + hostUnitId + " not found in IU " + clientUnitId,
+				clientUnitRequirements.anyMatch(elem -> hostUnitId.equals(elem.getAttributeValue("name"))));
+	}
+
+	private static List<Element> getUnits(String baseDir, String filePath) throws IOException {
+		File p2Content = new File(baseDir, filePath);
+		Document doc = XMLParser.parse(p2Content);
+
+		return doc.getChild("units").getChildren("unit");
+	}
+
 	private static Optional<Element> findUnit(List<Element> units, String hostUnitId) {
-		return units.stream()
-				.filter(elem -> hostUnitId.equals(elem.getAttributeValue("id")))
-				.findFirst();
+		return units.stream().filter(elem -> hostUnitId.equals(elem.getAttributeValue("id"))).findFirst();
 	}
 
 	private static Stream<Element> findRequirements(Optional<Element> hostUnit) {
