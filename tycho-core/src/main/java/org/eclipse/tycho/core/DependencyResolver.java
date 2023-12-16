db7f8e8ed34ee482c4a9de5ab80ef9f37b2e350c
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-12-16 05:56:15 +0100

    Cleanup the DependencyResolver interface and remove obsolete params
    (cherry picked from commit 70da15428e5c49b85d685a9fb5fd9b83f7cdc201)

--- tycho-core/src/main/java/org/eclipse/tycho/core/DependencyResolver.java
+++ tycho-core/src/main/java/org/eclipse/tycho/core/DependencyResolver.java
@@ -33,8 +33,7 @@ public interface DependencyResolver {
 
     PomDependencyCollector resolvePomDependencies(MavenSession session, MavenProject project);
 
-    public TargetPlatform computePreliminaryTargetPlatform(MavenSession session, MavenProject project,
-            List<ReactorProject> reactorProjects);
+    public TargetPlatform computePreliminaryTargetPlatform(MavenSession session, MavenProject project);
 
     /**
      * @param targetPlatform
@@ -42,8 +41,8 @@ public interface DependencyResolver {
      *            <code>null</code>, the final target platform of the project will be used.
      */
     public DependencyArtifacts resolveDependencies(MavenSession session, MavenProject project,
-            TargetPlatform targetPlatform, List<ReactorProject> reactorProjects,
-            DependencyResolverConfiguration resolverConfiguration, List<TargetEnvironment> environments);
+            TargetPlatform targetPlatform, DependencyResolverConfiguration resolverConfiguration,
+            List<TargetEnvironment> environments);
 
     public void injectDependenciesIntoMavenModel(MavenProject project, TychoProject projectType,
             DependencyArtifacts resolvedDependencies, DependencyArtifacts testDepedencyArtifacts, Logger logger);
