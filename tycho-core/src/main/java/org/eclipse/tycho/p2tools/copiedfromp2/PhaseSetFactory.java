9f1a7ea926047a49aa5eff8c0f1664290f238697
Christoph Läubrich <laeubi@laeubi-soft.de>
2024-01-25 17:01:25 +0100

    Skip CHECK_TRUST phase and allow parallel execution of product assembly
    Currently the DirectorApplication effectively accepts all content but
    executing the CHECK_TRUST already takes considerable amount of time,
    also each product is assembled one by one even though they operate on
    independent output folders and files.
    
    This now skips the CHECK_TRUST phase and adds a new parameter <parallel>
    that can be set to enable the parallel execution of assembly and
    archiving different product variants.

--- tycho-core/src/main/java/org/eclipse/tycho/p2tools/copiedfromp2/PhaseSetFactory.java
+++ tycho-core/src/main/java/org/eclipse/tycho/p2tools/copiedfromp2/PhaseSetFactory.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.eclipse.equinox.internal.p2.engine.EngineActivator;
 import org.eclipse.equinox.internal.p2.engine.Phase;
 import org.eclipse.equinox.internal.p2.engine.PhaseSet;
 import org.eclipse.equinox.internal.p2.engine.SizingPhaseSet;
@@ -38,48 +37,48 @@ import org.eclipse.equinox.p2.engine.ISizingPhaseSet;
 public class PhaseSetFactory {
 
     private static final boolean forcedUninstall = Boolean
-            .parseBoolean(EngineActivator.getContext().getProperty("org.eclipse.equinox.p2.engine.forcedUninstall")); //$NON-NLS-1$
+            .parseBoolean(System.getProperty("org.eclipse.equinox.p2.engine.forcedUninstall")); //$NON-NLS-1$
 
     /**
      * A phase id (value "checkTrust") describing the certificate trust check phase. This phase
      * examines the code signing certificates of the artifacts being installed to ensure they are
      * signed and trusted by the running system.
      */
-    public static String PHASE_CHECK_TRUST = "checkTrust"; //$NON-NLS-1$
+    public static final String PHASE_CHECK_TRUST = "checkTrust"; //$NON-NLS-1$
     /**
      * A phase id (value "collect") describing the collect phase. This phase gathers all the
      * artifacts to be installed, typically by copying them from some repository into a suitable
      * local location for the application being installed.
      */
-    public static String PHASE_COLLECT = "collect"; //$NON-NLS-1$
+    public static final String PHASE_COLLECT = "collect"; //$NON-NLS-1$
     /**
      * A phase id (value "configure") describing the configuration phase. This phase writes
      * configuration data related to the software being provisioned. Until configuration occurs the
      * end user of the software will be have access to the installed functionality.
      */
-    public static String PHASE_CONFIGURE = "configure"; //$NON-NLS-1$
+    public static final String PHASE_CONFIGURE = "configure"; //$NON-NLS-1$
     /**
      * A phase id (value "install") describing the install phase. This phase performs any necessary
      * transformations on the downloaded artifacts to put them in the correct shape for the running
      * application, such as decompressing or moving content, setting file permissions, etc).
      */
-    public static String PHASE_INSTALL = "install"; //$NON-NLS-1$
+    public static final String PHASE_INSTALL = "install"; //$NON-NLS-1$
     /**
      * A phase id (value "property") describing the property modification phase. This phase performs
      * changes to profile properties.
      */
-    public static String PHASE_PROPERTY = "property"; //$NON-NLS-1$
+    public static final String PHASE_PROPERTY = "property"; //$NON-NLS-1$
     /**
      * A phase id (value "unconfigure") describing the unconfigure phase. This phase removes
      * configuration data related to the software being removed. This phase is the inverse of the
      * changes performed in the configure phase.
      */
-    public static String PHASE_UNCONFIGURE = "unconfigure"; //$NON-NLS-1$
+    public static final String PHASE_UNCONFIGURE = "unconfigure"; //$NON-NLS-1$
     /**
      * A phase id (value "uninstall") describing the uninstall phase. This phase removes artifacts
      * from the system being provisioned that are no longer required in the new profile.
      */
-    public static String PHASE_UNINSTALL = "uninstall"; //$NON-NLS-1$
+    public static final String PHASE_UNINSTALL = "uninstall"; //$NON-NLS-1$
 
     private static final List<String> ALL_PHASES_LIST = Arrays.asList(new String[] { PHASE_COLLECT, PHASE_UNCONFIGURE,
             PHASE_UNINSTALL, PHASE_PROPERTY, PHASE_CHECK_TRUST, PHASE_INSTALL, PHASE_CONFIGURE });
