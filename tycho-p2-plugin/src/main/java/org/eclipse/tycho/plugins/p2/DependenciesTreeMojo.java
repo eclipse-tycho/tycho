fea2bf856d5849bb5ecc014b07e45e98e7220171
Patrick Ziegler <ziegler.patrick@hotmail.de>
2024-01-26 17:09:05 +0100

    Move p2 dependency tree logic from tycho-p2-plugin to tycho-core
    This moves the algorithm which is responsible for calculating the IU
    dependency tree used by the dependency-tree Mojo to tycho-core, so that
    it can be used by other projects.
    
    For a given project, this tree can be calculated using the
    P2DependencyTreeGenerator, which can be injected into a Mojo as a simple
    Plexus component.

--- tycho-p2-plugin/src/main/java/org/eclipse/tycho/plugins/p2/DependenciesTreeMojo.java
+++ tycho-p2-plugin/src/main/java/org/eclipse/tycho/plugins/p2/DependenciesTreeMojo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 Christoph Läubrich and others.
+ * Copyright (c) 2022, 2024 Christoph Läubrich and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -10,22 +10,15 @@
 package org.eclipse.tycho.plugins.p2;
 
 import java.util.AbstractMap.SimpleEntry;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.LegacySupport;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
@@ -41,74 +34,65 @@ import org.eclipse.tycho.ReactorProject;
 import org.eclipse.tycho.core.TychoProject;
 import org.eclipse.tycho.core.TychoProjectManager;
 import org.eclipse.tycho.core.osgitools.DefaultReactorProject;
-import org.eclipse.tycho.p2maven.InstallableUnitGenerator;
+import org.eclipse.tycho.p2.tools.P2DependencyTreeGenerator;
+import org.eclipse.tycho.p2.tools.P2DependencyTreeGenerator.DependencyTreeNode;
 
 /**
  * Similar to dependency:tree outputs a tree of P2 dependencies.
  */
 @Mojo(name = "dependency-tree", requiresProject = true, threadSafe = true, requiresDependencyCollection = ResolutionScope.TEST)
 public class DependenciesTreeMojo extends AbstractMojo {
-
-    private static final Comparator<IInstallableUnit> COMPARATOR = Comparator.comparing(IInstallableUnit::getId,
-            String.CASE_INSENSITIVE_ORDER);
     @Parameter(property = "project")
     private MavenProject project;
 
     @Component
-    private InstallableUnitGenerator generator;
-
-    @Component
-    private LegacySupport legacySupport;
+    private P2DependencyTreeGenerator generator;
 
     @Component
     private TychoProjectManager projectManager;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        //TODO maybe we can compute a org.apache.maven.shared.dependency.graph.DependencyNode and reuse org.apache.maven.plugins.dependency.tree.TreeMojo wich has a getSerializingDependencyNodeVisitor
-
         Optional<TychoProject> tychoProject = projectManager.getTychoProject(project);
         if (tychoProject.isEmpty()) {
             return;
         }
 
-        Set<String> written = new HashSet<String>();
-        written.add(project.getId());
         getLog().info(project.getId());
 
-        List<ArtifactDescriptor> artifacts = tychoProject.get()
-                .getDependencyArtifacts(DefaultReactorProject.adapt(project)).getArtifacts();
-        Map<IInstallableUnit, Set<ReactorProject>> projectMap = artifacts.stream()
-                .filter(a -> a.getMavenProject() != null).flatMap(a -> {
-                    return a.getInstallableUnits().stream().map(iu -> new SimpleEntry<>(iu, a.getMavenProject()));
-                })
+        List<ArtifactDescriptor> artifacts = tychoProject.get() //
+                .getDependencyArtifacts(DefaultReactorProject.adapt(project)) //
+                .getArtifacts();
+        Map<IInstallableUnit, Set<ReactorProject>> projectMap = artifacts.stream() //
+                .filter(a -> a.getMavenProject() != null) //
+                .flatMap(a -> a.getInstallableUnits().stream().map(iu -> new SimpleEntry<>(iu, a.getMavenProject()))) //
                 .collect(Collectors.groupingBy(Entry::getKey, Collectors.mapping(Entry::getValue, Collectors.toSet())));
-        Set<IInstallableUnit> units = artifacts.stream().flatMap(d -> d.getInstallableUnits().stream())
-                .collect(Collectors.toCollection(HashSet::new));
-        List<IInstallableUnit> initial;
+
+        Set<IInstallableUnit> unmapped = new HashSet<>();
+        List<DependencyTreeNode> dependencyTree;
         try {
-            initial = new ArrayList<IInstallableUnit>(
-                    generator.getInstallableUnits(project, legacySupport.getSession(), false));
+            dependencyTree = generator.buildDependencyTree(project, unmapped);
         } catch (CoreException e) {
             throw new MojoFailureException(e);
         }
-        units.removeAll(initial);
-        int size = initial.size();
-        for (int i = 0; i < size; i++) {
-            IInstallableUnit unit = initial.get(i);
-            printUnit(unit, null, units, projectMap, 0, i == size - 1);
+
+        for (DependencyTreeNode rootNode : dependencyTree) {
+            printUnit(rootNode, projectMap, 0);
         }
-        if (!units.isEmpty()) {
+
+        if (!unmapped.isEmpty()) {
             getLog().info("Units that cannot be matched to any requirement:");
-            for (IInstallableUnit unit : units) {
+            for (IInstallableUnit unit : unmapped) {
                 getLog().info(unit.toString());
             }
         }
 
     }
 
-    private void printUnit(IInstallableUnit unit, IRequirement satisfies, Set<IInstallableUnit> units,
-            Map<IInstallableUnit, Set<ReactorProject>> projectMap, int indent, boolean last) {
+    private void printUnit(DependencyTreeNode model, Map<IInstallableUnit, Set<ReactorProject>> projectMap,
+            int indent) {
+        IInstallableUnit unit = model.getInstallableUnit();
+        IRequirement satisfies = model.getRequirement();
         StringBuffer line = new StringBuffer();
         for (int i = 0; i < indent; i++) {
             line.append("   ");
@@ -129,24 +113,8 @@ public class DependenciesTreeMojo extends AbstractMojo {
                     .collect(Collectors.joining(", ", "[", "]")));
         }
         getLog().info(line.toString());
-        List<IInstallableUnit> collected = new ArrayList<IInstallableUnit>();
-        Map<IInstallableUnit, IRequirement> requirementsMap = new HashMap<IInstallableUnit, IRequirement>();
-        Stream.concat(unit.getRequirements().stream(), unit.getMetaRequirements().stream()).forEach(requirement -> {
-            for (Iterator<IInstallableUnit> iterator = units.iterator(); iterator.hasNext();) {
-                IInstallableUnit other = iterator.next();
-                if (other.satisfies(requirement)) {
-                    collected.add(other);
-                    requirementsMap.put(other, requirement);
-                    iterator.remove();
-                }
-            }
-        });
-        Collections.sort(collected, COMPARATOR);
-        int size = collected.size();
-        for (int i = 0; i < size; i++) {
-            IInstallableUnit iu = collected.get(i);
-            printUnit(iu, requirementsMap.get(iu), units, projectMap, indent + 1, i == size - 1);
+        for (DependencyTreeNode child : model.getChildren()) {
+            printUnit(child, projectMap, indent + 1);
         }
     }
-
 }
