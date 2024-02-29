d23230d9ea1c03e64184f3c69bfe69499e815ef8
Heiko Klare <heiko.klare@vector.com>
2024-02-29 10:36:34 +0100

    Add test case for PublishFeaturesAndBundlesMojo marking bundle unpacked
    The PublishFeaturesAndBundlesMojo produces a p2 repository in which
    plugins are marked as "zipped" and will thus be unpacked upon
    installation when a feature including them does not include the plugin
    with "unpack=false". The added test demonstrates this behavior and
    ensures that only the feature will properly be marked as "zipped" but
    not the included plugin.

--- tycho-its/src/test/java/org/eclipse/tycho/test/P2ExtrasPlugin.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/P2ExtrasPlugin.java
@@ -12,13 +12,24 @@
  *******************************************************************************/
 package org.eclipse.tycho.test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.junit.Test;
+
+import de.pdark.decentxml.Document;
+import de.pdark.decentxml.Element;
+import de.pdark.decentxml.XMLIOSource;
+import de.pdark.decentxml.XMLParser;
 
 public class P2ExtrasPlugin extends AbstractTychoIntegrationTest {
 
@@ -127,6 +138,40 @@ public class P2ExtrasPlugin extends AbstractTychoIntegrationTest {
 				verifier.verifyTextInLog("Failed to execute goal org.eclipse.tycho.extras:tycho-p2-extras-plugin");
 			}
 		}
+	}
+
+	@Test
+	public void testPublishFeaturesAndBundles_noUnpack() throws Exception {
+		final String pluginId = "test_plugin";
+		final String featureId = "test_feature.feature.jar";
+
+		Verifier verifier = getVerifier("p2extra/publisherNoUnpack", false, true);
+		verifier.executeGoals(List.of("clean", "package"));
+
+		Path contentXml = Path.of(verifier.getBasedir()).resolve("target/repository").resolve("content.xml");
+		Element pluginUnitInContentXml = extractUnitFromContentXml(contentXml, pluginId);
+		assertFalse("test plugin should not be marked as zipped", hasChildWithZippedAttribute(pluginUnitInContentXml));
+		Element featureUnitInContentXml = extractUnitFromContentXml(contentXml, featureId);
+		assertTrue("test feature should be marked as zipped", hasChildWithZippedAttribute(featureUnitInContentXml));
+	}
+
+	private static Element extractUnitFromContentXml(Path contentXml, String unitName) throws IOException {
+		XMLParser parser = new XMLParser();
+		Document document = parser.parse(new XMLIOSource(contentXml.toFile()));
+		Element unitElement = document.getChild("repository/units");
+		List<Element> units = unitElement.getChildren("unit");
+		Optional<Element> extractedUnit = units.stream()
+				.filter(element -> unitName.equals(element.getAttribute("id").getValue())).findFirst();
+		assertTrue(String.format("Unit with name '%s' not found in content.xml with units: %s", unitName, units),
+				extractedUnit.isPresent());
+		return extractedUnit.get();
+	}
+
+	private static boolean hasChildWithZippedAttribute(Element element) {
+		if ("zipped".equals(element.getAttributeValue("key"))) {
+			return true;
+		}
+		return element.getChildren().stream().anyMatch(P2ExtrasPlugin::hasChildWithZippedAttribute);
 	}
 
 }
