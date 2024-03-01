53191417481c97057e249a48d62d20628618d1ec
ivumwd <mwd@ivu.de>
2024-03-01 16:08:50 +0100

    reproduced #2983

---
+++ tycho-its/projects/TYCHO2983siteWithPubishedFeatures/helloworld/src/helloworld/MessageProvider.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 SAP AG and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     SAP AG - initial API and implementation
+ *******************************************************************************/
+package helloworld;
+
+public class MessageProvider {
+
+    public String getGreeting() {
+        return getGreeting("World");
+    }
+
+    public String getGreeting(String receiver) {
+        return "Hello " + receiver + "!!";
+    }
+
+}
---
+++ tycho-its/src/test/java/org/eclipse/tycho/test/TYCHO2983siteWithPubishedFeatures/TYCHO2983siteWithPubishedFeatures.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2023 SAP AG and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.tycho.test.TYCHO2983siteWithPubishedFeatures;
+
+import org.apache.maven.it.Verifier;
+import org.eclipse.tycho.TychoConstants;
+import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
+import org.eclipse.tycho.test.util.XMLTool;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class TYCHO2983siteWithPubishedFeatures extends AbstractTychoIntegrationTest {
+
+    @Test
+    public void testCheckSiteFeatures() throws Exception {
+        Verifier verifier = getVerifier("/TYCHO2983siteWithPubishedFeatures", false);
+        verifier.executeGoal("verify");
+        verifier.verifyErrorFreeLog();
+
+        File siteFeaturesFolder = new File(verifier.getBasedir(), "helloworld.updatesite/target/repository/features");
+
+        assertTrue(siteFeaturesFolder.exists());
+        assertNotNull(Arrays.stream(siteFeaturesFolder.listFiles()).filter(
+                file -> file.getName().startsWith("helloworld.feature"))
+                .findAny()
+                .orElse(null)
+        );
+        assertNotNull(Arrays.stream(siteFeaturesFolder.listFiles()).filter(
+                file -> file.getName().startsWith("io.spring.javaformat.eclipse.feature"))
+                .findAny()
+                .orElse(null)
+        );
+    }
+}
