f4752edc0adea7b3b262cf332144cdb78d47b543
Christoph Läubrich <laeubi@laeubi-soft.de>
2024-03-11 09:30:35 +0100

    Refactor Tycho to use copied application
    P2 and Tycho have different demands when it comes to "Applications" e.g.
    Tycho often want to programmatically set things, while in P2 they are
    called in means of CLI. Also in the past it has shown its very hard to
    maintain "extensibility" together with "compatibility".
    
    Because of this, this now also imports the set of Publisher applications
    to Tycho, this will open us with a large set of new opportunities e.g.
    getting rid of argument building just that these arguments are later
    passed back into fields/objects.

--- tycho-core/src/main/java/org/eclipse/tycho/p2tools/copiedfromp2/CategoryPublisherApplication.java
+++ tycho-core/src/main/java/org/eclipse/tycho/p2tools/copiedfromp2/CategoryPublisherApplication.java
@@ -12,82 +12,81 @@
  * Contributors: 
  *   EclipseSource - initial API and implementation
  ******************************************************************************/
-package org.eclipse.equinox.internal.p2.updatesite;
+package org.eclipse.tycho.p2tools.copiedfromp2;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+
 import org.eclipse.core.runtime.URIUtil;
+import org.eclipse.equinox.internal.p2.updatesite.CategoryXMLAction;
 import org.eclipse.equinox.p2.core.IProvisioningAgent;
 import org.eclipse.equinox.p2.core.ProvisionException;
-import org.eclipse.equinox.p2.publisher.*;
+import org.eclipse.equinox.p2.publisher.IPublisherAction;
+import org.eclipse.equinox.p2.publisher.Publisher;
+import org.eclipse.equinox.p2.publisher.PublisherInfo;
 import org.eclipse.equinox.p2.repository.IRepository;
 import org.eclipse.equinox.p2.repository.metadata.IMetadataRepository;
 
 /**
  * <p>
- * This application categorizes the elements in a repo based on a category
- * definition file. The category definition file is specified with
- * <source>-categoryDefinition</source>
+ * This application categorizes the elements in a repo based on a category definition file. The
+ * category definition file is specified with <source>-categoryDefinition</source>
  * </p>
  */
 public class CategoryPublisherApplication extends AbstractPublisherApplication {
 
-	private String categoryQualifier = null;
-	private URI categoryDefinition = null;
+    private String categoryQualifier = null;
+    private URI categoryDefinition = null;
 
-	public CategoryPublisherApplication() {
-		super();
-	}
+    public CategoryPublisherApplication(IProvisioningAgent agent) {
+        super(agent);
+    }
 
-	public CategoryPublisherApplication(IProvisioningAgent agent) {
-		super(agent);
-	}
+    /*
+     * Check to see if an existing repository already has the "compressed" flag set
+     */
+    @Override
+    protected void initializeRepositories(PublisherInfo publisherInfo) throws ProvisionException {
+        try {
+            if (metadataLocation != null) {
+                // Try to load the metadata repository. If it loads, check the "compressed"
+                // flag, and cache it.
+                // If there are any errors loading it (i.e. it doesn't exist), just skip this
+                // step
+                // If there are serious problems with the repository, the superclass
+                // initializeRepositories method
+                // will handle it.
+                IMetadataRepository result = Publisher.loadMetadataRepository(agent, metadataLocation, true, true);
+                if (result != null) {
+                    Object property = result.getProperties().get(IRepository.PROP_COMPRESSED);
+                    if (property != null) {
+                        boolean compressProperty = Boolean.valueOf((String) property);
+                        this.compress = compressProperty || compress;
+                    }
+                }
+            }
+        } catch (ProvisionException e) {
+            // do nothing
+        }
+        super.initializeRepositories(publisherInfo);
+    }
 
-	/*
-	 * Check to see if an existing repository already has the "compressed" flag set
-	 */
-	@Override
-	protected void initializeRepositories(PublisherInfo publisherInfo) throws ProvisionException {
-		try {
-			if (metadataLocation != null) {
-				// Try to load the metadata repository. If it loads, check the "compressed"
-				// flag, and cache it.
-				// If there are any errors loading it (i.e. it doesn't exist), just skip this
-				// step
-				// If there are serious problems with the repository, the superclass
-				// initializeRepositories method
-				// will handle it.
-				IMetadataRepository result = Publisher.loadMetadataRepository(agent, metadataLocation, true, true);
-				if (result != null) {
-					Object property = result.getProperties().get(IRepository.PROP_COMPRESSED);
-					if (property != null) {
-						boolean compressProperty = Boolean.valueOf((String) property);
-						this.compress = compressProperty || compress;
-					}
-				}
-			}
-		} catch (ProvisionException e) {
-			// do nothing
-		}
-		super.initializeRepositories(publisherInfo);
-	}
+    @Override
+    protected void processParameter(String arg, String parameter, PublisherInfo pinfo) throws URISyntaxException {
+        super.processParameter(arg, parameter, pinfo);
 
-	@Override
-	protected void processParameter(String arg, String parameter, PublisherInfo pinfo) throws URISyntaxException {
-		super.processParameter(arg, parameter, pinfo);
+        this.append = true; // Always append, otherwise we will end up with nothing
 
-		this.append = true; // Always append, otherwise we will end up with nothing
+        if (arg.equalsIgnoreCase("-categoryQualifier")) //$NON-NLS-1$
+            categoryQualifier = parameter;
 
-		if (arg.equalsIgnoreCase("-categoryQualifier")) //$NON-NLS-1$
-			categoryQualifier = parameter;
+        if (arg.equalsIgnoreCase("-categoryDefinition")) //$NON-NLS-1$
+            categoryDefinition = URIUtil.fromString(parameter);
 
-		if (arg.equalsIgnoreCase("-categoryDefinition")) //$NON-NLS-1$
-			categoryDefinition = URIUtil.fromString(parameter);
+    }
 
-	}
-
-	@Override
-	protected IPublisherAction[] createActions() {
-		return new IPublisherAction[] { new CategoryXMLAction(categoryDefinition, categoryQualifier) };
-	}
+    @Override
+    protected IPublisherAction[] createActions() {
+        return new IPublisherAction[] { new CategoryXMLAction(categoryDefinition, categoryQualifier) };
+    }
 }
