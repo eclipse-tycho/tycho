1f75cb490c9f0f2abc8c6c2f50d5ae435dc350ef
ivumwd <mwd@ivu.de>
2024-03-27 16:23:01 +0100

    renamed test class

--- tycho-its/src/test/java/org/eclipse/tycho/test/TYCHO2983siteWithPubishedFeatures/TYCHO2983siteWithPubishedFeaturesTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/TYCHO2983siteWithPubishedFeatures/TYCHO2983siteWithPubishedFeaturesTest.java
@@ -14,17 +14,16 @@
 package org.eclipse.tycho.test.TYCHO2983siteWithPubishedFeatures;
 
 import org.apache.maven.it.Verifier;
-import org.eclipse.tycho.TychoConstants;
 import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
-import org.eclipse.tycho.test.util.XMLTool;
 import org.junit.Test;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Objects;
 
 import static org.junit.Assert.*;
 
-public class TYCHO2983siteWithPubishedFeatures extends AbstractTychoIntegrationTest {
+public class TYCHO2983siteWithPubishedFeaturesTest extends AbstractTychoIntegrationTest {
 
     @Test
     public void testCheckSiteFeatures() throws Exception {
@@ -35,12 +34,12 @@ public class TYCHO2983siteWithPubishedFeatures extends AbstractTychoIntegrationT
         File siteFeaturesFolder = new File(verifier.getBasedir(), "helloworld.updatesite/target/repository/features");
 
         assertTrue(siteFeaturesFolder.exists());
-        assertNotNull(Arrays.stream(siteFeaturesFolder.listFiles()).filter(
+        assertNotNull(Arrays.stream(Objects.requireNonNull(siteFeaturesFolder.listFiles())).filter(
                 file -> file.getName().startsWith("helloworld.feature"))
                 .findAny()
                 .orElse(null)
         );
-        assertNotNull(Arrays.stream(siteFeaturesFolder.listFiles()).filter(
+        assertNotNull(Arrays.stream(Objects.requireNonNull(siteFeaturesFolder.listFiles())).filter(
                 file -> file.getName().startsWith("io.spring.javaformat.eclipse.feature"))
                 .findAny()
                 .orElse(null)
