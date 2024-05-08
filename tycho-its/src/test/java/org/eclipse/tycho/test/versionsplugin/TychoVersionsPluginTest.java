eclipse-tycho/tycho

commit f5968e4caa91e4ed42c114aeec75e83fc9382a04

Update versions if the project references another pom as module

tycho-versions-plugin/src/main/java/org/eclipse/tycho/versions/manipulation/PomManipulator.java
@@ -23,6 +23,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -43,9 +45,11 @@ import org.eclipse.tycho.versions.pom.Property;
 
 @Component(role = MetadataManipulator.class, hint = PomManipulator.HINT)
 public class PomManipulator extends AbstractMetadataManipulator {
+    private static final String POM = "pom";
+
     private static final String NULL = "<null>";
 
-    public static final String HINT = "pom";
+    public static final String HINT = POM;
 
     private static final Pattern CI_FRIENDLY_EXPRESSION = Pattern.compile("\\$\\{(.+?)\\}");
 
@@ -53,20 +57,47 @@ public class PomManipulator extends AbstractMetadataManipulator {
     public boolean addMoreChanges(ProjectMetadata project, VersionChangesDescriptor versionChangeContext) {
         PomFile pom = project.getMetadata(PomFile.class);
         if (pom == null) {
-            throw new RuntimeException("no pom avaiable for " + project.getBasedir());
+            throw new RuntimeException("no pom available for " + project.getBasedir());
         }
         GAV parent = pom.getParent();
 
-        boolean moreChanges = false;
-        for (PomVersionChange change : versionChangeContext.getVersionChanges()) {
-            if (parent != null && isGavEquals(parent, change)) {
-                if (isVersionEquals(pom.getVersion(), change.getVersion())) {
-                    moreChanges |= versionChangeContext
-                            .addVersionChange(new PomVersionChange(pom, change.getVersion(), change.getNewVersion()));
+        AtomicBoolean moreChanges = new AtomicBoolean();
+        if (parent != null) {
+            for (PomVersionChange change : versionChangeContext.getVersionChanges()) {
+                if (isGavEquals(parent, change)) {
+                    if (isVersionEquals(pom.getVersion(), change.getVersion())) {
+                        if (versionChangeContext.addVersionChange(
+                                new PomVersionChange(pom, change.getVersion(), change.getNewVersion()))) {
+                            moreChanges.set(true);
+                        }
+                    }
                 }
             }
         }
-        return moreChanges;
+        //if we are about to change we need to check the submodule
+        if (POM.equals(pom.getPackaging())) {
+            Optional<PomVersionChange> thisChange = versionChangeContext.getVersionChanges().stream()
+                    .filter(change -> change.getProject() == pom).findFirst();
+            if (thisChange.isPresent()) {
+                PomVersionChange change = thisChange.get();
+                List<String> modules = pom.getModules();
+                for (String module : modules) {
+                    versionChangeContext.findMetadataByBasedir(new File(project.getBasedir(), module))
+                            .ifPresent(moduleMeta -> {
+                                PomFile modulePom = moduleMeta.getMetadata(PomFile.class);
+                                if (modulePom != null && modulePom.isMutable()
+                                        && POM.equals(modulePom.getPackaging())) {
+                                    if (versionChangeContext.addVersionChange(
+                                            new PomVersionChange(modulePom, change.getNewVersion()))) {
+                                        moreChanges.set(true);
+                                    }
+                                }
+                            });
+                }
+            }
+        }
+
+        return moreChanges.get();
     }
 
     @Override
@@ -115,10 +146,9 @@ public class PomManipulator extends AbstractMetadataManipulator {
                     pom.setVersion(newVersion);
                 }
             } else {
-
                 GAV parent = pom.getParent();
                 if (parent != null && isGavEquals(parent, change) && !isCiFriendly(parent.getVersion())) {
-                    logger.info("  %s//project/version: %s => %s".formatted(pomName, version, newVersion));
+                    logger.info("  %s//project/parent/version: %s => %s".formatted(pomName, version, newVersion));
                     parent.setVersion(newVersion);
                 }
             }

tycho-its/src/test/java/org/eclipse/tycho/test/versionsplugin/TychoVersionsPluginTest.java
@@ -14,20 +14,27 @@ package org.eclipse.tycho.test.versionsplugin;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Properties;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 import org.apache.maven.it.Verifier;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.Parent;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
 import org.eclipse.tycho.version.TychoVersion;
 import org.junit.Test;
+import org.osgi.framework.Constants;
 
 public class TychoVersionsPluginTest extends AbstractTychoIntegrationTest {
 
@@ -97,6 +104,38 @@ public class TychoVersionsPluginTest extends AbstractTychoIntegrationTest {
 		String versionProperty = properties.getProperty("Bundle-Version");
 		assertNotNull("Bundle-Version is null", versionProperty);
 		assertEquals("Bundle-Version is not as expected!", expectedNewVersion, versionProperty);
+	}
+
+	@Test
+	public void updateProjectVersionWithNestedPom() throws Exception {
+		String expectedNewVersion = "1.1.0";
+
+		Verifier verifier = getVerifier("tycho-version-plugin/set-version/nested_modules", true);
+
+		verifier.addCliOption("-DnewVersion=" + expectedNewVersion);
+		verifier.executeGoal("org.eclipse.tycho:tycho-versions-plugin:" + VERSION + ":set-version");
+
+		verifier.verifyErrorFreeLog();
+		String bundlePom = "bundle/pom.xml";
+		List<String> poms = List.of("pom.xml", "parent/pom.xml", bundlePom);
+		for (String pom : poms) {
+			MavenXpp3Reader pomReader = new MavenXpp3Reader();
+			Model pomModel = pomReader.read(new FileReader(new File(verifier.getBasedir(), pom)));
+			if (bundlePom.equals(pom)) {
+				assertNull("child should inherit version from parent", pomModel.getVersion());
+				Parent parent = pomModel.getParent();
+				assertNotNull("project > parent is null", parent);
+				assertEquals("project > parent > version in " + pom + " has not been changed!", expectedNewVersion,
+						parent.getVersion());
+			} else {
+				assertEquals("project > version in " + pom + " has not been changed!", expectedNewVersion,
+						pomModel.getVersion());
+			}
+		}
+		Manifest manifest = new Manifest(
+				new FileInputStream(new File(verifier.getBasedir(), "bundle/" + JarFile.MANIFEST_NAME)));
+		assertEquals("version in manifest was not updated!", expectedNewVersion,
+				manifest.getMainAttributes().getValue(Constants.BUNDLE_VERSION));
 	}
 
 	@Test

tycho-its/projects/tycho-version-plugin/set-version/nested_modules/bundle/META-INF/MANIFEST.MF
@@ -1,0 +1,5 @@
+Manifest-Version: 1.0
+Bundle-ManifestVersion: 2
+Bundle-Name: Test Bundle
+Bundle-SymbolicName: bundle
+Bundle-Version: 1.0.0.qualifier

tycho-its/projects/tycho-version-plugin/set-version/nested_modules/bundle/pom.xml
@@ -1,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/maven-v4_0_0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+
+	<parent>
+		<groupId>org.tycho.its</groupId>
+		<artifactId>parent</artifactId>
+		<version>1.0.0-SNAPSHOT</version>
+		<relativePath>../parent</relativePath>
+	</parent>
+	<artifactId>bundle</artifactId>
+	<packaging>eclipse-plugin</packaging>
+</project>

tycho-its/projects/tycho-version-plugin/set-version/nested_modules/parent/pom.xml
@@ -1,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/maven-v4_0_0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+
+	<groupId>org.tycho.its</groupId>
+	<artifactId>parent</artifactId>
+	<version>1.0.0-SNAPSHOT</version>
+	<packaging>pom</packaging>
+	<modules>
+		<module>../bundle</module>
+	</modules>
+	<properties>
+		<tycho-version>5.0.0-SNAPSHOT</tycho-version>
+	</properties>
+	<build>
+		<plugins>
+			<plugin>
+				<groupId>org.eclipse.tycho</groupId>
+				<artifactId>tycho-maven-plugin</artifactId>
+				<version>${tycho-version}</version>
+				<extensions>true</extensions>
+			</plugin>
+		</plugins>
+	</build>
+</project>

tycho-its/projects/tycho-version-plugin/set-version/nested_modules/pom.xml
@@ -1,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/maven-v4_0_0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+
+	<groupId>org.tycho.its</groupId>
+	<artifactId>root</artifactId>
+	<version>1.0.0-SNAPSHOT</version>
+	<packaging>pom</packaging>
+
+	<modules>
+		<module>parent</module>
+	</modules>
+
+
+</project>

tycho-versions-plugin/src/main/java/org/eclipse/tycho/versions/engine/VersionChangesDescriptor.java
@@ -12,8 +12,15 @@
  *******************************************************************************/
 package org.eclipse.tycho.versions.engine;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -26,12 +33,29 @@ public class VersionChangesDescriptor {
     private final Set<PackageVersionChange> packageVersionChanges;
 
     private final VersionRangeUpdateStrategy versionRangeUpdateStrategy;
+    private Collection<ProjectMetadata> projects;
 
     public VersionChangesDescriptor(Set<PomVersionChange> originalVersionChanges,
-            VersionRangeUpdateStrategy versionRangeUpdateStrategy) {
-        this.aritfactsVersionChanges = new HashSet<>(originalVersionChanges);
+            VersionRangeUpdateStrategy versionRangeUpdateStrategy, Collection<ProjectMetadata> projects) {
+        this.projects = projects;
+        this.aritfactsVersionChanges = new LinkedHashSet<>(originalVersionChanges);
         this.versionRangeUpdateStrategy = versionRangeUpdateStrategy;
-        this.packageVersionChanges = new HashSet<>();
+        this.packageVersionChanges = new LinkedHashSet<>();
+    }
+
+    public Optional<ProjectMetadata> findMetadataByBasedir(File baseDir) {
+        Path path = baseDir.toPath();
+        for (ProjectMetadata meta : projects) {
+            Path projectPath = meta.getBasedir().toPath();
+            try {
+                if (Files.isSameFile(projectPath, path)) {
+                    return Optional.of(meta);
+                }
+            } catch (IOException e) {
+            }
+        }
+        return Optional.empty();
+
     }
 
     public Set<PomVersionChange> getVersionChanges() {

tycho-versions-plugin/src/main/java/org/eclipse/tycho/versions/engine/VersionsEngine.java
@@ -112,7 +112,7 @@ public class VersionsEngine {
     public void apply() throws IOException {
 
         VersionChangesDescriptor versionChangeContext = new VersionChangesDescriptor(originalVersionChanges,
-                new DefaultVersionRangeUpdateStrategy(updateVersionRangeMatchingBounds));
+                new DefaultVersionRangeUpdateStrategy(updateVersionRangeMatchingBounds), projects);
 
         // collecting secondary changes
         boolean newChanges = true;

commit ebcb491761d0f030ee4f6349b75d3437f6f03b31

set-version: Fix regression overwriting mismatching versions
Fix regression introduced in #3343.

Also for the child modules the version of the change must match, so that
it does not blindly overwrite the versions of the submodules.

Fixes #3808.

tycho-its/projects/tycho-version-plugin/set-version/only_same_version/p/m1/META-INF/MANIFEST.MF
@@ -1,0 +1,5 @@
+Manifest-Version: 1.0
+Bundle-ManifestVersion: 2
+Bundle-Name: Test Bundle
+Bundle-SymbolicName: m1
+Bundle-Version: 1.0.0

tycho-its/projects/tycho-version-plugin/set-version/only_same_version/p/m1/pom.xml
@@ -1,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/maven-v4_0_0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+
+	<parent>
+		<groupId>org.tycho.its</groupId>
+		<artifactId>p</artifactId>
+		<version>1.0.0</version>
+		<relativePath>..</relativePath>
+	</parent>
+
+	<groupId>org.tycho.its</groupId>
+	<artifactId>m1</artifactId>
+	<version>1.0.0</version>
+	<packaging>eclipse-plugin</packaging>
+
+</project>

tycho-its/projects/tycho-version-plugin/set-version/only_same_version/p/pom.xml
@@ -1,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/maven-v4_0_0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+	
+	<parent>
+		<groupId>org.tycho.its</groupId>
+		<artifactId>root</artifactId>
+		<version>1.0.0</version>
+		<relativePath>..</relativePath>
+	</parent>
+
+	<groupId>org.tycho.its</groupId>
+	<artifactId>p</artifactId>
+	<version>1.0.0</version><!-- This is the SAME version as the root -->
+	<packaging>pom</packaging>
+
+	<modules>
+		<module>m1</module>
+	</modules>
+
+</project>

tycho-its/projects/tycho-version-plugin/set-version/only_same_version/pom.xml
@@ -1,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/maven-v4_0_0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+
+	<groupId>org.tycho.its</groupId>
+	<artifactId>root</artifactId>
+	<version>1.0.0</version>
+	<packaging>pom</packaging>
+
+	<modules>
+		<module>p</module>
+		<module>q</module>
+	</modules>
+
+	<build>
+		<plugins>
+			<plugin>
+				<groupId>org.eclipse.tycho</groupId>
+				<artifactId>tycho-maven-plugin</artifactId>
+				<version>${tycho-version}</version>
+				<extensions>true</extensions>
+			</plugin>
+		</plugins>
+	</build>
+
+</project>

tycho-its/projects/tycho-version-plugin/set-version/only_same_version/q/m2/META-INF/MANIFEST.MF
@@ -1,0 +1,5 @@
+Manifest-Version: 1.0
+Bundle-ManifestVersion: 2
+Bundle-Name: Test Bundle
+Bundle-SymbolicName: m2
+Bundle-Version: 2.0.0

tycho-its/projects/tycho-version-plugin/set-version/only_same_version/q/m2/pom.xml
@@ -1,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/maven-v4_0_0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+
+	<parent>
+		<groupId>org.tycho.its</groupId>
+		<artifactId>q</artifactId>
+		<version>2.0.0</version>
+		<relativePath>..</relativePath>
+	</parent>
+
+	<groupId>org.tycho.its</groupId>
+	<artifactId>m2</artifactId>
+	<version>2.0.0</version>
+	<packaging>eclipse-plugin</packaging>
+
+</project>

tycho-its/projects/tycho-version-plugin/set-version/only_same_version/q/pom.xml
@@ -1,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/maven-v4_0_0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+	
+	<parent>
+		<groupId>org.tycho.its</groupId>
+		<artifactId>root</artifactId>
+		<version>1.0.0</version>
+		<relativePath>..</relativePath>
+	</parent>
+
+	<groupId>org.tycho.its</groupId>
+	<artifactId>q</artifactId>
+	<version>2.0.0</version><!-- This is DIFFERENT than the version of the root -->
+	<packaging>pom</packaging>
+
+	<modules>
+		<module>m2</module>
+	</modules>
+
+</project>

tycho-versions-plugin/src/main/java/org/eclipse/tycho/versions/manipulation/PomManipulator.java
@@ -86,7 +86,8 @@ public class PomManipulator extends AbstractMetadataManipulator {
                             .ifPresent(moduleMeta -> {
                                 PomFile modulePom = moduleMeta.getMetadata(PomFile.class);
                                 if (modulePom != null && modulePom.isMutable()
-                                        && POM.equals(modulePom.getPackaging())) {
+                                        && POM.equals(modulePom.getPackaging())
+                                        && isVersionEquals(modulePom.getVersion(), change.getVersion())) {
                                     if (versionChangeContext.addVersionChange(
                                             new PomVersionChange(modulePom, change.getNewVersion()))) {
                                         moreChanges.set(true);

tycho-its/src/test/java/org/eclipse/tycho/test/versionsplugin/TychoVersionsPluginTest.java
@@ -146,6 +146,45 @@ public class TychoVersionsPluginTest extends AbstractTychoIntegrationTest {
 	}
 
 	@Test
+	public void updateProjectVersionOnlyChangesVersionOfNestedProjectsIfSameVersionAsRoot() throws Exception {
+		Verifier verifier = getVerifier("tycho-version-plugin/set-version/only_same_version", false);
+
+		verifier.addCliOption("-DnewVersion=1.0.1");
+		verifier.executeGoal("org.eclipse.tycho:tycho-versions-plugin:" + VERSION + ":set-version");
+
+		verifier.verifyErrorFreeLog();
+
+		record Expectation(String pom, String expectedVersion, String expectedParentVersion) {
+		}
+		List<Expectation> expectations = List.of( //
+				new Expectation("pom.xml", "1.0.1", null), //
+				new Expectation("p/pom.xml", "1.0.1", "1.0.1"), //
+				new Expectation("p/m1/pom.xml", "1.0.1", "1.0.1"), //
+				new Expectation("q/pom.xml", "2.0.0", "1.0.1"), // only parent shall be changed
+				new Expectation("q/m2/pom.xml", "2.0.0", "2.0.0") // nothing shall be changed
+		);
+		for (Expectation expectation : expectations) {
+			MavenXpp3Reader pomReader = new MavenXpp3Reader();
+			String pom = expectation.pom();
+			Model pomModel = pomReader.read(new FileReader(new File(verifier.getBasedir(), pom)));
+			Parent parent = pomModel.getParent();
+
+			assertEquals("project > version in " + pom + " is not as expected!", expectation.expectedVersion(),
+					pomModel.getVersion());
+			if (expectation.expectedParentVersion() == null) {
+				assertNull("project > parent in " + pom + " should be null", parent);
+			} else {
+				assertEquals("project > parent > version in " + pom + " is not as expected!",
+						expectation.expectedParentVersion(), parent.getVersion());
+			}
+		}
+		assertEquals("version in manifest p/m1 is not as expected!", "1.0.1",
+				getManifest(verifier, "p/m1").getMainAttributes().getValue(Constants.BUNDLE_VERSION));
+		assertEquals("version in manifest q/m2 is not as expected!", "2.0.0",
+				getManifest(verifier, "q/m2").getMainAttributes().getValue(Constants.BUNDLE_VERSION));
+	}
+
+	@Test
 	public void updateVersionRanges() throws Exception {
 		String expectedNewMavenVersion = "1.1.0-SNAPSHOT";
 		String expectedNewOSGiVersion = "1.1.0.qualifier";