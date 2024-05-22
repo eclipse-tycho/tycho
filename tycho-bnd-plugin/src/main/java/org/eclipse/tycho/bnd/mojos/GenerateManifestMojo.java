f5144ace02665f9071d84ad2fab9fa6c9b4f01dd
Christoph Läubrich <laeubi@laeubi-soft.de>
2024-05-22 14:58:41 +0200

    Fix wrong (bnd resource) path is used under windows
    (cherry picked from commit 26eaabd405f16770560d71d03258b8fab78898c8)

--- tycho-bnd-plugin/src/main/java/org/eclipse/tycho/bnd/mojos/GenerateManifestMojo.java
+++ tycho-bnd-plugin/src/main/java/org/eclipse/tycho/bnd/mojos/GenerateManifestMojo.java
@@ -12,12 +12,14 @@
  *******************************************************************************/
 package org.eclipse.tycho.bnd.mojos;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.jar.Manifest;
 import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
@@ -26,6 +28,7 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -53,6 +56,7 @@ import aQute.bnd.build.Workspace;
 import aQute.bnd.osgi.Constants;
 import aQute.bnd.osgi.Jar;
 import aQute.bnd.osgi.Processor;
+import aQute.lib.manifest.ManifestUtil;
 
 /**
  * The mojos support generation of the manifest file like it is done in PDE if
@@ -63,7 +67,7 @@ public class GenerateManifestMojo extends AbstractMojo {
 
 	private static final Predicate<Path> CLASS_FILTER = resource -> {
 		if (Files.isRegularFile(resource)) {
-			return resource.getFileName().endsWith(".class");
+			return resource.getFileName().toString().toLowerCase().endsWith(".class");
 		}
 		return true;
 	};
@@ -89,41 +93,25 @@ public class GenerateManifestMojo extends AbstractMojo {
 			File basedir = mavenProject.getBasedir();
 			File instructionsFile = new File(basedir, TychoConstants.PDE_BND);
 			if (instructionsFile.isFile()) {
+				Log log = getLog();
+				log.debug("Generate final manifest for project " + mavenProject.getId());
 				try (Project project = new Project(getWorkspace(), basedir, instructionsFile);
-						ProjectBuilder builder = new ProjectBuilder(project) {
-							@Override
-							public Jar getJarFromName(String name, String from) {
-								Matcher m = TychoConstants.PLATFORM_URL_PATTERN.matcher(name);
-								if (m.matches()) {
-									TargetPlatform targetPlatform = projectManager.getTargetPlatform(mavenProject)
-											.orElse(null);
-									if (targetPlatform == null) {
-										return null;
-									}
-									String pluginId = m.group(2);
-									try {
-										ArtifactKey artifact = targetPlatform
-												.resolveArtifact(ArtifactType.TYPE_ECLIPSE_PLUGIN, pluginId, null);
-										File artifactLocation = targetPlatform.getArtifactLocation(artifact);
-										if (artifactLocation == null) {
-											return null;
-										}
-										return new Jar(artifactLocation);
-									} catch (Exception e) {
-										return null;
-									}
-								}
-								return super.getJarFromName(name, from);
-							}
-						};
-						Jar jar = new MavenProjectJar(mavenProject, CLASS_FILTER)) {
+						ProjectBuilder builder = new AutomaticManifestProjectBuilder(project);
+						Jar jar = new MavenProjectJar(mavenProject, CLASS_FILTER, log)) {
 					setupProject(project);
 					BundleClassPath bundleClassPath = osgi
 							.getBundleClassPath(DefaultReactorProject.adapt(mavenProject));
 					builder.setBase(project.getBase());
 					builder.setJar(jar);
 					for (ClasspathEntry cpe : bundleClassPath.getClasspathEntries()) {
-						cpe.getLocations().forEach(project::addClasspath);
+						cpe.getLocations().forEach(cp -> {
+							log.debug("Adding classpath " + cp);
+							try {
+								project.addClasspath(cp);
+							} catch (RuntimeException e) {
+								getLog().warn("Adding classpath file " + cp + " failed: " + e);
+							}
+						});
 					}
 					try {
 						pluginRealmHelper.visitPluginExtensions(mavenProject, session, ClasspathContributor.class,
@@ -131,11 +119,18 @@ public class GenerateManifestMojo extends AbstractMojo {
 									List<ClasspathEntry> list = cpc.getAdditionalClasspathEntries(mavenProject,
 											Artifact.SCOPE_COMPILE);
 									if (list != null && !list.isEmpty()) {
+										log.debug("Adding additional classpath entries from contributor "
+												+ cpc.getClass().getSimpleName());
 										for (ClasspathEntry entry : list) {
-											try {
-												builder.addClasspath(entry.getLocations());
-											} catch (IOException e) {
-												e.printStackTrace();
+											List<File> locations = entry.getLocations();
+											for (File file : locations) {
+												try {
+													log.debug(" --> " + file);
+													builder.addClasspath(file);
+												} catch (Exception e) {
+													log.warn("Adding additional classpath file " + file + " failed: "
+															+ e);
+												}
 											}
 										}
 									}
@@ -144,6 +139,17 @@ public class GenerateManifestMojo extends AbstractMojo {
 						throw new MojoExecutionException("can't call classpath contributors", e);
 					}
 					builder.build();
+					builder.getWarnings().forEach(log::warn);
+					builder.getErrors().forEach(log::error);
+					Manifest manifest = jar.getManifest();
+					if (manifest == null) {
+						log.debug("No Manifest was generated!");
+					} else if (log.isDebugEnabled()) {
+						ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+						ManifestUtil.write(manifest, outputStream);
+						String str = new String(outputStream.toByteArray(), StandardCharsets.UTF_8);
+						log.debug("Generated final manifest for " + mavenProject.getId() + ":\r\n" + str);
+					}
 				} catch (MojoExecutionException e) {
 					throw e;
 				} catch (Exception e) {
@@ -168,6 +174,36 @@ public class GenerateManifestMojo extends AbstractMojo {
 				new File(mavenProject.getBuild().getDirectory(), Project.BNDCNF).toURI());
 		bndPluginManager.setupWorkspace(workspace);
 		return workspace;
+	}
+
+	private final class AutomaticManifestProjectBuilder extends ProjectBuilder {
+		private AutomaticManifestProjectBuilder(Project project) {
+			super(project);
+		}
+
+		@Override
+		public Jar getJarFromName(String name, String from) {
+			Matcher m = TychoConstants.PLATFORM_URL_PATTERN.matcher(name);
+			if (m.matches()) {
+				TargetPlatform targetPlatform = projectManager.getTargetPlatform(mavenProject).orElse(null);
+				if (targetPlatform == null) {
+					return null;
+				}
+				String pluginId = m.group(2);
+				try {
+					ArtifactKey artifact = targetPlatform.resolveArtifact(ArtifactType.TYPE_ECLIPSE_PLUGIN, pluginId,
+							null);
+					File artifactLocation = targetPlatform.getArtifactLocation(artifact);
+					if (artifactLocation == null) {
+						return null;
+					}
+					return new Jar(artifactLocation);
+				} catch (Exception e) {
+					return null;
+				}
+			}
+			return super.getJarFromName(name, from);
+		}
 	}
 
 }
