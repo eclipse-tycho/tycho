54b04e0a055d1c494ee0a91a9d1759f0795e4778
Александър Куртаков <akurtakov@gmail.com>
2024-05-24 11:28:42 +0300

    Move away of depracated commons-exec apis

--- sisu-osgi/sisu-equinox-launching/src/main/java/org/eclipse/sisu/equinox/launching/internal/DefaultEquinoxLauncher.java
+++ sisu-osgi/sisu-equinox-launching/src/main/java/org/eclipse/sisu/equinox/launching/internal/DefaultEquinoxLauncher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2019 Sonatype Inc. and others.
+ * Copyright (c) 2008, 2024 Sonatype Inc. and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -14,12 +14,14 @@ package org.eclipse.sisu.equinox.launching.internal;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.DefaultExecutor.Builder;
 import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.ExecuteWatchdog;
 import org.apache.commons.exec.ShutdownHookProcessDestroyer;
@@ -57,12 +59,14 @@ public class DefaultEquinoxLauncher implements EquinoxLauncher {
 
         cli.addArguments(configuration.getProgramArguments(), handleQuotes);
 
-        DefaultExecutor executor = new DefaultExecutor();
+        Builder<?> executorBuilder = DefaultExecutor.builder();
+        executorBuilder.setWorkingDirectory(configuration.getWorkingDirectory());
+        DefaultExecutor executor = executorBuilder.get();
         ExecuteWatchdog watchdog = null;
         if (forkedProcessTimeoutInSeconds > 0) {
             long timeoutInMilliseconds = forkedProcessTimeoutInSeconds * 1000L;
             cli.addArguments(new String[] { "-timeout ", String.valueOf(timeoutInMilliseconds) });
-            watchdog = new ExecuteWatchdog(timeoutInMilliseconds);
+            watchdog = ExecuteWatchdog.builder().setTimeout(Duration.ofMillis(timeoutInMilliseconds)).get();
             executor.setWatchdog(watchdog);
         }
 
@@ -70,7 +74,6 @@ public class DefaultEquinoxLauncher implements EquinoxLauncher {
 
         // best effort to avoid orphaned child process
         executor.setProcessDestroyer(new ShutdownHookProcessDestroyer());
-        executor.setWorkingDirectory(configuration.getWorkingDirectory());
         try {
             return executor.execute(cli, getMergedEnvironment(configuration));
         } catch (ExecuteException e) {
