b221951fbaf6ac11938a0dbf9f9232d2c94b3062
Александър Куртаков <akurtakov@gmail.com>
2024-06-14 10:07:00 +0300

    Update eclipse latest repo to 2024-06
    And adjust tests for that.

--- tycho-its/projects/TYCHO0367localRepositoryCrosstalk/bundle02/src/TYCHO0367localRepositoryCrosstalk/bundle02/Eclipse35Test.java
+++ tycho-its/projects/TYCHO0367localRepositoryCrosstalk/bundle02/src/TYCHO0367localRepositoryCrosstalk/bundle02/Eclipse35Test.java
@@ -25,7 +25,7 @@ public class Eclipse35Test extends TestCase {
         Bundle equinox = getBundle("org.eclipse.osgi");
 
         assertEquals(3, equinox.getVersion().getMajor());
-        assertEquals(19, equinox.getVersion().getMinor());
+        assertEquals(20, equinox.getVersion().getMinor());
     }
 
     public Bundle getBundle(String id) {
