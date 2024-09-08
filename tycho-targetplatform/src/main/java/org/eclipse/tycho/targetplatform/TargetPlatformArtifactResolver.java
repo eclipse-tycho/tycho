87d592952304b874e664121ccf133a24d45f2e82
Christoph Läubrich <laeubi@laeubi-soft.de>
2024-09-08 19:49:31 +0200

    Migrate TargetPlatformArtifactResolver to Aether/Resolver + JSR330
    (cherry picked from commit 6f6cabbc77b495900b6470685389c53e14a38bd5)

--- tycho-targetplatform/src/main/java/org/eclipse/tycho/targetplatform/TargetPlatformArtifactResolver.java
+++ tycho-targetplatform/src/main/java/org/eclipse/tycho/targetplatform/TargetPlatformArtifactResolver.java
@@ -17,28 +17,19 @@ import java.util.List;
 import java.util.Optional;
 
 import org.apache.commons.io.FilenameUtils;
-import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.repository.RepositorySystem;
-import org.codehaus.plexus.component.annotations.Component;
-import org.codehaus.plexus.component.annotations.Requirement;
 
 /**
  * This component resolves a given target artifact to a target platform file
  *
  */
-@Component(role = TargetPlatformArtifactResolver.class)
-public class TargetPlatformArtifactResolver {
+public interface TargetPlatformArtifactResolver {
 
 	public static final String TARGET_TYPE = "target";
 
-    @Requirement
-    private RepositorySystem repositorySystem;
-
-    /**
+	/**
 	 * Resolves the target file artifact with the given coordinates, session and
 	 * remote repositories
 	 * 
@@ -51,28 +42,8 @@ public class TargetPlatformArtifactResolver {
 	 * @return the target file for the specified artifact
 	 * @throws TargetResolveException if resolving the target fails
 	 */
-    public File resolveTargetFile(String groupId, String artifactId, String version, String classifier,
-			MavenSession session, List<ArtifactRepository> remoteRepositories) throws TargetResolveException {
-        //check if target is part of reactor-build
-		Optional<File> reactorTargetFile = getReactorTargetFile(groupId, artifactId, version, classifier, session);
-		if (reactorTargetFile.isPresent()) {
-			return reactorTargetFile.get();
-		}
-        // resolve using maven
-		Artifact artifact = repositorySystem.createArtifactWithClassifier(groupId, artifactId, version, TARGET_TYPE,
-				classifier);
-
-        ArtifactResolutionRequest request = new ArtifactResolutionRequest();
-        request.setArtifact(artifact);
-        request.setLocalRepository(session.getLocalRepository());
-        request.setRemoteRepositories(remoteRepositories);
-        repositorySystem.resolve(request);
-
-        if (artifact.isResolved()) {
-            return artifact.getFile();
-        }
-		throw new TargetResolveException("Could not resolve target platform specification artifact " + artifact);
-    }
+	public File resolveTargetFile(String groupId, String artifactId, String version, String classifier,
+			MavenSession session, List<ArtifactRepository> remoteRepositories) throws TargetResolveException;
 
 	/**
 	 * Lookup a given target artifact in the current reactor
@@ -87,30 +58,10 @@ public class TargetPlatformArtifactResolver {
 	 * @throws TargetResolveException
 	 */
 	public Optional<File> getReactorTargetFile(String groupId, String artifactId, String version, String classifier,
-			MavenSession session) throws TargetResolveException {
-		for (MavenProject project : session.getProjects()) {
-            if (groupId.equals(project.getGroupId()) && artifactId.equals(project.getArtifactId())
-                    && version.equals(project.getVersion())) {
-				if (classifier == null || classifier.isBlank()) {
-					return Optional.of(getMainTargetFile(project));
-                } else {
-                    File target = new File(project.getBasedir(),
-                            classifier + TargetDefinitionFile.FILE_EXTENSION);
-                    if (TargetDefinitionFile.isTargetFile(target)) {
-						return Optional.of(target);
-                    } else {
-						throw new TargetResolveException("target definition file '" + target
-                                + "' not found in project '" + project.getName() + "'.");
-                    }
-                }
-            }
-        }
-		return Optional.empty();
-	}
+			MavenSession session) throws TargetResolveException;
 
 	public static File getMainTargetFile(MavenProject project) throws TargetResolveException {
-		File[] targetFiles = TargetDefinitionFile
-		        .listTargetFiles(project.getBasedir());
+		File[] targetFiles = TargetDefinitionFile.listTargetFiles(project.getBasedir());
 		if (targetFiles == null || targetFiles.length == 0) {
 			throw new TargetResolveException(
 					"No target definition file(s) found in project '" + project.getName() + "'.");
@@ -122,7 +73,7 @@ public class TargetPlatformArtifactResolver {
 			String baseName = FilenameUtils.getBaseName(targetFile.getName());
 			if (baseName.equalsIgnoreCase(project.getArtifactId())) {
 				return targetFile;
-		    }
+			}
 		}
 		throw new TargetResolveException("One target file must be named  '" + project.getArtifactId()
 				+ TargetDefinitionFile.FILE_EXTENSION + "' when multiple targets are present");
