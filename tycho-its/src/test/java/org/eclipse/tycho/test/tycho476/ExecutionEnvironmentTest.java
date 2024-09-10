30a265fa00b9c02f207195fc781d7da6bb7b2227
Александър Куртаков <akurtakov@gmail.com>
2024-09-10 12:14:16 +0300

    Bump tests to newer Java as ecj no longer compiles to pre 1.8 target
    Using 17 as preferred target in non compiler specific tests to prevent
    having to do similar update soon.
    Updates o.e.jdt.core to the version signed with same certificate.

--- tycho-its/src/test/java/org/eclipse/tycho/test/tycho476/ExecutionEnvironmentTest.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/tycho476/ExecutionEnvironmentTest.java
@@ -23,18 +23,18 @@ import org.junit.Test;
 
 public class ExecutionEnvironmentTest extends AbstractTychoIntegrationTest {
 
-    @Test
-    public void testCompilerSourceTargetConfigurationViaManifest() throws Exception {
-        Verifier verifier = getVerifier("TYCHO476", false);
-        verifier.executeGoal("compile");
-        // compile only succeeds with source level 1.6 which
-        // is configured indirectly via Bundle-RequiredExecutionEnvironment: JavaSE-1.6
-        verifier.verifyErrorFreeLog();
-        File classFile = new File(verifier.getBasedir(), "target/classes/TestRunnable.class");
-        Assert.assertTrue(classFile.canRead());
-        JavaClass javaClass = new ClassParser(classFile.getAbsolutePath()).parse();
-        // bytecode major level 50 == target 1.6 
-        Assert.assertEquals(50, javaClass.getMajor());
-    }
+	@Test
+	public void testCompilerSourceTargetConfigurationViaManifest() throws Exception {
+		Verifier verifier = getVerifier("TYCHO476", false);
+		verifier.executeGoal("compile");
+		// compile only succeeds with source level 1.6 which
+		// is configured indirectly via Bundle-RequiredExecutionEnvironment: JavaSE-1.6
+		verifier.verifyErrorFreeLog();
+		File classFile = new File(verifier.getBasedir(), "target/classes/TestRunnable.class");
+		Assert.assertTrue(classFile.canRead());
+		JavaClass javaClass = new ClassParser(classFile.getAbsolutePath()).parse();
+		// bytecode major level 61 == target 17
+		Assert.assertEquals(61, javaClass.getMajor());
+	}
 
 }
