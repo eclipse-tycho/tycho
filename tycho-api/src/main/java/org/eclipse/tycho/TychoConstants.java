f066d7a79db71b6ff2e76ac51ac5e5a8132bee1c
Александър Куртаков <akurtakov@gmail.com>
2024-09-11 09:27:38 +0300

    Update eclipse latest to 2024-09

--- tycho-api/src/main/java/org/eclipse/tycho/TychoConstants.java
+++ tycho-api/src/main/java/org/eclipse/tycho/TychoConstants.java
@@ -23,7 +23,7 @@ public interface TychoConstants {
     File USER_MAVEN_CONFIGURATION_HOME = new File(USER_HOME, ".m2");
     File DEFAULT_USER_LOCALREPOSITORY = new File(USER_MAVEN_CONFIGURATION_HOME, "repository");
 
-    public static final String ECLIPSE_LATEST = "https://download.eclipse.org/releases/2024-06/";
+    public static final String ECLIPSE_LATEST = "https://download.eclipse.org/releases/2024-09/";
 
     public static final String TYCHO_NOT_CONFIGURED = "Tycho build extension not configured for ";
 
