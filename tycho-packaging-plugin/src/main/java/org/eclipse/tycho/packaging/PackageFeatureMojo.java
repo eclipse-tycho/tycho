619a065c3361a01b9bf99779f24fc6b00d6ffd67
Hannes Wellmann <wellmann.hannes1@gmx.net>
2024-09-16 20:01:19 +0200

    Add a flag to the tycho-packaging-plugin to skip its execution
    This allows to selective execute packaging based on property values.

--- tycho-packaging-plugin/src/main/java/org/eclipse/tycho/packaging/PackageFeatureMojo.java
+++ tycho-packaging-plugin/src/main/java/org/eclipse/tycho/packaging/PackageFeatureMojo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2022 Sonatype Inc. and others.
+ * Copyright (c) 2008, 2024 Sonatype Inc. and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -119,6 +119,10 @@ public class PackageFeatureMojo extends AbstractTychoPackagingMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        if (skip) {
+            getLog().info("Skip packaging");
+            return;
+        }
         synchronized (LOCK) {
             outputDirectory.mkdirs();
 
