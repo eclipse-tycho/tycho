dbdc59b80e1e57958d76509e890c9d8583701f4f
Hannes Wellmann <wellmann.hannes1@gmx.net>
2024-09-16 20:01:19 +0200

    Add a flag to the tycho-packaging-plugin to skip its execution
    This allows to selective execute packaging based on property values.
    
    (cherry picked from commit 619a065c3361a01b9bf99779f24fc6b00d6ffd67)

--- tycho-packaging-plugin/src/main/java/org/eclipse/tycho/packaging/AbstractTychoPackagingMojo.java
+++ tycho-packaging-plugin/src/main/java/org/eclipse/tycho/packaging/AbstractTychoPackagingMojo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2022 Sonatype Inc. and others.
+ * Copyright (c) 2008, 2024 Sonatype Inc. and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -92,6 +92,10 @@ public abstract class AbstractTychoPackagingMojo extends AbstractMojo {
      */
     @Parameter
     protected DefaultFileSet[] additionalFileSets;
+
+    /** If {@code true}, skips the packaging entirely. */
+    @Parameter(defaultValue = "false")
+    protected boolean skip;
 
     @Component
     protected PlexusContainer plexus;
