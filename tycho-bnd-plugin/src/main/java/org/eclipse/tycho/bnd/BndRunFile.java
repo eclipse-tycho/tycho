483cc6751dd7fbc43468bd1f6b9304db2af86a44
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-02-03 11:37:27 +0100

    Consider runbundles when computing dependencies
    If we have a bndrun activated then the runbundles become dependencies of
    the maven project.
    
    This now calculates the enabled bndruns and return the bundle names so
    they can be used to add required reactor projects.
    
    (cherry picked from commit 807bd8ecff83422dc3953b5d7d02675348a25e79)

---
+++ tycho-bnd-plugin/src/main/java/org/eclipse/tycho/bnd/BndRunFile.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Christoph Läubrich and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.bnd;
+
+import java.nio.file.Path;
+
+public record BndRunFile(String name, Path path) {
+
+}
