c68fc13d536cff0b154df7ec76034667f135cbf2
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-02-23 09:02:33 +0100

    Use the new MavenDependencyCollector shared with m2e
    Currently Tycho uses the usual maven resolver to find transitive
    dependencies but this works differently than what we want here. Instead
    we want all dependencies and their dependencies and then filter for the
    given scopes provided if any match.As this is a non trivial operation,
    it seems useful to make the code also sharable with Tycho so we ensure
    there is always the same result in Tycho as in m2e.
    
    This now uses the new MavenDependencyCollector that is shared with Tm2e
    that traverses the dependency tree in a way we need here and collects
    everything along the way. This also has the advantage, that if we choose
    to only collect direct dependencies, not the whole tree has to be
    traversed as before.

---
+++ tycho-core/src/main/java/org/eclipse/m2e/pde/target/shared/MavenDependencyCollector.java
@@ -0,0 +1,152 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Christoph Läubrich and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2e.pde.target.shared;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+import org.eclipse.aether.RepositoryException;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.graph.DefaultDependencyNode;
+import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.graph.DependencyNode;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactDescriptorRequest;
+import org.eclipse.aether.resolution.ArtifactDescriptorResult;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResult;
+
+/**
+ * Collector to collect (and filter) all transitive dependencies of a maven target location
+ */
+public class MavenDependencyCollector {
+
+    private static final Set<String> VALID_EXTENSIONS = Set.of("jar", "pom");
+
+    private final RepositorySystem repoSystem;
+    private final RepositorySystemSession repositorySession;
+    private final List<RemoteRepository> repositories;
+    private final Collection<String> dependencyScopes;
+
+    public MavenDependencyCollector(RepositorySystem repoSystem, RepositorySystemSession repositorySession,
+            List<RemoteRepository> repositories, Collection<String> dependencyScopes) {
+        this.repoSystem = repoSystem;
+        this.repositorySession = repositorySession;
+        this.repositories = repositories;
+        this.dependencyScopes = dependencyScopes;
+    }
+
+    public DependencyResult collect(Dependency root, DependencyDepth depth) throws RepositoryException {
+        if (!isValidDependency(root)) {
+            throw new RepositoryException(
+                    "Invalid root dependency: " + root + " allowed extensions are " + VALID_EXTENSIONS);
+        }
+        List<Artifact> artifacts = new ArrayList<>();
+        List<DependencyNode> nodes = new ArrayList<>();
+        ArtifactDescriptor rootDescriptor = readArtifactDescriptor(root, null, artifacts, nodes);
+        if (depth == DependencyDepth.NONE) {
+            return new DependencyResult(artifacts, rootDescriptor.node(), nodes);
+        }
+        if (depth == DependencyDepth.DIRECT) {
+            for (Dependency dependency : rootDescriptor.dependencies()) {
+                readArtifactDescriptor(dependency, rootDescriptor.node(), artifacts, nodes);
+            }
+            return new DependencyResult(artifacts, rootDescriptor.node(), nodes);
+        }
+        // Add all dependencies with BFS method
+        Set<String> collected = new HashSet<>();
+        collected.add(getId(rootDescriptor.node().getDependency()));
+        Queue<ArtifactDescriptor> queue = new LinkedList<>();
+        queue.add(rootDescriptor);
+        while (!queue.isEmpty()) {
+            ArtifactDescriptor current = queue.poll();
+            for (Dependency dependency : current.dependencies()) {
+                if (isValidDependency(dependency) && collected.add(getId(dependency))) {
+                    ArtifactDescriptor dependencyDescriptor = readArtifactDescriptor(dependency, current.node(),
+                            artifacts, nodes);
+                    if (dependencyDescriptor != null) {
+                        queue.add(dependencyDescriptor);
+                    }
+                }
+            }
+        }
+        return new DependencyResult(artifacts, rootDescriptor.node(), nodes);
+    }
+
+    private String getId(Dependency dependency) {
+        Artifact artifact = dependency.getArtifact();
+        // This does not include the version so we always ever only collect one version
+        // of an (transitive) artifact
+        return artifact.getGroupId() + ":" + artifact.getArtifactId() + ":" + artifact.getClassifier();
+    }
+
+    /**
+     * This method reads the artifact descriptor and resolves the artifact.
+     * 
+     * @param artifact
+     *            the artifact to read its descriptor
+     * @param artifacts
+     * @param nodes
+     * @return the resolved artifact and the list of (managed) dependencies
+     * @throws RepositoryException
+     */
+    private ArtifactDescriptor readArtifactDescriptor(Dependency dependency, DependencyNode parent,
+            Collection<Artifact> artifacts, List<DependencyNode> nodes) throws RepositoryException {
+        if (isValidScope(dependency) && isValidDependency(dependency)) {
+            ArtifactDescriptorRequest descriptorRequest = new ArtifactDescriptorRequest();
+            descriptorRequest.setArtifact(dependency.getArtifact());
+            descriptorRequest.setRepositories(repositories);
+            ArtifactDescriptorResult result = repoSystem.readArtifactDescriptor(repositorySession, descriptorRequest);
+            ArtifactRequest artifactRequest = new ArtifactRequest();
+            artifactRequest.setArtifact(result.getArtifact());
+            artifactRequest.setRepositories(repositories);
+            ArtifactResult artifactResult = repoSystem.resolveArtifact(repositorySession, artifactRequest);
+            Artifact resolved = artifactResult.getArtifact();
+            artifacts.add(resolved);
+            DefaultDependencyNode dependencyNode = new DefaultDependencyNode(
+                    new Dependency(resolved, dependency.getScope()));
+            nodes.add(dependencyNode);
+            if (parent != null) {
+                parent.getChildren().add(dependencyNode);
+            }
+            return new ArtifactDescriptor(dependencyNode, result.getDependencies(), result.getManagedDependencies());
+        }
+        return null;
+    }
+
+    private boolean isValidDependency(Dependency dependency) {
+        if (dependency.isOptional()) {
+            // optional in maven means do not include in transitive dependency chains see
+            // https://maven.apache.org/guides/introduction/introduction-to-optional-and-excludes-dependencies.html
+            return false;
+        }
+        return VALID_EXTENSIONS.contains(dependency.getArtifact().getExtension());
+    }
+
+    private boolean isValidScope(Dependency dependency) {
+        String scope = dependency.getScope();
+        if (scope == null || scope.isEmpty()) {
+            return true;
+
+        }
+        return dependencyScopes.contains(scope);
+    }
+
+}
