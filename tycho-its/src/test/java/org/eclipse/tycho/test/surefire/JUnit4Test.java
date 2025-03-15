8ca85ac17eb0327d49b565f458eb1d40cf9427c1
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-03-15 08:04:31 +0100

    Move JUnit 4 tests into tycho-surefire-plugin directory structure

--- tycho-its/src/test/java/org/eclipse/tycho/test/surefire/JUnit4Test.java
+++ tycho-its/src/test/java/org/eclipse/tycho/test/surefire/JUnit4Test.java
@@ -15,17 +15,19 @@ package org.eclipse.tycho.test.surefire;
 import static org.eclipse.tycho.test.util.SurefireUtil.testResultFile;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+
 import org.apache.maven.it.Verifier;
 import org.eclipse.tycho.test.AbstractTychoIntegrationTest;
 import org.junit.Test;
 
-public class Junit4TestBundleTest extends AbstractTychoIntegrationTest {
+public class JUnit4Test extends AbstractTychoIntegrationTest {
 
 	@Test
-	public void test() throws Exception {
+	public void basicTest() throws Exception {
 
 		// a eclipse-test-plugin using JUnit 4 -> supported since MNGECLIPSE-1031
-		Verifier verifier = getVerifier("surefire.junit4/bundle.test");
+		Verifier verifier = getVerifier("tycho-surefire-plugin/junit4/bundle.test");
 
 		verifier.executeGoal("integration-test");
 		verifier.verifyErrorFreeLog();
@@ -34,6 +36,19 @@ public class Junit4TestBundleTest extends AbstractTychoIntegrationTest {
 
 		// ensure that JUnit 3 style tests also work -> related to bug 388909
 		assertTrue(testResultFile(verifier.getBasedir(), "bundle.test", "JUnit3Test").exists());
+	}
+
+	@Test
+	public void contextClassLoaderTest() throws Exception {
+
+		// a eclipse-test-plugin using JUnit 4 -> supported since MNGECLIPSE-1031
+		Verifier verifier = getVerifier("tycho-surefire-plugin/junit4/contextclassloader.test/");
+
+		verifier.executeGoal("integration-test");
+		verifier.verifyErrorFreeLog();
+		File testResultFile = testResultFile(verifier.getBasedir(), "contextclassloader.test", "JUnit4Test");
+		assertTrue("Test Result File" + testResultFile + " is missing", testResultFile.exists());
+
 	}
 
 }
