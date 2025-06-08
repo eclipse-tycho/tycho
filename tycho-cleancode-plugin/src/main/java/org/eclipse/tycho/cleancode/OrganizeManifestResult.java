e20ff1c70196e9e3a092d4c2c0dbd606d29bf0d8
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-06-08 19:39:57 +0200

    Add a mojo that executes the PDE 'Organize Manifest' cleanups
    PDE offers a way to cleanup manifests, but this seems not really often
    used. One drawback is that this has to be manually performed by the
    developer.
    
    This now adds a new tycho-cleancode:manifest mojo that allows automatic
    execution of this task inside a maven build. This will hopefully make
    more projects to cleanup their manifests as well as lead to more
    automatic cleanup actions added in PDE itself leading to a constant
    improvement in manifest metadata.

---
+++ tycho-cleancode-plugin/src/main/java/org/eclipse/tycho/cleancode/OrganizeManifestResult.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Christoph Läubrich and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tycho.cleancode;
+
+import org.eclipse.tycho.eclipsebuild.EclipseBuildResult;
+
+public class OrganizeManifestResult extends EclipseBuildResult {
+
+}
