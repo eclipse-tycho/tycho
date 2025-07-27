663992f6f242476d183e91306f950aba793e5350
Hannes Wellmann <wellmann.hannes1@gmx.net>
2025-07-27 14:23:29 +0200

    Add mojo to modify a composite P2-repository

--- p2-maven-plugin/src/main/java/org/eclipse/tycho/p2maven/tools/P2RepositoryDataManipulator.java
+++ p2-maven-plugin/src/main/java/org/eclipse/tycho/p2maven/tools/P2RepositoryDataManipulator.java
@@ -13,12 +13,20 @@
 package org.eclipse.tycho.p2maven.tools;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.SequencedSet;
 import java.util.function.Consumer;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
 import java.util.stream.Stream;
 
 import javax.inject.Inject;
@@ -26,10 +34,17 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.apache.maven.model.Repository;
+import org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository;
+import org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepository;
+import org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepositoryFactory;
+import org.eclipse.equinox.internal.p2.persistence.CompositeRepositoryIO;
+import org.eclipse.equinox.internal.p2.persistence.CompositeRepositoryState;
 import org.eclipse.equinox.internal.p2.repository.helpers.RepositoryHelper;
 import org.eclipse.equinox.p2.core.ProvisionException;
 import org.eclipse.equinox.p2.internal.repository.tools.XZCompressor;
+import org.eclipse.equinox.p2.repository.ICompositeRepository;
 import org.eclipse.equinox.p2.repository.IRepository;
+import org.eclipse.equinox.p2.repository.IRepositoryManager;
 import org.eclipse.tycho.MavenRepositoryLocation;
 import org.eclipse.tycho.p2maven.repository.P2RepositoryKind;
 import org.eclipse.tycho.p2maven.repository.P2RepositoryManager;
@@ -73,6 +88,14 @@ public class P2RepositoryDataManipulator {
 
 	public record ModifiedRepositoryDescriptor(MavenRepositoryLocation repository, P2RepositoryKind repositoryKind,
 			Path outputLocation, boolean compress, boolean xzCompress, boolean keepNonXzIndexFiles) {
+
+		public boolean isArtifact() {
+			return repositoryKind == null || repositoryKind == P2RepositoryKind.artifact;
+		}
+
+		public boolean isMetadata() {
+			return repositoryKind == null || repositoryKind == P2RepositoryKind.metadata;
+		}
 	}
 
 	@Inject
@@ -100,14 +123,13 @@ public class P2RepositoryDataManipulator {
 		};
 
 		MavenRepositoryLocation location = repository.repository();
-		P2RepositoryKind kind = repository.repositoryKind();
 		Path outputLocation = repository.outputLocation();
 
-		if (kind == null || kind == P2RepositoryKind.artifact) {
+		if (repository.isArtifact()) {
 			modifyOutputRepository(manager.getArtifactRepository(location), outputLocation,
 					manager::mirrorArtifactRepositoryData, r -> r.executeBatch(m -> modification.accept(r), null));
 		}
-		if (kind == null || kind == P2RepositoryKind.metadata) {
+		if (repository.isMetadata()) {
 			modifyOutputRepository(manager.getMetadataRepository(location), outputLocation,
 					manager::mirrorMetadataRepository, r -> r.executeBatch(m -> modification.accept(r), null));
 		}
@@ -139,6 +161,124 @@ public class P2RepositoryDataManipulator {
 			xzCompressor.setPreserveOriginalFile(repository.keepNonXzIndexFiles());
 			xzCompressor.setRepoFolder(outputLocation.toAbsolutePath().toString());
 			xzCompressor.compressRepo();
+		}
+	}
+
+	public void modifyCompositeRepository(ModifiedRepositoryDescriptor repository, String repositoryName,
+			List<URI> childrenToAdd, List<URI> childrenToRemove, int childLimit)
+			throws ProvisionException, IOException {
+		if (childLimit > 0 && childrenToAdd.size() > childLimit) {
+			throw new IllegalArgumentException("Cannot add more children than the specified limit of " + childLimit);
+		}
+
+		Consumer<CompositeRepositoryState> modification = repo -> {
+			if (repositoryName != null) {
+				repo.setName(repositoryName);
+			}
+			SequencedSet<URI> children = new LinkedHashSet<>(Arrays.asList(repo.getChildren()));
+			for (URI child : childrenToRemove) {
+				children.remove(child); // Child may or may not be in the list
+			}
+			for (URI child : childrenToAdd) {
+				children.add(child);
+			}
+			if (childLimit > 0) {
+				while (children.size() > childLimit) {
+					children.removeFirst();
+				}
+			}
+			repo.setChildren(children.toArray(URI[]::new));
+		};
+
+		if (repository.isArtifact()) {
+			modifyOutputCompositeRepository(repository,
+					(m, r) -> ((CompositeArtifactRepository) m.getArtifactRepository(r)).toState(), state -> {
+						state.setType(CompositeArtifactRepository.REPOSITORY_TYPE);
+						state.setVersion("1");
+						state.getProperties().put(CompositeArtifactRepository.PROP_ATOMIC_LOADING, "true");
+					}, modification, CompositeArtifactRepository.PI_REPOSITORY_TYPE,
+					CompositeArtifactRepository.CONTENT_FILENAME);
+		}
+		if (repository.isMetadata()) {
+			modifyOutputCompositeRepository(repository,
+					(m, r) -> ((CompositeMetadataRepository) m.getMetadataRepository(r)).toState(), state -> {
+						state.setType(CompositeMetadataRepository.REPOSITORY_TYPE);
+						state.setVersion("1");
+						state.getProperties().put(CompositeMetadataRepository.PROP_ATOMIC_LOADING, "true");
+					}, modification, CompositeMetadataRepository.PI_REPOSITORY_TYPE,
+					CompositeMetadataRepositoryFactory.CONTENT_FILENAME);
+		}
+		createP2Index(repository.outputLocation(), repository.isMetadata(), repository.isArtifact());
+	}
+
+	private <T, M extends IRepositoryManager<T>, R extends ICompositeRepository<T>> void modifyOutputCompositeRepository(
+			ModifiedRepositoryDescriptor descriptor,
+			ThrowingBiFunction<P2RepositoryManager, MavenRepositoryLocation, CompositeRepositoryState, ProvisionException> getState,
+			Consumer<CompositeRepositoryState> initState, Consumer<CompositeRepositoryState> modification,
+			String piRepositoryType, String filename) throws ProvisionException, IOException {
+
+		MavenRepositoryLocation repository = descriptor.repository();
+		CompositeRepositoryState state;
+		try {
+			state = getState.apply(manager, repository);
+			state.setProperties(new LinkedHashMap<>(state.getProperties())); // ensure modifiable
+		} catch (ProvisionException e) {
+			if (e.getStatus().getCode() == ProvisionException.REPOSITORY_NOT_FOUND) {
+				state = new CompositeRepositoryState();
+				state.setProperties(new LinkedHashMap<>());
+				state.setChildren(new URI[0]);
+				initState.accept(state);
+			} else {
+				throw e;
+			}
+		}
+		Path outputLocation = descriptor.outputLocation();
+		if (outputLocation == null) {
+			try {
+				outputLocation = Path.of(repository.getURL());
+			} catch (Exception e) {
+				throw new IllegalStateException(
+						"Repository is not modifable and no output location is defined: " + repository.getURL(), e);
+			}
+		}
+
+		modification.accept(state);
+
+		boolean compress = descriptor.compress();
+		try (OutputStream output = createCompositeFileOutputStream(outputLocation, filename, compress);) {
+			state.getProperties().put(IRepository.PROP_TIMESTAMP, Long.toString(System.currentTimeMillis()));
+			state.getProperties().put(IRepository.PROP_COMPRESSED, Boolean.toString(compress));
+			new CompositeRepositoryIO().write(state, output, piRepositoryType);
+		}
+	}
+
+	private static OutputStream createCompositeFileOutputStream(Path directory, String filename, boolean compress)
+			throws IOException {
+		Path file = directory.resolve(filename + CompositeArtifactRepository.XML_EXTENSION);
+		Path jarFile = directory.resolve(filename + CompositeArtifactRepository.JAR_EXTENSION);
+		Files.createDirectories(directory);
+		if (!compress) {
+			Files.deleteIfExists(jarFile);
+			return Files.newOutputStream(file);
+		} else {
+			Files.deleteIfExists(file);
+			JarOutputStream stream = new JarOutputStream(Files.newOutputStream(jarFile));
+			stream.putNextEntry(new JarEntry(file.getFileName().toString()));
+			return stream;
+		}
+	}
+
+	private void createP2Index(Path directory, boolean metadata, boolean artifacts) throws IOException {
+		Properties p2Index = new Properties();
+		p2Index.setProperty("version", "1"); //$NON-NLS-1$//$NON-NLS-2$
+		if (metadata) {
+			p2Index.setProperty("metadata.repository.factory.order", "compositeContent.xml,!"); //$NON-NLS-1$//$NON-NLS-2$
+		}
+		if (artifacts) {
+			p2Index.setProperty("artifact.repository.factory.order", "compositeArtifacts.xml,!"); //$NON-NLS-1$//$NON-NLS-2$
+		}
+		try (OutputStream output = Files.newOutputStream(directory.resolve("p2.index"))) {
+			p2Index.store(output, null);
 		}
 	}
 
--- tycho-p2-repository-plugin/src/main/java/org/eclipse/tycho/plugins/p2/repository/ModifyCompositeRepositoryMojo.java
+++ tycho-p2-repository-plugin/src/main/java/org/eclipse/tycho/plugins/p2/repository/ModifyCompositeRepositoryMojo.java
@@ -13,9 +13,9 @@
 package org.eclipse.tycho.plugins.p2.repository;
 
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -26,25 +26,28 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.codehaus.plexus.logging.Logger;
+import org.eclipse.equinox.p2.core.IProvisioningAgent;
 import org.eclipse.equinox.p2.core.ProvisionException;
+import org.eclipse.tycho.MavenRepositoryLocation;
 import org.eclipse.tycho.helper.StatusTool;
 import org.eclipse.tycho.p2maven.repository.P2RepositoryKind;
+import org.eclipse.tycho.p2maven.repository.P2RepositoryManager;
 import org.eclipse.tycho.p2maven.tools.P2RepositoryDataManipulator;
 import org.eclipse.tycho.p2maven.tools.P2RepositoryDataManipulator.ModifiedRepositoryDescriptor;
 import org.eclipse.tycho.p2maven.tools.P2RepositoryDataManipulator.RepositoryLocationURISyntaxException;
 
 /**
- * Modifies metadata of a P2-repository like its name or properties.
+ * Modifies the content or name of a composite P2-repository.
  * <p>
- * This can be used to customize metadata of a just assembled repository or to modify an already
- * existing (remote) repository. The resulting repository metadata files
- * ({@code artifact.xml/jar/xml.xz} or {@code content.xml/jar/xml.xz}) are always written to a local
- * directory. For a remote repository, a suitable other tool (such as SSH) must be used to copy
- * these files subsequently back to the remote server.
+ * This can be used to create a new composite repository or to modify an already existing (remote)
+ * repository. The resulting repository metadata files ({@code compositeArtifacts.xml/jar} or
+ * {@code compositeContent.xml/jar}) are always written to a local directory. For a remote
+ * repository, a suitable other tool (such as SSH) must be used to copy these files subsequently
+ * back to the remote server.
  * </p>
  */
-@Mojo(name = "modify-repository-properties", threadSafe = true)
-public class ModifyRepositoryPropertiesMojo extends AbstractMojo {
+@Mojo(name = "modify-composite-repository")
+public class ModifyCompositeRepositoryMojo extends AbstractMojo {
 
     /**
      * Location of the <em>source</em> repository to modify.<br>
@@ -76,48 +79,45 @@ public class ModifyRepositoryPropertiesMojo extends AbstractMojo {
     /** Optional repository name to set. An existing name is overwritten. */
     @Parameter(property = "p2.repository.name")
     private String repositoryName;
+    /** The children to append to the composite repository. */
+    @Parameter(property = "p2.composite.children.add")
+    private List<URI> childrenToAdd = List.of();
+    /** The children to remove from the composite repository. */
+    @Parameter(property = "p2.composite.children.remove")
+    private List<URI> childrenToRemove = List.of();
+
     /**
-     * The properties to remove.<br>
-     * Properties that don't exist in the repository are just silently ignored.
+     * The maximum number of children to keep in the composite repository.<br>
+     * If this limit is exceeded (after adding children) a corresponding count is removed from the
+     * beginning of the list of children. A value of zero or less, is considered as no-limit.
      */
-    @Parameter(property = "p2.repository.properties.remove")
-    private List<String> propertiesToRemove = List.of();
-    /**
-     * The properties to add.<br>
-     * Already existing properties are overwritten and updated to the value specified.
-     */
-    @Parameter(property = "p2.repository.properties.add")
-    private Map<String, String> propertiesToAdd = Map.of();
+    @Parameter(property = "p2.composite.children.limit", defaultValue = "0")
+    private int childCountLimit = 0;
+
+    /** Validate the existence of the repository and all added children. */
+    @Parameter(property = "p2.composite.validate", defaultValue = "true")
+    private boolean validateChildren;
 
     /** Compress the repository index files {@code content.xml} and {@code artifacts.xml}. */
     @Parameter(property = "p2.repository.compress", defaultValue = "true")
     private boolean compress;
 
-    /**
-     * Add XZ-compressed repository index files. XZ offers better compression ratios esp. for highly
-     * redundant file content.
-     */
-    @Parameter(property = "p2.repository.xz", defaultValue = "true")
-    private boolean xzCompress;
-
-    /**
-     * If {@link #xzCompress} is {@code true}, whether jar or xml index files should be kept in
-     * addition to XZ-compressed index files. This fallback provides backwards compatibility for
-     * pre-Mars p2 clients which cannot read XZ-compressed index files.
-     */
-    @Parameter(property = "p2.repository.xz.keep", defaultValue = "true")
-    private boolean keepNonXzIndexFiles;
-
     @Inject
     private P2RepositoryDataManipulator manipulator;
     @Inject
     private Logger log;
+    @Inject
+    private P2RepositoryManager manager;
+    @Inject
+    private IProvisioningAgent agent;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        // Get transport service here to trigger its information print-out early, avoiding it in-between other information that are then disrupted.
+        agent.getService(org.eclipse.equinox.internal.p2.repository.Transport.class);
         try {
             ModifiedRepositoryDescriptor descriptor = P2RepositoryDataManipulator.createDescriptor(repository,
-                    repositoryKind, outputLocation, compress, xzCompress, keepNonXzIndexFiles);
+                    repositoryKind, outputLocation, compress, false, false);
 
             String type = "repository";
             if (repositoryKind != null) {
@@ -132,17 +132,19 @@ public class ModifyRepositoryPropertiesMojo extends AbstractMojo {
             if (repositoryName != null) {
                 log.info("Set name to: " + repositoryName);
             }
-            if (!propertiesToRemove.isEmpty()) {
-                log.info("Remove properties: " + String.join(", ", propertiesToRemove));
+            printChildren(childrenToRemove, "Remove");
+            printChildren(childrenToAdd, "Add");
+            if (childCountLimit > 0) {
+                log.info("Limiting number of children to " + childCountLimit);
             }
-            if (!propertiesToAdd.isEmpty()) {
-                log.info("Add properties:");
-                propertiesToAdd.forEach((key, value) -> {
-                    log.info("  " + key + " = " + value);
-                });
+            if (validateChildren && !childrenToAdd.isEmpty()) {
+                for (URI child : childrenToAdd) {
+                    validateChildRepository(child, descriptor);
+                }
             }
 
-            manipulator.modifyRepositoryMetadata(descriptor, repositoryName, propertiesToRemove, propertiesToAdd);
+            manipulator.modifyCompositeRepository(descriptor, repositoryName, childrenToAdd, childrenToRemove,
+                    childCountLimit);
         } catch (RepositoryLocationURISyntaxException e) {
             throw new MojoFailureException("Illegal repository location URL: " + repository.getUrl(), e.getCause());
         } catch (IOException e) {
@@ -151,6 +153,31 @@ public class ModifyRepositoryPropertiesMojo extends AbstractMojo {
             log.error(StatusTool.toLogMessage(e.getStatus()), e);
             throw new MojoExecutionException("Failed to modify repository " + repository.getUrl() + ": "
                     + StatusTool.collectProblems(e.getStatus()), e);
+        }
+    }
+
+    private void validateChildRepository(URI child, ModifiedRepositoryDescriptor baseRepository)
+            throws ProvisionException {
+        MavenRepositoryLocation repositoryLocation = baseRepository.repository();
+        URI childLocation = repositoryLocation.getURL().resolve(child);
+        log.info("Verifying p2 repository child at " + childLocation);
+        String artifacts = "";
+        String units = "";
+        if (baseRepository.isArtifact()) {
+            artifacts = manager.allArtifacts(childLocation, repositoryLocation.getId()).stream().count() + " artifacts";
+        }
+        if (baseRepository.isMetadata()) {
+            units = manager.allMetadataUnits(childLocation, repositoryLocation.getId()).stream().count() + " units";
+        }
+        log.info("  Found " + artifacts + (!artifacts.isEmpty() && !units.isEmpty() ? " and " : "") + units + ".");
+    }
+
+    private void printChildren(List<URI> children, String action) {
+        if (!children.isEmpty()) {
+            log.info(action + " children:");
+            for (URI child : children) {
+                log.info("  " + child);
+            }
         }
     }
 
