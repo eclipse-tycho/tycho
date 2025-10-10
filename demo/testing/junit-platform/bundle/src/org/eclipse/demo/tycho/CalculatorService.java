51f6097e6bddec8fa32d302d9ee3166610e723e2
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-10-10 05:29:59 +0200

    Add a new Tycho testing framework plugin

---
+++ demo/testing/junit-platform/bundle/src/org/eclipse/demo/tycho/CalculatorService.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Christoph Läubrich and others.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.demo.tycho;
+
+public interface CalculatorService {
+
+	int addTwoPositiveNumbers(int a, int b);
+
+}
