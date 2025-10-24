c2fb35aa37d4fb0813c6648262957f81906035e7
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-10-24 12:27:11 +0200

    Replace reference to PlexusTestCase by inline the code

--- tycho-testing-harness/src/main/java/org/eclipse/tycho/testing/TestUtil.java
+++ tycho-testing-harness/src/main/java/org/eclipse/tycho/testing/TestUtil.java
@@ -17,21 +17,20 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 
-import org.codehaus.plexus.PlexusTestCase;
 import org.codehaus.plexus.util.FileUtils;
 
 public class TestUtil {
 
     public static File getTestResourceLocation(String name) throws IOException {
-        File src = new File(PlexusTestCase.getBasedir(), "src/test/resources/" + name);
+        File src = new File(getBasedir(), "src/test/resources/" + name);
         assertTrue(src.exists());
         return src;
     }
 
     // TODO rename to clarify that this creates a copy
     public static File getBasedir(String name) throws IOException {
-        File src = new File(PlexusTestCase.getBasedir(), "src/test/resources/" + name);
-        File dst = new File(PlexusTestCase.getBasedir(), "target/" + name);
+        File src = new File(getBasedir(), "src/test/resources/" + name);
+        File dst = new File(getBasedir(), "target/" + name);
 
         if (dst.isDirectory()) {
             FileUtils.deleteDirectory(dst);
@@ -45,4 +44,18 @@ public class TestUtil {
 
         return dst;
     }
+
+    private static final class Lazy {
+        static {
+            final String path = System.getProperty("basedir");
+            BASEDIR = null != path ? path : new File("").getAbsolutePath();
+        }
+
+        static final String BASEDIR;
+    }
+
+    public static String getBasedir() {
+        return Lazy.BASEDIR;
+    }
+
 }
